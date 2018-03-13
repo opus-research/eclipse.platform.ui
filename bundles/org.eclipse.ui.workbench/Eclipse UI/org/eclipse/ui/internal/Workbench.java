4c1cc06942414c96daaf30d0372b5d06d2efdd46
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:23:22 +0000

    First commited as before_14833_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -1661,8 +1661,9 @@ public final class Workbench extends EventManager implements IWorkbench {
 			public void runWithException() {
 				ColorDefinition[] colorDefinitions = WorkbenchPlugin.getDefault()
 						.getThemeRegistry().getColors();
-				ThemeElementHelper.populateRegistry(getThemeManager().getCurrentTheme(),
-						colorDefinitions, PrefUtil.getInternalPreferenceStore());
+				ThemeElementHelper.populateRegistry(getThemeManager().getTheme(
+						IThemeManager.DEFAULT_THEME), colorDefinitions, PrefUtil
+						.getInternalPreferenceStore());
 			}
 		});
 	}
