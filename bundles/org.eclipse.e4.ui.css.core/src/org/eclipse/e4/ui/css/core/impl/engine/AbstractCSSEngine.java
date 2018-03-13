2849202de81df69727a989c3efc89b145ad3338a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:11:03 +0000

    First commited as after_14712_rev5

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngine.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Angelo Zerr and others.
+ * Copyright (c) 2008, 2013 Angelo Zerr and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  *     IBM Corporation - ongoing development
+ *     Red Hat Inc. (mistria) - Bug 413348: fix stream leak
  *******************************************************************************/
 package org.eclipse.e4.ui.css.core.impl.engine;
 
@@ -201,16 +202,24 @@ public abstract class AbstractCSSEngine implements CSSEngine {
 		    		url = new URL(path);
 		    	}
 		    }
-			InputStream stream = url.openStream();
-			InputSource tempStream = new InputSource();
-			tempStream.setURI(url.toString());
-			tempStream.setByteStream(stream);
-			parseImport = true;
-			styleSheet = (CSSStyleSheet) this.parseStyleSheet(tempStream);
-			parseImport = false;
-			CSSRuleList tempRules = styleSheet.getCssRules();
-			for (int j = 0; j < tempRules.getLength(); j++) {
-				masterList.add(tempRules.item(j));
+		    InputStream stream = null;
+		    try {
+				stream = url.openStream();
+				InputSource tempStream = new InputSource();
+				tempStream.setURI(url.toString());
+				tempStream.setByteStream(stream);
+				parseImport = true;
+				styleSheet = (CSSStyleSheet) this.parseStyleSheet(tempStream);
+				parseImport = false;
+				CSSRuleList tempRules = styleSheet.getCssRules();
+				for (int j = 0; j < tempRules.getLength(); j++) {
+					masterList.add(tempRules.item(j));
+				}
+			} catch (IOException ex) {
+				if (stream != null) {
+					stream.close();
+				}
+				throw ex;
 			}
 		}
 		
