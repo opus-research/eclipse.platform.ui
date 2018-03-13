c42fbd43450bcf700a642d6c712f63dfb66fcf22
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:09:25 +0000

    First commited as before_14554_rev5

--- bundles/org.eclipse.jface/src/org/eclipse/jface/resource/ImageDescriptor.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/resource/ImageDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -74,7 +74,7 @@ public abstract class ImageDescriptor extends DeviceResourceDescriptor {
      * @param filename the file name
      * @return a new image descriptor
      */
-    public static ImageDescriptor createFromFile(Class location, String filename) {
+    public static ImageDescriptor createFromFile(Class<?> location, String filename) {
         return new FileImageDescriptor(location, filename);
     }
     
