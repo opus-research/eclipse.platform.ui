99163287679b5f2144c27740df8e94bc8f003066
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 21:50:58 +0000

    First commited as after_12131_rev1

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/UIAllTests.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/UIAllTests.java
@@ -36,7 +36,6 @@ import org.eclipse.e4.ui.tests.workbench.MWindowTest;
 import org.eclipse.e4.ui.tests.workbench.PartFocusTest;
 import org.eclipse.e4.ui.tests.workbench.PartRenderingEngineTests;
 import org.eclipse.e4.ui.tests.workbench.SashRendererTest;
-import org.eclipse.e4.ui.workbench.renderers.swt.StackRendererTest;
 import org.eclipse.e4.ui.workbench.renderers.swt.TabStateHandlerTest;
 
 //import org.eclipse.e4.ui.workbench.renderers.swt.StackRendererTest;
@@ -73,7 +72,8 @@ public class UIAllTests extends TestSuite {
 		addTestSuite(InjectionEventTest.class);
 		addTestSuite(PartFocusTest.class);
 		addTestSuite(ModelElementTest.class);
-		addTestSuite(StackRendererTest.class);
+		// addTestSuite(StackRendererTest.class); //temporarily disabled since
+		// needs to be fixed
 		addTestSuite(TabStateHandlerTest.class);
 		// addTestSuite(SWTPartRendererTest.class);
 	}
