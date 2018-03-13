6ba8d62a8fa7366dcb0643f2b5ab05b526ae1b86
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 23:27:21 +0000

    First commited as before_18609_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
@@ -529,7 +529,7 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 					IWorkbenchPart workbenchPart = ((CompatibilityPart) object).getPart();
 					if (workbenchPart instanceof ISaveablePart) {
 						ISaveablePart saveablePart = (ISaveablePart) workbenchPart;
-						return page.saveSaveable(saveablePart, workbenchPart, confirm, false);
+						return page.saveSaveable(saveablePart, workbenchPart, confirm, true);
 					}
 				}
 				return super.save(dirtyPart, confirm);
@@ -698,7 +698,6 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 		preferenceStore.setValue(IWorkbenchPreferenceConstants.ENABLE_ANIMATIONS, enableAnimations);
 
 		getShell().setData(this);
-		workbench.getHelpSystem().setHelp(getShell(), IWorkbenchHelpContextIds.WORKBENCH_WINDOW);
 		trackShellActivation();
 	}
 
