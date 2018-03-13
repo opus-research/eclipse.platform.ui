60e09ef1c9e100e857df4c9cfee1611c664ff1e2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:27:21 +0000

    First commited as after_15287_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/CompatibilityWorkbenchWindowControlContribution.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/CompatibilityWorkbenchWindowControlContribution.java
@@ -63,25 +63,26 @@ public class CompatibilityWorkbenchWindowControlContribution {
 				}
 
 				MUIElement parent = toolControl.getParent();
-				while (!(parent instanceof MTrimBar)) {
+				while (!(parent instanceof MTrimBar) && parent != null) {
 					parent = parent.getParent();
 				}
 
-				switch (((MTrimBar) parent).getSide()) {
-				case BOTTOM:
-					contribution.setCurSide(SWT.BOTTOM);
-					break;
-				case LEFT:
-					contribution.setCurSide(SWT.LEFT);
-					break;
-				case RIGHT:
-					contribution.setCurSide(SWT.RIGHT);
-					break;
-				case TOP:
-					contribution.setCurSide(SWT.TOP);
-					break;
+				if (parent instanceof MTrimBar) {
+					switch (((MTrimBar) parent).getSide()) {
+					case BOTTOM:
+						contribution.setCurSide(SWT.BOTTOM);
+						break;
+					case LEFT:
+						contribution.setCurSide(SWT.LEFT);
+						break;
+					case RIGHT:
+						contribution.setCurSide(SWT.RIGHT);
+						break;
+					case TOP:
+						contribution.setCurSide(SWT.TOP);
+						break;
+					}
 				}
-
 				contribution.delegateCreateControl(composite);
 			}
 		}
