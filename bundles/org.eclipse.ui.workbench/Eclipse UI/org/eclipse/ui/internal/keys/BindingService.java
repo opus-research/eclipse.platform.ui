6386d82c1c7d0e7c0d85b487df0b172079dcf2a9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 21:44:55 +0000

    First commited as after_10967_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/BindingService.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/BindingService.java
@@ -13,7 +13,6 @@ package org.eclipse.ui.internal.keys;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -282,28 +281,7 @@ public final class BindingService implements IBindingService {
 	 * .bindings.TriggerSequence)
 	 */
 	public Map getPartialMatches(TriggerSequence trigger) {
-		final TriggerSequence[] prefixes = trigger.getPrefixes();
-		final int prefixesLength = prefixes.length;
-		if (prefixesLength == 0) {
-			return Collections.EMPTY_MAP;
-		}
-
-		Collection<Binding> partialMatches = bindingService.getPartialMatches(trigger);
-		Map<TriggerSequence, Object> prefixTable = new HashMap<TriggerSequence, Object>();
-		for (Binding binding : partialMatches) {
-			for (int i = 0; i < prefixesLength; i++) {
-				final TriggerSequence prefix = prefixes[i];
-				final Object value = prefixTable.get(prefix);
-				if ((prefixTable.containsKey(prefix)) && (value instanceof Map)) {
-					((Map) value).put(prefixTable, binding);
-				} else {
-					final Map map = new HashMap();
-					prefixTable.put(prefix, map);
-					map.put(prefixTable, binding);
-				}
-			}
-		}
-		return prefixTable;
+		return manager.getPartialMatches(trigger);
 	}
 
 	/*
