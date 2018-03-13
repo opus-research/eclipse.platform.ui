11353882d9dbb1dc8504eb3d78861f581eab5606
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:33:52 +0000

    First commited as before_16117_rev2

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/ZipLeveledStructureProvider.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/ZipLeveledStructureProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -156,7 +156,7 @@ public class ZipLeveledStructureProvider implements
 	 * (non-Javadoc) Method declared on IImportStructureProvider
 	 */
 	public String getFullPath(Object element) {
-		return stripPath(new Path(((ZipEntry) element).getName()).toString());
+		return stripPath(((ZipEntry) element).getName());
 	}
 
 	/*
