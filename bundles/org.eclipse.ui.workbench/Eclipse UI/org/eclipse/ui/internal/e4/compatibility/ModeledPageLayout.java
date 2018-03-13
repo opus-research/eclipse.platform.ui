1c656d6facdf55a74308a19d7b674a56cba5ac5e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 23:19:43 +0000

    First commited as after_18561_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/ModeledPageLayout.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/ModeledPageLayout.java
@@ -285,9 +285,8 @@ public class ModeledPageLayout implements IPageLayout {
 
 	public IPlaceholderFolderLayout createPlaceholderFolder(String folderId,
 			int relationship, float ratio, String refId) {
-		MPartStack Stack = insertStack(folderId, relationship, ratio, refId,
-				false);
-		return new ModeledPlaceholderFolderLayout(this, application, Stack);
+		MPartStack stack = insertStack(folderId, relationship, ratio, refId, true);
+		return new ModeledPlaceholderFolderLayout(this, application, stack);
 	}
 
 	public IPerspectiveDescriptor getDescriptor() {
