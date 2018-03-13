61901fb2db77f411309db535b484ccb69e199ad9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:09:19 +0000

    First commited as before_14554_rev3

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StructuredViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/StructuredViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -586,7 +586,7 @@ public abstract class StructuredViewer extends ContentViewer implements IPostSel
 		if (filters == null) {
 			filters = new ArrayList();
 		}
-		filters.add(0, filter);
+		filters.add(filter);
 		refresh();
 	}
 
