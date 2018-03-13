69104bb4d9db3839d2e735de372dc7a515b8c8a0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 21:50:00 +0000

    First commited as after_11922_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/WorkbenchSiteProgressService.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/progress/WorkbenchSiteProgressService.java
@@ -334,7 +334,12 @@ public class WorkbenchSiteProgressService implements
      * @see org.eclipse.ui.progress.IWorkbenchSiteProgressService#warnOfContentChange()
      */
     public void warnOfContentChange() {
-		// site.getPane().showHighlight();
+		System.err.println("warnOfContentChange"); //$NON-NLS-1$
+		MPart part = site.getModel();
+		// if (!part.getTags().contains(CSSConstants.CSS_CONTENT_CHANGED_CLASS))
+		// {
+			part.getTags().add(CSSConstants.CSS_CONTENT_CHANGED_CLASS);
+		// }
     }
 
     /*
