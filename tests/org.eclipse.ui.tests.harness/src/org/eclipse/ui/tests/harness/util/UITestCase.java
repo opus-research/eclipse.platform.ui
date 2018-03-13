b1a68b0f2fa043c8d182e0f77a70bf8c9f197dfe
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 21:44:20 +0000

    First commited as before_10847_rev1

--- tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/harness/util/UITestCase.java
+++ tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/harness/util/UITestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -316,7 +316,7 @@ public abstract class UITestCase extends TestCase {
         IWorkbenchPage[] pages = openTestPage(win, 1);
         if (pages != null) {
             return pages[0];
-        }
+        }        
         return null;
     }
 
