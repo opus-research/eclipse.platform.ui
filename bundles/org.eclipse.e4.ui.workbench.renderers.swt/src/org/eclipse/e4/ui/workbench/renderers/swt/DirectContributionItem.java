1781d492e12d1cd1cb535b249538a29552cb821a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:08:19 +0000

    First commited as after_14093_rev1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/DirectContributionItem.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/DirectContributionItem.java
@@ -443,6 +443,15 @@ public class DirectContributionItem extends ContributionItem {
 					return menu;
 				}
 			}
+		} else {
+			final IEclipseContext lclContext = getContext(mmenu);
+			IPresentationEngine engine = lclContext
+					.get(IPresentationEngine.class);
+			obj = engine.createGui(mmenu, toolItem.getParent(), lclContext);
+			if (obj instanceof Menu) {
+				return (Menu) obj;
+			}
+			System.err.println("Rendering returned " + obj); //$NON-NLS-1$
 		}
 		return null;
 	}
