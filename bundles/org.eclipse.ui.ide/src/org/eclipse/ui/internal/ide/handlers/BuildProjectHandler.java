e4e905e621a419dba1cda431d42326ecc6586a86
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:09:57 +0000

    First commited as before_14573_rev4

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/handlers/BuildProjectHandler.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/handlers/BuildProjectHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2013 IBM Corporation and others.
+ * Copyright (c) 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,28 +7,19 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Freescale - Bug 411287 - Quick Access > Build Project is offered even if no valid selection exists
  ******************************************************************************/
 
 package org.eclipse.ui.internal.ide.handlers;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IPageListener;
-import org.eclipse.ui.ISources;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.BuildAction;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -42,8 +33,6 @@ import org.eclipse.ui.part.FileEditorInput;
  */
 public class BuildProjectHandler extends AbstractHandler {
 
-	private Map buildActions = Collections.synchronizedMap(new HashMap());
-	
 	/**
 	 * @throws ExecutionException
 	 */
@@ -76,56 +65,12 @@ public class BuildProjectHandler extends AbstractHandler {
 		return null;
 	}
 
-	private void runBuildAction(IWorkbenchWindow window, 
-			ISelection currentSelection) { 
-		BuildAction buildAction = getBuildAction(window); 
-		buildAction.selectionChanged((IStructuredSelection) currentSelection); 
-		buildAction.run(); 
-	} 
-	
-	private BuildAction getBuildAction(IWorkbenchWindow window) {
-		
-		// Try to get the buildAction from our cache
-		BuildAction buildAction = (BuildAction) buildActions.get(window);
-		if (buildAction != null) {
-			return buildAction;
-		}
-		
-		// Create a new buildAction for the window and cache it
-		buildAction = new BuildAction(window, 
+	private void runBuildAction(IWorkbenchWindow window,
+			ISelection currentSelection) {
+		BuildAction buildAction = new BuildAction(window,
 				IncrementalProjectBuilder.INCREMENTAL_BUILD);
-		buildActions.put(window, buildAction);
-		
-		// Set up a listener to remove the cache entry when the window is closed
-		window.addPageListener(new IPageListener() {
-			public void pageOpened(IWorkbenchPage page) {
-			}
-			public void pageClosed(IWorkbenchPage page) {
-				IWorkbenchWindow closedWindow = page.getWorkbenchWindow();
-				buildActions.remove(closedWindow);
-				closedWindow.removePageListener(this);
-			}
-			public void pageActivated(IWorkbenchPage page) {
-			}
-		});
-		
-		return buildAction;
+		buildAction.selectionChanged((IStructuredSelection) currentSelection);
+		buildAction.run();
 	}
-
-	/* (non-Javadoc) 
-	 * @see org.eclipse.core.commands.AbstractHandler#setEnabled(java.lang.Object) 
-	 */ 
-	public void setEnabled(Object evaluationContext) { 
-		boolean enabled = false; 
-		if ((evaluationContext instanceof IEvaluationContext)) { 
-			IEvaluationContext context = (IEvaluationContext) evaluationContext; 
-			Object object = context.getVariable(ISources.ACTIVE_WORKBENCH_WINDOW_NAME); 
-			if (object instanceof IWorkbenchWindow) { 
-				BuildAction buildAction = getBuildAction((IWorkbenchWindow) object); 
-				enabled = buildAction.isEnabled(); 
-			} 
-		} 
-		setBaseEnabled(enabled); 
-	} 		  
 
 }
