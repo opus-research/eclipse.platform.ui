5e2cbd0982d9c1bbf5a65dcf220f754e40af9634
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 23:51:52 +0000

    First commited as after_19225_rev1

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/OpenResourceDialog.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/OpenResourceDialog.java
@@ -7,13 +7,17 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Sebastian Bauer
  *******************************************************************************/
 package org.eclipse.ui.internal.ide.dialogs;
 
 import java.util.Collections;
+import java.util.Iterator;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.Action;
@@ -27,6 +31,9 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.util.Util;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
@@ -207,6 +214,45 @@ public class OpenResourceDialog extends FilteredResourcesSelectionDialog {
 		ShowInMenu showInMenu = new ResourceShowInMenu(selectedItems, activePage.getWorkbenchWindow());
 		showInMenuManager.add(showInMenu);
 		menuManager.add(showInMenuManager);
+
+		// Add 'Copy Qualified Name' menu item
+		menuManager.add(new Action(IDEWorkbenchMessages.OpenResourceDialog_copyLocation_label) {
+			/* (non-Javadoc)
+			 * @see org.eclipse.jface.action.Action#run()
+			 */
+			public void run() {
+				if (getSelectedItems().isEmpty()) {
+					return;
+				}
+
+				StringBuilder str = new StringBuilder();
+				Iterator iter = getSelectedItems().iterator();
+				while (iter.hasNext()) {
+					Object o = iter.next();
+					if (o instanceof IFile) {
+						IFile f = (IFile)o;
+						IPath p = f.getLocation();
+						if (p != null) {
+							str.append(p.toOSString());
+							str.append('\n');
+						}
+					}
+				}
+
+				if (str.length() > 0) {
+					Clipboard clipboard = null;
+					try {
+						clipboard = new Clipboard(getShell().getDisplay());
+						clipboard.setContents(new Object[] { str.toString() },
+								new Transfer[] { TextTransfer.getInstance() });
+					} finally {
+						if (clipboard != null) {
+							clipboard.dispose();
+						}
+					}
+				}
+			}
+		});
 	}
 
 	/*
