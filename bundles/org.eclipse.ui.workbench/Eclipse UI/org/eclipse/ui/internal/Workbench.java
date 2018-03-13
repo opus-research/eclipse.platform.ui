34adc80dea5d5646ecee37ec834d3fe92343a43c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:07:20 +0000

    First commited as before_13872_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -591,8 +591,11 @@ public final class Workbench extends EventManager implements IWorkbench {
 
 					AbstractSplashHandler handler = getSplash();
 
+					boolean showProgress = PrefUtil.getAPIPreferenceStore().getBoolean(
+									IWorkbenchPreferenceConstants.SHOW_PROGRESS_ON_STARTUP);
+
 					IProgressMonitor progressMonitor = null;
-					if (handler != null) {
+					if (handler != null && showProgress) {
 						progressMonitor = handler.getBundleProgressMonitor();
 						if (progressMonitor != null) {
 							double cutoff = 0.95;
@@ -1661,9 +1664,8 @@ public final class Workbench extends EventManager implements IWorkbench {
 			public void runWithException() {
 				ColorDefinition[] colorDefinitions = WorkbenchPlugin.getDefault()
 						.getThemeRegistry().getColors();
-				ThemeElementHelper.populateRegistry(getThemeManager().getTheme(
-						IThemeManager.DEFAULT_THEME), colorDefinitions, PrefUtil
-						.getInternalPreferenceStore());
+				ThemeElementHelper.populateRegistry(getThemeManager().getCurrentTheme(),
+						colorDefinitions, PrefUtil.getInternalPreferenceStore());
 			}
 		});
 	}
