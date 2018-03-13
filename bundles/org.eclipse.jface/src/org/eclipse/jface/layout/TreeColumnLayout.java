ff381e45c49776a2ad2a62c6ca9947b954582e1a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:25:12 +0000

    First commited as before_15029_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/layout/TreeColumnLayout.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/layout/TreeColumnLayout.java
@@ -68,7 +68,6 @@ public class TreeColumnLayout extends AbstractColumnLayout {
 	
 	private static final TreeLayoutListener listener = new TreeLayoutListener();
 	
-	@Override
 	protected void layout(Composite composite, boolean flushCache) {
 		super.layout(composite, flushCache);
 		if( addListener ) {
@@ -82,7 +81,6 @@ public class TreeColumnLayout extends AbstractColumnLayout {
 	 * 
 	 * @since 3.5
 	 */
-	@Override
 	protected int getColumnCount(Scrollable tree) {
 		return ((Tree) tree).getColumnCount();
 	}
@@ -92,7 +90,6 @@ public class TreeColumnLayout extends AbstractColumnLayout {
 	 * 
 	 * @since 3.5
 	 */
-	@Override
 	protected void setColumnWidths(Scrollable tree, int[] widths) {
 		TreeColumn[] columns = ((Tree) tree).getColumns();
 		for (int i = 0; i < widths.length; i++) {
@@ -105,7 +102,6 @@ public class TreeColumnLayout extends AbstractColumnLayout {
 	 * 
 	 * @since 3.5
 	 */
-	@Override
 	protected ColumnLayoutData getLayoutData(Scrollable tableTree, int columnIndex) {
 		TreeColumn column = ((Tree) tableTree).getColumn(columnIndex);
 		return (ColumnLayoutData) column.getData(LAYOUT_DATA);
@@ -116,7 +112,6 @@ public class TreeColumnLayout extends AbstractColumnLayout {
 	 * 
 	 * @since 3.5
 	 */
-	@Override
 	protected void updateColumnData(Widget column) {
 		TreeColumn tColumn = (TreeColumn) column;
 		Tree t = tColumn.getParent();
