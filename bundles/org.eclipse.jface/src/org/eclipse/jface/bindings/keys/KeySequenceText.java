9b95fa98f06ebe3e3c357a992ff922d8f2ab5bb7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:42:04 +0000

    First commited as before_16762_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/bindings/keys/KeySequenceText.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/bindings/keys/KeySequenceText.java
@@ -473,7 +473,6 @@ public final class KeySequenceText {
 		trappedKeys.add(SWTKeySupport.convertAcceleratorToKeyStroke(SWT.TAB
 				| SWT.SHIFT));
 		trappedKeys.add(SWTKeySupport.convertAcceleratorToKeyStroke(SWT.BS));
-		trappedKeys.add(SWTKeySupport.convertAcceleratorToKeyStroke(SWT.DEL));
 		List trappedKeyList = new ArrayList(trappedKeys);
 		TRAPPED_KEYS = Collections.unmodifiableList(trappedKeyList);
 	}
