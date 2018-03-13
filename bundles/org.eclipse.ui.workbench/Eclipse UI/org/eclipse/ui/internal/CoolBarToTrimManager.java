4e1399a26597222b3a2d6bc2e0ee84a001488e31
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 22:00:29 +0000

    First commited as before_12819_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CoolBarToTrimManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/CoolBarToTrimManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2013 IBM Corporation and others.
+ * Copyright (c) 2011, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,9 +19,11 @@ import org.eclipse.e4.ui.model.application.ui.SideValue;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimBar;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimmedWindow;
+import org.eclipse.e4.ui.model.application.ui.menu.MOpaqueToolItem;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolBarElement;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolBarSeparator;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolItem;
 import org.eclipse.e4.ui.model.application.ui.menu.impl.MenuFactoryImpl;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.renderers.swt.ToolBarManagerRenderer;
@@ -705,8 +707,9 @@ public class CoolBarToTrimManager extends ContributionManager implements ICoolBa
 			if (item == null) {
 				continue;
 			}
-			MToolBarElement toolItem = renderer.getToolElement(item);
-
+			if (renderer.getToolElement(item) != null) {
+				continue;
+			}
 			if (item instanceof IToolBarContributionItem) {
 				IToolBarManager manager2 = ((IToolBarContributionItem) item).getToolBarManager();
 				//new Exception("fill(MToolBar container, IContributionManager manager) with " //$NON-NLS-1$
@@ -717,43 +720,22 @@ public class CoolBarToTrimManager extends ContributionManager implements ICoolBa
 				//		"fill(MToolBar container, IContributionManager manager) with rogue contribution manager: " //$NON-NLS-1$
 				// + item).printStackTrace();
 				fill(container, (IContributionManager) item);
-			} else if (item instanceof CommandContributionItem && toolItem == null) {
+			} else if (item instanceof CommandContributionItem) {
 				CommandContributionItem cci = (CommandContributionItem) item;
-				toolItem = MenuHelper.createToolItem(application, cci);
+				MToolItem toolItem = MenuHelper.createToolItem(application, cci);
 				manager.remove(item);
 				if (toolItem != null) {
 					container.getChildren().add(toolItem);
 				}
 			} else {
-				if (toolItem == null) {
-					toolItem = MenuFactoryImpl.eINSTANCE.createOpaqueToolItem();
-					toolItem.setElementId(item.getId());
-					renderer.linkModelToContribution(toolItem, item);
-				}
-				if (item instanceof AbstractGroupMarker && container.getWidget() instanceof Control
-						&& !((Control) container.getWidget()).isDisposed()) {
-					toolItem.setVisible(isChildVisible(item));
-					// force the EMF model refresh
-					container.getChildren().remove(toolItem);
+				MOpaqueToolItem toolItem = MenuFactoryImpl.eINSTANCE.createOpaqueToolItem();
+				toolItem.setElementId(item.getId());
+				if (item instanceof AbstractGroupMarker) {
+					toolItem.setVisible(item.isVisible());
 				}
 				container.getChildren().add(toolItem);
+				renderer.linkModelToContribution(toolItem, item);
 			}
 		}
-	}
-
-	private boolean isChildVisible(IContributionItem item) {
-		Boolean v;
-
-		IContributionManagerOverrides overrides = getOverrides();
-		if (overrides == null) {
-			v = null;
-		} else {
-			v = getOverrides().getVisible(item);
-		}
-
-		if (v != null) {
-			return v.booleanValue();
-		}
-		return item.isVisible();
 	}
 }
