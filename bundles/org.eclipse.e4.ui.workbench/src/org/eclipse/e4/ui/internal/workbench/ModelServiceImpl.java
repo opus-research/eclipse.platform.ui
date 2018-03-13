0e426e9ace51e21a96a0d2d1de933660ed43d376
Matheus Paixao <mhepaixao@gmail.com>
2018-03-13 23:29:03 +0000

    First commited as before_18658_rev6

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelServiceImpl.java
@@ -19,6 +19,8 @@ import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.MApplicationElement;
+import org.eclipse.e4.ui.model.application.commands.MHandler;
+import org.eclipse.e4.ui.model.application.commands.MHandlerContainer;
 import org.eclipse.e4.ui.model.application.descriptor.basic.MPartDescriptor;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MGenericTile;
@@ -40,6 +42,8 @@ import org.eclipse.e4.ui.model.application.ui.basic.MTrimmedWindow;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindowElement;
 import org.eclipse.e4.ui.model.application.ui.basic.impl.BasicFactoryImpl;
+import org.eclipse.e4.ui.model.application.ui.menu.MMenu;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
 import org.eclipse.e4.ui.model.application.ui.menu.MToolControl;
 import org.eclipse.e4.ui.model.internal.ModelUtils;
 import org.eclipse.e4.ui.workbench.IPresentationEngine;
@@ -102,26 +106,20 @@ public class ModelServiceImpl implements EModelService {
 		IEventBroker eventBroker = appContext.get(IEventBroker.class);
 		eventBroker.subscribe(UIEvents.UIElement.TOPIC_WIDGET, hostedElementHandler);
 
-		mApplicationElementFactory = new GenericMApplicationElementFactoryImpl(appContext.get(IExtensionRegistry.class));
+		mApplicationElementFactory = new GenericMApplicationElementFactoryImpl(
+				appContext.get(IExtensionRegistry.class));
 	}
 
-	/**
-	 * @see EModelService#createModelElement(Class)
-	 * @generated
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.e4.ui.workbench.modeling.EModelService#createModelElement(java.lang.Class)
 	 */
 	@SuppressWarnings("unchecked")
 	public final <T extends MApplicationElement> T createModelElement(Class<T> elementType) {
 		if (elementType == null) {
 			throw new NullPointerException("Argument cannot be null."); //$NON-NLS-1$
 		}
-
-		/*
-		 * TODO: We can even tune the performance of this method if we add the previous generated
-		 * if-else-if-else... block. The eObjectFactory#createEObject(Class) could afterwards be the
-		 * fallback if a user specific model element should be created. But for this we need to
-		 * adapt the generator of Paul Elder and to be honest I wasn't able to find it (either its
-		 * call during the build nor its template) ;-( .
-		 */
 
 		T back = (T) mApplicationElementFactory.createEObject(elementType);
 		if (back != null) {
@@ -222,7 +220,13 @@ public class ModelServiceImpl implements EModelService {
 			for (MWindow dw : window.getWindows()) {
 				findElementsRecursive(dw, id, type, tagsToMatch, elements, searchFlags);
 			}
+
+			MMenu menu = window.getMainMenu();
+			if (menu != null && (searchFlags & IN_MAIN_MENU) != 0) {
+				findElementsRecursive(menu, id, type, tagsToMatch, elements, searchFlags);
+			}
 		}
+
 		if (searchRoot instanceof MPerspective) {
 			MPerspective persp = (MPerspective) searchRoot;
 			for (MWindow dw : persp.getWindows()) {
@@ -237,6 +241,19 @@ public class ModelServiceImpl implements EModelService {
 			if (ph.getRef() != null
 					&& (!(ph.getRef() instanceof MArea) || (searchFlags & IN_SHARED_AREA) != 0)) {
 				findElementsRecursive(ph.getRef(), id, type, tagsToMatch, elements, searchFlags);
+			}
+		}
+
+		if (searchRoot instanceof MPart && (searchFlags & IN_PART) != 0) {
+			MPart part = (MPart) searchRoot;
+
+			for (MMenu menu : part.getMenus()) {
+				findElementsRecursive(menu, id, type, tagsToMatch, elements, searchFlags);
+			}
+
+			MToolBar toolBar = part.getToolbar();
+			if (toolBar != null) {
+				findElementsRecursive(toolBar, id, type, tagsToMatch, elements, searchFlags);
 			}
 		}
 	}
@@ -343,6 +360,10 @@ public class ModelServiceImpl implements EModelService {
 				snippetContainer.getSnippets().remove(snippet);
 			snippetContainer.getSnippets().add(clone);
 		}
+
+		// Cache the original element in the clone's transientData
+		clone.getTransientData().put(CLONED_FROM_KEY, element);
+
 		return clone;
 	}
 
@@ -398,6 +419,26 @@ public class ModelServiceImpl implements EModelService {
 		for (MUIElement snippet : snippets) {
 			if (id.equals(snippet.getElementId()))
 				return snippet;
+		}
+
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.e4.ui.workbench.modeling.EModelService#findHandler(org.eclipse.e4.ui.model.
+	 * application.commands.MHandlerContainer, java.lang.String)
+	 */
+	public MHandler findHandler(MHandlerContainer handlerContainer, String id) {
+		if (handlerContainer == null || id == null || id.length() == 0) {
+			return null;
+		}
+
+		for (MHandler handler : handlerContainer.getHandlers()) {
+			if (id.equals(handler.getElementId())) {
+				return handler;
+			}
 		}
 
 		return null;
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -12,6 +12,7 @@
  *     Tristan Hume - <trishume@gmail.com> -
  *     		Fix for Bug 2369 [Workbench] Would like to be able to save workspace without exiting
  *     		Implemented workbench auto-save to correctly restore state in case of crash.
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 422533
  *******************************************************************************/
 
 package org.eclipse.ui.internal;
@@ -140,6 +141,7 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IDecoratorManager;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorRegistry;
 import org.eclipse.ui.IElementFactory;
@@ -162,6 +164,7 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPreferenceConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkingSetManager;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.Saveable;
 import org.eclipse.ui.WorkbenchException;
@@ -313,15 +316,10 @@ public final class Workbench extends EventManager implements IWorkbench {
 				}
 			}
 
-			String taskName;
-
-			if (bundleName == null) {
-				taskName = WorkbenchMessages.Startup_Loading_Workbench;
-			} else {
-				taskName = NLS.bind(WorkbenchMessages.Startup_Loading, bundleName);
+			if (bundleName != null) {
+				String taskName = NLS.bind(WorkbenchMessages.Startup_Loading, bundleName);
+				progressMonitor.subTask(taskName);
 			}
-
-			progressMonitor.subTask(taskName);
 		}
 	}
 
@@ -609,12 +607,16 @@ public final class Workbench extends EventManager implements IWorkbench {
 					}
 					// run the legacy workbench once
 					returnCode[0] = workbench.runUI();
-					// run the e4 event loop and instantiate ... well, stuff
-					e4Workbench.createAndRunUI(e4Workbench.getApplication());
-					WorkbenchMenuService wms = (WorkbenchMenuService) e4Workbench.getContext().get(
-							IMenuService.class);
-					wms.dispose();
-					e4app.saveModel();
+					if (returnCode[0] == PlatformUI.RETURN_OK) {
+						// run the e4 event loop and instantiate ... well, stuff
+						e4Workbench.createAndRunUI(e4Workbench.getApplication());
+						WorkbenchMenuService wms = (WorkbenchMenuService) e4Workbench.getContext()
+								.get(IMenuService.class);
+						wms.dispose();
+					}
+					if (returnCode[0] != PlatformUI.RETURN_UNSTARTABLE) {
+						e4app.saveModel();
+					}
 					e4Workbench.close();
 					returnCode[0] = workbench.returnCode;
 				}
@@ -1602,8 +1604,8 @@ public final class Workbench extends EventManager implements IWorkbench {
 					// TODO compat: open the windows here/instantiate the model
 					// TODO compat: instantiate the WW around the model
 					initializationDone = true;
-					// if (isClosing() || !advisor.openWindows()) {
-					if (isClosing()) {
+					if (isClosing() || !advisor.openWindows()) {
+						// if (isClosing()) {
 						bail[0] = true;
 					}
 
@@ -1915,7 +1917,29 @@ UIEvents.Context.TOPIC_CONTEXT,
 			WorkbenchPage page = getWorkbenchPage(part);
 			EditorReference ref = page.getEditorReference(part);
 			if (ref == null) {
-				ref = createEditorReference(part, page);
+				// If this editor was cloned from an existing editor (as
+				// part of a split...) then re-create a valid EditorReference
+				// from the existing editor's ref.
+				MPart clonedFrom = (MPart) part.getTransientData().get(
+						EModelService.CLONED_FROM_KEY);
+				if (clonedFrom != null && clonedFrom.getContext() != null) {
+					EditorReference originalRef = page.getEditorReference(clonedFrom);
+					if (originalRef != null) {
+						IEditorInput partInput = null;
+						String editorId = originalRef.getDescriptor().getId();
+						try {
+							partInput = originalRef.getEditorInput();
+						} catch (PartInitException e) {
+							System.out.println("Ooops !!!"); //$NON-NLS-1$
+						}
+						ref = page.createEditorReferenceForPart(part, partInput, editorId, null);
+					}
+				}
+
+				// Fallback code
+				if (ref == null) {
+					ref = createEditorReference(part, page);
+				}
 			}
 			context.set(EditorReference.class.getName(), ref);
 		} else {
@@ -2781,6 +2805,9 @@ UIEvents.Context.TOPIC_CONTEXT,
 				// runEventLoop(handler, display);
 			}
 			returnCode = PlatformUI.RETURN_OK;
+			if (!initOK[0]) {
+				returnCode = PlatformUI.RETURN_UNSTARTABLE;
+			}
 		} catch (final Exception e) {
 			if (!display.isDisposed()) {
 				handler.handleException(e);
