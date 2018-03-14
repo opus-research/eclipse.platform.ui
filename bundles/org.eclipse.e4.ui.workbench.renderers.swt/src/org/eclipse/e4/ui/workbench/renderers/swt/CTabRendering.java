61962531976c32f29f050812c2c0c1be3dc8d083
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:29:50 +0000

    First commited as before_26684_rev3

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/CTabRendering.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/CTabRendering.java
@@ -4,14 +4,11 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
-
-import static org.eclipse.e4.ui.css.swt.dom.CTabFolderElement.setBackgroundOverriddenDuringRenderering;
-import static org.eclipse.e4.ui.css.swt.dom.CompositeElement.hasBackgroundOverriddenByCSS;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -30,13 +27,11 @@ import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.graphics.Region;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
 @SuppressWarnings("restriction")
 public class CTabRendering extends CTabFolderRenderer implements
-ICTabRendering {
+ ICTabRendering {
 	// Constants for circle drawing
 	final static int LEFT_TOP = 0;
 	final static int LEFT_BOTTOM = 1;
@@ -116,19 +111,19 @@ ICTabRendering {
 				y = onBottom ? y - paddingTop - marginHeight - borderTop
 						- (cornerSize / 4) : y - paddingTop - marginHeight
 						- tabHeight - borderTop - (cornerSize / 4);
-						width = 2 + paddingLeft + paddingRight;
-						height += paddingTop + paddingBottom;
-						height += tabHeight + (cornerSize / 4) + borderBottom
-								+ borderTop;
+				width = 2 + paddingLeft + paddingRight;
+				height += paddingTop + paddingBottom;
+				height += tabHeight + (cornerSize / 4) + borderBottom
+						+ borderTop;
 			} else {
 				x = x - marginWidth - OUTER_KEYLINE - INNER_KEYLINE
 						- sideDropWidth - (cornerSize / 2);
 				width = width + 2 * OUTER_KEYLINE + 2 * INNER_KEYLINE + 2
 						* marginWidth + 2 * sideDropWidth + cornerSize;
 				int tabHeight = parent.getTabHeight() + 1; // TODO: Figure out
-				// what
-				// to do about the
-				// +1
+															// what
+															// to do about the
+															// +1
 				// TODO: Fix
 				if (parent.getMinimized()) {
 					y = onBottom ? y - borderTop - 5 : y - tabHeight
@@ -143,10 +138,10 @@ ICTabRendering {
 					y = onBottom ? y - marginHeight - borderTop
 							- (cornerSize / 4) : y - marginHeight - tabHeight
 							- borderTop - (cornerSize / 4);
-							height = height + borderBottom + borderTop + 2
-									* marginHeight + tabHeight + cornerSize / 2
-									+ cornerSize / 4
-									+ (shadowEnabled ? BOTTOM_DROP_WIDTH : 0);
+					height = height + borderBottom + borderTop + 2
+							* marginHeight + tabHeight + cornerSize / 2
+							+ cornerSize / 4
+							+ (shadowEnabled ? BOTTOM_DROP_WIDTH : 0);
 				}
 			}
 			break;
@@ -156,7 +151,7 @@ ICTabRendering {
 			break;
 		case PART_BORDER:
 			x = x - INNER_KEYLINE - OUTER_KEYLINE - sideDropWidth
-			- (cornerSize / 4);
+					- (cornerSize / 4);
 			width = width + 2 * (INNER_KEYLINE + OUTER_KEYLINE + sideDropWidth)
 					+ cornerSize / 2;
 			height = height + borderTop + borderBottom;
@@ -268,16 +263,16 @@ ICTabRendering {
 		gc.setClipping(region);
 
 		int header = shadowEnabled ? onBottom ? 6 : 3 : 1; // TODO: this needs
-		// to be added to
-		// computeTrim for
+															// to be added to
+															// computeTrim for
 		// HEADER
 		Rectangle trim = computeTrim(PART_HEADER, state, 0, 0, 0, 0);
 		trim.width = bounds.width - trim.width;
-
+		
 		// XXX: The magic numbers need to be cleaned up. See https://bugs.eclipse.org/425777 for details.
 		trim.height = (parent.getTabHeight() + (onBottom ? 7 : 4))
 				- trim.height;
-
+		
 		trim.x = -trim.x;
 		trim.y = onBottom ? bounds.height - parent.getTabHeight() - 1 - header
 				: -trim.y;
@@ -329,8 +324,8 @@ ICTabRendering {
 		int height = Math.max(parent.getTabHeight() + INNER_KEYLINE
 				+ OUTER_KEYLINE + (shadowEnabled ? BOTTOM_DROP_WIDTH : 0),
 				bounds.height - INNER_KEYLINE - OUTER_KEYLINE - 2
-				* marginHeight
-				- (shadowEnabled ? BOTTOM_DROP_WIDTH : 0));
+						* marginHeight
+						- (shadowEnabled ? BOTTOM_DROP_WIDTH : 0));
 
 		int circX = bounds.x + delta / 2 + radius;
 		int circY = bounds.y + radius;
@@ -375,7 +370,7 @@ ICTabRendering {
 		Rectangle mappedBounds = display
 				.map(parent, parent.getParent(), bounds);
 		parent.getParent().drawBackground(gc, bounds.x, bounds.y, bounds.width,
-				bounds.height, mappedBounds.x, mappedBounds.y);
+ bounds.height, mappedBounds.x, mappedBounds.y);
 
 		// Shadow
 		if (shadowEnabled)
@@ -478,10 +473,10 @@ ICTabRendering {
 		}
 		gc.setClipping(0, onBottom ? bounds.y - header : bounds.y,
 				parent.getSize().x
-				- (shadowEnabled ? SIDE_DROP_WIDTH : 0 + INNER_KEYLINE
-						+ OUTER_KEYLINE), bounds.y + bounds.height);// bounds.height
-		// +
-		// 4);
+						- (shadowEnabled ? SIDE_DROP_WIDTH : 0 + INNER_KEYLINE
+								+ OUTER_KEYLINE), bounds.y + bounds.height);// bounds.height
+																			// +
+																			// 4);
 
 		Pattern backgroundPattern = null;
 		if (selectedTabFillColors == null) {
@@ -515,7 +510,7 @@ ICTabRendering {
 			gradientLineTop = new Color(gc.getDevice(), topGradient);
 			foregroundPattern = new Pattern(gc.getDevice(), 0, 0, 0,
 					bounds.height + 1, gradientLineTop, gc.getDevice()
-					.getSystemColor(SWT.COLOR_WHITE));
+							.getSystemColor(SWT.COLOR_WHITE));
 			gc.setForegroundPattern(foregroundPattern);
 		}
 		gc.drawPolyline(tmpPoints);
@@ -645,8 +640,8 @@ ICTabRendering {
 			}
 			gc.setClipping(points[0], onBottom ? bounds.y - header : bounds.y,
 					parent.getSize().x
-					- (shadowEnabled ? SIDE_DROP_WIDTH : 0
-							+ INNER_KEYLINE + OUTER_KEYLINE), bounds.y
+							- (shadowEnabled ? SIDE_DROP_WIDTH : 0
+									+ INNER_KEYLINE + OUTER_KEYLINE), bounds.y
 							+ bounds.height);
 
 			gc.setBackground(gc.getDevice().getSystemColor(SWT.COLOR_WHITE));
@@ -1052,37 +1047,41 @@ ICTabRendering {
 		boolean vertical = selected ? parentWrapper
 				.isSelectionGradientVertical() : parentWrapper
 				.isGradientVertical();
-				Rectangle partHeaderBounds = computeTrim(PART_HEADER, state, bounds.x,
-						bounds.y, bounds.width, bounds.height);
-
-				drawUnselectedTabBackground(gc, partHeaderBounds, state, vertical,
-						defaultBackground);
-				drawTabBackground(gc, partHeaderBounds, state, vertical,
-						defaultBackground);
-				drawChildrenBackground(partHeaderBounds);
+		Rectangle partHeaderBounds = computeTrim(PART_HEADER, state, bounds.x,
+				bounds.y, bounds.width, bounds.height);
+		
+		drawUnselectedTabBackground(gc, partHeaderBounds, state, vertical,
+				defaultBackground);
+		drawTabBackground(gc, partHeaderBounds, state, vertical,
+				defaultBackground);
 	}
 
 	private void drawUnselectedTabBackground(GC gc, Rectangle partHeaderBounds,
 			int state, boolean vertical, Color defaultBackground) {
-		if (unselectedTabsColors == null) {
-			boolean selected = (state & SWT.SELECTED) != 0;
-			unselectedTabsColors = selected ? parentWrapper
-					.getSelectionGradientColors() : parentWrapper
-					.getGradientColors();
-					unselectedTabsPercents = selected ? parentWrapper
-							.getSelectionGradientPercents() :
-								parentWrapper.getGradientPercents();
-		}
-		if (unselectedTabsColors == null) {
-			unselectedTabsColors = new Color[] { gc.getDevice().getSystemColor(
-					SWT.COLOR_WHITE) };
-			unselectedTabsPercents = new int[] { 100 };
-		}
-
 		rendererWrapper.drawBackground(gc, partHeaderBounds.x,
 				partHeaderBounds.y - 1, partHeaderBounds.width,
 				partHeaderBounds.height, defaultBackground,
-				unselectedTabsColors, unselectedTabsPercents, vertical);
+				getUnselectedTabsColors(state),
+				getUnselectedTabsPercents(state),
+				vertical);
+	}
+
+	private Color[] getUnselectedTabsColors(int state) {
+		if (unselectedTabsColors == null) {
+			return (state & SWT.SELECTED) != 0 ? parentWrapper
+					.getSelectionGradientColors() : parentWrapper
+					.getGradientColors();
+		}
+		return unselectedTabsColors;
+	}
+
+	private int[] getUnselectedTabsPercents(int state) {
+		if (unselectedTabsPercents == null) {
+			return (state & SWT.SELECTED) != 0 ? parentWrapper
+					.getSelectionGradientPercents() : parentWrapper
+					.getGradientPercents();
+		}
+		return unselectedTabsPercents;
 	}
 
 	private void drawTabBackground(GC gc, Rectangle partHeaderBounds,
@@ -1095,46 +1094,25 @@ ICTabRendering {
 		}
 		if (colors == null) {
 			boolean selected = (state & SWT.SELECTED) != 0;
-			colors = selected ? parentWrapper.getSelectionGradientColors() :
-				parentWrapper.getGradientColors();
-			percents = selected ? parentWrapper.getSelectionGradientPercents() :
-				parentWrapper.getGradientPercents();
+			colors = selected ? parentWrapper.getSelectionGradientColors()
+					: parentWrapper.getGradientColors();
+			percents = selected ? parentWrapper.getSelectionGradientPercents()
+					: parentWrapper.getGradientPercents();
 		}
 		if (colors == null) {
-			colors = new Color[] { gc.getDevice().getSystemColor(SWT.COLOR_WHITE) };
+			colors = new Color[] { gc.getDevice().getSystemColor(
+					SWT.COLOR_WHITE) };
 			percents = new int[] { 100 };
 		}
-		rendererWrapper.drawBackground(gc, partHeaderBounds.x,  partHeaderBounds.height - 1, partHeaderBounds.width,
+
+		rendererWrapper.drawBackground(gc, partHeaderBounds.x,
+				partHeaderBounds.height - 1, partHeaderBounds.width,
 				parent.getBounds().height, defaultBackground, colors, percents,
 				vertical);
 	}
 
-	private void drawChildrenBackground(Rectangle partHeaderBounds) {
-		for (Control control : parent.getChildren()) {
-			if (control instanceof Composite
-					&& !hasBackgroundOverriddenByCSS(control)) {
-				drawChildBackground((Composite) control, partHeaderBounds);
-			}
-		}
-	}
-
-	private void drawChildBackground(Composite composite,
-			Rectangle partHeaderBounds) {
-		Rectangle rec = composite.getBounds();
-		Color background = null;
-		boolean partOfHeader = rec.y >= partHeaderBounds.y
-				&& rec.y < partHeaderBounds.height;
-
-		if (!partOfHeader && selectedTabFillColors != null) {
-			background = selectedTabFillColors.length == 2 ? selectedTabFillColors[1]
-					: selectedTabFillColors[0];
-		}
-
-		setBackgroundOverriddenDuringRenderering(composite, background);
-	}
-
 	private static class CTabFolderRendererWrapper extends
-	ReflectionSupport<CTabFolderRenderer> {
+			ReflectionSupport<CTabFolderRenderer> {
 		private Method drawBackgroundMethod;
 
 		public CTabFolderRendererWrapper(CTabFolderRenderer instance) {
@@ -1147,9 +1125,9 @@ ICTabRendering {
 			if (drawBackgroundMethod == null) {
 				drawBackgroundMethod = getMethod("drawBackground", //$NON-NLS-1$
 						new Class<?>[] { GC.class, int[].class, int.class,
-						int.class, int.class, int.class, Color.class,
-						Image.class, Color[].class, int[].class,
-						boolean.class });
+								int.class, int.class, int.class, Color.class,
+								Image.class, Color[].class, int[].class,
+								boolean.class });
 			}
 			executeMethod(drawBackgroundMethod, new Object[] { gc, null, x, y,
 					width, height, defaultBackground, null, colors, percents,
@@ -1158,7 +1136,7 @@ ICTabRendering {
 	}
 
 	private static class CTabFolderWrapper extends
-	ReflectionSupport<CTabFolder> {
+			ReflectionSupport<CTabFolder> {
 		private Field selectionGradientVerticalField;
 
 		private Field gradientVerticalField;
@@ -1254,7 +1232,7 @@ ICTabRendering {
 			}
 			return null;
 		}
-
+		
 		protected Object executeMethod(Method method, Object... params) {
 			Object value = null;
 			if (method != null) {
