bb2807efd6c1b3a3e55c9f2b6b666d0c964f63fc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 22:10:41 +0000

    First commited as before_60963_rev3

--- tests/org.eclipse.jface.tests.databinding/src/org/eclipse/core/tests/databinding/ListBindingTest.java
+++ tests/org.eclipse.jface.tests.databinding/src/org/eclipse/core/tests/databinding/ListBindingTest.java
@@ -224,6 +224,8 @@ public class ListBindingTest extends AbstractDefaultRealmTestCase {
 		model.add("first");
 
 		Assert.assertEquals(0, latch.getCount());
+
+		Policy.setLog(null);
 	}
 
 	/**
@@ -315,35 +317,5 @@ public class ListBindingTest extends AbstractDefaultRealmTestCase {
 		model.set(0, "second");
 
 		Assert.assertEquals(0, latch.getCount());
-	}
-
-	public void testConversion() {
-		UpdateListStrategy modelToTarget = new UpdateListStrategy();
-		modelToTarget.setConverter(new IConverter() {
-
-			@Override
-			public Object getFromType() {
-				return String.class;
-			}
-
-			@Override
-			public Object getToType() {
-				return String.class;
-			}
-
-			@Override
-			public Object convert(Object fromObject) {
-				return ((String) fromObject) + "converted";
-			}
-
-		});
-
-		dbc.bindList(target, model, new UpdateListStrategy(), modelToTarget);
-
-		model.add("1");
-		assertEquals("1converted", target.get(0));
-
-		model.set(0, "2");
-		assertEquals("2converted", target.get(0));
 	}
 }
