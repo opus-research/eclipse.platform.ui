8a500abd0060230f62b43e8bdfb3944d7405aa9c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 05:23:41 +0000

    First commited as after_30284_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/ErrorDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/ErrorDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -177,11 +177,6 @@ public class ErrorDialog extends IconAndMessageDialog {
 		shell.setText(title);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.dialogs.Dialog#createButtonsForButtonBar(org.eclipse.swt.widgets.Composite)
-	 */
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
 		// create OK and Details buttons
@@ -272,9 +267,6 @@ public class ErrorDialog extends IconAndMessageDialog {
 		return composite;
 	}
 
-	/*
-	 * @see IconAndMessageDialog#createDialogAndButtonArea(Composite)
-	 */
 	@Override
 	protected void createDialogAndButtonArea(Composite parent) {
 		super.createDialogAndButtonArea(parent);
@@ -287,11 +279,6 @@ public class ErrorDialog extends IconAndMessageDialog {
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.dialogs.IconAndMessageDialog#getImage()
-	 */
 	@Override
 	protected Image getImage() {
 		if (status != null) {
@@ -335,9 +322,6 @@ public class ErrorDialog extends IconAndMessageDialog {
 		Menu copyMenu = new Menu(list);
 		MenuItem copyItem = new MenuItem(copyMenu, SWT.NONE);
 		copyItem.addSelectionListener(new SelectionListener() {
-			/*
-			 * @see SelectionListener.widgetSelected (SelectionEvent)
-			 */
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				copyToClipboard();
@@ -715,11 +699,6 @@ public class ErrorDialog extends IconAndMessageDialog {
 				new Transfer[] { TextTransfer.getInstance() });
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.window.Window#close()
-	 */
 	@Override
 	public boolean close() {
 		if (clipboard != null) {
@@ -790,9 +769,6 @@ public class ErrorDialog extends IconAndMessageDialog {
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.dialogs.IconAndMessageDialog#getColumnCount()
-	 */
 	@Override
 	int getColumnCount() {
 		if (Policy.getErrorSupportProvider() == null)
@@ -800,10 +776,6 @@ public class ErrorDialog extends IconAndMessageDialog {
 		return 3;
 	}
 	
-    /*
-     * (non-Javadoc)
-     * @see org.eclipse.jface.dialogs.Dialog#isResizable()
-     */
     @Override
 	protected boolean isResizable() {
     	return true;
