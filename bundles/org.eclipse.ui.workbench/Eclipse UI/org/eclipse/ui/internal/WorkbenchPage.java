86984b65f7cba1f7968d18cc35fcfdf24b173fb3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 10:41:56 +0000

    First commited as before_39604_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1781,13 +1781,7 @@ public class WorkbenchPage extends CompatibleWorkbenchPage implements
 			return false;
 		}
 
-		Collection<MPart> partsToHide = partService.getParts();
-		// workaround for bug 455281
-		List<MPart> partsOutsidePersp = modelService.findElements(window, null, MPart.class, null,
-				EModelService.OUTSIDE_PERSPECTIVE);
-		partsToHide.removeAll(partsOutsidePersp);
-
-		for (MPart part : partsToHide) {
+		for (MPart part : partService.getParts()) {
 			// no save, no confirm, force
 			hidePart(part, false, true, true);
 		}
