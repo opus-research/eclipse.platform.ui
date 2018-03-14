0d9e13a8308c701d2378b121f8ff03163a971fc5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:29:09 +0000

    First commited as before_24603_rev1

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
@@ -15,10 +15,8 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
@@ -27,8 +25,6 @@ import org.eclipse.core.databinding.observable.Realm;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.ISafeRunnable;
 import org.eclipse.core.runtime.SafeRunner;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
@@ -42,7 +38,6 @@ import org.eclipse.e4.ui.bindings.keys.KeyBindingDispatcher;
 import org.eclipse.e4.ui.css.core.util.impl.resources.OSGiResourceLocator;
 import org.eclipse.e4.ui.css.swt.dom.WidgetElement;
 import org.eclipse.e4.ui.css.swt.engine.CSSSWTEngineImpl;
-import org.eclipse.e4.ui.css.swt.helpers.EclipsePreferencesHelper;
 import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
 import org.eclipse.e4.ui.css.swt.theme.IThemeManager;
 import org.eclipse.e4.ui.di.Focus;
@@ -77,16 +72,12 @@ import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.jface.bindings.keys.SWTKeySupport;
 import org.eclipse.jface.bindings.keys.formatting.KeyFormatterFactory;
 import org.eclipse.jface.databinding.swt.SWTObservables;
-import org.eclipse.osgi.service.resolver.BundleDescription;
-import org.eclipse.osgi.service.resolver.PlatformAdmin;
-import org.eclipse.osgi.service.resolver.State;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Widget;
 import org.eclipse.ui.testing.TestableObject;
@@ -349,8 +340,6 @@ public class PartRenderingEngine implements IPresentationEngine {
 		}
 	};
 
-	private StylingPreferencesHandler cssThemeChangedHandler;
-
 	private IEclipseContext appContext;
 
 	protected Shell testShell;
@@ -481,11 +470,6 @@ public class PartRenderingEngine implements IPresentationEngine {
 					windowsHandler);
 			eventBroker.subscribe(UIEvents.TrimmedWindow.TOPIC_TRIMBARS,
 					trimHandler);
-
-			cssThemeChangedHandler = new StylingPreferencesHandler(
-					context.get(Display.class));
-			eventBroker.subscribe(IThemeEngine.Events.THEME_CHANGED,
-					cssThemeChangedHandler);
 		}
 	}
 
@@ -497,7 +481,6 @@ public class PartRenderingEngine implements IPresentationEngine {
 		eventBroker.unsubscribe(visibilityHandler);
 		eventBroker.unsubscribe(childrenHandler);
 		eventBroker.unsubscribe(trimHandler);
-		eventBroker.unsubscribe(cssThemeChangedHandler);
 	}
 
 	private static void populateModelInterfaces(MContext contextModel,
@@ -1250,39 +1233,9 @@ public class PartRenderingEngine implements IPresentationEngine {
 			IEclipseContext appContext) {
 		String cssTheme = (String) appContext.get(E4Application.THEME_ID);
 		String cssURI = (String) appContext.get(IWorkbench.CSS_URI_ARG);
-		IThemeEngine themeEngineForEvent = null;
-		if ("none".equals(cssTheme)) {
-			appContext.set(IStylingEngine.SERVICE_NAME, new IStylingEngine() {
-				@Override
-				public void setClassname(Object widget, String classname) {
-					WidgetElement.setCSSClass((Widget) widget, classname);
-				}
+		final IThemeEngine themeEngine = createThemeEngine(display, appContext);
 
-				@Override
-				public void setId(Object widget, String id) {
-					WidgetElement.setID((Widget) widget, id);
-				}
-
-				@Override
-				public void style(Object widget) {
-				}
-
-				@Override
-				public CSSStyleDeclaration getStyle(Object widget) {
-					return null;
-				}
-
-				@Override
-				public void setClassnameAndId(Object widget, String classname,
-						String id) {
-					WidgetElement.setCSSClass((Widget) widget, classname);
-					WidgetElement.setID((Widget) widget, id);
-				}
-			});
-		} else if (cssTheme != null) {
-			final IThemeEngine themeEngine = createThemeEngine(display,
-					appContext);
-			themeEngineForEvent = themeEngine;
+		if (cssTheme != null) {
 			String cssResourcesURI = (String) appContext
 					.get(IWorkbench.CSS_RESOURCE_URI_ARG);
 
@@ -1373,6 +1326,7 @@ public class PartRenderingEngine implements IPresentationEngine {
 					WidgetElement.setID((Widget) widget, id);
 					cssEngine.applyStyles(widget, true);
 				}
+
 			});
 
 			URL url;
@@ -1419,16 +1373,6 @@ public class PartRenderingEngine implements IPresentationEngine {
 
 		IEventBroker broker = appContext.get(IEventBroker.class);
 		if (broker != null) {
-			Map<String, Object> data = null;
-			if (themeEngineForEvent != null) {
-				data = new HashMap<String, Object>();
-				data.put(IThemeEngine.Events.THEME_ENGINE, themeEngineForEvent);
-				data.put(IThemeEngine.Events.THEME,
-						themeEngineForEvent.getActiveTheme());
-				data.put(IThemeEngine.Events.DEVICE, display);
-				data.put(IThemeEngine.Events.RESTORE, false);
-			}
-			broker.send(IThemeEngine.Events.THEME_CHANGED, data);
 			broker.send(UIEvents.UILifeCycle.THEME_CHANGED, null);
 		}
 	}
@@ -1446,82 +1390,5 @@ public class PartRenderingEngine implements IPresentationEngine {
 
 		appContext.set(IThemeEngine.class.getName(), themeEngine);
 		return themeEngine;
-	}
-
-	public static class StylingPreferencesHandler implements EventHandler {
-		private HashSet<IEclipsePreferences> prefs = null;
-
-		public StylingPreferencesHandler(Display display) {
-			if (display != null) {
-				display.addListener(SWT.Dispose, new Listener() {
-					@Override
-					public void handleEvent(org.eclipse.swt.widgets.Event event) {
-						resetOverriddenPreferences();
-					}
-				});
-			}
-		}
-
-		@Override
-		public void handleEvent(Event event) {
-			resetOverriddenPreferences();
-			overridePreferences(getThemeEngine(event));
-		}
-
-		protected void resetOverriddenPreferences() {
-			for (IEclipsePreferences preferences : getPreferences()) {
-				resetOverriddenPreferences(preferences);
-			}
-		}
-
-		protected void resetOverriddenPreferences(
-				IEclipsePreferences preferences) {
-			for (String name : getOverriddenPropertyNames(preferences)) {
-				preferences.remove(name);
-			}
-			removeOverriddenPropertyNames(preferences);
-		}
-
-		protected void removeOverriddenPropertyNames(
-				IEclipsePreferences preferences) {
-			EclipsePreferencesHelper.removeOverriddenPropertyNames(preferences);
-		}
-
-		protected List<String> getOverriddenPropertyNames(
-				IEclipsePreferences preferences) {
-			return EclipsePreferencesHelper
-					.getOverriddenPropertyNames(preferences);
-		}
-
-		protected Set<IEclipsePreferences> getPreferences() {
-			if (prefs == null) {
-				prefs = new HashSet<IEclipsePreferences>();
-				PlatformAdmin admin = WorkbenchSWTActivator.getDefault()
-						.getPlatformAdmin();
-
-				State state = admin.getState(false);
-				BundleDescription[] bundles = state.getBundles();
-
-				for (BundleDescription desc : bundles) {
-					if (desc.getName() != null) {
-						prefs.add(InstanceScope.INSTANCE.getNode(desc.getName()));
-					}
-				}
-			}
-			return prefs;
-		}
-
-		private void overridePreferences(IThemeEngine themeEngine) {
-			if (themeEngine != null) {
-				for (IEclipsePreferences preferences : getPreferences()) {
-					themeEngine.applyStyles(preferences, false);
-				}
-			}
-		}
-
-		private IThemeEngine getThemeEngine(Event event) {
-			return (IThemeEngine) event
-					.getProperty(IThemeEngine.Events.THEME_ENGINE);
-		}
 	}
 }
