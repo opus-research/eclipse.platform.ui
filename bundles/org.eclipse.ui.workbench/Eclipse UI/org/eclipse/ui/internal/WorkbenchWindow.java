df12b0bac83ca46891666fa8dfcf8a6d47988027
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:37:22 +0000

    First commited as before_38259_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
@@ -17,6 +17,8 @@
 
 package org.eclipse.ui.internal;
 
+import org.eclipse.ui.internal.dialogs.cpd.CustomizePerspectiveDialog;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -160,7 +162,6 @@ import org.eclipse.ui.internal.StartupThreading.StartupRunnable;
 import org.eclipse.ui.internal.actions.CommandAction;
 import org.eclipse.ui.internal.commands.SlaveCommandService;
 import org.eclipse.ui.internal.contexts.ContextService;
-import org.eclipse.ui.internal.dialogs.CustomizePerspectiveDialog;
 import org.eclipse.ui.internal.e4.compatibility.CompatibilityPart;
 import org.eclipse.ui.internal.e4.compatibility.ModeledPageLayout;
 import org.eclipse.ui.internal.e4.compatibility.SelectionService;
@@ -169,6 +170,7 @@ import org.eclipse.ui.internal.handlers.IActionCommandMappingService;
 import org.eclipse.ui.internal.handlers.LegacyHandlerService;
 import org.eclipse.ui.internal.layout.ITrimManager;
 import org.eclipse.ui.internal.layout.IWindowTrim;
+import org.eclipse.ui.internal.menus.ActionSet;
 import org.eclipse.ui.internal.menus.IActionSetsListener;
 import org.eclipse.ui.internal.menus.LegacyActionPersistence;
 import org.eclipse.ui.internal.menus.MenuHelper;
@@ -205,7 +207,7 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 	 */
 	public static final String PERSPECTIVE_SPACER_ID = "PerspectiveSpacer"; //$NON-NLS-1$
 
-	private static final String MAIN_TOOLBAR_ID = "org.eclipse.ui.main.toolbar"; //$NON-NLS-1$
+	private static final String MAIN_TOOLBAR_ID = ActionSet.MAIN_TOOLBAR;
 	private static final String COMMAND_ID_TOGGLE_COOLBAR = "org.eclipse.ui.ToggleCoolbarAction"; //$NON-NLS-1$
 
 	public static final String ACTION_SET_CMD_PREFIX = "AS::"; //$NON-NLS-1$
@@ -246,7 +248,7 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 
 	private PerspectiveListenerList perspectiveListeners = new PerspectiveListenerList();
 
-	private PartService partService = new WWinPartService();
+	private PartService partService = new PartService();
 
 	private WWinActionBars actionBars;
 
@@ -673,7 +675,7 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 			Shell shell = (Shell) model.getWidget();
 			if (model.getMainMenu() == null) {
 				final MMenu mainMenu = modelService.createModelElement(MMenu.class);
-				mainMenu.setElementId("org.eclipse.ui.main.menu"); //$NON-NLS-1$
+				mainMenu.setElementId(ActionSet.MAIN_MENU);
 
 				final MenuManagerRenderer renderer = (MenuManagerRenderer) rendererFactory
 						.getRenderer(mainMenu, null);
@@ -2389,7 +2391,7 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 
 	private ListenerList backgroundSaveListeners = new ListenerList(ListenerList.IDENTITY);
 
-	private SelectionService selectionService;
+	private ISelectionService selectionService;
 
 	private ITrimManager trimManager;
 
@@ -2970,7 +2972,7 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 		}
 	};
 
-	MenuManager menuManager = new MenuManager("MenuBar", "org.eclipse.ui.main.menu"); //$NON-NLS-1$//$NON-NLS-2$
+	MenuManager menuManager = new MenuManager("MenuBar", ActionSet.MAIN_MENU); //$NON-NLS-1$
 
 	public MenuManager getMenuManager() {
 		return menuManager;
@@ -3043,15 +3045,5 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 	public CustomizePerspectiveDialog createCustomizePerspectiveDialog(Perspective persp,
 			IEclipseContext context) {
 		return new CustomizePerspectiveDialog(getWindowConfigurer(), persp, context);
-	}
-
-	private class WWinPartService extends PartService {
-
-		@Override
-		public void partActivated(IWorkbenchPart part) {
-			super.partActivated(part);
-			selectionService.notifyListeners(part);
-		}
-
 	}
 }
