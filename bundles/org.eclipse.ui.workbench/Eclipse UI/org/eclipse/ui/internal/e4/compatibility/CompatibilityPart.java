e90f46d32103b95b278b75104aa4bb4eef0f71b2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 16:46:17 +0000

    First commited as before_47396_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityPart.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityPart.java
@@ -240,7 +240,6 @@ public abstract class CompatibilityPart implements ISelectionChangedListener {
 				// client code may have errors so we need to catch it
 				logger.error(e);
 			}
-			wrapped = null;
 		}
 
 		internalDisposeSite(site);
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/services/ServiceLocator.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/services/ServiceLocator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 IBM Corporation and others.
+ * Copyright (c) 2006, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Andrey Loskutov <loskutov@gmx.de> - Bug 436225
  *******************************************************************************/
 
 package org.eclipse.ui.internal.services;
@@ -77,7 +76,7 @@ public final class ServiceLocator implements IDisposable, INestable,
 
 	private boolean disposed;
 
-	private IDisposable owner;
+	private final IDisposable owner;
 
 	private IEclipseContext e4Context;
 
@@ -141,7 +140,6 @@ public final class ServiceLocator implements IDisposable, INestable,
 		servicesToDispose.clear();
 		e4Context = null;
 		disposed = true;
-		owner = null;
 	}
 
 	@Override
