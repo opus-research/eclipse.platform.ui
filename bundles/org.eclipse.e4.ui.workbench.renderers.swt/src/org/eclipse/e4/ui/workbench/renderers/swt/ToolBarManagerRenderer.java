e999af0f8f26c7112dbf2a3868faa45e70a048c6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:54:38 +0000

    First commited as after_25265_rev4

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
@@ -33,7 +33,6 @@ import org.eclipse.e4.ui.internal.workbench.ContributionsAnalyzer;
 import org.eclipse.e4.ui.internal.workbench.OpaqueElementUtil;
 import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
 import org.eclipse.e4.ui.internal.workbench.swt.CSSRenderingUtils;
-import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.MApplicationElement;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
@@ -54,7 +53,6 @@ import org.eclipse.e4.ui.workbench.Selector;
 import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.UIEvents.ElementContainer;
 import org.eclipse.e4.ui.workbench.UIEvents.EventTags;
-import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.action.AbstractGroupMarker;
 import org.eclipse.jface.action.ContributionItem;
@@ -103,7 +101,7 @@ public class ToolBarManagerRenderer
 	 * This is a persistedState 'key' which can be used by the renderer
 	 * implementation to decide that a user interface element has been hidden by
 	 * the user
-	 * 
+	 *
 	 */
 	// TODO migrate to IPresentationEngine after the Luna release
 	public static final String HIDDEN_BY_USER = "HIDDEN_BY_USER"; //$NON-NLS-1$
@@ -114,12 +112,6 @@ public class ToolBarManagerRenderer
 	 * The context menu for this trim stack's items.
 	 */
 	private Menu toolbarMenu;
-
-	@Inject
-	private MApplication application;
-
-	@Inject
-	EModelService modelService;
 
 	private EventHandler itemUpdater = new EventHandler() {
 		@Override
@@ -181,7 +173,6 @@ public class ToolBarManagerRenderer
 				} else {
 					IContributionItem ici = getContribution(itemModel);
 					clearModelToContribution(itemModel, ici);
-
 					if (ici != null && parent != null) {
 						parent.remove(ici);
 					}
@@ -596,7 +587,8 @@ public class ToolBarManagerRenderer
 	 * @param element
 	 */
 	protected void cleanUp(MToolBar toolbarModel) {
-		Collection<ToolBarContributionRecord> vals = getList(toolbarModel);
+		Collection<ToolBarContributionRecord> vals = getModelContributionToRecord()
+				.values();
 		for (ToolBarContributionRecord record : vals
 				.toArray(new ToolBarContributionRecord[vals.size()])) {
 			if (record.toolbarModel == toolbarModel) {
@@ -615,7 +607,7 @@ public class ToolBarManagerRenderer
 
 	public void cleanUpCopy(ToolBarContributionRecord record,
 			MToolBarElement copy) {
-		removeContributionRecord(copy);
+		getModelContributionToRecord().remove(copy);
 		IContributionItem ici = getContribution(copy);
 		clearModelToContribution(copy, ici);
 		if (ici != null) {
@@ -913,11 +905,11 @@ public class ToolBarManagerRenderer
 	}
 
 	public MToolBar getToolBarModel(ToolBarManager manager) {
-		return getModel(manager);
+		return super.getModel(manager);
 	}
 
 	public MToolBarElement getToolElement(IContributionItem item) {
-		return getModelElement(item);
+		return super.getModelElement(item);
 	}
 
 	public void reconcileManagerToModel(IToolBarManager menuManager,
