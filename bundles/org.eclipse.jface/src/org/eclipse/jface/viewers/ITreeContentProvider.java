5ff4738c513f7dcd3b42d6c0ec63f02cdc968eaf
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 20:06:07 +0000

    First commited as before_56215_rev2

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ITreeContentProvider.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ITreeContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,12 +7,12 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Simon Scholz <simon.scholz@vogella.com> - Bug 477774
  *******************************************************************************/
 package org.eclipse.jface.viewers;
 
 /**
- * An interface to content providers for tree-structure-oriented viewers.
+ * An interface to content providers for tree-structure-oriented
+ * viewers.
  *
  * @see AbstractTreeViewer
  */
@@ -30,56 +30,44 @@ public interface ITreeContentProvider extends IStructuredContentProvider {
 	@Override
 	public Object[] getElements(Object inputElement);
 
-	/**
-	 * Returns the child elements of the given parent element.
-	 * <p>
-	 * The difference between this method and
-	 * <code>IStructuredContentProvider.getElements</code> is that
-	 * <code>getElements</code> is called to obtain the tree viewer's root
-	 * elements, whereas <code>getChildren</code> is used to obtain the children
-	 * of a given parent element in the tree (including a root).
-	 * </p>
-	 * The result is not modified by the viewer.
-	 *
-	 * @param parentElement
-	 *            the parent element
-	 * @return an array of child elements. The default defined by this interface
-	 *         is an empty object array.
-	 */
-	default public Object[] getChildren(Object parentElement) {
-		return new Object[0];
-	}
+    /**
+     * Returns the child elements of the given parent element.
+     * <p>
+     * The difference between this method and <code>IStructuredContentProvider.getElements</code>
+     * is that <code>getElements</code> is called to obtain the
+     * tree viewer's root elements, whereas <code>getChildren</code> is used
+     * to obtain the children of a given parent element in the tree (including a root).
+     * </p>
+     * The result is not modified by the viewer.
+     *
+     * @param parentElement the parent element
+     * @return an array of child elements
+     */
+    public Object[] getChildren(Object parentElement);
 
-	/**
-	 * Returns the parent for the given element, or <code>null</code> indicating
-	 * that the parent can't be computed. In this case the tree-structured
-	 * viewer can't expand a given node correctly if requested.
-	 *
-	 * @param element
-	 *            the element
-	 * @return the parent element, or <code>null</code> if it has none or if the
-	 *         parent cannot be computed. The default defined by this interface
-	 *         is null.
-	 */
-	default public Object getParent(Object element) {
-		return null;
-	}
+    /**
+     * Returns the parent for the given element, or <code>null</code>
+     * indicating that the parent can't be computed.
+     * In this case the tree-structured viewer can't expand
+     * a given node correctly if requested.
+     *
+     * @param element the element
+     * @return the parent element, or <code>null</code> if it
+     *   has none or if the parent cannot be computed
+     */
+    public Object getParent(Object element);
 
-	/**
-	 * Returns whether the given element has children.
-	 * <p>
-	 * Intended as an optimization for when the viewer does not need the actual
-	 * children. Clients may be able to implement this more efficiently than
-	 * <code>getChildren</code>.
-	 * </p>
-	 *
-	 * @param element
-	 *            the element
-	 * @return <code>true</code> if the given element has children, and
-	 *         <code>false</code> if it has no children. The default defined by
-	 *         this interface is <code>false</code>.
-	 */
-	default public boolean hasChildren(Object element) {
-		return false;
-	}
+    /**
+     * Returns whether the given element has children.
+     * <p>
+     * Intended as an optimization for when the viewer does not
+     * need the actual children.  Clients may be able to implement
+     * this more efficiently than <code>getChildren</code>.
+     * </p>
+     *
+     * @param element the element
+     * @return <code>true</code> if the given element has children,
+     *  and <code>false</code> if it has no children
+     */
+    public boolean hasChildren(Object element);
 }
