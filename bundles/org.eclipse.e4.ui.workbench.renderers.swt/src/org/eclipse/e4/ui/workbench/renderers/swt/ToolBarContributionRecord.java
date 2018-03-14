aba79cbeed70fc9ae4f4b481420a94ff38fbe45f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 01:19:17 +0000

    First commited as before_21602_rev2

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarContributionRecord.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarContributionRecord.java
@@ -144,16 +144,7 @@ public class ToolBarContributionRecord {
 		if (toolbarContribution.getVisibleWhen() != null) {
 			return true;
 		}
-
-		List<MToolBarElement> childrenToInspect;
-		if (toolbarContribution.getTransientData().get(FACTORY) != null) {
-			// See mergeIntoModel
-			childrenToInspect = toolbarModel.getChildren();
-		} else {
-			childrenToInspect = toolbarContribution.getChildren();
-		}
-
-		for (MToolBarElement child : childrenToInspect) {
+		for (MToolBarElement child : toolbarContribution.getChildren()) {
 			if (child.getVisibleWhen() != null
 					|| child.getPersistedState().get(
 							MenuManagerRenderer.VISIBILITY_IDENTIFIER) != null) {
