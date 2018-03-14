641a1236791985ae3db804cccea47da354b2fa9b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 08:09:55 +0000

    First commited as after_35109_rev2

--- tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/FilterTest.java
+++ tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/FilterTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Oakland Software Incorporated and others.
+ * Copyright (c) 2010, 2014 Oakland Software Incorporated and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,13 +7,17 @@
  *
  * Contributors:
  *     Oakland Software Incorporated - initial API and implementation
+ *     Bachmann electronic GmbH - Bug 447530 - adding a test for active non visible filters
  *******************************************************************************/
 package org.eclipse.ui.tests.navigator;
 
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.internal.navigator.NavigatorContentService;
+import org.eclipse.ui.internal.navigator.NavigatorFilterService;
 
 public class FilterTest extends NavigatorTestBase {
+
+	private static final String TEST_FILTER_ACTIVE_NOT_VISIBLE = "org.eclipse.ui.tests.navigator.filters.nonvisibleactive";
 
 	public FilterTest() {
 		_navigatorInstanceId = TEST_VIEWER_FILTER;
@@ -61,6 +65,16 @@ public class FilterTest extends NavigatorTestBase {
 		assertEquals("p2", items[1].getText());
 		assertEquals(0, ncs.getContributionMemorySize());
 
+	}
+
+	// bug 447530, when a filter is active by default but not visible in the ui,
+	// it must still be active after
+	// restoring the active filters from the preferences.
+	public void testNonVisibleFilters() {
+		_contentService.getFilterService().persistFilterActivationState();
+		// "restore" by creating a new instance of the serivce
+		NavigatorFilterService filterService = new NavigatorFilterService((NavigatorContentService) _contentService);
+		assertTrue(filterService.isActive(TEST_FILTER_ACTIVE_NOT_VISIBLE));
 	}
 
 }
