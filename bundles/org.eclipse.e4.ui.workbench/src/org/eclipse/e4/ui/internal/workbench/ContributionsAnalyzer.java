1f90c8721fedf976dae3ffef405c77f5d1087476
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 02:30:30 +0000

    First commited as before_23612_rev3

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ContributionsAnalyzer.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ContributionsAnalyzer.java
@@ -393,18 +393,7 @@ public final class ContributionsAnalyzer {
 			}
 			idx++;
 		}
-
-		if (id.equals("additions")) { //$NON-NLS-1$
-			idx = menuModel.getChildren().size() - 1;
-			while (idx > -1) {
-				if ("PerspectiveSpacer".equals(menuModel.getChildren().get(idx).getElementId())) { //$NON-NLS-1$
-					break;
-				}
-				idx--;
-			}
-		}
-
-		return idx;
+		return id.equals("additions") ? menuModel.getChildren().size() : -1; //$NON-NLS-1$
 	}
 
 	public static MCommand getCommandById(MApplication app, String cmdId) {
