96cc2d076f51f6000c7ffd2c5a0a8c9d7bfba89b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:16:55 +0000

    First commited as before_37302_rev3

--- bundles/org.eclipse.e4.emf.xpath/src/org/eclipse/e4/emf/internal/xpath/EStructuralFeatureOwnerPointer.java
+++ bundles/org.eclipse.e4.emf.xpath/src/org/eclipse/e4/emf/internal/xpath/EStructuralFeatureOwnerPointer.java
@@ -53,8 +53,7 @@ public abstract class EStructuralFeatureOwnerPointer extends NodePointer {
 
     private Object value = UNINITIALIZED;
 
-    @Override
-	public NodeIterator childIterator(NodeTest test, boolean reverse,
+    public NodeIterator childIterator(NodeTest test, boolean reverse,
             NodePointer startWith) {
         if (test == null) {
             return createNodeIterator(null, reverse, startWith);
@@ -84,8 +83,7 @@ public abstract class EStructuralFeatureOwnerPointer extends NodePointer {
         return new EStructuralFeatureIterator(this, property, reverse, startWith);
     }
 
-    @Override
-	public NodeIterator attributeIterator(QName name) {
+    public NodeIterator attributeIterator(QName name) {
         return new EObjectAttributeIterator(this, name);
     }
 
@@ -106,16 +104,14 @@ public abstract class EStructuralFeatureOwnerPointer extends NodePointer {
         super(parent);
     }
 
-    @Override
-	public void setIndex(int index) {
+    public void setIndex(int index) {
         if (this.index != index) {
             super.setIndex(index);
             value = UNINITIALIZED;
         }
     }
 
-    @Override
-	public Object getImmediateNode() {
+    public Object getImmediateNode() {
         if (value == UNINITIALIZED) {
             value = index == WHOLE_COLLECTION ? ValueUtils.getValue(getBaseValue())
                     : ValueUtils.getValue(getBaseValue(), index);
@@ -123,8 +119,7 @@ public abstract class EStructuralFeatureOwnerPointer extends NodePointer {
         return value;
     }
 
-    @Override
-	public abstract QName getName();
+    public abstract QName getName();
 
     /**
      * Learn whether <code>name</code> is a valid child name for this PropertyOwnerPointer.
@@ -141,8 +136,7 @@ public abstract class EStructuralFeatureOwnerPointer extends NodePointer {
      * forwards the call to the parent pointer.
      * @param value to set
      */
-    @Override
-	public void setValue(Object value) {
+    public void setValue(Object value) {
         this.value = value;
         if (parent != null) {
             if (parent.isContainer()) {
@@ -168,8 +162,7 @@ public abstract class EStructuralFeatureOwnerPointer extends NodePointer {
      * If this is a root node pointer, throws an exception; otherwise
      * forwards the call to the parent node.
      */
-    @Override
-	public void remove() {
+    public void remove() {
         this.value = null;
         if (parent != null) {
             parent.remove();
@@ -197,8 +190,7 @@ public abstract class EStructuralFeatureOwnerPointer extends NodePointer {
         return false;
     }
 
-    @Override
-	public int compareChildNodePointers(NodePointer pointer1,
+    public int compareChildNodePointers(NodePointer pointer1,
             NodePointer pointer2) {
         int r = pointer1.getName().toString().compareTo(pointer2.getName().toString());
         return r == 0 ? pointer1.getIndex() - pointer2.getIndex() : r;
