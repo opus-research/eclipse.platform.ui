dc5a62439c10698e4aa740dbd297c68a49b8b7da
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 06:42:09 +0000

    First commited as before_33116_rev1

--- bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/dialogs/RecentWorkspacesPreferencePage.java
+++ bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/dialogs/RecentWorkspacesPreferencePage.java
@@ -58,12 +58,10 @@ public class RecentWorkspacesPreferencePage extends PreferencePage
 	private Button removeButton;
 
 
-	@Override
 	public void init(IWorkbench workbench) {
 		workspacesData = new ChooseWorkspaceData(Platform.getInstanceLocation().getURL());
 	}
 
-	@Override
 	public Control createContents(Composite parent) {
     	PlatformUI.getWorkbench().getHelpSystem().setHelp(parent,
 				IWorkbenchHelpContextIds.WORKSPACES_PREFERENCE_PAGE);
@@ -92,7 +90,6 @@ public class RecentWorkspacesPreferencePage extends PreferencePage
 
 		promptOption.setSelection(workspacesData.getShowDialog());
 		promptOption.addSelectionListener(new SelectionAdapter(){
-				@Override
 				public void widgetSelected(SelectionEvent event) {
 					workspacesData.toggleShowDialog();
 				}
@@ -131,7 +128,6 @@ public class RecentWorkspacesPreferencePage extends PreferencePage
 		removeButton.setEnabled(false);
 
 		removeButton.addSelectionListener(new SelectionAdapter(){
-				@Override
 				public void widgetSelected(SelectionEvent event) {
 					removeSelectedWorkspaces();
 					updateRemoveButton();
@@ -139,7 +135,6 @@ public class RecentWorkspacesPreferencePage extends PreferencePage
 			});
 
 		workspacesList.addSelectionListener(new SelectionAdapter() {
-				@Override
 				public void widgetSelected(SelectionEvent event) {
 					updateRemoveButton();
 				}
@@ -172,14 +167,12 @@ public class RecentWorkspacesPreferencePage extends PreferencePage
 	}
 
 
-	@Override
 	protected void performDefaults() {
 		promptOption.setSelection(true);
 		super.performDefaults();
 	}
 
 
-	@Override
 	public boolean performOk() {
 		int maxWorkspaces = maxWorkspacesField.getSelection();
 		String[] workspaces = new String[maxWorkspaces];
