9c329126616924901536a64e848a1b3f540a5abc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 02:59:40 +0000

    First commited as before_24006_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/TreeSelection.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/TreeSelection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2014 IBM Corporation and others.
+ * Copyright (c) 2005, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 430873
  *******************************************************************************/
 package org.eclipse.jface.viewers;
 
@@ -185,8 +184,8 @@ public class TreeSelection extends StructuredSelection implements ITreeSelection
 	public int hashCode() {
 		int code = getClass().hashCode();
 		if (paths != null) {
-			for (TreePath path : paths) {
-				code = code * 17 + path.hashCode(getElementComparer());
+			for (int i = 0; i < paths.length; i++) {
+				code = code * 17 + paths[i].hashCode(getElementComparer());
 			}
 		}
 		return code;
