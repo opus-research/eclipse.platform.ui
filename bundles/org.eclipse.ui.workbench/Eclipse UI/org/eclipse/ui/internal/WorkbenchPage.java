2dc22fda6402a1f84bf1c376a3323fd4c01b0ad8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 10:39:37 +0000

    First commited as after_39564_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
@@ -147,7 +147,6 @@ import org.eclipse.ui.WorkbenchException;
 import org.eclipse.ui.XMLMemento;
 import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.dialogs.EditorSelectionDialog;
-import org.eclipse.ui.internal.dialogs.cpd.CustomizePerspectiveDialog;
 import org.eclipse.ui.internal.e4.compatibility.CompatibilityEditor;
 import org.eclipse.ui.internal.e4.compatibility.CompatibilityPart;
 import org.eclipse.ui.internal.e4.compatibility.CompatibilityView;
@@ -1965,26 +1964,6 @@ public class WorkbenchPage implements IWorkbenchPage {
 	public INavigationHistory getNavigationHistory() {
         return navigationHistory;
     }
-
-    public boolean editActionSets() {
-		Perspective persp = getActivePerspective();
-		if (persp == null) {
-			return false;
-		}
-
-		// Create list dialog.
-		CustomizePerspectiveDialog dlg = legacyWindow.createCustomizePerspectiveDialog(persp,
-				window.getContext());
-		// Open.
-		boolean ret = (dlg.open() == Window.OK);
-		if (ret) {
-			legacyWindow.updateActionSets();
-			legacyWindow.firePerspectiveChanged(this, getPerspective(), CHANGE_RESET);
-			legacyWindow.firePerspectiveChanged(this, getPerspective(), CHANGE_RESET_COMPLETE);
-		}
-		return ret;
-    }
-
 
     /**
      * See IWorkbenchPage@findView.
@@ -4318,7 +4297,7 @@ public class WorkbenchPage implements IWorkbenchPage {
 		return stack == null ? null : stack.getSelectedElement();
 	}
 
-	Perspective getActivePerspective() {
+	public Perspective getActivePerspective() {
 		return getPerspective(getCurrentPerspective());
 	}
 
