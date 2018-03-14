d212bf8cc3bb6927937ef53277286ed47866a282
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 21:11:45 +0000

    First commited as before_58555_rev2

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/NestedProjectManager.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/NestedProjectManager.java
@@ -11,8 +11,7 @@
  ******************************************************************************/
 package org.eclipse.ui.internal.navigator.resources.nested;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 import org.eclipse.core.resources.IContainer;
@@ -32,10 +31,7 @@ public class NestedProjectManager {
 
 	private static NestedProjectManager INSTANCE = new NestedProjectManager();
 
-	private Map<IPath, IProject> locationsToProjects = Collections
-			.synchronizedMap(new TreeMap<IPath, IProject>(new PathComparator()));
-
-	private int knownProjectsCount;
+	private SortedMap<IPath, IProject> locationsToProjects = new TreeMap<IPath, IProject>(new PathComparator());
 
 	private NestedProjectManager() {
 		refreshProjectsList();
@@ -51,7 +47,7 @@ public class NestedProjectManager {
 	}
 
 	private void refreshProjectsListIfNeeded() {
-		if (knownProjectsCount != ResourcesPlugin.getWorkspace().getRoot().getProjects().length) {
+		if (this.locationsToProjects.size() != ResourcesPlugin.getWorkspace().getRoot().getProjects().length) {
 			// TODO: find other cheap checks to try in condition
 			// Need to find a cheap way to react to project refactoring (moved
 			// or renamed...)
@@ -60,18 +56,18 @@ public class NestedProjectManager {
 	}
 
 	private void refreshProjectsList() {
-		locationsToProjects.clear();
-		IProject[] knownProjects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-		knownProjectsCount = knownProjects.length;
-		for (IProject project : knownProjects) {
-			IPath location = project.getLocation();
-			if (location != null) {
-				locationsToProjects.put(location, project);
-			}
+		this.locationsToProjects.clear();
+		for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+			this.locationsToProjects.put(project.getLocation(), project);
 		}
 	}
 
 	public static NestedProjectManager getInstance() {
+		synchronized (INSTANCE) {
+			if (INSTANCE == null) {
+				INSTANCE = new NestedProjectManager();
+			}
+		}
 		return INSTANCE;
 	}
 
@@ -84,12 +80,8 @@ public class NestedProjectManager {
 			return null;
 		}
 		refreshProjectsListIfNeeded();
-		IPath location = folder.getLocation();
-		if (location == null) {
-			return null;
-		}
-		IProject res = locationsToProjects.get(location);
-		if (res != null && (!res.exists() || !location.equals(res.getLocation()))) {
+		IProject res = this.locationsToProjects.get(folder.getLocation());
+		if (res != null && (!res.exists() || !res.getLocation().equals(folder.getLocation()))) {
 			// project was deleted and state not refreshed
 			refreshProjectsList();
 			return getProject(folder);
@@ -111,13 +103,9 @@ public class NestedProjectManager {
 		if (!project.exists()) {
 			return false;
 		}
-		IPath location = project.getLocation();
-		if (location == null) {
-			return false;
-		}
-		IPath queriedLocation = location.removeLastSegments(1);
+		IPath queriedLocation = project.getLocation().removeLastSegments(1);
 		while (queriedLocation.segmentCount() > 0) {
-			if (locationsToProjects.containsKey(queriedLocation)) {
+			if (this.locationsToProjects.containsKey(queriedLocation)) {
 				return true;
 			}
 			queriedLocation = queriedLocation.removeLastSegments(1);
@@ -126,31 +114,22 @@ public class NestedProjectManager {
 	}
 
 	public IContainer getMostDirectOpenContainer(IProject project) {
-		IPath location = project.getLocation();
-		if (location == null) {
-			return null;
-		}
 		IProject mostDirectParentProject = null;
-		IPath queriedLocation = location.removeLastSegments(1);
+		IPath queriedLocation = project.getLocation().removeLastSegments(1);
 		while (mostDirectParentProject == null && queriedLocation.segmentCount() > 0) {
-			mostDirectParentProject = locationsToProjects.get(queriedLocation);
-			if (mostDirectParentProject != null && mostDirectParentProject.getLocation() == null) {
-				mostDirectParentProject = null;
+			if (this.locationsToProjects.containsKey(queriedLocation)) {
+				mostDirectParentProject = this.locationsToProjects.get(queriedLocation);
 			}
 			queriedLocation = queriedLocation.removeLastSegments(1);
 		}
 		if (mostDirectParentProject != null) {
-			IPath parentContainerAbsolutePath = location.removeLastSegments(1);
-			IPath location2 = mostDirectParentProject.getLocation();
-			if (location2 == null) {
-				return null;
-			}
-			if (parentContainerAbsolutePath.equals(location2)) {
+			IPath parentContainerAbsolutePath = project.getLocation().removeLastSegments(1);
+			if (parentContainerAbsolutePath.equals(mostDirectParentProject.getLocation())) {
 				return mostDirectParentProject;
+			} else {
+				IPath parentFolderPathRelativeToProject = parentContainerAbsolutePath.removeFirstSegments(mostDirectParentProject.getLocation().segmentCount());
+				return mostDirectParentProject.getFolder(parentFolderPathRelativeToProject);
 			}
-			IPath parentFolderPathRelativeToProject = parentContainerAbsolutePath
-					.removeFirstSegments(location2.segmentCount());
-			return mostDirectParentProject.getFolder(parentFolderPathRelativeToProject);
 		}
 		return null;
 	}
