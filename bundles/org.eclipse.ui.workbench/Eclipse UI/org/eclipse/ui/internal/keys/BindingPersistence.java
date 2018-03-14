19f2cf6471dc83d3902e16247ba8c582e40796ab
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 07:30:42 +0000

    First commited as before_34454_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/BindingPersistence.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/BindingPersistence.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.StringTokenizer;
 import org.eclipse.core.commands.Command;
@@ -282,7 +281,7 @@ public class BindingPersistence extends PreferencePersistence {
 		if (size % 2 == 1) {
 			String token = tokens[size - 1];
 			final Integer integer = (Integer) r2_1KeysByName.get(token
-					.toUpperCase(Locale.ENGLISH));
+					.toUpperCase());
 
 			if (integer != null) {
 				value = integer.intValue();
