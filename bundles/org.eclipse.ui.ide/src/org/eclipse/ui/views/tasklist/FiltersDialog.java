af7d2e756e6c0ba8eed0b7d95751d67b6ea7d187
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 15:47:35 +0000

    First commited as before_46544_rev5

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/tasklist/FiltersDialog.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/tasklist/FiltersDialog.java
@@ -346,12 +346,7 @@ class FiltersDialog extends TrayDialog {
         }
     };
 
-    private ICheckStateListener checkStateListener = new ICheckStateListener() {
-        @Override
-		public void checkStateChanged(CheckStateChangedEvent event) {
-            FiltersDialog.this.checkStateChanged(event);
-        }
-    };
+    private ICheckStateListener checkStateListener = event -> FiltersDialog.this.checkStateChanged(event);
 
     /**
      * Creates a new filters dialog.
@@ -363,9 +358,6 @@ class FiltersDialog extends TrayDialog {
         initTypes();
     }
 
-    /* (non-Javadoc)
-     * Method declared on Dialog.
-     */
     @Override
 	protected void buttonPressed(int buttonId) {
         if (RESET_ID == buttonId) {
@@ -392,9 +384,6 @@ class FiltersDialog extends TrayDialog {
         updateEnabledState();
     }
 
-    /* (non-Javadoc)
-     * Method declared on Window.
-     */
     @Override
 	protected void configureShell(Shell newShell) {
         super.configureShell(newShell);
@@ -482,9 +471,6 @@ class FiltersDialog extends TrayDialog {
         return combo;
     }
 
-    /* (non-Javadoc)
-     * Method declared on Dialog.
-     */
     @Override
 	protected Control createDialogArea(Composite parent) {
         Composite composite = (Composite) super.createDialogArea(parent);
