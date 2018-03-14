f6fa9ec61cbc5925ef5744d880877479d6e4648d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 00:21:16 +0000

    First commited as before_19995_rev1

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/workbench/modeling/EModelService.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/workbench/modeling/EModelService.java
@@ -69,18 +69,10 @@ public interface EModelService {
 	/** Returned Location if the element is in an MTrimBar */
 	public static final int IN_TRIM = 0x10;
 
-	/**
-	 * Returned Location if the element is in a main menu of an MWindow
-	 * 
-	 * @since 1.1
-	 */
+	/** Returned Location if the element is in a main menu of an MWindow */
 	public static final int IN_MAIN_MENU = 0x20;
 
-	/**
-	 * Returned Location if the element is in a menu or a tool bar of an MPart
-	 * 
-	 * @since 1.1
-	 */
+	/** Returned Location if the element is in a menu or a tool bar of an MPart */
 	public static final int IN_PART = 0x40;
 
 	// 'Standard' searches
@@ -89,13 +81,13 @@ public interface EModelService {
 	public static final int PRESENTATION = OUTSIDE_PERSPECTIVE | IN_ACTIVE_PERSPECTIVE
 			| IN_SHARED_AREA;
 
-	/** Searches for elements in the UI presentation, including all perspectives */
+	/** Searches for elements in the UI that the user is currently seeing */
 	public static final int ANYWHERE = OUTSIDE_PERSPECTIVE | IN_ANY_PERSPECTIVE | IN_SHARED_AREA
-			| IN_TRIM;
+			| IN_TRIM | IN_MAIN_MENU | IN_PART;
 
 	/**
 	 * Searches for elements in the UI that the user is currently seeing that are OUTSIDE the
-	 * perspective (i.e. visible regardless of the current perspective)
+	 * perspective
 	 */
 	public static final int GLOBAL = OUTSIDE_PERSPECTIVE | IN_SHARED_AREA;
 
@@ -278,7 +270,6 @@ public interface EModelService {
 	 * @param id
 	 *            The ID of the handler
 	 * @return The handler or <code>null</code> if none is found
-	 * @since 1.1
 	 */
 	public MHandler findHandler(MHandlerContainer handlerContainer, String id);
 
