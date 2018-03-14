93fd247ae0b94369b0fefc585382e5fac2db7ecf
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 02:42:42 +0000

    First commited as before_23785_rev13

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/handlers/ShowViewHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/handlers/ShowViewHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 430988
  *******************************************************************************/
 package org.eclipse.ui.handlers;
 
@@ -15,50 +14,64 @@ import java.util.Map;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.descriptor.basic.MPartDescriptor;
-import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.workbench.modeling.EModelService;
-import org.eclipse.e4.ui.workbench.modeling.EPartService;
-import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
 import org.eclipse.jface.window.Window;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchCommandConstants;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.internal.WorkbenchMessages;
+import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.eclipse.ui.internal.dialogs.ShowViewDialog;
+import org.eclipse.ui.internal.misc.StatusUtil;
+import org.eclipse.ui.statushandlers.StatusManager;
+import org.eclipse.ui.views.IViewDescriptor;
 
 /**
  * Shows the given view. If no view is specified in the parameters, then this
  * opens the view selection dialog.
- *
+ * 
  * @since 3.1
  */
 public final class ShowViewHandler extends AbstractHandler {
 
-	@Override
-	public final Object execute(final ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		Shell shell = HandlerUtil.getActiveShell(event);
-		// Get the view identifier, if any.
-		IEclipseContext ctx = (IEclipseContext) workbenchWindow.getService(IEclipseContext.class);
-		EModelService modelService = (EModelService) workbenchWindow.getService(EModelService.class);
-		EPartService partService = (EPartService) workbenchWindow.getService(EPartService.class);
-		MApplication app = (MApplication) workbenchWindow.getService(MApplication.class);
-		MWindow window = (MWindow) workbenchWindow.getService(MWindow.class);
+  
+    /**
+     * Creates a new ShowViewHandler that will open the view in its default location.
+     */
+    public ShowViewHandler() {
+    }
+    
+    /**
+     * Creates a new ShowViewHandler that will optionally force the view to become
+     * a fast view.
+     * 
+     * @param makeFast if true, the view will be moved to the fast view bar (even if it already
+     * exists elsewhere). If false, the view will be shown in its default location. Calling with
+     * false is equivalent to using the default constructor.
+     */    
+    public ShowViewHandler(boolean makeFast) {
 
+    }
+    
+	@Override
+	public final Object execute(final ExecutionEvent event)
+			throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil
+				.getActiveWorkbenchWindowChecked(event);
+		// Get the view identifier, if any.
 		final Map parameters = event.getParameters();
-		final Object value = parameters.get(IWorkbenchCommandConstants.VIEWS_SHOW_VIEW_PARM_ID);
+		final Object value = parameters
+				.get(IWorkbenchCommandConstants.VIEWS_SHOW_VIEW_PARM_ID);
+		
 
 		if (value == null) {
-			openOther(shell, app, window, modelService, ctx, partService);
+			openOther(window);
 		} else {
-			try {
-				openView((String) value, partService);
-			} catch (PartInitException e) {
-				throw new ExecutionException("Part could not be initialized", e); //$NON-NLS-1$
-			}
+            try {
+                openView((String) value, window);
+            } catch (PartInitException e) {
+                throw new ExecutionException("Part could not be initialized", e); //$NON-NLS-1$
+            }
 		}
 
 		return null;
@@ -67,34 +80,51 @@ public final class ShowViewHandler extends AbstractHandler {
 	/**
 	 * Opens a view selection dialog, allowing the user to chose a view.
 	 */
-	private final void openOther(final Shell shell, MApplication app, MWindow window,
-			EModelService modelService,
-			IEclipseContext context,
-			EPartService partService) {
-
-		final ShowViewDialog dialog = new ShowViewDialog(shell, app, window, modelService, context);
+	private final void openOther(final IWorkbenchWindow window) {
+		final IWorkbenchPage page = window.getActivePage();
+		if (page == null) {
+			return;
+		}
+		
+		final ShowViewDialog dialog = new ShowViewDialog(window,
+				WorkbenchPlugin.getDefault().getViewRegistry());
 		dialog.open();
-
+		
 		if (dialog.getReturnCode() == Window.CANCEL) {
 			return;
 		}
-
-		final MPartDescriptor[] descriptors = dialog.getSelection();
-		for (MPartDescriptor descriptor : descriptors) {
-			partService.showPart(descriptor.getElementId(), PartState.ACTIVATE);
+		
+		final IViewDescriptor[] descriptors = dialog.getSelection();
+		for (int i = 0; i < descriptors.length; ++i) {
+			try {
+                openView(descriptors[i].getId(), window);
+			} catch (PartInitException e) {
+				StatusUtil.handleStatus(e.getStatus(),
+						WorkbenchMessages.ShowView_errorTitle
+								+ ": " + e.getMessage(), //$NON-NLS-1$
+						StatusManager.SHOW);
+			}
 		}
 	}
 
 	/**
 	 * Opens the view with the given identifier.
-	 *
+	 * 
 	 * @param viewId
 	 *            The view to open; must not be <code>null</code>
 	 * @throws PartInitException
 	 *             If the part could not be initialized.
 	 */
-	private final void openView(final String viewId, EPartService partService)
+	private final void openView(final String viewId,
+			final IWorkbenchWindow activeWorkbenchWindow)
 			throws PartInitException {
-		partService.showPart(viewId, PartState.ACTIVATE);
+
+		final IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
+		if (activePage == null) {
+			return;
+		}
+
+		activePage.showView(viewId);
+		
 	}
 }
