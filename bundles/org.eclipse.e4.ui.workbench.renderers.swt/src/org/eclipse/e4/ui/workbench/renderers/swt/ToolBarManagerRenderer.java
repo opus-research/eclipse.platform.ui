85aefebe45d3d2f9cd14907643a05c450089b260
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 11:20:55 +0000

    First commited as before_41284_rev1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
@@ -11,7 +11,6 @@
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 426535, 433234, 431868
  *     Maxime Porhel <maxime.porhel@obeo.fr> Obeo - Bug 431778
  *     Andrey Loskutov <loskutov@gmx.de> - Bugs 383569, 457198
- *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 431990
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
@@ -213,9 +212,6 @@ public class ToolBarManagerRenderer extends SWTPartRenderer {
 			ToolBar tb = parent.getControl();
 			if (tb != null && !tb.isDisposed()) {
 				tb.pack(true);
-				if (tb.getParent() != null) {
-					tb.getParent().pack(true);
-				}
 				tb.getShell().layout(new Control[] { tb }, SWT.DEFER);
 			}
 		}
