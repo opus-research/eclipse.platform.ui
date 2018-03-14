878d8174b444b1aebd581252bbe6f06a9075ef60
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 16:53:22 +0000

    First commited as after_48011_rev2

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerRenderer.java
@@ -16,6 +16,7 @@
  *     Andrey Loskutov <loskutov@gmx.de> - Bug 378849
  *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 460556
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 391430
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 461655
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
@@ -307,6 +308,84 @@ public class MenuManagerRenderer extends SWTPartRenderer {
 		if (UIEvents.isADD(event)) {
 			Object obj = menuModel;
 			processContents((MElementContainer<MUIElement>) obj);
+		}
+	}
+
+	@Inject
+	@Optional
+	private void subscribeTopicChildrenRemoved(@UIEventTopic(ElementContainer.TOPIC_CHILDREN) Event event) {
+
+		if (!(event.getProperty(UIEvents.EventTags.ELEMENT) instanceof MMenu)) {
+			return;
+		}
+
+		if (!UIEvents.isREMOVE(event)) {
+			return;
+		}
+
+		Object element = event.getProperty(UIEvents.EventTags.ELEMENT);// MMEnu
+		Object oldValue = event.getProperty(UIEvents.EventTags.OLD_VALUE);// MenuElement
+
+		MMenu menuModel = (MMenu) element;
+
+		if (oldValue instanceof MMenu) {
+			MMenu oldMMenu = (MMenu) oldValue;
+			handleMenuElementRemove(menuModel, oldMMenu);
+		}
+		else if (oldValue instanceof MMenuElement) {
+			MMenuElement oldMMenuElement = (MMenuElement) oldValue;
+			handleMenuElementRemove(menuModel, oldMMenuElement);
+		}
+		// REMOVE_MANY
+		else if (oldValue instanceof List) {
+			@SuppressWarnings("rawtypes")
+			List oldList = (List) oldValue;
+			handleMenuElementRemove(menuModel, oldList);
+		}
+		else{
+			throw new RuntimeException("Deleted menu element type is not matching"); //$NON-NLS-1$
+		}
+
+	}
+
+	void handleMenuElementRemove(MMenu menuModel, MMenu oldValue) {
+		// delete/reorder problems are solved with this check
+		if (menuModel.getLabel() == null){
+			return;
+		}
+		MenuManager menuManager = getManager(menuModel);
+		menuManager.remove(getManager(oldValue));
+		menuManager.update(false);
+	}
+
+	void handleMenuElementRemove(MMenu menuModel, MMenuElement oldValue) {
+		// delete/reorder problems are solved with this check
+		if (menuModel.getLabel() == null){
+			return;
+		}
+		MenuManager menuManager = getManager(menuModel);
+		IContributionItem contribution = getContribution(oldValue);
+		menuManager.remove(contribution);
+		menuManager.update(false);
+	}
+
+	void handleMenuElementRemove(MMenu menuModel, @SuppressWarnings("rawtypes") List list) {
+		MenuManager menuManager = getManager(menuModel);
+		if (!list.isEmpty()) {
+			for (int i = 0; i < list.size(); i++) {
+				Object item = list.get(i);
+				if (item instanceof MMenu) {
+					// this should not happen
+				} else if (item instanceof MMenuElement) {
+					// this should be the only case for remove many
+					MMenuElement iMenuElement = (MMenuElement) item;
+					IContributionItem contribution = getContribution(iMenuElement);
+					menuManager.remove(contribution);
+					menuManager.update(true);
+				} else {
+					// this should not happen
+				}
+			}
 		}
 	}
 
