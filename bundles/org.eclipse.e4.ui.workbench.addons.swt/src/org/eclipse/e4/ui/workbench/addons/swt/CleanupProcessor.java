c545df64f1a4ad0659528d75ce88d071313c372a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:27:07 +0000

    First commited as after_24547_rev3

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/CleanupProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/CleanupProcessor.java
@@ -11,9 +11,7 @@
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.addons.swt;
 
-import java.util.List;
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
@@ -23,20 +21,9 @@ import org.eclipse.e4.ui.workbench.modeling.EModelService;
 public class CleanupProcessor {
 	@Execute
 	void addCleanupAddon(MApplication app, EModelService modelService) {
-		List<MAddon> addons = app.getAddons();
+		final String contributionURI = "bundleclass://org.eclipse.e4.ui.workbench.addons.swt/org.eclipse.e4.ui.workbench.addons.cleanupaddon.CleanupAddon"; //$NON-NLS-1$
+		final String id = "CleanupAddon"; //$NON-NLS-1$
 
-		// prevent multiple copies
-		for (MAddon addon : addons) {
-			if (addon.getContributionURI()
-					.contains("ui.workbench.addons.cleanupaddon.CleanupAddon")) //$NON-NLS-1$
-				return;
-		}
-
-		// adds the add-on to the application model
-		MAddon cleanupAddon = modelService.createModelElement(MAddon.class);
-		cleanupAddon.setElementId("CleanupAddon"); //$NON-NLS-1$
-		cleanupAddon
-				.setContributionURI("bundleclass://org.eclipse.e4.ui.workbench.addons.swt/org.eclipse.e4.ui.workbench.addons.cleanupaddon.CleanupAddon"); //$NON-NLS-1$
-		app.getAddons().add(cleanupAddon);
+		ProcessorUtil.addAddon(app, modelService, contributionURI, id);
 	}
 }
--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/DnDProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/DnDProcessor.java
@@ -11,9 +11,7 @@
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.addons.swt;
 
-import java.util.List;
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
@@ -23,18 +21,9 @@ import org.eclipse.e4.ui.workbench.modeling.EModelService;
 public class DnDProcessor {
 	@Execute
 	void addDnDAddon(MApplication app, EModelService modelService) {
-		List<MAddon> addons = app.getAddons();
+		final String contributionURI = "bundleclass://org.eclipse.e4.ui.workbench.addons.swt/org.eclipse.e4.ui.workbench.addons.dndaddon.DnDAddon"; //$NON-NLS-1$
+		final String id = "DnDAddon"; //$NON-NLS-1$
 
-		// prevent multiple copies
-		for (MAddon addon : addons) {
-			if (addon.getContributionURI().contains("ui.workbench.addons.dndaddon.DnDAddon")) //$NON-NLS-1$
-				return;
-		}
-
-		// adds the add-on to the application model
-		MAddon dndAddon = modelService.createModelElement(MAddon.class);
-		dndAddon.setElementId("DnDAddon"); //$NON-NLS-1$
-		dndAddon.setContributionURI("bundleclass://org.eclipse.e4.ui.workbench.addons.swt/org.eclipse.e4.ui.workbench.addons.dndaddon.DnDAddon"); //$NON-NLS-1$
-		app.getAddons().add(dndAddon);
+		ProcessorUtil.addAddon(app, modelService, contributionURI, id);
 	}
 }
