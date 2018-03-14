eeeb4215d0be5e93186d4a0432fdca3a217d6d3b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 14:28:34 +0000

    First commited as before_45317_rev1

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
@@ -8,6 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Simon Scholz <simon.scholz@vogella.com> - Bug 462056
+ *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 457939
  *******************************************************************************/
 package org.eclipse.e4.ui.internal.workbench.swt;
 
@@ -847,6 +848,13 @@ public class PartRenderingEngine implements IPresentationEngine {
 				: null;
 		if (parentRenderer != null) {
 			parentRenderer.hideChild(element.getParent(), element);
+		}
+
+		if (element instanceof MPlaceholder) {
+			MPlaceholder ph = (MPlaceholder) element;
+			if (ph.getRef() != null && ph.getRef().getCurSharedRef() == ph) {
+				ph.getRef().setCurSharedRef(null);
+			}
 		}
 
 		AbstractPartRenderer renderer = getRendererFor(element);
