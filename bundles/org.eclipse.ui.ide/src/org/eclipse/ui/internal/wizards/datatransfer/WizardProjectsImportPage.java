14d4d87e7206f8ee1a6863d5b9eaf6d94b48311c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 07:03:20 +0000

    First commited as after_33985_rev2

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/WizardProjectsImportPage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/WizardProjectsImportPage.java
@@ -134,7 +134,7 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 		@Override
 		public Color getForeground(Object element) {
 			ProjectRecord projectRecord = (ProjectRecord) element;
-			if(projectRecord.hasConflicts) {
+			if (projectRecord.hasConflicts || projectRecord.isInvalid) {
 				return getShell().getDisplay().getSystemColor(SWT.COLOR_GRAY);
 			}
 			return null;
@@ -157,6 +157,8 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 		int level;
 		
 		boolean hasConflicts;
+
+		boolean isInvalid = false;
 
 		IProjectDescription description;
 
@@ -231,9 +233,13 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 
 				}
 			} catch (CoreException e) {
-				// no good couldn't get the name
+				// project definition file could not be parsed
+				this.projectName = DataTransferMessages.WizardProjectsImportPage_invalidProjectName;
+				this.isInvalid = true;
+
 			} catch (IOException e) {
-				// no good couldn't get the name
+				this.projectName = DataTransferMessages.WizardProjectsImportPage_invalidProjectName;
+				this.isInvalid = true;
 			}
 		}
 
@@ -265,6 +271,15 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 		}
 
 		/**
+		 * Returns whether the given project description file was valid
+		 *
+		 * @return boolean
+		 */
+		public boolean isInvalidProject() {
+			return isInvalid;
+		}
+
+		/**
 		 * Gets the label to be used when rendering this project record in the
 		 * UI.
 		 * 
@@ -272,9 +287,9 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 		 * @since 3.4
 		 */
 		public String getProjectLabel() {
-			if (description == null) {
-				return projectName;
-			}
+			// if (description == null) {
+			// return projectName;
+			// }
 
 			String path = projectSystemFile == null ? structureProvider
 					.getLabel(parent) : projectSystemFile
@@ -301,7 +316,8 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 		@Override
 		public boolean select(Viewer viewer, Object parentElement,
 				Object element) {
-			return !((ProjectRecord) element).hasConflicts;
+			ProjectRecord projectRecord = (ProjectRecord) element;
+			return !(projectRecord.hasConflicts || projectRecord.isInvalid);
 		}
 
 	}
@@ -556,7 +572,7 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 			@Override
 			public void checkStateChanged(CheckStateChangedEvent event) {
 				ProjectRecord element = (ProjectRecord) event.getElement();
-				if(element.hasConflicts) {
+				if (element.hasConflicts || element.isInvalid) {
 					projectsList.setChecked(element, false);
 				}
 				setPageComplete(projectsList.getCheckedElements().length > 0);
@@ -589,7 +605,7 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				for (ProjectRecord selectedProject : selectedProjects) {
-					if(selectedProject.hasConflicts) {
+					if (selectedProject.hasConflicts || selectedProject.isInvalid) {
 						projectsList.setChecked(selectedProject, false);
 					} else {
 						projectsList.setChecked(selectedProject, true);
@@ -931,8 +947,9 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 								.subTask(DataTransferMessages.WizardProjectsImportPage_ProcessingMessage);
 						while (filesIterator.hasNext()) {
 							File file = (File) filesIterator.next();
-							selectedProjects[index] = new ProjectRecord(file);
-							index++;
+							ProjectRecord projectRecord = new ProjectRecord(file);
+								selectedProjects[index] = projectRecord;
+								index++;
 						}
 					} else {
 						monitor.worked(60);
@@ -949,28 +966,32 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 
 		projectsList.refresh(true);
 		ProjectRecord[] projects = getProjectRecords();
-		boolean displayWarning = false;
+
+		boolean displayConflictWarning = false;
+		boolean displayInvalidWarning = false;
+
 		for (ProjectRecord project : projects) {
-			if(project.hasConflicts) {
-				displayWarning = true;
+			if (project.hasConflicts || project.isInvalid) {
 				projectsList.setGrayed(project, true);
-			}else {
+				displayConflictWarning |= project.hasConflicts;
+				displayInvalidWarning |= project.isInvalid;
+			} else {
 				projectsList.setChecked(project, true);
 			}
 		}
 		
-		if (displayWarning) {
-			setMessage(
-					DataTransferMessages.WizardProjectsImportPage_projectsInWorkspace,
-					WARNING);
+		if (displayConflictWarning && displayInvalidWarning) {
+			setMessage(DataTransferMessages.WizardProjectsImportPage_projectsInWorkspaceAndInvalid, WARNING);
+		} else if (displayConflictWarning) {
+			setMessage(DataTransferMessages.WizardProjectsImportPage_projectsInWorkspace, WARNING);
+		} else if (displayInvalidWarning) {
+			setMessage(DataTransferMessages.WizardProjectsImportPage_projectsInvalid, WARNING);
 		} else {
 			setMessage(DataTransferMessages.WizardProjectsImportPage_ImportProjectsDescription);
 		}
 		setPageComplete(projectsList.getCheckedElements().length > 0);
 		if(selectedProjects.length == 0) {
-			setMessage(
-					DataTransferMessages.WizardProjectsImportPage_noProjectsToImport,
-					WARNING);
+			setMessage(DataTransferMessages.WizardProjectsImportPage_noProjectsToImport, WARNING);
 		}
 	}
 
@@ -1120,7 +1141,8 @@ public class WizardProjectsImportPage extends WizardDataTransferPage {
 			}
 			String elementLabel = structureProvider.getLabel(child);
 			if (elementLabel.equals(IProjectDescription.DESCRIPTION_FILE_NAME)) {
-				files.add(new ProjectRecord(child, entry, level));
+				ProjectRecord projectRecord = new ProjectRecord(child, entry, level);
+				files.add(projectRecord);
 			}
 		}
 		return true;
