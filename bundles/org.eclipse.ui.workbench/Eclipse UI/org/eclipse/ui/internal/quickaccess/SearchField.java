fbc675064c1601b62d65a4f37d1a5452da1c378c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 01:37:49 +0000

    First commited as after_21919_rev10

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/SearchField.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/SearchField.java
@@ -277,13 +277,12 @@ public class SearchField {
 	}
 
 	private Text createText(Composite parent) {
-		Text text = new Text(parent, SWT.SEARCH | SWT.ICON_SEARCH);
-		text.setMessage(QuickAccessMessages.QuickAccess_EnterSearch);
-
+		Text text = new Text(parent, SWT.SEARCH | SWT.ICON_SEARCH | SWT.CANCEL);
+		text.setToolTipText(QuickAccessMessages.QuickAccess_EnterSearch);
 		GC gc = new GC(text);
 		FontMetrics fm = gc.getFontMetrics();
-		int width = text.computeSize(fm.getAverageCharWidth() * text.getMessage().length(),
-				SWT.DEFAULT).x + 15 /* some extra space */;
+		int width = text.computeSize(fm.getAverageCharWidth() * 10,
+				SWT.DEFAULT).x + 10 /* some extra space */;
 		gc.dispose();
 
 		GridDataFactory.fillDefaults().hint(width, SWT.DEFAULT).applyTo(text);
