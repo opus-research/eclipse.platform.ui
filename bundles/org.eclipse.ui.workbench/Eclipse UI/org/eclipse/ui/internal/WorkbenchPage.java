d8610cca9b1329a8a6d57a6fdb598a92443edeb7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:03:55 +0000

    First commited as before_25587_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
@@ -10,7 +10,6 @@
  *     Christian Janz  - <christian.janz@gmail.com> Fix for Bug 385592
  *     Marc-Andre Laperle (Ericsson) - Fix for Bug 413590
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 431340, 431348
- *     Dmitry Spiridenok <d.spiridenok@gmail.com> - Bug 433215
  *******************************************************************************/
 
 package org.eclipse.ui.internal;
@@ -2713,9 +2712,7 @@ public class WorkbenchPage extends CompatibleWorkbenchPage implements
 						partService, temporary, desc, this, true);
 
 				PerspectiveExtensionReader reader = new PerspectiveExtensionReader();
-				reader.setIncludeOnlyTags(new String[] {
-						IWorkbenchRegistryConstants.TAG_ACTION_SET,
-						IWorkbenchRegistryConstants.TAG_NEW_WIZARD_SHORTCUT });
+				reader.setIncludeOnlyTags(new String[] { IWorkbenchRegistryConstants.TAG_ACTION_SET });
 				reader.extendLayout(null, id, modelLayout);
 
 				addActionSet(perspective, temporary);
