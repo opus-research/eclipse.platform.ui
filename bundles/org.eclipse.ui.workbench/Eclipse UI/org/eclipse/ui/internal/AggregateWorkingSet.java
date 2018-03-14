a2a41defe68df3464115a31504f55bac095c3648
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 23:28:39 +0000

    First commited as before_63525_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/AggregateWorkingSet.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/AggregateWorkingSet.java
@@ -12,7 +12,7 @@ package org.eclipse.ui.internal;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.Set;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -96,8 +96,7 @@ public class AggregateWorkingSet extends AbstractWorkingSet implements
 		}
 		inElementConstruction = true;
 		try {
-			// use *linked* set to maintain predictable elements order
-			Set<IAdaptable> elements = new LinkedHashSet<>();
+			Set elements = new HashSet();
 			IWorkingSet[] localComponents = getComponentsInternal();
 			for (int i = 0; i < localComponents.length; i++) {
 				IWorkingSet workingSet = localComponents[i];
@@ -116,7 +115,8 @@ public class AggregateWorkingSet extends AbstractWorkingSet implements
 					continue;
 				}
 			}
-			internalSetElements(elements.toArray(new IAdaptable[elements.size()]));
+			internalSetElements((IAdaptable[]) elements
+					.toArray(new IAdaptable[elements.size()]));
 			if (fireEvent) {
 				fireWorkingSetChanged(
 					IWorkingSetManager.CHANGE_WORKING_SET_CONTENT_CHANGE, null);
@@ -319,7 +319,7 @@ public class AggregateWorkingSet extends AbstractWorkingSet implements
 
 	@Override
 	public String toString() {
-		return "AWS [name=" + getName() + ", components=" + Arrays.toString(getComponentsInternal()) + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		return "AWS [name=" + getName() + ", components=" + Arrays.toString(components) + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 
 }
