0017ad4614f3df210b6cb1d02ecc9baa8517e4dd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:22:07 +0000

    First commited as before_37437_rev7

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IStartup.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/IStartup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Thirumala Reddy Mutchukota <thirumala@google.com> - Bug 451687
  *******************************************************************************/
 package org.eclipse.ui;
 
@@ -21,8 +20,7 @@ package org.eclipse.ui;
  */
 public interface IStartup {
     /**
-     * Will be called in a separate thread and possibly in parallel with other
-     * startup extensions after the workbench initializes.
+     * Will be called in a separate thread after the workbench initializes.
      * <p>
      * Note that most workbench methods must be called in the UI thread
      * since they may access SWT.  For example, to obtain the current workbench
