0cd0d8ff09eb67716f227717ab1cbc461df6c38e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 22:09:46 +0000

    First commited as before_60963_rev1

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/EModelServiceInsertTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/EModelServiceInsertTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2015 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -97,7 +97,8 @@ public class EModelServiceInsertTest {
 	}
 
 	private void testInsert(MApplication app, String relToId, int where, float ratio) {
-		EModelService modelService = app.getContext().get(EModelService.class);
+		EModelService modelService = (EModelService) app.getContext().get(
+				EModelService.class.getName());
 		assertNotNull(modelService);
 
 		MUIElement relTo = modelService.find(relToId, app);
--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/Bug308317Test.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/workbench/Bug308317Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2015 IBM Corporation and others.
+ * Copyright (c) 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -94,7 +94,7 @@ public class Bug308317Test {
 		partB.setContext(partContextB);
 
 		application.setContext(appContext);
-		appContext.set(MApplication.class, application);
+		appContext.set(MApplication.class.getName(), application);
 
 		PartConsumer getter = ContextInjectionFactory.make(PartConsumer.class,
 				window.getContext());
