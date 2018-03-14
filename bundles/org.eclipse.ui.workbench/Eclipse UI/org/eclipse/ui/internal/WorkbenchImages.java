9c868c15fa159f76c8d0edd70aac3e213224b805
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 00:54:47 +0000

    First commited as before_21004_rev4

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchImages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchImages.java
@@ -154,16 +154,20 @@ public/*final*/class WorkbenchImages {
 
         // other toolbar buttons
 
-		declareImage(ISharedImages.IMG_ETOOL_SAVE_EDIT, PATH_ETOOL + "save_edit.gif", true); //$NON-NLS-1$
-		declareImage(ISharedImages.IMG_ETOOL_SAVE_EDIT_DISABLED, PATH_DTOOL + "save_edit.gif", true); //$NON-NLS-1$
+        declareImage(ISharedImages.IMG_ETOOL_SAVE_EDIT, PATH_ETOOL
+				+ "save_edit.png", true); //$NON-NLS-1$
+		declareImage(ISharedImages.IMG_ETOOL_SAVE_EDIT_DISABLED, PATH_DTOOL
+				+ "save_edit.png", true); //$NON-NLS-1$
         
-		declareImage(ISharedImages.IMG_ETOOL_SAVEAS_EDIT, PATH_ETOOL + "saveas_edit.gif", true); //$NON-NLS-1$
-		declareImage(ISharedImages.IMG_ETOOL_SAVEAS_EDIT_DISABLED,
-				PATH_DTOOL + "saveas_edit.gif", true); //$NON-NLS-1$
+		declareImage(ISharedImages.IMG_ETOOL_SAVEAS_EDIT, PATH_ETOOL
+				+ "saveas_edit.png", true); //$NON-NLS-1$
+		declareImage(ISharedImages.IMG_ETOOL_SAVEAS_EDIT_DISABLED, PATH_DTOOL
+				+ "saveas_edit.png", true); //$NON-NLS-1$
         
-		declareImage(ISharedImages.IMG_ETOOL_SAVEALL_EDIT, PATH_ETOOL + "saveall_edit.gif", true); //$NON-NLS-1$
-		declareImage(ISharedImages.IMG_ETOOL_SAVEALL_EDIT_DISABLED,
-				PATH_DTOOL + "saveall_edit.gif", true); //$NON-NLS-1$
+		declareImage(ISharedImages.IMG_ETOOL_SAVEALL_EDIT, PATH_ETOOL
+				+ "saveall_edit.png", true); //$NON-NLS-1$
+		declareImage(ISharedImages.IMG_ETOOL_SAVEALL_EDIT_DISABLED, PATH_DTOOL
+				+ "saveall_edit.png", true); //$NON-NLS-1$
 
         declareImage(ISharedImages.IMG_TOOL_UNDO,
                 PATH_ETOOL + "undo_edit.png", true); //$NON-NLS-1$
