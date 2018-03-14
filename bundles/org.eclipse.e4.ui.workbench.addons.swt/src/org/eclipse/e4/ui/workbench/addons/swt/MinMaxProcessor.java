28374fe520bc74d98c2faed4725d3293371f8205
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 00:55:14 +0000

    First commited as after_21025_rev1

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/MinMaxProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/MinMaxProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2013, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 426553
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.addons.swt;
 
@@ -14,21 +15,26 @@ import java.util.List;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.impl.ApplicationFactoryImpl;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+
+/**
+ * Adds the MinxMaxAddon to the application model. Registered via plugin.xml
+ */
 
 public class MinMaxProcessor {
 	@Execute
-	void addMinMaxAddon(MApplication app) {
-		List<MAddon> addons = app.getAddons();
+	void addMinMaxAddon(MApplication app, EModelService modelService) {
 
-		// Prevent multiple copies
-		for (MAddon addon : addons) {
-			if (addon.getContributionURI().contains("ui.workbench.addons.minmax.MinMaxAddon"))
-				return;
+		// search for existing add-on in the model
+		List<MAddon> elements = modelService.findElements(app,
+				"ui.workbench.addons.minmax.MinMaxAddon", MAddon.class, null); //$NON-NLS-1$
+		// prevent multiple additions of the addon to the model
+		if (elements.size() > 0) {
+			return;
 		}
 
-		// Insert the addon into the system
-		MAddon minMaxAddon = ApplicationFactoryImpl.eINSTANCE.createAddon();
+		// insert the add-on into the model
+		MAddon minMaxAddon = modelService.createModelElement(MAddon.class);
 		minMaxAddon.setElementId("MinMaxAddon"); //$NON-NLS-1$
 		minMaxAddon
 				.setContributionURI("bundleclass://org.eclipse.e4.ui.workbench.addons.swt/org.eclipse.e4.ui.workbench.addons.minmax.MinMaxAddon"); //$NON-NLS-1$
