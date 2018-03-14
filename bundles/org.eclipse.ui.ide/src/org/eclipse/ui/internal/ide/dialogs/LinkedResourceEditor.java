da6641f34b2ab91b03a7016aee6ccdbae36b0d41
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 19:32:16 +0000

    First commited as before_54933_rev2

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/LinkedResourceEditor.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/LinkedResourceEditor.java
@@ -27,14 +27,12 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -45,11 +43,9 @@ import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.IContentProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.window.Window;
@@ -223,12 +219,7 @@ public class LinkedResourceEditor {
 
 		fTree = new TreeViewer(treeComposite, SWT.BORDER | SWT.MULTI | SWT.FULL_SELECTION);
 
-		fTree.addSelectionChangedListener(new ISelectionChangedListener() {
-			@Override
-			public void selectionChanged(SelectionChangedEvent event) {
-				updateSelection();
-			}
-		});
+		fTree.addSelectionChangedListener(event -> updateSelection());
 
 		data = new GridData(GridData.FILL_BOTH);
 		data.heightHint = fTree.getTree().getItemHeight() * 10;
@@ -443,18 +434,11 @@ public class LinkedResourceEditor {
 																		 * >
 																		 */();
 			try {
-				fProject.accept(new IResourceVisitor() {
-					/**
-					 * @throws CoreException
-					 */
-					@Override
-					public boolean visit(IResource resource)
-							throws CoreException {
-						if (resource.isLinked() && !resource.isVirtual())
-							resources.add(resource);
-						return true;
-					}
-				});
+				fProject.accept(resource -> {
+if (resource.isLinked() && !resource.isVirtual())
+				resources.add(resource);
+return true;
+});
 			} catch (CoreException e) {
 			}
 			projectFiles = (IResource[]) resources.toArray(new IResource[0]);
@@ -561,32 +545,23 @@ public class LinkedResourceEditor {
 				IDEWorkbenchMessages.LinkedResourceEditor_removeTitle,
 				IDEWorkbenchMessages.LinkedResourceEditor_removeMessage)) {
 			final IResource[] selectedResources = getSelectedResource();
-			final ArrayList/*<IResource>*/ removedResources = new ArrayList();
+			final ArrayList<IResource> removedResources = new ArrayList<>();
 
-			IRunnableWithProgress op = new IRunnableWithProgress() {
-				@Override
-				public void run(IProgressMonitor monitor) {
+			IRunnableWithProgress op = monitor -> {
+				SubMonitor subMonitor = SubMonitor.convert(monitor,
+						IDEWorkbenchMessages.LinkedResourceEditor_removingMessage, selectedResources.length);
+				for (int i = 0; i < selectedResources.length; i++) {
+					if (subMonitor.isCanceled())
+						break;
+					String fullPath = selectedResources[i].getFullPath().toPortableString();
 					try {
-						monitor.beginTask(
-								IDEWorkbenchMessages.LinkedResourceEditor_removingMessage,
-								selectedResources.length);
-						for (int i = 0; i < selectedResources.length; i++) {
-							if (monitor.isCanceled())
-								break;
-							String fullPath = selectedResources[i]
-									.getFullPath().toPortableString();
-							try {
-								selectedResources[i].delete(true, new SubProgressMonitor(monitor, 1));
-								removedResources.add(selectedResources[i]);
-								fBrokenResources.remove(fullPath);
-								fFixedResources.remove(fullPath);
-								fAbsoluteResources.remove(fullPath);
-							} catch (CoreException e) {
-								e.printStackTrace();
-							}
-						}
-					} finally {
-						monitor.done();
+						selectedResources[i].delete(true, subMonitor.newChild(1));
+						removedResources.add(selectedResources[i]);
+						fBrokenResources.remove(fullPath);
+						fFixedResources.remove(fullPath);
+						fAbsoluteResources.remove(fullPath);
+					} catch (CoreException e) {
+						e.printStackTrace();
 					}
 				}
 			};
