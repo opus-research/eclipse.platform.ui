423aa3b588f21e65a66411b6ec3931bbdf69aad1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 19:49:39 +0000

    First commited as before_55249_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/views/IViewRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/views/IViewRegistry.java
@@ -11,8 +11,6 @@
  *******************************************************************************/
 package org.eclipse.ui.views;
 
-import org.eclipse.e4.ui.model.application.descriptor.basic.MPartDescriptor;
-import org.eclipse.ui.part.ViewPart;
 
 /**
  * The view registry maintains a list of views explicitly registered
@@ -30,21 +28,6 @@ import org.eclipse.ui.part.ViewPart;
  * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface IViewRegistry {
-
-	/**
-	 * This constant is used as key for persisting the original class for a
-	 * legacy {@link ViewPart} in the persisted state of a
-	 * {@link MPartDescriptor}.
-	 */
-	public static final String ORIGINAL_COMPATIBILITY_VIEW_CLASS = "originalCompatibilityViewClass"; //$NON-NLS-1$
-
-	/**
-	 * This constant is used as key for persisting the original bundle for a
-	 * legacy {@link ViewPart} in the persisted state of a
-	 * {@link MPartDescriptor}.
-	 */
-	public static final String ORIGINAL_COMPATIBILITY_VIEW_BUNDLE = "originalCompatibilityViewBundle"; //$NON-NLS-1$
-
     /**
      * Return a view descriptor with the given extension id.  If no view exists
      * with the id return <code>null</code>.
