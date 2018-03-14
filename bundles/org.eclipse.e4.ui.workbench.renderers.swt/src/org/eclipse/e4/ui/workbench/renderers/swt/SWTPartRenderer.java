3a5263976ef0df482c4df65378264236c88e5159
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 08:09:47 +0000

    First commited as after_35002_rev1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/SWTPartRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/SWTPartRenderer.java
@@ -20,6 +20,7 @@ import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.css.swt.dom.WidgetElement;
 import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
 import org.eclipse.e4.ui.internal.workbench.swt.CSSConstants;
+import org.eclipse.e4.ui.model.application.descriptor.basic.MPartDescriptor;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.MUILabel;
@@ -40,6 +41,7 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Widget;
 
 public abstract class SWTPartRenderer extends AbstractPartRenderer {
+	private static final String ICON_URI_FROM_DESC = "IconUriFromDesc"; //$NON-NLS-1$
 
 	Map<String, Image> imageMap = new HashMap<String, Image>();
 
@@ -231,8 +233,7 @@ public abstract class SWTPartRenderer extends AbstractPartRenderer {
 		Image image = (Image) ((MUIElement) element).getTransientData().get(
 				IPresentationEngine.OVERRIDE_ICON_IMAGE_KEY);
 		if (image == null || image.isDisposed()) {
-			String iconURI = element.getIconURI();
-			image = getImageFromURI(iconURI);
+			image = getImageFromURI(getIconURI(element));
 		}
 
 		if (image != null) {
@@ -240,6 +241,25 @@ public abstract class SWTPartRenderer extends AbstractPartRenderer {
 		}
 
 		return image;
+	}
+
+	private String getIconURI(MUILabel element) {
+		if (element instanceof MPart) {
+			MPart part = (MPart) element;
+			if (part.getTransientData().containsKey(ICON_URI_FROM_DESC)) {
+				return (String) part.getTransientData().get(ICON_URI_FROM_DESC);
+			}
+
+			MPartDescriptor desc = modelService.getPartDescriptor(part
+					.getElementId());
+			if (desc != null && desc.getIconURI() != null) {
+				part.getTransientData().put(ICON_URI_FROM_DESC,
+						desc.getIconURI());
+				return desc.getIconURI();
+			}
+
+		}
+		return element.getIconURI();
 	}
 
 	/**
