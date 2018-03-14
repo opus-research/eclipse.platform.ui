142f41841a0707f1ab4a1e4380d942b603f9af3c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 14:33:11 +0000

    First commited as after_45722_rev2

--- tests/org.eclipse.ui.tests.browser/src/org/eclipse/ui/tests/browser/internal/WebBrowserUtilTestCase.java
+++ tests/org.eclipse.ui.tests.browser/src/org/eclipse/ui/tests/browser/internal/WebBrowserUtilTestCase.java
@@ -12,11 +12,63 @@ package org.eclipse.ui.tests.browser.internal;
 
 import static org.eclipse.ui.internal.browser.IBrowserDescriptor.URL_PARAMETER;
 import static org.eclipse.ui.internal.browser.WebBrowserUtil.createParameterArray;
-import static org.junit.Assert.assertArrayEquals;
 import junit.framework.TestCase;
 
-@SuppressWarnings("restriction")
+import org.eclipse.ui.internal.browser.WebBrowserUtil;
+
+@SuppressWarnings({ "restriction", "deprecation" })
 public class WebBrowserUtilTestCase extends TestCase {
+
+	private static final String URL = "http://127.0.0.1:3873/help/index.jsp";
+
+	public void testNullParameters() {
+		assertEquals(URL, WebBrowserUtil.createParameterString(null, URL));
+	}
+
+	public void testEmptyParameters() {
+		assertEquals(URL, WebBrowserUtil.createParameterString("", URL));
+	}
+
+	public void testNullURL() {
+		assertEquals("", WebBrowserUtil.createParameterString("", null));
+	}
+
+	public void testNoSubstitution() {
+		assertEquals("-console " + URL, WebBrowserUtil.createParameterString("-console", URL));
+	}
+
+	public void testSubstitution() {
+		assertEquals("-url " + URL + " -console", WebBrowserUtil.createParameterString("-url %URL% -console", URL));
+	}
+
+	// Remove when we move to JUnit 4
+	private void assertArrayEquals(String[] a1, String[] a2) {
+		assertEquals("Arrays have different lengths", a1.length, a2.length);
+		for (int i = 0; i < a1.length; i++) {
+			assertEquals("Elements differ at index " + i, a1[i], a2[i]);
+		}
+	}
+
+	public void testArrayNullParameters() {
+		assertArrayEquals(new String[] { URL }, WebBrowserUtil.createParameterArray(null, URL));
+	}
+
+	public void testArrayEmptyParameters() {
+		assertArrayEquals(new String[] { URL }, WebBrowserUtil.createParameterArray("", URL));
+	}
+
+	public void testArrayNullURL() {
+		assertArrayEquals(new String[0], WebBrowserUtil.createParameterArray("", null));
+	}
+
+	public void testArrayNoSubstitution() {
+		assertArrayEquals(new String[] { "-console", URL }, WebBrowserUtil.createParameterArray("-console", URL));
+	}
+
+	public void testArraySubstitution() {
+		assertArrayEquals(new String[] { "-url", URL, "-console" },
+				WebBrowserUtil.createParameterArray("-url %URL% -console", URL));
+	}
 
 	public void testCreateParameterArray() {
 		assertArrayEquals(new String[0], createParameterArray(null, null));
@@ -28,7 +80,7 @@ public class WebBrowserUtilTestCase extends TestCase {
 				createParameterArray("param1 " + URL_PARAMETER + " param2", null));
 		assertArrayEquals(new String[] { "param1", "url", "param2" },
 				createParameterArray("param1 " + URL_PARAMETER + " param2", "url"));
-		assertArrayEquals(new String[] { "param1", "url", "param2", "url" },
+		assertArrayEquals(new String[] { "param1", "url", "param2", URL_PARAMETER },
 				createParameterArray("param1 " + URL_PARAMETER + " param2 " + URL_PARAMETER, "url"));
 	}
 
