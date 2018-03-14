9d93f49ce0ef5f4cd2ddbef5df1792d23cc918d1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 11:14:19 +0000

    First commited as after_40450_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/menus/CommandContributionItem.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/menus/CommandContributionItem.java
@@ -74,13 +74,12 @@ import org.eclipse.ui.statushandlers.StatusManager;
  * It currently supports placement in menus and toolbars.
  * </p>
  * <p>
- * This class may be instantiated; it is not intended to be subclassed.
+ * This class may be instantiated.
  * </p>
  * 
- * @noextend This class is not intended to be subclassed by clients.
  * @since 3.3
  */
-public class CommandContributionItem extends ContributionItem {
+public final class CommandContributionItem extends ContributionItem {
 	/**
 	 * A push button tool item or menu item.
 	 */
