818dc468ced2cb9f48382f2b62f95847d7fccb8f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 01:12:15 +0000

    First commited as before_21389_rev1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/StackRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/StackRenderer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2014 IBM Corporation and others.
+ * Copyright (c) 2008, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -670,7 +670,6 @@ public class StackRenderer extends LazyStackRenderer {
 		}
 
 		trComp.pack();
-		trComp.layout(true, true);
 	}
 
 	private MToolBar getViewTB(CTabFolder ctf) {
