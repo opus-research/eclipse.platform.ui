469a9337b68b016b9214ef9887bbf978782647f2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:52:28 +0000

    First commited as before_38803_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
@@ -17,6 +17,8 @@
 
 package org.eclipse.ui.internal;
 
+import org.eclipse.ui.internal.dialogs.cpd.CustomizePerspectiveDialog;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -160,7 +162,6 @@ import org.eclipse.ui.internal.StartupThreading.StartupRunnable;
 import org.eclipse.ui.internal.actions.CommandAction;
 import org.eclipse.ui.internal.commands.SlaveCommandService;
 import org.eclipse.ui.internal.contexts.ContextService;
-import org.eclipse.ui.internal.dialogs.cpd.CustomizePerspectiveDialog;
 import org.eclipse.ui.internal.e4.compatibility.CompatibilityPart;
 import org.eclipse.ui.internal.e4.compatibility.ModeledPageLayout;
 import org.eclipse.ui.internal.e4.compatibility.SelectionService;
@@ -737,18 +738,6 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 			}
 			updateActionSets();
 
-			// synchronize the main menu with the e4 model
-			IMenuManager menuBarManager = getMenuBarManager();
-			if (menuBarManager instanceof MenuManager) {
-				// TODO: remove this hack to retrieve the MenuManagerRenderer
-				MenuManagerRenderer mr = (MenuManagerRenderer) rendererFactory.getRenderer(
-						modelService.createModelElement(MMenu.class), null);
-				MMenu parent = mr.getMenuModel((MenuManager) menuBarManager);
-				if (parent != null) {
-					mr.reconcileManagerToModel((MenuManager) menuBarManager, parent);
-				}
-			}
-
 			IPreferenceStore preferenceStore = PrefUtil.getAPIPreferenceStore();
 			boolean enableAnimations = preferenceStore
 					.getBoolean(IWorkbenchPreferenceConstants.ENABLE_ANIMATIONS);
@@ -1259,7 +1248,7 @@ public class WorkbenchWindow implements IWorkbenchWindow {
 		return null;
 	}
 
-	public void fill(MenuManagerRenderer renderer, MMenu menu, IMenuManager manager) {
+	private void fill(MenuManagerRenderer renderer, MMenu menu, IMenuManager manager) {
 		for (IContributionItem item : manager.getItems()) {
 			if (item instanceof MenuManager) {
 				MenuManager menuManager = (MenuManager) item;
