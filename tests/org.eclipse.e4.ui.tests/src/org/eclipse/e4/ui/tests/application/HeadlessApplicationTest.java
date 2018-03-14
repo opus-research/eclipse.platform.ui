49e7d1974a3961beced332e554c8d871452f5f1f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:44:05 +0000

    First commited as before_24770_rev18

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/HeadlessApplicationTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/application/HeadlessApplicationTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 IBM Corporation and others.
+ * Copyright (c) 2009, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -255,7 +255,7 @@ public abstract class HeadlessApplicationTest extends
 
 		MApplication application = (MApplication) resource.getContents().get(0);
 		application.setContext(appContext);
-		appContext.set(MApplication.class.getName(), application); // XXX
+		appContext.set(MApplication.class, application); // XXX
 		appContext.set(EModelService.class, new ModelServiceImpl(appContext));
 
 		ECommandService cs = (ECommandService) appContext
