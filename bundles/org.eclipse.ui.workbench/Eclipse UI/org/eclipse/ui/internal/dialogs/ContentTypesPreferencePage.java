c912401f14d40cb8ce6747f6011b7883a1b0c80a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 16:52:27 +0000

    First commited as after_47742_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/ContentTypesPreferencePage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/ContentTypesPreferencePage.java
@@ -312,9 +312,6 @@ public class ContentTypesPreferencePage extends PreferencePage implements
 			public void widgetSelected(SelectionEvent e) {
 				try {
 					String text = charsetField.getText().trim();
-					if (text.length() == 0) {
-						text = null;
-					}
 					getSelectedContentType().setDefaultCharset(text);
 					setButton.setEnabled(false);
 				} catch (CoreException e1) {
