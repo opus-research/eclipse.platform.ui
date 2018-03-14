8500d31b093d04c7e8321be8baf5c957a2f91258
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:30:32 +0000

    First commited as before_27002_rev1

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/navigator/ResourceNavigator.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/navigator/ResourceNavigator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -447,9 +447,7 @@ public class ResourceNavigator extends ViewPart implements ISetSelectionTarget,
     protected void initListeners(final TreeViewer viewer) {
         viewer.addSelectionChangedListener(new ISelectionChangedListener() {
             public void selectionChanged(SelectionChangedEvent event) {
-            	viewer.getControl().setRedraw(false);
                 handleSelectionChanged(event);
-                viewer.getControl().setRedraw(true);
             }
         });
         viewer.addDoubleClickListener(new IDoubleClickListener() {
