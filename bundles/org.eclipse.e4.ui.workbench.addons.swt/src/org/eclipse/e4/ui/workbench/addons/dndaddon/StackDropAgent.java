15d8a99981fb039b485a56de86d05b941d0059d3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 14:59:13 +0000

    First commited as after_46038_rev5

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/dndaddon/StackDropAgent.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/dndaddon/StackDropAgent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 IBM Corporation and others.
+ * Copyright (c) 2010, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 431404
  ******************************************************************************/
 
 package org.eclipse.e4.ui.workbench.addons.dndaddon;
@@ -24,6 +25,7 @@ import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -193,15 +195,13 @@ public class StackDropAgent extends DropAgent {
 		} else {
 			if (dropIndex < dropCTF.getItemCount()) {
 				Rectangle itemBounds = dropCTF.getItem(dropIndex).getBounds();
-				itemBounds.width = 2;
 				itemBounds = Display.getCurrent().map(dropCTF, null, itemBounds);
-				dndManager.frameRect(itemBounds);
+				addDropFeedback(itemBounds);
 			} else if (dropCTF.getItemCount() > 0) {
 				Rectangle itemBounds = dropCTF.getItem(dropIndex - 1).getBounds();
 				itemBounds.x = itemBounds.x + itemBounds.width;
-				itemBounds.width = 2;
 				itemBounds = Display.getCurrent().map(dropCTF, null, itemBounds);
-				dndManager.frameRect(itemBounds);
+				addDropFeedback(itemBounds);
 			} else {
 				Rectangle fr = new Rectangle(tabArea.x, tabArea.y, tabArea.width, tabArea.height);
 				fr.width = 2;
@@ -216,6 +216,29 @@ public class StackDropAgent extends DropAgent {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Add drop feedback image, centered at the top-left point of the given item
+	 * bounds. The image is an arrow made by stacked rectangles
+	 */
+	private void addDropFeedback(Rectangle itemBounds) {
+		// drop base area is 16x16
+		int x = itemBounds.x - 16 / 2;
+		int y = itemBounds.y - 16 / 2;
+		dndManager.clearOverlay();
+		// dropdown is made by stacked rectangles
+		addRect(x, y, 2, 0, 12, 2);
+		addRect(x, y, 3, 2, 10, 2);
+		addRect(x, y, 4, 4, 8, 2);
+		addRect(x, y, 5, 6, 6, 2);
+		addRect(x, y, 6, 8, 4, 2);
+		addRect(x, y, 7, 10, 2, 2);
+	}
+
+	/** Adds a rectangle to the dndManager */
+	private void addRect(int xBase, int yBase, int x, int y, int w, int h) {
+		dndManager.addImage(new Rectangle(xBase + x, yBase + y, w, h), new Image(Display.getCurrent(), w, h));
 	}
 
 	/**
