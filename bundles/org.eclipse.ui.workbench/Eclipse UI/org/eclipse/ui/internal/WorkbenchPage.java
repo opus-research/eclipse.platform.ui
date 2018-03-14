f4d343cd7ce3cb239015c2577c164f0307bf2575
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:04:04 +0000

    First commited as before_25587_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPage.java
@@ -9,8 +9,8 @@
  *     IBM Corporation - initial API and implementation
  *     Christian Janz  - <christian.janz@gmail.com> Fix for Bug 385592
  *     Marc-Andre Laperle (Ericsson) - Fix for Bug 413590
- *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 431340, 431348
- *     Dmitry Spiridenok <d.spiridenok@gmail.com> - Fix for Bug 433215
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 431340, 431348, 426535, 433234
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 431868
  *******************************************************************************/
 
 package org.eclipse.ui.internal;
@@ -63,6 +63,7 @@ import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainerElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MStackElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MTrimElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindowElement;
 import org.eclipse.e4.ui.workbench.IPresentationEngine;
@@ -1246,6 +1247,23 @@ public class WorkbenchPage extends CompatibleWorkbenchPage implements
 			break;
 		case VIEW_CREATE:
 			partService.showPart(part, PartState.CREATE);
+
+			// Report the visibility of the created part
+			MStackElement sElement = part;
+			if (part.getCurSharedRef() != null)
+				sElement = part.getCurSharedRef();
+			MUIElement parentElement = sElement.getParent();
+			if (parentElement instanceof MPartStack) {
+				MPartStack partStack = (MPartStack) parentElement;
+				if (partStack.getSelectedElement() == sElement
+						&& !partStack.getTags().contains(IPresentationEngine.MINIMIZED)) {
+					firePartVisible(part);
+				} else {
+					firePartHidden(part);
+				}
+			} else {
+				firePartVisible(part); // Stand-alone part
+			}
 			break;
 		}
 		return part;
@@ -2713,9 +2731,7 @@ public class WorkbenchPage extends CompatibleWorkbenchPage implements
 						partService, temporary, desc, this, true);
 
 				PerspectiveExtensionReader reader = new PerspectiveExtensionReader();
-				reader.setIncludeOnlyTags(new String[] {
-						IWorkbenchRegistryConstants.TAG_ACTION_SET,
-						IWorkbenchRegistryConstants.TAG_NEW_WIZARD_SHORTCUT });
+				reader.setIncludeOnlyTags(new String[] { IWorkbenchRegistryConstants.TAG_ACTION_SET });
 				reader.extendLayout(null, id, modelLayout);
 
 				addActionSet(perspective, temporary);
@@ -3360,6 +3376,13 @@ public class WorkbenchPage extends CompatibleWorkbenchPage implements
 		tags.clear();
 		tags.addAll(dummyPerspective.getTags());
 
+		// remove HIDDEN_EXPLICITLY tag from trim elements
+		List<MTrimElement> trimElements = modelService.findElements(window, null,
+				MTrimElement.class, null);
+		for (MTrimElement mTrimElement : trimElements) {
+			mTrimElement.getTags().remove(IPresentationEngine.HIDDEN_EXPLICITLY);
+		}
+
 		partService.requestActivation();
 
 		// reset complete
@@ -3937,6 +3960,9 @@ public class WorkbenchPage extends CompatibleWorkbenchPage implements
 	 * @return the stack of perspectives of this page's containing window
 	 */
 	private MPerspectiveStack getPerspectiveStack() {
+		if (_perspectiveStack != null) {
+			return _perspectiveStack;
+		}
 		List<MPerspectiveStack> theStack = modelService.findElements(window, null,
 				MPerspectiveStack.class, null);
 		if (theStack.size() > 0) {
