bdcbf18db80b9b6b90fc7178709ea5b0fe32a190
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 01:00:02 +0000

    First commited as before_21111_rev12

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
@@ -44,7 +44,6 @@ import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.PersistState;
 import org.eclipse.e4.ui.internal.workbench.Activator;
 import org.eclipse.e4.ui.internal.workbench.E4Workbench;
-import org.eclipse.e4.ui.internal.workbench.ModelAssembler;
 import org.eclipse.e4.ui.internal.workbench.Policy;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.MApplicationElement;
@@ -515,12 +514,11 @@ public class PartRenderingEngine implements IPresentationEngine {
 				if (e instanceof Error) {
 					// errors are deadly, we shouldn't ignore these
 					throw (Error) e;
-				} else {
-					// log exceptions otherwise
-					if (logger != null) {
-						String message = "Exception occurred while rendering: {0}"; //$NON-NLS-1$
-						logger.error(e, NLS.bind(message, element));
-					}
+				}
+				// log exceptions otherwise
+				if (logger != null) {
+					String message = "Exception occurred while rendering: {0}"; //$NON-NLS-1$
+					logger.error(e, NLS.bind(message, element));
 				}
 			}
 
@@ -697,12 +695,11 @@ public class PartRenderingEngine implements IPresentationEngine {
 				if (e instanceof Error) {
 					// errors are deadly, we shouldn't ignore these
 					throw (Error) e;
-				} else {
-					// log exceptions otherwise
-					if (logger != null) {
-						String message = "Exception occurred while rendering: {0}"; //$NON-NLS-1$
-						logger.error(e, NLS.bind(message, element));
-					}
+				}
+				// log exceptions otherwise
+				if (logger != null) {
+					String message = "Exception occurred while rendering: {0}"; //$NON-NLS-1$
+					logger.error(e, NLS.bind(message, element));
 				}
 			}
 
@@ -820,12 +817,11 @@ public class PartRenderingEngine implements IPresentationEngine {
 				if (e instanceof Error) {
 					// errors are deadly, we shouldn't ignore these
 					throw (Error) e;
-				} else {
-					// log exceptions otherwise
-					if (logger != null) {
-						String message = "Exception occurred while unrendering: {0}"; //$NON-NLS-1$
-						logger.error(e, NLS.bind(message, element));
-					}
+				}
+				// log exceptions otherwise
+				if (logger != null) {
+					String message = "Exception occurred while unrendering: {0}"; //$NON-NLS-1$
+					logger.error(e, NLS.bind(message, element));
 				}
 			}
 
@@ -1076,22 +1072,6 @@ public class PartRenderingEngine implements IPresentationEngine {
 						// Special handling for partial models (for testing...)
 						testShell = new Shell(display, SWT.SHELL_TRIM);
 						createGui((MUIElement) uiRoot, testShell, null);
-					}
-				}
-
-				// Starts the final model assembly phase (bug 376486) (if
-				// cleaning)
-				Object o = appContext.get(E4Workbench.NO_SAVED_MODEL_FOUND);
-				if (o instanceof Boolean) {
-					if (((Boolean) o).booleanValue()) {
-						String xmiUriArg = System
-								.getProperty(org.eclipse.e4.ui.workbench.IWorkbench.XMI_URI_ARG);
-						if ("org.eclipse.ui.workbench/LegacyIDE.e4xmi".equals(xmiUriArg)) { //$NON-NLS-1$
-							ModelAssembler contribProcessor = ContextInjectionFactory
-									.make(ModelAssembler.class, appContext);
-							contribProcessor
-									.processModel(ModelAssembler.AFTER_MODEL_CREATION);
-						}
 					}
 				}
 
--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelAssembler.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelAssembler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 BestSolution.at and others.
+ * Copyright (c) 2010, 2014 BestSolution.at and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,8 @@
  *
  * Contributors:
  *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 430075, 430080
+ *     René Brandstetter - Bug 419749 - [Workbench] [e4 Workbench] - Remove the deprecated PackageAdmin
  ******************************************************************************/
 
 package org.eclipse.e4.ui.internal.workbench;
@@ -19,13 +21,13 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.inject.Inject;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IContributor;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.core.runtime.RegistryFactory;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
@@ -47,31 +49,14 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EContentsEList;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.osgi.framework.Bundle;
-import org.osgi.service.packageadmin.PackageAdmin;
-import org.osgi.service.packageadmin.RequiredBundle;
+import org.osgi.framework.namespace.BundleNamespace;
+import org.osgi.framework.wiring.BundleWire;
+import org.osgi.framework.wiring.BundleWiring;
 
 /**
  *
  */
 public class ModelAssembler {
-
-	/**
-	 * The name of the extension point attribute.
-	 */
-	private static final String POST_MODEL_CREATION = "postmodelcreation"; //$NON-NLS-1$
-
-	/**
-	 * This field, used in the {@link #processModel(int)} method, indicates that the legacy
-	 * workbench model is not yet created.
-	 */
-	public static final int DURING_MODEL_CREATION = 0;
-
-	/**
-	 * This field, used in the {@link #processModel(int)} method, indicates that the workbench model
-	 * has been created.
-	 */
-	public static final int AFTER_MODEL_CREATION = 1;
-
 	@Inject
 	private Logger logger;
 
@@ -81,190 +66,160 @@ public class ModelAssembler {
 	@Inject
 	private IEclipseContext context;
 
+	@Inject
+	private IExtensionRegistry registry;
+
 	final private static String extensionPointID = "org.eclipse.e4.workbench.model"; //$NON-NLS-1$
 
 	/**
-	 * Process the model based on the step field which can be {@link #PURE_E4},
-	 * {@link #LEGACY_E4STEP} or {@link #LEGACY_E3STEP}.
-	 *
-	 * @param step
-	 * @see #PURE_E4
-	 * @see #LEGACY_E4STEP
-	 * @see #LEGACY_E3STEP
+	 * Process the model
 	 */
-	public void processModel(int step) {
-
-		IExtensionRegistry registry = RegistryFactory.getRegistry();
+	public void processModel() {
 		IExtensionPoint extPoint = registry.getExtensionPoint(extensionPointID);
 		IExtension[] extensions = topoSort(extPoint.getExtensions());
 
 		List<MApplicationElement> imports = new ArrayList<MApplicationElement>();
 		List<MApplicationElement> addedElements = new ArrayList<MApplicationElement>();
 
-		E4XMIResource applicationResource = (E4XMIResource) ((EObject) application).eResource();
-		ResourceSet resourceSet = applicationResource.getResourceSet();
-
-		for (IExtension extension : extensions) {
-			IConfigurationElement[] ces = extension.getConfigurationElements();
-			for (IConfigurationElement ce : ces) {
-				if (!"processor".equals(ce.getName()) || !Boolean.parseBoolean(ce.getAttribute("beforefragment"))) { //$NON-NLS-1$ //$NON-NLS-2$
-					continue;
-				}
-				if (!isCorrectPhase(step, ce)) {
-					continue;
-				}
-				runProcessor(ce);
-			}
-		}
-
-		for (IExtension extension : extensions) {
-			IConfigurationElement[] ces = extension.getConfigurationElements();
-			for (IConfigurationElement ce : ces) {
-				if (!"fragment".equals(ce.getName())) { //$NON-NLS-1$
-					continue;
-				}
-				if (!isCorrectPhase(step, ce)) {
-					continue;
-				}
-				IContributor contributor = ce.getContributor();
-				String attrURI = ce.getAttribute("uri"); //$NON-NLS-1$
-				if (attrURI == null) {
-					logger.warn("Unable to find location for the model extension \"{0}\"", //$NON-NLS-1$
-							contributor.getName());
-					continue;
-				}
-
-				URI uri;
-
-				try {
-					// check if the attrURI is already a platform URI
-					if (URIHelper.isPlatformURI(attrURI)) {
-						uri = URI.createURI(attrURI);
-					} else {
-						String bundleName = contributor.getName();
-						String path = bundleName + '/' + attrURI;
-						uri = URI.createPlatformPluginURI(path, false);
-					}
-				} catch (RuntimeException e) {
-					logger.warn(e, "Model extension has invalid location"); //$NON-NLS-1$
-					continue;
-				}
-
-				String contributorURI = URIHelper.constructPlatformURI(contributor);
-				Resource resource;
-				try {
-					resource = resourceSet.getResource(uri, true);
-				} catch (RuntimeException e) {
-					logger.warn(e, "Unable to read model extension from " + uri.toString()); //$NON-NLS-1$
-					continue;
-				}
-
-				EList<?> contents = resource.getContents();
-				if (contents.isEmpty()) {
-					continue;
-				}
-
-				Object extensionRoot = contents.get(0);
-
-				if (!(extensionRoot instanceof MModelFragments)) {
-					logger.warn("Unable to create model extension \"{0}\"", //$NON-NLS-1$
-							contributor.getName());
-					continue;
-				}
-
-				MModelFragments fragmentsContainer = (MModelFragments) extensionRoot;
-				List<MModelFragment> fragments = fragmentsContainer.getFragments();
-				boolean evalImports = false;
-				for (MModelFragment fragment : fragments) {
-					List<MApplicationElement> elements = fragment.getElements();
-					if (elements.size() == 0) {
-						continue;
-					}
-
-					for (MApplicationElement el : elements) {
-						EObject o = (EObject) el;
-
-						E4XMIResource r = (E4XMIResource) o.eResource();
-						applicationResource.setID(o, r.getID(o));
-
-						if (contributorURI != null)
-							el.setContributorURI(contributorURI);
-
-						// Remember IDs of subitems
-						TreeIterator<EObject> treeIt = EcoreUtil.getAllContents(o, true);
-						while (treeIt.hasNext()) {
-							EObject eObj = treeIt.next();
-							r = (E4XMIResource) eObj.eResource();
-							if (contributorURI != null && (eObj instanceof MApplicationElement))
-								((MApplicationElement) eObj).setContributorURI(contributorURI);
-							applicationResource.setID(eObj, r.getInternalId(eObj));
-						}
-					}
-
-					List<MApplicationElement> merged = fragment.merge(application);
-
-					if (merged.size() > 0) {
-						evalImports = true;
-						addedElements.addAll(merged);
-					} else {
-						logger.info("Nothing to merge for \"{0}\"", uri); //$NON-NLS-1$				
-					}
-				}
-
-				if (evalImports) {
-					List<MApplicationElement> localImports = fragmentsContainer.getImports();
-					if (localImports != null) {
-						imports.addAll(localImports);
-					}
-				}
-			}
-		}
-
-		for (IExtension extension : extensions) {
-			IConfigurationElement[] ces = extension.getConfigurationElements();
-			for (IConfigurationElement ce : ces) {
-				if (!"processor".equals(ce.getName()) || Boolean.parseBoolean(ce.getAttribute("beforefragment"))) { //$NON-NLS-1$ //$NON-NLS-2$
-					continue;
-				}
-				if (!isCorrectPhase(step, ce)) {
-					continue;
-				}
-
-				runProcessor(ce);
-			}
-		}
+		// run processors which are marked to run before fragments
+		runProcessors(extensions, false);
+		processFragments(extensions, imports, addedElements);
+		// run processors which are marked to run after fragments
+		runProcessors(extensions, true);
 
 		resolveImports(imports, addedElements);
 	}
 
 	/**
-	 * Check if this configuration element must be run now based on the
-	 * <code>postmodelcreation</code> attribute which can be {@link #DURING_MODEL_CREATION} or
-	 * {@link #AFTER_MODEL_CREATION}.
-	 *
-	 * @param step
-	 * @param ce
-	 * @return true if the element must be processed now.
+	 * @param extensions
+	 * @param imports
+	 * @param addedElements
 	 */
-	private boolean isCorrectPhase(int step, IConfigurationElement ce) {
+	private void processFragments(IExtension[] extensions, List<MApplicationElement> imports,
+			List<MApplicationElement> addedElements) {
 
-		// Running before the workbench model is created
-		if (step == DURING_MODEL_CREATION) {
-			if (ce.getAttribute(POST_MODEL_CREATION) == null) {
-				return true;
+		for (IExtension extension : extensions) {
+			IConfigurationElement[] ces = extension.getConfigurationElements();
+			for (IConfigurationElement ce : ces) {
+				if ("fragment".equals(ce.getName())) { //$NON-NLS-1$
+					processFragment(ce, imports, addedElements);
+				}
 			}
-			return Boolean.FALSE.toString().equals(ce.getAttribute(POST_MODEL_CREATION));
+		}
+	}
+
+	private void processFragment(IConfigurationElement ce, List<MApplicationElement> imports,
+			List<MApplicationElement> addedElements) {
+		E4XMIResource applicationResource = (E4XMIResource) ((EObject) application).eResource();
+		ResourceSet resourceSet = applicationResource.getResourceSet();
+		IContributor contributor = ce.getContributor();
+		String attrURI = ce.getAttribute("uri"); //$NON-NLS-1$
+		if (attrURI == null) {
+			logger.warn("Unable to find location for the model extension \"{0}\"", //$NON-NLS-1$
+					contributor.getName());
+			return;
 		}
 
-		// Running after the workbench model has been created
-		else if (step == AFTER_MODEL_CREATION) {
-			if (ce.getAttribute(POST_MODEL_CREATION) == null) {
-				return false;
+		URI uri;
+
+		try {
+			// check if the attrURI is already a platform URI
+			if (URIHelper.isPlatformURI(attrURI)) {
+				uri = URI.createURI(attrURI);
+			} else {
+				String bundleName = contributor.getName();
+				String path = bundleName + '/' + attrURI;
+				uri = URI.createPlatformPluginURI(path, false);
 			}
-			return Boolean.TRUE.toString().equals(ce.getAttribute(POST_MODEL_CREATION));
+		} catch (RuntimeException e) {
+			logger.warn(e, "Model extension has invalid location"); //$NON-NLS-1$
+			return;
 		}
 
-		return false;
+		String contributorURI = URIHelper.constructPlatformURI(contributor);
+		Resource resource;
+		try {
+			resource = resourceSet.getResource(uri, true);
+		} catch (RuntimeException e) {
+			logger.warn(e, "Unable to read model extension from " + uri.toString()); //$NON-NLS-1$
+			return;
+		}
+
+		EList<?> contents = resource.getContents();
+		if (contents.isEmpty()) {
+			return;
+		}
+
+		Object extensionRoot = contents.get(0);
+
+		if (!(extensionRoot instanceof MModelFragments)) {
+			logger.warn("Unable to create model extension \"{0}\"", //$NON-NLS-1$
+					contributor.getName());
+			return;
+		}
+
+		MModelFragments fragmentsContainer = (MModelFragments) extensionRoot;
+		List<MModelFragment> fragments = fragmentsContainer.getFragments();
+		boolean evalImports = false;
+		for (MModelFragment fragment : fragments) {
+			List<MApplicationElement> elements = fragment.getElements();
+			if (elements.size() == 0) {
+				continue;
+			}
+
+			for (MApplicationElement el : elements) {
+				EObject o = (EObject) el;
+
+				E4XMIResource r = (E4XMIResource) o.eResource();
+				applicationResource.setID(o, r.getID(o));
+
+				if (contributorURI != null)
+					el.setContributorURI(contributorURI);
+
+				// Remember IDs of subitems
+				TreeIterator<EObject> treeIt = EcoreUtil.getAllContents(o, true);
+				while (treeIt.hasNext()) {
+					EObject eObj = treeIt.next();
+					r = (E4XMIResource) eObj.eResource();
+					if (contributorURI != null && (eObj instanceof MApplicationElement))
+						((MApplicationElement) eObj).setContributorURI(contributorURI);
+					applicationResource.setID(eObj, r.getInternalId(eObj));
+				}
+			}
+
+			List<MApplicationElement> merged = fragment.merge(application);
+
+			if (merged.size() > 0) {
+				evalImports = true;
+				addedElements.addAll(merged);
+			} else {
+				logger.info("Nothing to merge for \"{0}\"", uri); //$NON-NLS-1$
+			}
+		}
+
+		if (evalImports) {
+			List<MApplicationElement> localImports = fragmentsContainer.getImports();
+			if (localImports != null) {
+				imports.addAll(localImports);
+			}
+		}
+	}
+
+	/**
+	 * @param extensions
+	 * @param afterFragments
+	 */
+	private void runProcessors(IExtension[] extensions, Boolean afterFragments) {
+		for (IExtension extension : extensions) {
+			IConfigurationElement[] ces = extension.getConfigurationElements();
+			for (IConfigurationElement ce : ces) {
+				boolean parseBoolean = Boolean.parseBoolean(ce.getAttribute("beforefragment")); //$NON-NLS-1$
+				if ("processor".equals(ce.getName()) && !afterFragments.equals(parseBoolean)) { //$NON-NLS-1$
+					runProcessor(ce);
+				}
+
+			}
+		}
 	}
 
 	private void runProcessor(IConfigurationElement ce) {
@@ -350,6 +305,7 @@ public class ModelAssembler {
 						public void run() {
 							if (internalFeature.isMany()) {
 								System.err.println("Replacing"); //$NON-NLS-1$
+								@SuppressWarnings("unchecked")
 								List<Object> l = (List<Object>) interalTarget.eGet(internalFeature);
 								int index = l.indexOf(internalImportObject);
 								if (index >= 0) {
@@ -372,7 +328,7 @@ public class ModelAssembler {
 	/**
 	 * Sort the provided extensions by the dependencies of their contributors. Note that sorting is
 	 * done in-place.
-	 *
+	 * 
 	 * @param extensions
 	 *            the list of extensions to be sorted
 	 * @return the same list of extensions in a topologically-sorted order
@@ -382,7 +338,6 @@ public class ModelAssembler {
 			return extensions;
 		}
 
-		PackageAdmin admin = Activator.getDefault().getBundleAdmin();
 		final Map<String, Collection<IExtension>> mappedExtensions = new HashMap<String, Collection<IExtension>>();
 		// Captures the bundles that are listed as requirements for a particular bundle.
 		final Map<String, Collection<String>> requires = new HashMap<String, Collection<String>>();
@@ -423,9 +378,13 @@ public class ModelAssembler {
 		// now populate the dependency graph
 		for (String bundleId : mappedExtensions.keySet()) {
 			assert requires.containsKey(bundleId) && depends.containsKey(bundleId);
-			for (RequiredBundle requiredBundle : admin.getRequiredBundles(bundleId)) {
+
+			// can only be one, because ExtensionPoints require the singleton setting
+			// on a bundle
+			Bundle requiredBundle = Activator.getDefault().getBundleForName(bundleId);
+			if (requiredBundle != null) {
 				assert requiredBundle.getSymbolicName().equals(bundleId);
-				for (Bundle dependentBundle : requiredBundle.getRequiringBundles()) {
+				for (Bundle dependentBundle : resolveRequiringBundle(requiredBundle)) {
 					if (!mappedExtensions.containsKey(dependentBundle.getSymbolicName())) {
 						// not a contributor of an extension
 						continue;
@@ -477,6 +436,64 @@ public class ModelAssembler {
 		}
 		assert resultIndex == extensions.length;
 		return extensions;
+	}
+
+	/**
+	 * Returns the bundles that currently require the given bundle.
+	 * <p>
+	 * If this required bundle is required and then re-exported by another bundle then all the
+	 * requiring bundles of the re-exporting bundle are included in the returned array.
+	 * </p>
+	 * @return An unmodifiable {@link Iterable} set of bundles currently requiring this required
+	 *         bundle. An empty {@link Iterable} will be returned if the given {@code Bundle} object
+	 *         has become stale or no bundles require the given bundle.
+	 * @throws NullPointerException
+	 *             if the given bundle is <code>null</code>
+	 */
+	private static Iterable<Bundle> resolveRequiringBundle(Bundle bundle) {
+		BundleWiring providerWiring = bundle.adapt(BundleWiring.class);
+		if (!providerWiring.isInUse()) {
+			return Collections.emptySet();
+		}
+
+		Set<Bundle> requiring = new HashSet<Bundle>();
+
+		addRequirers(requiring, providerWiring);
+		return Collections.unmodifiableSet(requiring);
+	}
+
+	/**
+	 * Recursively collects all bundles which depend-on/require the given {@link BundleWiring}.
+	 * <p>
+	 * All re-exports will be followed and also be contained in the result.
+	 * </p>
+	 * @param requiring
+	 *            the result which will contain all the bundles which require the given
+	 *            {@link BundleWiring}
+	 * @param providerWiring
+	 *            the {@link BundleWiring} for which the requirers should be resolved
+	 * @throws NullPointerException
+	 *             if either the requiring or the providerWiring is <code>null</code>
+	 */
+	private static void addRequirers(Set<Bundle> requiring, BundleWiring providerWiring) {
+		List<BundleWire> requirerWires = providerWiring
+				.getProvidedWires(BundleNamespace.BUNDLE_NAMESPACE);
+		if (requirerWires == null) {
+			// we don't hold locks while checking the graph, just return if no longer isInUse
+			return;
+		}
+		for (BundleWire requireBundleWire : requirerWires) {
+			Bundle requirer = requireBundleWire.getRequirer().getBundle();
+			if (requiring.contains(requirer)) {
+				continue;
+			}
+			requiring.add(requirer);
+			String reExport = requireBundleWire.getRequirement().getDirectives()
+					.get(BundleNamespace.REQUIREMENT_VISIBILITY_DIRECTIVE);
+			if (BundleNamespace.VISIBILITY_REEXPORT.equals(reExport)) {
+				addRequirers(requiring, requireBundleWire.getRequirerWiring());
+			}
+		}
 	}
 
 	// FIXME Should we not reuse ModelUtils???
