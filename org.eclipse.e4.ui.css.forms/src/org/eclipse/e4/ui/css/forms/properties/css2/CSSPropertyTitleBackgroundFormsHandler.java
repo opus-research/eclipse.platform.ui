3bf86aeca7f4a685c53da68c302c174f24d3c8c7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 02:20:01 +0000

    First commited as after_23565_rev2

---
+++ org.eclipse.e4.ui.css.forms/src/org/eclipse/e4/ui/css/forms/properties/css2/CSSPropertyTitleBackgroundFormsHandler.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Lars Vogel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 422702
+ *******************************************************************************/
+package org.eclipse.e4.ui.css.forms.properties.css2;
+
+import org.eclipse.e4.ui.css.core.dom.properties.ICSSPropertyHandler;
+import org.eclipse.e4.ui.css.core.engine.CSSEngine;
+import org.eclipse.e4.ui.css.forms.dom.SectionElement;
+import org.eclipse.e4.ui.css.swt.helpers.CSSSWTColorHelper;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.forms.widgets.Section;
+import org.w3c.dom.css.CSSValue;
+
+public class CSSPropertyTitleBackgroundFormsHandler implements
+ICSSPropertyHandler {
+
+	@Override
+	public boolean applyCSSProperty(Object element, String property,
+			CSSValue value, String pseudo, CSSEngine engine) throws Exception {
+		SectionElement sectionElement = (SectionElement) element;
+		Section section = (Section) sectionElement.getNativeWidget();
+		section.setTitleBarBackground(CSSSWTColorHelper.getSWTColor(value,
+				Display.getCurrent()));
+		return false;
+	}
+
+	@Override
+	public String retrieveCSSProperty(Object element, String property,
+			String pseudo, CSSEngine engine) throws Exception {
+		return null;
+	}
+
+}
