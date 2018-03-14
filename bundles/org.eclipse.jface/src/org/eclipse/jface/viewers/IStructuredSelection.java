6f637157b0930e9087ca692975b97e72b746ad1f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 05:36:53 +0000

    First commited as after_31028_rev6

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/IStructuredSelection.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/IStructuredSelection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 234331
  *******************************************************************************/
 package org.eclipse.jface.viewers;
 
@@ -16,7 +17,7 @@ import java.util.List;
 /**
  * A selection containing elements.
  */
-public interface IStructuredSelection extends ISelection {
+public interface IStructuredSelection extends Iterable, ISelection {
     /**
      * Returns the first element in this selection, or <code>null</code>
      * if the selection is empty.
@@ -30,6 +31,7 @@ public interface IStructuredSelection extends ISelection {
      *
      * @return an iterator over the selected elements
      */
+	@Override
     public Iterator iterator();
 
     /**
