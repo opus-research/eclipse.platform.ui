e71408be0a41904c03b53b0e0236ab228fba67a7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 17:16:01 +0000

    First commited as before_49794_rev2

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelAssembler.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelAssembler.java
@@ -194,8 +194,6 @@ public class ModelAssembler {
 			if (merged.size() > 0) {
 				evalImports = true;
 				addedElements.addAll(merged);
-			} else {
-				logger.debug("Nothing to merge for \"{0}\"", uri); //$NON-NLS-1$
 			}
 		}
 
