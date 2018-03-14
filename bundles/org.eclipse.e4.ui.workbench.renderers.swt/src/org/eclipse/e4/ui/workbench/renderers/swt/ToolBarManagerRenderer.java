a392f14922e7619c0e859139dae7a2f8aabbd703
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 00:56:01 +0000

    First commited as after_21063_rev4

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarManagerRenderer.java
@@ -8,6 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Maxime Porhel <maxime.porhel@obeo.fr> Obeo - Bug 410426
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 426535 
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
@@ -66,6 +67,9 @@ import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.swt.widgets.Widget;
@@ -99,6 +103,11 @@ public class ToolBarManagerRenderer extends SWTPartRenderer {
 	private Map<MToolBarElement, ArrayList<ToolBarContributionRecord>> sharedElementToRecord = new HashMap<MToolBarElement, ArrayList<ToolBarContributionRecord>>();
 
 	private ToolItemUpdater enablementUpdater = new ToolItemUpdater();
+
+	/**
+	 * The context menu for this trim stack's items.
+	 */
+	private Menu toolbarMenu;
 
 	// @Inject
 	// private Logger logger;
@@ -352,7 +361,22 @@ public class ToolBarManagerRenderer extends SWTPartRenderer {
 			}
 		}
 
+		createMenuToCloseToolbar(toolbarModel, renderedCtrl);
+
 		return renderedCtrl;
+	}
+
+	private void createMenuToCloseToolbar(final MToolBar toolbarModel,
+			Control renderedCtrl) {
+		toolbarMenu = new Menu(renderedCtrl);
+		MenuItem closeItem = new MenuItem(toolbarMenu, SWT.NONE);
+		closeItem.setText(Messages.ToolBarManagerRenderer_MenuCloseText);
+		closeItem.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(org.eclipse.swt.widgets.Event event) {
+				toolbarModel.setToBeRendered(false);
+			}
+		});
+		renderedCtrl.setMenu(toolbarMenu);
 	}
 
 	/**
@@ -474,6 +498,7 @@ public class ToolBarManagerRenderer extends SWTPartRenderer {
 		bar.addDisposeListener(new DisposeListener() {
 			public void widgetDisposed(DisposeEvent e) {
 				cleanUp((MToolBar) element);
+				toolbarMenu = null;
 			}
 		});
 		return bar;
@@ -934,4 +959,5 @@ public class ToolBarManagerRenderer extends SWTPartRenderer {
 	ToolItemUpdater getUpdater() {
 		return enablementUpdater;
 	}
+
 }
