633d9d5e8cf7b0f1e0a022415cebb98966157ca3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 06:49:46 +0000

    First commited as before_33379_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPlugin.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchPlugin.java
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 400714, 441267, 441184
+ *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 400714, 441267
  *******************************************************************************/
 
 package org.eclipse.ui.internal;
@@ -76,6 +76,7 @@ import org.eclipse.ui.internal.wizards.ImportWizardRegistry;
 import org.eclipse.ui.internal.wizards.NewWizardRegistry;
 import org.eclipse.ui.operations.IWorkbenchOperationSupport;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.presentations.AbstractPresentationFactory;
 import org.eclipse.ui.testing.TestableObject;
 import org.eclipse.ui.views.IViewRegistry;
 import org.eclipse.ui.wizards.IWizardRegistry;
@@ -523,6 +524,24 @@ public class WorkbenchPlugin extends AbstractUIPlugin {
             factory = null;
         }
         return factory;
+    }
+
+    /**
+     * Returns the presentation factory with the given id, or <code>null</code> if not found.
+     * @param targetID The id of the presentation factory to use.
+     * @return AbstractPresentationFactory or <code>null</code>
+     * if not factory matches that id.
+     */
+    public AbstractPresentationFactory getPresentationFactory(String targetID) {
+        Object o = createExtension(
+                IWorkbenchRegistryConstants.PL_PRESENTATION_FACTORIES,
+                "factory", targetID); //$NON-NLS-1$
+        if (o instanceof AbstractPresentationFactory) {
+            return (AbstractPresentationFactory) o;
+        }
+        WorkbenchPlugin
+                .log("Error creating presentation factory: " + targetID + " -- class is not an AbstractPresentationFactory"); //$NON-NLS-1$ //$NON-NLS-2$
+        return null;
     }
 
     /**
