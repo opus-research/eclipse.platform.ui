b8f592c3aba15a8a60eb50a760948aefc59c7057
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 14:08:59 +0000

    First commited as after_44841_rev1

--- bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/fragment/MModelFragment.java
+++ bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/fragment/MModelFragment.java
@@ -62,19 +62,21 @@ public interface MModelFragment {
 	List<MApplicationElement> getElements();
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * Merges an MModelFragment into an MApplication.
-	 * @param application The MApplication to merge into.  Must not be null.
-	 * @return A list of MApplicationElements that were added to the MApplication.  Must not be null.
-	 * <!-- end-user-doc -->
-	 * <!-- begin-model-doc -->
-	 * <p>
-	 * <strong>Developers</strong>:
-	 * Add more detailed documentation by editing this comment in
-	 * /org.eclipse.e4.ui.model.workbench/model/ModelFragment.ecore.
-	 * There is a GenModel/documentation node under each type and attribute.
-	 * </p>
-	 * <!-- end-model-doc -->
+	 * <!-- begin-user-doc --> Merges this MModelFragment into an MApplication.
+	 * 
+	 * @param application
+	 *            The MApplication to merge into. Must not be null.
+	 * @return A list of MApplicationElements that were added to the
+	 *         MApplication. Must not be null. <!-- end-user-doc --> <!--
+	 *         begin-model-doc -->
+	 *         <p>
+	 *         <strong>Developers</strong>: Add more detailed documentation by
+	 *         editing this comment in
+	 *         /org.eclipse.e4.ui.model.workbench/model/ModelFragment.ecore.
+	 *         There is a GenModel/documentation node under each type and
+	 *         attribute.
+	 *         </p>
+	 *         <!-- end-model-doc -->
 	 * @model
 	 * @generated
 	 */
