65789b8a64ab2c0aafdf48abcf400aec62b19a86
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:08:34 +0000

    First commited as after_25752_rev2

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ViewerFilter.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ViewerFilter.java
@@ -29,31 +29,34 @@ public abstract class ViewerFilter {
     protected ViewerFilter() {
     }
 
-    /**
-     * Filters the given elements for the given viewer.
-     * The input array is not modified.
-     * <p>
-     * The default implementation of this method calls 
-     * <code>select</code> on each element in the array, 
-     * and returns only those elements for which <code>select</code>
-     * returns <code>true</code>.
-     * </p>
-     * @param viewer the viewer
-     * @param parent the parent element
-     * @param elements the elements to filter
-     * @return the filtered elements
-     */
-    public Object[] filter(Viewer viewer, Object parent, Object[] elements) {
-        int size = elements.length;
-        ArrayList out = new ArrayList(size);
-        for (int i = 0; i < size; ++i) {
-            Object element = elements[i];
-            if (select(viewer, parent, element)) {
+	/**
+	 * Filters the given elements for the given viewer. The input array is not
+	 * modified.
+	 * <p>
+	 * The default implementation of this method calls <code>select</code> on
+	 * each element in the array, and returns only those elements for which
+	 * <code>select</code> returns <code>true</code>.
+	 * </p>
+	 * 
+	 * @param viewer
+	 *            the viewer
+	 * @param parent
+	 *            the parent element
+	 * @param elements
+	 *            the elements to filter
+	 * @return the filtered elements
+	 */
+	public Object[] filter(Viewer viewer, Object parent, Object[] elements) {
+		int size = elements.length;
+		ArrayList<Object> out = new ArrayList<Object>(size);
+		for (int i = 0; i < size; ++i) {
+			Object element = elements[i];
+			if (select(viewer, parent, element)) {
 				out.add(element);
 			}
-        }
-        return out.toArray();
-    }
+		}
+		return out.toArray();
+	}
 
     /**
      * Filters the given elements for the given viewer.
