8b0d548ddc51b0c64bd5e11e163ad8b2ba2de862
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 10:29:30 +0000

    First commited as after_39340_rev15

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessContents.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessContents.java
@@ -11,6 +11,7 @@
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 472654, 491272, 491398
  *     Leung Wang Hei <gemaspecial@yahoo.com.hk> - Bug 483343
  *     Patrik Suzzi <psuzzi@gmail.com> - Bug 491291, 491529, 491293, 492434, 492452, 459989
+ *     Wayne Beaton (The Eclipse Foundation) - Bug 162006
  *******************************************************************************/
 package org.eclipse.ui.internal.quickaccess;
 
@@ -21,6 +22,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import org.eclipse.core.runtime.Adapters;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.IRegistryChangeEvent;
+import org.eclipse.core.runtime.IRegistryChangeListener;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.jface.bindings.TriggerSequence;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.layout.GridDataFactory;
@@ -77,6 +84,10 @@ import org.eclipse.ui.themes.ColorUtil;
  */
 public abstract class QuickAccessContents {
 	/**
+	 *
+	 */
+	static final String QUICK_ACCESS_EXTENSION_POINT = "org.eclipse.ui.workbench.quickAccess"; //$NON-NLS-1$
+	/**
 	 * When opened in a popup we were given the command used to open it. Now
 	 * that we have a shell, we are just using a hard coded command id.
 	 */
@@ -88,7 +99,20 @@ public abstract class QuickAccessContents {
 
 	protected Text filterText;
 
+	private QuickAccessProvider[] baseProviders;
 	private QuickAccessProvider[] providers;
+	private IEclipseContext context;
+
+	/**
+	 * When the extension registry changes, recompute the providers.
+	 */
+	private IRegistryChangeListener registryChangeListener = new IRegistryChangeListener() {
+		@Override
+		public void registryChanged(IRegistryChangeEvent event) {
+			computeProviders();
+			resetProviders();
+		}
+	};
 
 	protected Table table;
 	protected Label infoLabel;
@@ -108,8 +132,33 @@ public abstract class QuickAccessContents {
 	protected boolean resized = false;
 	private TriggerSequence keySequence;
 
-	public QuickAccessContents(QuickAccessProvider[] providers) {
-		this.providers = providers;
+	public QuickAccessContents(QuickAccessProvider[] providers, IEclipseContext context) {
+		baseProviders = providers;
+		this.context = context;
+		computeProviders();
+
+		Platform.getExtensionRegistry().addRegistryChangeListener(registryChangeListener, "org.eclipse.ui.workbench"); //$NON-NLS-1$
+	}
+
+	/**
+	 * Computes the full set of providers starting with the base
+	 * (built-in/system) providers, then adding providers contributed via the
+	 * "quickAccess" extension point. This method destructively modifies the
+	 * {@link QuickAccessContents#providers} field with up-to-date information
+	 * based on information provided from the extension registry.
+	 */
+	private void computeProviders() {
+		List<QuickAccessProvider> extensionProviders = new ArrayList<>();
+		IExtensionRegistry registry = Platform.getExtensionRegistry();
+		IConfigurationElement[] config = registry.getConfigurationElementsFor(QUICK_ACCESS_EXTENSION_POINT);
+		for (IConfigurationElement element : config) {
+			extensionProviders.add(new ExtensionQuickAccessProvider(element, context));
+		}
+
+		providers = new QuickAccessProvider[baseProviders.length + extensionProviders.size()];
+		System.arraycopy(baseProviders, 0, providers, 0, baseProviders.length);
+		for (int index = 0; index < extensionProviders.size(); index++)
+			providers[index + baseProviders.length] = extensionProviders.get(index);
 	}
 
 	/**
@@ -388,7 +437,7 @@ public abstract class QuickAccessContents {
 	 * @param extraEntries
 	 *            extra entries that will be added to the tabular visualization
 	 *            after computing matching entries, i.e. Search in Help
-	 * @return the array of lists (one per provider) contains the quick access
+	 * @return the array of lists (one per provider) containing the quick access
 	 *         entries that should be added to the table, possibly empty
 	 */
 	private List<QuickAccessEntry>[] computeMatchingEntries(String filter,
@@ -559,6 +608,9 @@ public abstract class QuickAccessContents {
 			resourceManager.dispose();
 			resourceManager = null;
 		}
+
+		Platform.getExtensionRegistry().removeRegistryChangeListener(registryChangeListener);
+
 	}
 
 	protected IDialogSettings getDialogSettings() {
@@ -887,6 +939,13 @@ public abstract class QuickAccessContents {
 		for (QuickAccessProvider provider : providers) {
 			provider.reset();
 		}
+	}
+
+	/**
+	 * @return
+	 */
+	public QuickAccessProvider[] getProviders() {
+		return providers;
 	}
 
 }
