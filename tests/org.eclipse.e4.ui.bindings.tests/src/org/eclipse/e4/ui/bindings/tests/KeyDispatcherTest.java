c8c3d8d1f9e8be2af26e3a25abd34bec36955fd7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 05:27:15 +0000

    First commited as before_30802_rev4

--- tests/org.eclipse.e4.ui.bindings.tests/src/org/eclipse/e4/ui/bindings/tests/KeyDispatcherTest.java
+++ tests/org.eclipse.e4.ui.bindings.tests/src/org/eclipse/e4/ui/bindings/tests/KeyDispatcherTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 440893
  *******************************************************************************/
 package org.eclipse.e4.ui.bindings.tests;
 
@@ -269,8 +268,7 @@ public class KeyDispatcherTest extends TestCase {
 	}
 
 	public void testSendKeyStroke() throws Exception {
-		@SuppressWarnings("cast")
-		KeyBindingDispatcher dispatcher = ContextInjectionFactory
+		KeyBindingDispatcher dispatcher = (KeyBindingDispatcher) ContextInjectionFactory
 				.make(KeyBindingDispatcher.class, workbenchContext);
 		final Listener listener = dispatcher.getKeyDownFilter();
 		display.addFilter(SWT.KeyDown, listener);
