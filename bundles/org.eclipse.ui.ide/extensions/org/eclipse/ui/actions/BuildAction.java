bf68c8864cc89691dafac4a296d4303099c7e1c6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 21:05:13 +0000

    First commited as before_58303_rev1

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/BuildAction.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/BuildAction.java
@@ -284,7 +284,7 @@ public class BuildAction extends WorkspaceAction {
 					// Backwards compatibility: check shouldPerformResourcePruning().
 					// Previously if this returned true, the full reference graph is built, otherwise just build the selected configurations
 					ResourcesPlugin.getWorkspace().build(configs, kind, shouldPerformResourcePruning(),
-							progress.split(1));
+							progress.newChild(1));
 				} catch (CoreException e) {
 					status = e.getStatus();
 				}
