0c31a883f960752f8564b0c3bffeacef90035f14
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 19:04:34 +0000

    First commited as before_53990_rev9

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/CleanDialog.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/CleanDialog.java
@@ -31,7 +31,9 @@ import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.window.IShellProvider;
@@ -295,10 +297,13 @@ public class CleanDialog extends MessageDialog {
         }
         //table is disabled to start because all button is selected
         projectNames.getTable().setEnabled(selectedButton.getSelection());
-        projectNames.addCheckStateListener(event -> {
-		    selection = projectNames.getCheckedElements();
-		    updateEnablement();
-		});
+        projectNames.addCheckStateListener(new ICheckStateListener() {
+            @Override
+			public void checkStateChanged(CheckStateChangedEvent event) {
+                selection = projectNames.getCheckedElements();
+                updateEnablement();
+            }
+        });
     }
 
     /**
