834388285ef86b047414205f0ea8d54da7e0a2bd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 23:05:23 +0000

    First commited as before_62588_rev3

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerRenderer.java
@@ -60,7 +60,6 @@ import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenuItem;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenuSeparator;
 import org.eclipse.e4.ui.model.application.ui.menu.MPopupMenu;
-import org.eclipse.e4.ui.workbench.IPresentationEngine;
 import org.eclipse.e4.ui.workbench.IResourceUtilities;
 import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.UIEvents.ElementContainer;
@@ -221,25 +220,10 @@ public class MenuManagerRenderer extends SWTPartRenderer {
 					if (manager == null) {
 						return;
 					}
-					boolean visible = menuModel.isVisible();
-					manager.setVisible(visible);
+					manager.setVisible(menuModel.isVisible());
 					if (manager.getParent() != null) {
 						manager.getParent().markDirty();
 						scheduleManagerUpdate(manager.getParent());
-					}
-					if (menuModel.getParent() == null) {
-						if (menuModel instanceof MPopupMenu) {
-							Object data = menuModel.getTransientData().get(IPresentationEngine.RENDERING_PARENT_KEY);
-							if (data instanceof Control) {
-								Menu menu = (Menu) menuModel.getWidget();
-								if (visible && menuModel.isToBeRendered() && menu != null && !menu.isDisposed()) {
-									((Control) data).setMenu(menu);
-								}
-								if (!visible) {
-									((Control) data).setMenu(null);
-								}
-							}
-						}
 					}
 				} else if (element instanceof MMenuElement) {
 					MMenuElement itemModel = (MMenuElement) element;
