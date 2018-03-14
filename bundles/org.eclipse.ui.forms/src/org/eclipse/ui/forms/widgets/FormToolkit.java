3a49dafbac6b0890937f5cf8f823d206f27e68c3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 20:34:56 +0000

    First commited as before_57345_rev3

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/widgets/FormToolkit.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/widgets/FormToolkit.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,9 +38,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Tree;
@@ -317,9 +315,9 @@ public class FormToolkit {
 	}
 
 	/**
-	 * Creats the composite that can server as a separator between various parts
-	 * of a form. Separator height should be controlled by setting the height
-	 * hint on the layout data for the composite.
+	 * Creates the composite that can server as a separator between various
+	 * parts of a form. Separator height should be controlled by setting the
+	 * height hint on the layout data for the composite.
 	 *
 	 * @param parent
 	 *            the separator parent
@@ -327,19 +325,15 @@ public class FormToolkit {
 	 */
 	public Composite createCompositeSeparator(Composite parent) {
 		final Composite composite = new Composite(parent, orientation);
-		composite.addListener(SWT.Paint, new Listener() {
-			@Override
-			public void handleEvent(Event e) {
-				if (composite.isDisposed())
-					return;
-				Rectangle bounds = composite.getBounds();
-				GC gc = e.gc;
-				gc.setForeground(colors.getColor(IFormColors.SEPARATOR));
-				if (colors.getBackground() != null)
-					gc.setBackground(colors.getBackground());
-				gc.fillGradientRectangle(0, 0, bounds.width, bounds.height,
-						false);
-			}
+		composite.addListener(SWT.Paint, e -> {
+			if (composite.isDisposed())
+				return;
+			Rectangle bounds = composite.getBounds();
+			GC gc = e.gc;
+			gc.setForeground(colors.getColor(IFormColors.SEPARATOR));
+			if (colors.getBackground() != null)
+				gc.setBackground(colors.getBackground());
+			gc.fillGradientRectangle(0, 0, bounds.width, bounds.height, false);
 		});
 		if (parent instanceof Section)
 			((Section) parent).setSeparatorControl(composite);
