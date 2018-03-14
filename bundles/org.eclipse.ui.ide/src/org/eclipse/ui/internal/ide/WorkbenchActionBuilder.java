371528acd45375e55a51d1e99dd7b4a91836d27e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 15:11:24 +0000

    First commited as after_46221_rev1

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/WorkbenchActionBuilder.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/WorkbenchActionBuilder.java
@@ -674,7 +674,12 @@ public final class WorkbenchActionBuilder extends ActionBarAdvisor {
 
 		// Workaround for bug 461311. Radio buttons in the main menu can cause
 		// Eclipse to crash on GTK.
-		if (!SWT.getPlatform().equals("gtk")) { //$NON-NLS-1$
+		String menuProxy = System.getenv("UBUNTU_MENUPROXY"); //$NON-NLS-1$
+
+		boolean radioButtonsMightCauseCrash = ((menuProxy == null) || !menuProxy.equals("0")) //$NON-NLS-1$
+				&& SWT.getPlatform().equals("gtk"); //$NON-NLS-1$
+
+		if (!radioButtonsMightCauseCrash) {
 			menu.add(ContributionItemFactory.OPEN_WINDOWS.create(getWindow()));
 		}
         return menu;
