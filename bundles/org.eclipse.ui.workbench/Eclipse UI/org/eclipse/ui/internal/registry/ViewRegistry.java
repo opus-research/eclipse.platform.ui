b1681cb338f3285d25da428b5161cd590ef9e3c5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 08:54:59 +0000

    First commited as before_36071_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/ViewRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/ViewRegistry.java
@@ -96,7 +96,7 @@ public class ViewRegistry implements IViewRegistry {
 				if (element.getName().equals(IWorkbenchRegistryConstants.TAG_VIEW)) {
 					createDescriptor(element, false);
 				}
-				if (element.getName().equals(IWorkbenchRegistryConstants.TAG_E4VIEW)) {
+				if (element.getName().equals("e4view")) { //$NON-NLS-1$
 					createDescriptor(element, true);
 				}
 			}
