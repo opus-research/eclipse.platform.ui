91f34d947757d495fad8f386a46902575a49f6a5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 20:08:18 +0000

    First commited as before_56418_rev5

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ObjectFilterTest.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ObjectFilterTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,10 +12,9 @@ package org.eclipse.ui.internal;
 
 import java.util.HashMap;
 import java.util.Iterator;
-
+import org.eclipse.core.runtime.Adapters;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.ui.IActionFilter;
-import org.eclipse.ui.internal.util.Util;
 
 /**
  * An ObjectFilterTest is used to read an object filter from XML,
@@ -85,7 +84,7 @@ public class ObjectFilterTest {
      */
     private boolean preciselyMatches(Object object) {
         // Get the action filter.
-        IActionFilter filter = Util.getAdapter(object, IActionFilter.class);
+        IActionFilter filter = Adapters.adapt(object, IActionFilter.class);
         if (filter == null) {
 			return false;
 		}
