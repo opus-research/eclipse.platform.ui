f1b61f1b69f81b055bbb5783adfa2ae2abe58ab9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 23:27:53 +0000

    First commited as after_63494_rev1

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/statushandlers/StatusHandlingTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/statushandlers/StatusHandlingTestSuite.java
@@ -11,6 +11,8 @@
 
 package org.eclipse.ui.tests.statushandlers;
 
+import org.junit.runner.RunWith;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -19,6 +21,7 @@ import junit.framework.TestSuite;
  *
  * @since 3.3
  */
+@RunWith(org.junit.runners.AllTests.class)
 public class StatusHandlingTestSuite extends TestSuite {
 
 	public StatusHandlingTestSuite() {
