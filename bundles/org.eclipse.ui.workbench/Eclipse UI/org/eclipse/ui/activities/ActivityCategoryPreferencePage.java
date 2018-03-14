8f1b6882b94835bd1897cb5709c012da90232734
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 02:25:12 +0000

    First commited as after_23575_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/activities/ActivityCategoryPreferencePage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/activities/ActivityCategoryPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 20014 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -71,7 +71,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
  * Activities preference page that primarily shows categories and can optionally
- * show an advanced dialog that allows fine-tune adjustmenet of activities. This
+ * show an advanced dialog that allows fine-tune adjustment of activities. This
  * page may be used by product developers to provide basic ability to tweak the
  * enabled activity set. You may provide certain strings to this class via
  * method #2 of {@link org.eclipse.core.runtime.IExecutableExtension}.
@@ -193,12 +193,7 @@ public final class ActivityCategoryPreferencePage extends PreferencePage impleme
                     PlatformUI.PLUGIN_ID, "icons/full/ovr16/lock_ovr.png"); //$NON-NLS-1$
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnImage(java.lang.Object,
-         *      int)
-         */
+		@Override
         public Image getColumnImage(Object element, int columnIndex) {
             ICategory category = (ICategory) element;
             ImageDescriptor descriptor = PlatformUI.getWorkbench()
@@ -240,9 +235,7 @@ public final class ActivityCategoryPreferencePage extends PreferencePage impleme
             return name;
         }   
        
-        /* (non-Javadoc)
-         * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnText(java.lang.Object, int)
-         */
+		@Override
         public String getColumnText(Object element, int columnIndex) {
         	return getText(element);
         }
@@ -253,11 +246,7 @@ public final class ActivityCategoryPreferencePage extends PreferencePage impleme
             manager.dispose();
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.eclipse.ui.activities.IActivityManagerListener#activityManagerChanged(org.eclipse.ui.activities.ActivityManagerEvent)
-         */
+		@Override
         public void activityManagerChanged(
                 ActivityManagerEvent activityManagerEvent) {
             if (activityManagerEvent.haveEnabledActivityIdsChanged()) {
@@ -269,32 +258,19 @@ public final class ActivityCategoryPreferencePage extends PreferencePage impleme
 
     private class CategoryContentProvider implements IStructuredContentProvider {
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
-         */
+		@Override
         public Object[] getElements(Object inputElement) {
             // convert to category objects
             return WorkbenchActivityHelper.resolveCategories(workingCopy,
                     (Set) inputElement);
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.eclipse.jface.viewers.IContentProvider#dispose()
-         */
+		@Override
         public void dispose() {
 
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer,
-         *      java.lang.Object, java.lang.Object)
-         */
+		@Override
         public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 
         }
@@ -524,12 +500,8 @@ public final class ActivityCategoryPreferencePage extends PreferencePage impleme
         categoryViewer
                 .addSelectionChangedListener(new ISelectionChangedListener() {
 
-                    /*
-                     * (non-Javadoc)
-                     * 
-                     * @see org.eclipse.jface.viewers.ISelectionChangedListener#selectionChanged(org.eclipse.jface.viewers.SelectionChangedEvent)
-                     */
-                    public void selectionChanged(SelectionChangedEvent event) {
+                    @Override
+					public void selectionChanged(SelectionChangedEvent event) {
                         ICategory element = (ICategory) ((IStructuredSelection) event
                                 .getSelection()).getFirstElement();
                         setDetails(element);
@@ -600,12 +572,8 @@ public final class ActivityCategoryPreferencePage extends PreferencePage impleme
         descriptionText.setText(""); //$NON-NLS-1$
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
-     */
-    public void init(IWorkbench workbench) {
+    @Override
+	public void init(IWorkbench workbench) {
         this.workbench = workbench;
         workingCopy = workbench.getActivitySupport().createWorkingCopy();
         setPreferenceStore(WorkbenchPlugin.getDefault().getPreferenceStore());
@@ -657,13 +625,8 @@ public final class ActivityCategoryPreferencePage extends PreferencePage impleme
         workingCopy.setEnabledActivityIds(defaultEnabled);
     }
     
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.core.runtime.IExecutableExtension#setInitializationData(org.eclipse.core.runtime.IConfigurationElement,
-     *      java.lang.String, java.lang.Object)
-     */
-    public void setInitializationData(IConfigurationElement config,
+    @Override
+	public void setInitializationData(IConfigurationElement config,
             String propertyName, Object data) {
         if (data instanceof Hashtable) {
             Hashtable table = (Hashtable)data;
