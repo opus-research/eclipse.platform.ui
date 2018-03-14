25c1aeebc3561ac15cf873cc82cabf0e0690422c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 06:30:41 +0000

    First commited as before_32703_rev2

--- tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/CssCoreTestSuite.java
+++ tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/tests/css/core/CssCoreTestSuite.java
@@ -7,27 +7,40 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Stefan Winkler <stefan@winklerweb.net> - Bug 430052
- *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 430468
+ *     Stefan Winkler <stefan@winklerweb.net> - Bug 430052 
  *******************************************************************************/
 package org.eclipse.e4.ui.tests.css.core;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
 import org.eclipse.e4.ui.tests.css.core.parser.CascadeTest;
 import org.eclipse.e4.ui.tests.css.core.parser.FontFaceRulesTest;
 import org.eclipse.e4.ui.tests.css.core.parser.ImportTest;
-import org.eclipse.e4.ui.tests.css.core.parser.MediaRulesTest;
 import org.eclipse.e4.ui.tests.css.core.parser.RGBColorImplTest;
 import org.eclipse.e4.ui.tests.css.core.parser.SelectorTest;
 import org.eclipse.e4.ui.tests.css.core.parser.StyleRuleTest;
 import org.eclipse.e4.ui.tests.css.core.parser.ValueTest;
 import org.eclipse.e4.ui.tests.css.core.parser.ViewCSSTest;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({ CascadeTest.class, FontFaceRulesTest.class, MediaRulesTest.class, RGBColorImplTest.class,
-	StyleRuleTest.class, ViewCSSTest.class, ValueTest.class, SelectorTest.class, CSSEngineTest.class,
-	ImportTest.class })
-public class CssCoreTestSuite {
+public class CssCoreTestSuite extends TestSuite {
+
+	public static Test suite() {
+		return new CssCoreTestSuite();
+	}
+
+	public CssCoreTestSuite() {
+		// $JUnit-BEGIN$
+		addTestSuite(CascadeTest.class);
+		addTestSuite(FontFaceRulesTest.class);
+//		addTestSuite(MediaRulesTest.class);
+		addTestSuite(RGBColorImplTest.class);
+		addTestSuite(StyleRuleTest.class);
+		addTestSuite(ViewCSSTest.class);
+		addTestSuite(ValueTest.class);
+		addTestSuite(SelectorTest.class);
+		addTestSuite(CSSEngineTest.class);
+		addTestSuite(ImportTest.class);
+		// $JUnit-END$
+	}
 }
