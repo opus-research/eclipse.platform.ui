64b179bb0c0c2a51f6331906876dec34fe7b985a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 23:28:54 +0000

    First commited as before_63527_rev1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerHideProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerHideProcessor.java
@@ -116,6 +116,8 @@ public class MenuManagerHideProcessor implements IMenuListener2 {
 								item.setVisible(false);
 							}
 						}
+						currentMenuElement.getTransientData()
+								.remove(MenuManagerShowProcessor.DYNAMIC_ELEMENT_STORAGE_KEY);
 					}
 				}
 			});
