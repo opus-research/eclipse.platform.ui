a9e4e2bcb9b6408439fb723ff9475acc96b55b8a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 17:28:06 +0000

    First commited as before_50118_rev1

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/RegistryCSSPropertyHandlerProvider.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/RegistryCSSPropertyHandlerProvider.java
@@ -152,6 +152,16 @@ public class RegistryCSSPropertyHandlerProvider extends
 		return handlers;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.eclipse.e4.ui.css.core.dom.properties.providers.
+	 * AbstractCSSPropertyHandlerProvider
+	 * #getDefaultCSSStyleDeclaration(org.eclipse
+	 * .e4.ui.css.core.engine.CSSEngine,
+	 * org.eclipse.e4.ui.css.core.dom.CSSStylableElement,
+	 * org.w3c.dom.css.CSSStyleDeclaration)
+	 */
 	@Override
 	protected CSSStyleDeclaration getDefaultCSSStyleDeclaration(
 			CSSEngine engine, CSSStylableElement stylableElement,
