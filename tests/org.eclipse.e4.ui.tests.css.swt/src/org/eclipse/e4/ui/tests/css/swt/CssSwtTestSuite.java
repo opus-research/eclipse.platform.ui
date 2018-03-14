ad175e21f6a0c46be2e35ca0f710013400491fa4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:19:25 +0000

    First commited as before_24358_rev2

--- tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/swt/CssSwtTestSuite.java
+++ tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/tests/css/swt/CssSwtTestSuite.java
@@ -6,7 +6,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Stefan Winkler <stefan@winklerweb.net> - Bug 419482
  *******************************************************************************/
 package org.eclipse.e4.ui.tests.css.swt;
 
@@ -51,6 +50,7 @@ public class CssSwtTestSuite extends TestSuite {
 		//		addTestSuite(ShellActiveTest.class);  //TODO see bug #273582
 		addTestSuite(GradientTest.class);
 		addTestSuite(MarginTest.class);
+		addTestSuite(InnerClassElementTest.class);
 
 		// text-transform tests
 		addTestSuite(ButtonTextTransformTest.class);
@@ -61,6 +61,5 @@ public class CssSwtTestSuite extends TestSuite {
 		addTestSuite(DescendentTest.class);
 
 		addTestSuite(ThemeTest.class);
-		addTestSuite(Bug419482Test.class);
 	}
 }
