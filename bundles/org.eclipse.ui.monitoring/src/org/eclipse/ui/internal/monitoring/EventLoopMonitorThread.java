7f01a748deae429909ed9b707d9ef09c8d7de144
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:11:20 +0000

    First commited as before_36455_rev1

--- bundles/org.eclipse.ui.monitoring/src/org/eclipse/ui/internal/monitoring/EventLoopMonitorThread.java
+++ bundles/org.eclipse.ui.monitoring/src/org/eclipse/ui/internal/monitoring/EventLoopMonitorThread.java
@@ -73,13 +73,11 @@ public class EventLoopMonitorThread extends Thread {
 		public int maxStackSamples;
 		/** If true, log freeze events to the Eclipse error log on the local machine. */
 		public boolean logToErrorLog;
-		/** @see org.eclipse.ui.monitoring.PreferenceConstants#UI_THREAD_FILTER */
-		public String uiThreadFilter;
-		/** @see org.eclipse.ui.monitoring.PreferenceConstants#NONINTERESTING_THREAD_FILTER */
-		public String noninterestingThreadFilter;
+		/** Contains the list of fully qualified methods to filter out. */
+		public String filterTraces;
 
 		/**
-		 * Checks if the values of parameters for UI responsiveness monitoring are valid.
+		 * Checks if parameters for plug-in are valid before startup.
 		 *
 		 * @throws IllegalArgumentException if the parameter values are invalid or inconsistent.
 		 */
@@ -248,7 +246,7 @@ public class EventLoopMonitorThread extends Thread {
 
 			if (!haveAlreadyLoggedPossibleDeadlock && lastActive > 0 &&
 					totalDuration > deadlockThreshold &&
-					uiThreadFilter.shouldLogEvent(stackSamples, numSamples, uiThreadId)) {
+					filterHandler.shouldLogEvent(stackSamples, numSamples, uiThreadId)) {
 				stackSamples = Arrays.copyOf(stackSamples, numSamples);
 				logEvent(new UiFreezeEvent(lastActive, totalDuration,
 						Arrays.copyOf(stackSamples, numSamples), true));
@@ -357,8 +355,7 @@ public class EventLoopMonitorThread extends Thread {
 			new ArrayList<IUiFreezeEventLogger>();
 	private DefaultUiFreezeEventLogger defaultLogger;
 	private final Display display;
-	private final FilterHandler uiThreadFilter;
-	private final FilterHandler noninterestingThreadFilter;
+	private final FilterHandler filterHandler;
 	private final int longEventErrorThreshold;
 	private final long sampleInterval;
 	private final long allThreadsSampleInterval;
@@ -403,8 +400,7 @@ public class EventLoopMonitorThread extends Thread {
 		allThreadsSampleInterval = longEventErrorThreshold * 2 / 3;
 		deadlockThreshold = args.deadlockThreshold;
 		logToErrorLog = args.logToErrorLog;
-		uiThreadFilter = new FilterHandler(args.uiThreadFilter);
-		noninterestingThreadFilter = new FilterHandler(args.noninterestingThreadFilter);
+		filterHandler = new FilterHandler(args.filterTraces);
 		sleepMonitor = new Object();
 	}
 
@@ -614,7 +610,7 @@ public class EventLoopMonitorThread extends Thread {
 						numSamples = maxLoggedStackSamples;
 					}
 
-					if (uiThreadFilter.shouldLogEvent(stackSamples, numSamples, uiThreadId)) {
+					if (filterHandler.shouldLogEvent(stackSamples, numSamples, uiThreadId)) {
 						logEvent(new UiFreezeEvent(eventSnapshot.start, eventSnapshot.duration,
 								Arrays.copyOf(stackSamples, numSamples), false));
 					}
@@ -629,45 +625,33 @@ public class EventLoopMonitorThread extends Thread {
 	}
 
 	private ThreadInfo[] captureThreadStacks(boolean dumpAllThreads) {
+		ThreadInfo[] threadStacks;
 		if (dumpAllThreads) {
-			ThreadInfo[] threadStacks =
+			ThreadInfo[] rawThreadStacks =
 					threadMXBean.dumpAllThreads(dumpLockedMonitors, dumpLockedSynchronizers);
 			// Remove the info for the monitoring thread.
+			threadStacks = new ThreadInfo[rawThreadStacks.length - 1];
 			int index = 0;
-			for (int i = 0; i < threadStacks.length; i++) {
-				ThreadInfo thread = threadStacks[i];
+
+			for (int i = 0; i < rawThreadStacks.length; i++) {
+				ThreadInfo thread = rawThreadStacks[i];
 				long threadId = thread.getThreadId();
 				// Skip the stack trace of the event loop monitoring thread.
 				if (threadId != monitoringThreadId) {
-					if (threadId == uiThreadId) {
-						// Swap the UI thread to first slot in the array if it is not there already.
-						if (index != 0) {
-							thread = threadStacks[0];
-							threadStacks[0] = threadStacks[i];
-						}
-					} else if (!isInteresting(thread)) {
-						continue; // Skip the non-interesting thread.
+					if (threadId == uiThreadId && i != 0) {
+						// Swap the UI thread to first slot in the array if it is not
+						// there already.
+						thread = threadStacks[0];
+						threadStacks[0] = rawThreadStacks[i];
 					}
 					threadStacks[index++] = thread;
 				}
 			}
-			return Arrays.copyOf(threadStacks, index);
 		} else {
-			return new ThreadInfo[] { threadMXBean.getThreadInfo(uiThreadId, Integer.MAX_VALUE) };
+			threadStacks =
+					new ThreadInfo[] { threadMXBean.getThreadInfo(uiThreadId, Integer.MAX_VALUE) };
 		}
-	}
-
-	/**
-	 * A thread is considered interesting if its stack trace includes at least one frame not
-	 * matching any of the methods in {@link #noninterestingThreadFilter}.
-	 */
-	private boolean isInteresting(ThreadInfo thread) {
-		for (StackTraceElement element : thread.getStackTrace()) {
-			if (!noninterestingThreadFilter.matchesFilter(element)) {
-				return true;
-			}
-		}
-		return false;
+		return threadStacks;
 	}
 
 	private static Display getDisplay() throws IllegalStateException {
