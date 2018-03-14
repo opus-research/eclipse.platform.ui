a28a20045e3e0d922160a5d2d61a1de7097e82c2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 06:51:10 +0000

    First commited as before_33526_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/themes/ThemesExtension.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/themes/ThemesExtension.java
@@ -13,6 +13,7 @@ package org.eclipse.ui.internal.themes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 import org.eclipse.e4.ui.internal.css.swt.definition.IThemeElementDefinitionOverridable;
 import org.eclipse.e4.ui.internal.css.swt.definition.IThemesExtension;
 import org.eclipse.ui.internal.WorkbenchPlugin;
@@ -24,6 +25,7 @@ import org.eclipse.ui.internal.WorkbenchPlugin;
 public class ThemesExtension implements IThemesExtension {
 	public final static String DEFAULT_CATEGORY_ID = "org.eclipse.ui.themes.CssTheme"; //$NON-NLS-1$
 
+	private String description;
 
 	private List<IThemeElementDefinitionOverridable<?>> definitions = new ArrayList<IThemeElementDefinitionOverridable<?>>();
 
@@ -36,7 +38,8 @@ public class ThemesExtension implements IThemesExtension {
 	@Override
 	public void addFontDefinition(String symbolicName) {
 		FontDefinition definition = new FontDefinition(formatDefaultName(FontDefinition.class,
-				symbolicName), symbolicName, null, null, DEFAULT_CATEGORY_ID, true, null);
+				symbolicName), symbolicName, null, null, DEFAULT_CATEGORY_ID, true,
+				getDefaultDescription());
 		definition.appendState(ThemeElementDefinition.State.ADDED_BY_CSS);
 		definitions.add(definition);
 	}
@@ -50,8 +53,8 @@ public class ThemesExtension implements IThemesExtension {
 	@Override
 	public void addColorDefinition(String symbolicName) {
 		ColorDefinition definition = new ColorDefinition(formatDefaultName(ColorDefinition.class,
-				symbolicName), symbolicName, null, null, DEFAULT_CATEGORY_ID, true, null,
-				getPluginId());
+				symbolicName), symbolicName, null, null, DEFAULT_CATEGORY_ID, true,
+				getDefaultDescription(), getPluginId());
 		definition.appendState(ThemeElementDefinition.State.ADDED_BY_CSS);
 		definitions.add(definition);
 	}
@@ -66,5 +69,13 @@ public class ThemesExtension implements IThemesExtension {
 
 	private String formatDefaultName(Class<?> cls, String symbolicName) {
 		return String.format("%s #%s", cls.getSimpleName(), symbolicName); //$NON-NLS-1$
+	}
+
+	public String getDefaultDescription() {
+		if (description == null) {
+			ResourceBundle resourceBundle = ResourceBundle.getBundle(Theme.class.getName());
+			description = resourceBundle.getString("Added.by.css.label"); //$NON-NLS-1$
+		}
+		return description;
 	}
 }
