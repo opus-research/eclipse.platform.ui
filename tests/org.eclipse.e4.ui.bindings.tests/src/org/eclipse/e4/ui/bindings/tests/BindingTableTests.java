77fa67f692fe98c9e9f4404b15ffe9b83e65d0e7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 06:40:50 +0000

    First commited as after_32997_rev3

--- tests/org.eclipse.e4.ui.bindings.tests/src/org/eclipse/e4/ui/bindings/tests/BindingTableTests.java
+++ tests/org.eclipse.e4.ui.bindings.tests/src/org/eclipse/e4/ui/bindings/tests/BindingTableTests.java
@@ -8,6 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 440893
+ *     Simon Scholz <simon.scholz@vogella.com> - Bug 436344
  *******************************************************************************/
 package org.eclipse.e4.ui.bindings.tests;
 
@@ -109,6 +110,7 @@ public class BindingTableTests extends TestCase {
 						BINDINGS[i + 2], null, null, null, Binding.SYSTEM));
 			}
 		}
+		System.out.println("Made setup");
 	}
 
 	public void testOneTable() throws Exception {
