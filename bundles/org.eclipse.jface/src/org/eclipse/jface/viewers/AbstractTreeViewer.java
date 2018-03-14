6b4c2ab2b23b61a2bbb303e5ca4734fa981eac2b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 16:35:17 +0000

    First commited as before_47041_rev2

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/AbstractTreeViewer.java
@@ -2367,6 +2367,17 @@ public abstract class AbstractTreeViewer extends ColumnViewer {
 		expandToLevel = level;
 	}
 
+	/**
+	 * The <code>AbstractTreeViewer</code> implementation of this method
+	 * checks to ensure that the content provider is an
+	 * <code>ITreeContentProvider</code>.
+	 */
+	@Override
+	public void setContentProvider(IContentProvider provider) {
+		// the actual check is in assertContentProviderType
+		super.setContentProvider(provider);
+	}
+
 	@Override
 	protected void assertContentProviderType(IContentProvider provider) {
 		Assert.isTrue(provider instanceof ITreeContentProvider
