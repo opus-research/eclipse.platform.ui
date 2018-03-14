fe8b49343b284388bdaed073d62e4a6f1ba84d7a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 23:34:57 +0000

    First commited as before_63696_rev2

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/resources/AbstractResourcesRegistry.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/resources/AbstractResourcesRegistry.java
@@ -60,7 +60,7 @@ public abstract class AbstractResourcesRegistry implements IResourcesRegistry {
 				return resourcesMap;
 			}
 		}
-		return Collections.<Object, Object> emptyMap();
+		return Collections.EMPTY_MAP;
 	}
 
 	@Override
