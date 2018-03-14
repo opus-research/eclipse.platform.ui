30b56d4a3026b5a6218e60af38b632343d6be07b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:27:26 +0000

    First commited as after_26307_rev3

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/fieldassist/ComboFieldAssistWindow.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/fieldassist/ComboFieldAssistWindow.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Remy Chi Jian Suen and others.
+ * Copyright (c) 2009, 2014 Remy Chi Jian Suen and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,8 @@
  * Contributors:
  *     Remy Chi Jian Suen <remy.suen@gmail.com> - initial API and implementation
  *     IBM - ongoing development
-******************************************************************************/
+ *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 433608
+ ******************************************************************************/
 package org.eclipse.jface.tests.fieldassist;
 
 import org.eclipse.jface.fieldassist.ComboContentAdapter;
@@ -28,6 +29,11 @@ public class ComboFieldAssistWindow extends AbstractFieldAssistWindow {
 	@Override
 	protected Control createFieldAssistControl(Composite parent) {
 		return new Combo(parent, SWT.DROP_DOWN);
+	}
+
+	@Override
+	protected void createExtraControls(Composite parent) {
+		// Default is to do nothing
 	}
 
 }
