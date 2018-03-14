14e8d5a571bf18bf7c4e1fbfe0ba37762e5b595c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 20:54:47 +0000

    First commited as after_58184_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/PopupDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/PopupDialog.java
@@ -581,15 +581,17 @@ public class PopupDialog extends Window {
 
 		shell.addListener(SWT.Deactivate, event -> {
 			/*
-			 * Close if we are deactivating and have no child shells. If we
-			 * have child shells, we are deactivating due to their opening.
-			 * On X, we receive this when a menu child (such as the system
-			 * menu) of the shell opens, but I have not found a way to
-			 * distinguish that case here. Hence bug #113577 still exists.
+			 * Close if we are deactivating and have no child shells. If we have
+			 * child shells, we are deactivating due to their opening. On X, we
+			 * receive this when a menu child (such as the system menu) of the
+			 * shell opens, but I have not found a way to distinguish that case
+			 * here. Hence bug #113577 still exists.
 			 */
 			if (listenToDeactivate && event.widget == getShell()
 					&& getShell().getShells().length == 0) {
-				asyncClose();
+				if (!Util.isGtk()) {
+					asyncClose();
+				}
 			} else {
 				/*
 				 * We typically ignore deactivates to work around
@@ -963,6 +965,7 @@ public class PopupDialog extends Window {
 
 		Tracker tracker = new Tracker(shell.getDisplay(), style);
 		tracker.setStippled(true);
+		// tracker.setStippled(false);
 		Rectangle[] r = new Rectangle[] { shell.getBounds() };
 		tracker.setRectangles(r);
 
@@ -974,9 +977,9 @@ public class PopupDialog extends Window {
 			if (!shell.isDisposed()) {
 				shell.setBounds(tracker.getRectangles()[0]);
 			}
-		}
-		tracker.dispose();
-		listenToDeactivate = oldListenToDeactivate;
+		 }
+		 tracker.dispose();
+		 listenToDeactivate = oldListenToDeactivate;
 	}
 
 	/**
