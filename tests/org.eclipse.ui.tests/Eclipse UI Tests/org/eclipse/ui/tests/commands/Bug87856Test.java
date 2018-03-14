bcbefc19222cb0967d426c07dbb2cd398a733840
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 06:41:19 +0000

    First commited as before_33045_rev1

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/commands/Bug87856Test.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/commands/Bug87856Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2014 IBM Corporation and others.
+ * Copyright (c) 2005, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 433603
  *******************************************************************************/
 
 package org.eclipse.ui.tests.commands;
@@ -59,7 +58,8 @@ public class Bug87856Test extends UITestCase {
 		IHandler handler = new AbstractHandler() {
 
 			@Override
-			public Object execute(Map parameterValuesByName) {
+			public Object execute(Map parameterValuesByName)
+					throws org.eclipse.ui.commands.ExecutionException {
 				// Do nothing
 				return null;
 			}
