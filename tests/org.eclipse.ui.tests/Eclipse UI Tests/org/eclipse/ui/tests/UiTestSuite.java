ecd842b880553c44cc4b9bb99cb134b69229e3a8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 23:27:17 +0000

    First commited as before_63486_rev3

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/UiTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/UiTestSuite.java
@@ -12,7 +12,6 @@
 package org.eclipse.ui.tests;
 
 import org.eclipse.ui.tests.activities.ActivitiesTestSuite;
-import org.eclipse.ui.tests.adaptable.AdaptableTestSuite;
 import org.eclipse.ui.tests.api.ApiTestSuite;
 import org.eclipse.ui.tests.api.StartupTest;
 import org.eclipse.ui.tests.datatransfer.DataTransferTestSuite;
@@ -51,14 +50,14 @@ import org.junit.runners.Suite;
 	FieldAssistTestSuite.class,
 	ServicesTestSuite.class,
 	ProgressTestSuite.class,
-	PropertySheetTestSuite.class,
-	AdaptableTestSuite.class
+	PropertySheetTestSuite.class
 })
 public class UiTestSuite {
 
 	// Not enabled tests:
 	// QuickAccessTestSuite.class,
 	// InternalTestSuite.class,
+	// AdaptableTestSuite.class,
 	// ZoomTestSuite.class,
 	// DynamicPluginsTestSuite.class,
 	// MultiPageEditorTestSuite.class,
