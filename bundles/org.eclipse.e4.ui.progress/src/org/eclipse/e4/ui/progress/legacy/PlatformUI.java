1e9d0edb0656563d884b262fc45dcb8c7c3307ee
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 01:02:35 +0000

    First commited as after_21240_rev5

---
+++ bundles/org.eclipse.e4.ui.progress/src/org/eclipse/e4/ui/progress/legacy/PlatformUI.java
@@ -0,0 +1,15 @@
+package org.eclipse.e4.ui.progress.legacy;
+
+//TODO E4
+public class PlatformUI {
+
+	public static boolean isWorkbenchRunning() {
+
+	    return true;
+    }
+
+	public static boolean isWorkbenchStarting() {
+	    return false;
+    }
+
+}
