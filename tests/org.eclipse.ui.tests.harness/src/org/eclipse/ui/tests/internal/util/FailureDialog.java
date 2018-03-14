06efe9c1f95520ece7325036e0a4c4dbcef3e1cc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 07:16:16 +0000

    First commited as before_34400_rev3

--- tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/internal/util/FailureDialog.java
+++ tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/internal/util/FailureDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 444070
  *******************************************************************************/
 package org.eclipse.ui.tests.internal.util;
 
@@ -27,75 +26,96 @@ import org.eclipse.swt.widgets.Text;
  * the failure of a test.
  */
 public class FailureDialog extends Dialog {
-	private Text _text;
+    private Text _text;
 
-	private String _log;
+    private String _log;
 
-	private int SIZING_TEXT_WIDTH = 400;
+    private int SIZING_TEXT_WIDTH = 400;
 
-	private int SIZING_TEXT_HEIGHT = 200;
+    private int SIZING_TEXT_HEIGHT = 200;
 
-	/**
-	 * Constructor for FailureDialog
-	 */
-	public FailureDialog(Shell parentShell) {
-		super(parentShell);
-	}
+    /**
+     * Constructor for FailureDialog
+     */
+    public FailureDialog(Shell parentShell) {
+        super(parentShell);
+    }
 
-	@Override
-	protected void configureShell(Shell newShell) {
-		super.configureShell(newShell);
-		newShell.setText("Dialog Test Failed");
-	}
+    /* (non-Javadoc)
+     * Method declared on Window.
+     */
+    protected void configureShell(Shell newShell) {
+        super.configureShell(newShell);
+        newShell.setText("Dialog Test Failed");
+    }
 
-	@Override
-	protected void createButtonsForButtonBar(Composite parent) {
-		createButton(parent, IDialogConstants.OK_ID, "&OK", true);
-		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
-	}
+    /* (non-Javadoc)
+     * Method declared on Dialog.
+     */
+    protected void createButtonsForButtonBar(Composite parent) {
+        createButton(parent, IDialogConstants.OK_ID, "&OK", true);
+        createButton(parent, IDialogConstants.CANCEL_ID,
+                IDialogConstants.CANCEL_LABEL, false);
+    }
 
-	@Override
-	protected Control createDialogArea(Composite parent) {
-		// page group
-		Composite composite = (Composite) super.createDialogArea(parent);
-		composite.setSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+    /* (non-Javadoc)
+     * Method declared on Dialog.
+     */
+    protected Control createDialogArea(Composite parent) {
+        // page group
+        Composite composite = (Composite) super.createDialogArea(parent);
+        composite.setSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 
-		Label label = new Label(composite, SWT.WRAP);
-		label.setText("&Enter a note regarding the failure:");
+        Label label = new Label(composite, SWT.WRAP);
+        label.setText("&Enter a note regarding the failure:");
 
-		_text = new Text(composite, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		_text.setFont(JFaceResources.getFontRegistry().get(JFaceResources.TEXT_FONT));
-		GridData data = new GridData(GridData.FILL_BOTH);
-		data.widthHint = SIZING_TEXT_WIDTH;
-		data.heightHint = SIZING_TEXT_HEIGHT;
-		_text.setLayoutData(data);
+        _text = new Text(composite, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL
+                | SWT.V_SCROLL);
+        _text.setFont(JFaceResources.getFontRegistry().get(
+                JFaceResources.TEXT_FONT));
+        GridData data = new GridData(GridData.FILL_BOTH);
+        data.widthHint = SIZING_TEXT_WIDTH;
+        data.heightHint = SIZING_TEXT_HEIGHT;
+        _text.setLayoutData(data);
 
-		return composite;
-	}
+        return composite;
+    }
 
-	@Override
-	protected void okPressed() {
-		_log = _text.getText();
-		super.okPressed();
-	}
+    /* (non-Javadoc)
+     * Method declared on Dialog.
+     */
+    protected void okPressed() {
+        _log = _text.getText();
+        super.okPressed();
+    }
 
-	/*
-	 * @return String the text contained in the input area of the dialog.
-	 */
-	String getText() {
-		return (_log == null) ? "Empty entry." : _log;
-	}
+    /*
+     * @return String the text contained in the input area of
+     * the dialog.
+     */
+    String getText() {
+        if (_log == null) {
+            return "Empty entry.";
+        } else {
+            return _log;
+        }
+    }
 
-	/*
-	 * Sets the text of the input area. This should only be called to set the
-	 * initial text so only call before invoking open().
-	 */
-	void setText(String text) {
-		_text.setText(text);
-	}
+    /*
+     * Sets the text of the input area.  This should only be
+     * called to set the initial text so only call before invoking
+     * open().
+     */
+    void setText(String text) {
+        _text.setText(text);
+    }
 
-	@Override
-	public String toString() {
-		return getText();
-	}
+    /*
+     * Returns a string representation of this class which
+     * the text contained in the input area of the dialog.
+     */
+    public String toString() {
+        return getText();
+    }
 }
+
