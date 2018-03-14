5624b660547240a6583d0fe5790b359e2033d374
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:14:23 +0000

    First commited as after_36718_rev1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/HandledContributionItem.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/HandledContributionItem.java
@@ -11,7 +11,7 @@
  *     Snjezana Peco <snjezana.peco@redhat.com> - Memory leaks in Juno when opening and closing XML Editor - http://bugs.eclipse.org/397909
  *     Marco Descher <marco@descher.at> - Bug 397677
  *     Dmitry Spiridenok - Bug 429756
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 445723
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 445723, 450863
  ******************************************************************************/
 package org.eclipse.e4.ui.workbench.renderers.swt;
 
@@ -146,7 +146,7 @@ public class HandledContributionItem extends ContributionItem {
 
 	private ISWTResourceUtilities resUtils = null;
 
-	private IStateListener stateListener = new IStateListener() {
+	private final IStateListener stateListener = new IStateListener() {
 		@Override
 		public void handleStateChange(State state, Object oldValue) {
 			updateState();
@@ -197,7 +197,7 @@ public class HandledContributionItem extends ContributionItem {
 		SafeRunner.run(getUpdateRunner());
 	}
 
-	private IMenuListener menuListener = new IMenuListener() {
+	private final IMenuListener menuListener = new IMenuListener() {
 		@Override
 		public void menuAboutToShow(IMenuManager manager) {
 			update(null);
@@ -232,6 +232,12 @@ public class HandledContributionItem extends ContributionItem {
 	private void generateCommand() {
 		if (model.getCommand() != null && model.getWbCommand() == null) {
 			String cmdId = model.getCommand().getElementId();
+			if (cmdId == null) {
+				Activator.log(IStatus.ERROR, "Unable to generate parameterized command for " + model //$NON-NLS-1$
+						+ ". ElementId is not allowed to be null."); //$NON-NLS-1$
+				return;
+			}
+
 			List<MParameter> modelParms = model.getParameters();
 			Map<String, Object> parameters = new HashMap<String, Object>(4);
 			for (MParameter mParm : modelParms) {
