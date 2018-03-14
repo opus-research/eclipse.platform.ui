c30b6169d9c0c4a4a9c773397d1f71c56ddd8747
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 17:34:57 +0000

    First commited as before_50299_rev14

--- bundles/org.eclipse.core.databinding.observable/src/org/eclipse/core/databinding/observable/set/ComputedSet.java
+++ bundles/org.eclipse.core.databinding.observable/src/org/eclipse/core/databinding/observable/set/ComputedSet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2015 Matthew Hall and others.
+ * Copyright (c) 2008, 2009 Matthew Hall and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,7 +9,6 @@
  *     Matthew Hall - initial API and implementation (bug 237703)
  *     Matthew Hall - bug 274081
  *     Abel Hegedus - bug 414297
- *     Stefan Xenos <sxenos@gmail.com> - Bug 335792
  *******************************************************************************/
 package org.eclipse.core.databinding.observable.set;
 
@@ -68,13 +67,10 @@ import org.eclipse.core.databinding.observable.value.IObservableValue;
  * System.out.println(primes); // =&gt; &quot;[2, 3, 5, 7, 11, 13, 17, 19]&quot;
  * </pre>
  *
- * @param <E>
- *            the type of the elements in this set
- *
  * @since 1.2
  */
-public abstract class ComputedSet<E> extends AbstractObservableSet<E> {
-	private Set<E> cachedSet = new HashSet<>();
+public abstract class ComputedSet extends AbstractObservableSet {
+	private Set cachedSet = new HashSet();
 
 	private boolean dirty = true;
 	private boolean stale = false;
@@ -178,24 +174,24 @@ public abstract class ComputedSet<E> extends AbstractObservableSet<E> {
 		return doGetSet().size();
 	}
 
-	private final Set<E> getSet() {
+	private final Set getSet() {
 		getterCalled();
 		return doGetSet();
 	}
 
 	@Override
-	protected Set<E> getWrappedSet() {
+	protected Set getWrappedSet() {
 		return doGetSet();
 	}
 
-	final Set<E> doGetSet() {
+	final Set doGetSet() {
 		if (dirty) {
 			// This line will do the following:
 			// - Run the calculate method
 			// - While doing so, add any observable that is touched to the
 			// dependencies list
 			IObservable[] newDependencies = ObservableTracker.runAndMonitor(
-privateInterface, privateInterface, null);
+					privateInterface, privateInterface, null);
 
 			// If any dependencies are stale, a stale event will be fired here
 			// even if we were already stale before recomputing. This is in case
@@ -230,7 +226,7 @@ privateInterface, privateInterface, null);
 	 *
 	 * @return the object's set.
 	 */
-	protected abstract Set<E> calculate();
+	protected abstract Set calculate();
 
 	private void makeDirty() {
 		if (!dirty) {
@@ -240,29 +236,29 @@ privateInterface, privateInterface, null);
 			// bug 414297: moved before makeStale(), as cachedSet may be
 			// overwritten
 			// in makeStale() if a listener calls isStale()
-			final Set<E> oldSet = new HashSet<>(cachedSet);
+			final Set oldSet = new HashSet(cachedSet);
 			makeStale();
 
 			stopListening();
 
 			// Fire the "dirty" event. This implementation recomputes the new
 			// set lazily.
-			fireSetChange(new SetDiff<E>() {
-				SetDiff<E> delegate;
+			fireSetChange(new SetDiff() {
+				SetDiff delegate;
 
-				private SetDiff<E> getDelegate() {
+				private SetDiff getDelegate() {
 					if (delegate == null)
 						delegate = Diffs.computeSetDiff(oldSet, getSet());
 					return delegate;
 				}
 
 				@Override
-				public Set<E> getAdditions() {
+				public Set getAdditions() {
 					return getDelegate().getAdditions();
 				}
 
 				@Override
-				public Set<E> getRemovals() {
+				public Set getRemovals() {
 					return getDelegate().getRemovals();
 				}
 			});
@@ -309,8 +305,7 @@ privateInterface, privateInterface, null);
 	}
 
 	@Override
-	public synchronized void addSetChangeListener(
-			ISetChangeListener<? super E> listener) {
+	public synchronized void addSetChangeListener(ISetChangeListener listener) {
 		super.addSetChangeListener(listener);
 		// If somebody is listening, we need to make sure we attach our own
 		// listeners
