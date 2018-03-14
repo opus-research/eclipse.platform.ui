b22fb699615709e2fc9f01f076a99c05e069af0b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 16:02:34 +0000

    First commited as after_46964_rev3

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/cleanupaddon/CleanupAddon.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/cleanupaddon/CleanupAddon.java
@@ -23,6 +23,7 @@ import org.eclipse.e4.ui.model.application.ui.advanced.MArea;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspectiveStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimBar;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
@@ -138,25 +139,38 @@ public class CleanupAddon {
 		}
 	}
 
+	/**
+	 * Returns true if and only if the given element should make itself visible
+	 * when its first child becomes visible and make itself invisible whenever
+	 * its last child becomes invisible. Defaults to false for unknown element
+	 * types
+	 */
+	private static boolean shouldReactToChildVisibilityChanges(MUIElement theElement) {
+		return (theElement instanceof MPartSashContainer || theElement instanceof MPartStack);
+	}
+
 	@Inject
 	@Optional
 	private void subscribeVisibilityChanged(
 			@UIEventTopic(UIEvents.UIElement.TOPIC_VISIBLE) Event event) {
 		MUIElement changedObj = (MUIElement) event.getProperty(UIEvents.EventTags.ELEMENT);
-		if (changedObj instanceof MTrimBar || ((Object) changedObj.getParent()) instanceof MToolBar)
+		if (changedObj instanceof MTrimBar || (changedObj.getParent()) instanceof MToolBar)
 			return;
 
 		if (changedObj.getWidget() instanceof Shell) {
 			((Shell) changedObj.getWidget()).setVisible(changedObj.isVisible());
 		} else if (changedObj.getWidget() instanceof Rectangle) {
+			MElementContainer<MUIElement> parent = changedObj.getParent();
+			if (!shouldReactToChildVisibilityChanges(parent)) {
+				return;
+			}
+
 			if (changedObj.isVisible()) {
 				// Make all the parents visible
-				MUIElement parent = changedObj.getParent();
 				if (!parent.isVisible())
 					parent.setVisible(true);
 			} else {
 				// If there are no more 'visible' children then make the parent go away too
-				MElementContainer<MUIElement> parent = changedObj.getParent();
 				boolean makeInvisible = true;
 				for (MUIElement kid : parent.getChildren()) {
 					if (kid.isToBeRendered() && kid.isVisible()) {
@@ -170,9 +184,10 @@ public class CleanupAddon {
 		} else if (changedObj.getWidget() instanceof Control) {
 			Control ctrl = (Control) changedObj.getWidget();
 			MElementContainer<MUIElement> parent = changedObj.getParent();
-			if (parent == null || ((Object) parent) instanceof MToolBar) {
+			if (parent == null || (parent) instanceof MToolBar) {
 				return;
 			}
+
 			if (changedObj.isVisible()) {
 				if (parent.getRenderer() != null) {
 					Object myParent = ((AbstractPartRenderer) parent.getRenderer())
@@ -196,6 +211,10 @@ public class CleanupAddon {
 						ctrl.getShell().layout(new Control[] { ctrl }, SWT.DEFER);
 					}
 
+					if (!shouldReactToChildVisibilityChanges(parent)) {
+						return;
+					}
+
 					// Check if the parent is visible
 					if (!parent.isVisible())
 						parent.setVisible(true);
@@ -211,7 +230,7 @@ public class CleanupAddon {
 					curParent.getShell().layout(new Control[] { curParent }, SWT.DEFER);
 
 				// Always leave Window's in the presentation
-				if ((Object) parent instanceof MWindow)
+				if (parent instanceof MWindow)
 					return;
 
 				// If there are no more 'visible' children then make the parent go away too
@@ -221,6 +240,10 @@ public class CleanupAddon {
 						makeParentInvisible = false;
 						break;
 					}
+				}
+
+				if (!shouldReactToChildVisibilityChanges(parent)) {
+					return;
 				}
 
 				// Special check: If a perspective goes invisibe we need to make its
