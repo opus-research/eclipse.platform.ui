8299ee7aa99c2fdb9cb5e277dfa2d7f08926f36e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 05:45:08 +0000

    First commited as before_31479_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/fieldassist/ControlDecoration.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/fieldassist/ControlDecoration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 IBM Corporation and others.
+ * Copyright (c) 2006, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -362,9 +362,8 @@ public class ControlDecoration {
 			int y = -extent.y - hah + 1;
 			int x = arrowOnLeft ? -hao + haw / 2 : -extent.x + hao + haw / 2;
 
-			Point hoverSize = hoverShell.getSize();
-			Rectangle hoverBounds = control.getDisplay().map(control.getParent(), null, decorationRectangle.x + x, decorationRectangle.y + y, hoverSize.x, hoverSize.y);
-			hoverShell.setLocation(hoverBounds.x, hoverBounds.y);
+			hoverShell.setLocation(control.getParent().toDisplay(
+					decorationRectangle.x + x, decorationRectangle.y + y));
 		}
 
 		/*
