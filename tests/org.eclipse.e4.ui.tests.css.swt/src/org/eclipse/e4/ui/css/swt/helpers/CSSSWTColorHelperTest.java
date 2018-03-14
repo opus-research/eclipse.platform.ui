278630cec9145336dd564eb710b6dc8deca2bd49
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:07:07 +0000

    First commited as before_36278_rev3

--- tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/css/swt/helpers/CSSSWTColorHelperTest.java
+++ tests/org.eclipse.e4.ui.tests.css.swt/src/org/eclipse/e4/ui/css/swt/helpers/CSSSWTColorHelperTest.java
@@ -1,38 +1,31 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others. All rights reserved. This
+ * Copyright (c) 2013 IBM Corporation and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Thibault Le Ouay <thibaultleouay@gmail.com> - Bug 443094
  *******************************************************************************/
 package org.eclipse.e4.ui.css.swt.helpers;
 
 import static org.eclipse.e4.ui.css.swt.helpers.CSSSWTColorHelper.getSWTColor;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
-import org.junit.Before;
-import org.junit.Test;
 import org.w3c.dom.css.CSSValue;
 
 @SuppressWarnings("restriction")
 public class CSSSWTColorHelperTest extends CSSSWTHelperTestCase {
 	private Display display;
 
-	@Before
-	public void setUp() {
+	@Override
+	protected void setUp() throws Exception {
 		display = Display.getDefault();
 	}
 
-	@Test
-	public void testGetSWTColor() {
+	public void testGetSWTColor() throws Exception {
 		Color result = getSWTColor(colorValue("red"), display);
 
 		assertNotNull(result);
@@ -41,16 +34,14 @@ public class CSSSWTColorHelperTest extends CSSSWTHelperTestCase {
 		assertEquals(0, result.getGreen());
 	}
 
-	@Test
-	public void testGetSWTColorWhenNotSupportedColorType() {
+	public void testGetSWTColorWhenNotSupportedColorType() throws Exception {
 		Color result = getSWTColor(colorValue("123213", CSSValue.CSS_CUSTOM),
 				display);
 
 		assertNull(result);
 	}
 
-	@Test
-	public void testGetSWTColorWhenInvalidColorValue() {
+	public void testGetSWTColorWhenInvalidColorValue() throws Exception {
 		Color result = getSWTColor(colorValue("asdsad12"), display);
 
 		assertNotNull(result);
@@ -59,8 +50,7 @@ public class CSSSWTColorHelperTest extends CSSSWTHelperTestCase {
 		assertEquals(0, result.getGreen());
 	}
 
-	@Test
-	public void testGetSWTColorWhenColorFromDefinition() {
+	public void testGetSWTColorWhenColorFromDefinition() throws Exception {
 		registerColorProviderWith("org.eclipse.jdt.debug.ui.InDeadlockColor", new RGB(0, 255, 0));
 
 		Color result = getSWTColor(
