374f7f105a5e64805667898d887f50e1c72282c5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 11:34:25 +0000

    First commited as before_41508_rev2

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/IDEWorkspacePreferencePage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/IDEWorkspacePreferencePage.java
@@ -11,7 +11,6 @@
  *     Markus Schorn (Wind River Systems) -  bug 284447
  *     James Blackburn (Broadcom Corp.)   -  bug 340978
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 458832
- *     Christian Georgi (SAP SE)          -  bug 458811
  *******************************************************************************/
 package org.eclipse.ui.internal.ide.dialogs;
 
@@ -23,10 +22,9 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Preferences;
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.ui.internal.workbench.E4Workbench;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.IntegerFieldEditor;
@@ -51,7 +49,6 @@ import org.eclipse.ui.WorkbenchEncoding;
 import org.eclipse.ui.dialogs.PreferenceLinkArea;
 import org.eclipse.ui.ide.IDEEncoding;
 import org.eclipse.ui.ide.dialogs.ResourceEncodingFieldEditor;
-import org.eclipse.ui.internal.WorkbenchMessages;
 import org.eclipse.ui.internal.ide.IDEInternalPreferences;
 import org.eclipse.ui.internal.ide.IDEWorkbenchMessages;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
@@ -68,8 +65,6 @@ import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
  */
 public class IDEWorkspacePreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 
-	private IEclipseContext e4Context;
-
 	private Button autoBuildButton;
 
     private Button autoSaveAllButton;
@@ -77,7 +72,7 @@ public class IDEWorkspacePreferencePage extends PreferencePage implements IWorkb
     private IntegerFieldEditor saveInterval;
 
 	private FieldEditor workspaceName;
-	private Button showLocationInWindowTitle;
+	private FieldEditor showLocationInWindowTitle;
 
 	private Button autoRefreshButton;
 
@@ -117,14 +112,14 @@ public class IDEWorkspacePreferencePage extends PreferencePage implements IWorkb
         createSaveAllBeforeBuildPref(composite);
         createCloseUnrelatedProjPrefControls(composite);
 
-		createSpace(composite);
-		createSaveIntervalGroup(composite);
-		createWindowTitleGroup(composite);
-
-		createSpace(composite);
+        createSpace(composite);
 		createWorkspaceLocationGroup(composite);
 
 		createSpace(composite);
+        createSaveIntervalGroup(composite);
+        createWindowTitleGroup(composite);
+		createSpace(composite);
+		
 		createOpenPrefControls(composite);
 
 		createSpace(composite);
@@ -216,22 +211,14 @@ public class IDEWorkspacePreferencePage extends PreferencePage implements IWorkb
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false)
 				.hint(convertHorizontalDLUsToPixels(200), SWT.DEFAULT).applyTo(workspacePath);
 
-		// show workspace location in window title
-		boolean showLocationIsSetOnCommandLine = getWorkspaceLocationFromCommandLine(e4Context) != null;
-		showLocationInWindowTitle = new Button(groupComposite, SWT.CHECK);
-		showLocationInWindowTitle.setText(IDEWorkbenchMessages.IDEWorkspacePreference_showLocationInWindowTitle);
-		showLocationInWindowTitle.setSelection(showLocationIsSetOnCommandLine
-				|| getIDEPreferenceStore().getBoolean(IDEInternalPreferences.SHOW_LOCATION));
-		showLocationInWindowTitle.setEnabled(!showLocationIsSetOnCommandLine);
-		GridDataFactory.fillDefaults().grab(true, false).span(2, 1).applyTo(showLocationInWindowTitle);
-
-		if (showLocationIsSetOnCommandLine) {
-			Composite noteComposite = createNoteComposite(composite.getFont(), groupComposite,
-					WorkbenchMessages.Preference_note,
-					IDEWorkbenchMessages.IDEWorkspacePreference_showLocationInWindowTitle_lockedByCommandLine);
-			GridDataFactory.fillDefaults().grab(true, false).span(2, 1).indent(convertHorizontalDLUsToPixels(12), 0)
-					.applyTo(noteComposite);
-		}
+		Composite showLocationComposite = new Composite(composite, SWT.LEFT);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(showLocationComposite);
+		GridLayoutFactory.fillDefaults().applyTo(showLocationComposite);
+		showLocationInWindowTitle = new BooleanFieldEditor(IDEInternalPreferences.SHOW_LOCATION,
+				IDEWorkbenchMessages.IDEWorkspacePreference_showLocationInWindowTitle, showLocationComposite);
+		showLocationInWindowTitle.setPage(this);
+		showLocationInWindowTitle.setPreferenceStore(getIDEPreferenceStore());
+		showLocationInWindowTitle.load();
 	}
 
     /**
@@ -461,7 +448,6 @@ public class IDEWorkspacePreferencePage extends PreferencePage implements IWorkb
 
 	@Override
 	public void init(org.eclipse.ui.IWorkbench workbench) {
-		e4Context = workbench.getService(IEclipseContext.class);
     }
     
     /**
@@ -480,7 +466,7 @@ public class IDEWorkspacePreferencePage extends PreferencePage implements IWorkb
                 .setSelection(store
                         .getDefaultBoolean(IDEInternalPreferences.SAVE_ALL_BEFORE_BUILD));
         saveInterval.loadDefault();
-		showLocationInWindowTitle.setSelection(store.getDefaultBoolean(IDEInternalPreferences.SHOW_LOCATION));
+		showLocationInWindowTitle.loadDefault();
         workspaceName.loadDefault();
         
         boolean closeUnrelatedProj = store.getDefaultBoolean(IDEInternalPreferences.CLOSE_UNRELATED_PROJECTS);
@@ -556,8 +542,7 @@ public class IDEWorkspacePreferencePage extends PreferencePage implements IWorkb
             }
         }
         
-		store.setValue(IDEInternalPreferences.SHOW_LOCATION, showLocationInWindowTitle.getSelection());
-
+		showLocationInWindowTitle.store();
         workspaceName.store();
 
 		systemExplorer.store();
@@ -583,16 +568,5 @@ public class IDEWorkspacePreferencePage extends PreferencePage implements IWorkb
 
 		return super.performOk();
     }
-
-	private static String getWorkspaceLocationFromCommandLine(IEclipseContext context) {
-		String showLocation = (String) context.get(E4Workbench.FORCED_SHOW_LOCATION);
-		if (showLocation != null) { // arg is present
-			if (showLocation.length() > 0) {
-				return showLocation; // second arg value
-			}
-			return Platform.getLocation().toOSString(); // no second arg value
-		}
-		return null; // no arg at all
-	}
 
 }
