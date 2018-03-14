84ba0e889a112c20dd27f9605011c43f0c9c94ff
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 19:49:50 +0000

    First commited as after_55249_rev5

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/views/IViewRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/views/IViewRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,9 +8,12 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Jan-Hendrik Diederich, Bredex GmbH - bug 201052
+ *     Simon Scholz <scholzsimon@vogella.com> - Bug 473845
  *******************************************************************************/
 package org.eclipse.ui.views;
 
+import org.eclipse.e4.ui.model.application.descriptor.basic.MPartDescriptor;
+import org.eclipse.ui.part.ViewPart;
 
 /**
  * The view registry maintains a list of views explicitly registered
@@ -28,6 +31,25 @@ package org.eclipse.ui.views;
  * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface IViewRegistry {
+
+	/**
+	 * This constant is used as key for persisting the original class for a
+	 * legacy {@link ViewPart} in the persisted state of a
+	 * {@link MPartDescriptor}.
+	 *
+	 * @since 3.108
+	 */
+	public static final String ORIGINAL_COMPATIBILITY_VIEW_CLASS = "originalCompatibilityViewClass"; //$NON-NLS-1$
+
+	/**
+	 * This constant is used as key for persisting the original bundle for a
+	 * legacy {@link ViewPart} in the persisted state of a
+	 * {@link MPartDescriptor}.
+	 *
+	 * @since 3.108
+	 */
+	public static final String ORIGINAL_COMPATIBILITY_VIEW_BUNDLE = "originalCompatibilityViewBundle"; //$NON-NLS-1$
+
     /**
      * Return a view descriptor with the given extension id.  If no view exists
      * with the id return <code>null</code>.
