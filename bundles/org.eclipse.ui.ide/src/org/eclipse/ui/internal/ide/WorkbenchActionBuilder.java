ae5c94b1105c1f560ceafe24d0cfb2241b82c54b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 22:55:46 +0000

    First commited as before_62483_rev5

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/WorkbenchActionBuilder.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/WorkbenchActionBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,7 +11,6 @@
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 440810, 440975, 431862
  *     Andrey Loskutov <loskutov@gmx.de> - Bug 445538
  *     Patrik Suzzi <psuzzi@gmail.com> - Bug 487570, 494289
- *     Kaloyan Raev <kaloyan.r@zend.com> - Bug 322002
  *******************************************************************************/
 package org.eclipse.ui.internal.ide;
 
@@ -399,18 +398,6 @@ public final class WorkbenchActionBuilder extends ActionBarAdvisor {
             // Add to the cool bar manager
             coolBar.add(actionBarConfigurer.createToolBarContributionItem(fileToolBar,
                     IWorkbenchActionConstants.TOOLBAR_FILE));
-        }
-
-        coolBar.add(new GroupMarker(IIDEActionConstants.GROUP_EDIT));
-        { // Edit group
-            IToolBarManager editToolBar = actionBarConfigurer.createToolBarManager();
-            editToolBar.add(new Separator(IWorkbenchActionConstants.EDIT_GROUP));
-            editToolBar.add(undoAction);
-            editToolBar.add(redoAction);
-
-            // Add to the cool bar manager
-            coolBar.add(actionBarConfigurer.createToolBarContributionItem(editToolBar,
-                    IWorkbenchActionConstants.TOOLBAR_EDIT));
         }
 
         coolBar.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
