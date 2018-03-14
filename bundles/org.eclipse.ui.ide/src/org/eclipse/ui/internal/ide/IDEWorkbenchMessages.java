6899599c163ee947038e690b3c869a4843df3d18
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 11:15:48 +0000

    First commited as before_40794_rev5

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/IDEWorkbenchMessages.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/IDEWorkbenchMessages.java
@@ -505,8 +505,6 @@ public class IDEWorkbenchMessages extends NLS {
 	// ---workspace ---
 	public static String IDEWorkspacePreference_autobuild;
 	public static String IDEWorkspacePreference_autobuildToolTip;
-	public static String IDEWorkspacePreference_autoSaveEditors;
-	public static String IDEWorkspacePreference_autoSaveEditorsToolTip;
 	public static String IDEWorkspacePreference_savePriorToBuilding;
 	public static String IDEWorkspacePreference_savePriorToBuildingToolTip;
 	public static String IDEWorkspacePreference_RefreshButtonText;
