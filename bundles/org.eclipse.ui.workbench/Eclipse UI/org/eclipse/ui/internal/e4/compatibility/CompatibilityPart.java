a2e8bd11d20421d665bd6baac9feb6274053d402
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 16:24:31 +0000

    First commited as before_47020_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityPart.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityPart.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Steven Spungin <steven@spungin.tv> - Bug 436908
- *     Andrey Loskutov <loskutov@gmx.de> - Bug 372799
+ *     Andrey Loskutov <loskutov@gmx.de> - Bug 372799, 446864
  *     Snjezana Peco <snjezana.peco@redhat.com> - Bug 414888
  ******************************************************************************/
 
@@ -240,6 +240,7 @@ public abstract class CompatibilityPart implements ISelectionChangedListener {
 				// client code may have errors so we need to catch it
 				logger.error(e);
 			}
+			wrapped = null;
 		}
 
 		internalDisposeSite(site);
@@ -367,10 +368,9 @@ public abstract class CompatibilityPart implements ISelectionChangedListener {
 						part.getTransientData().put(IPresentationEngine.OVERRIDE_ICON_IMAGE_KEY,
 								newImage);
 					}
-					if (wrapped.getTitleToolTip() != null && wrapped.getTitleToolTip().length() > 0) {
-						part.getTransientData()
-								.put(IPresentationEngine.OVERRIDE_TITLE_TOOL_TIP_KEY,
-								wrapped.getTitleToolTip());
+					String titleToolTip = wrapped.getTitleToolTip();
+					if (titleToolTip != null) {
+						part.getTransientData().put(IPresentationEngine.OVERRIDE_TITLE_TOOL_TIP_KEY, titleToolTip);
 					}
 					break;
 				case IWorkbenchPartConstants.PROP_DIRTY:
