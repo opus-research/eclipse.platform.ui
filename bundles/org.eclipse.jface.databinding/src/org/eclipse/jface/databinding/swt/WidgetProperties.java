6827b6fcb948969f55ba2a6f6afc159d7ab57fc3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 22:02:16 +0000

    First commited as before_60718_rev2

--- bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/databinding/swt/WidgetProperties.java
+++ bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/databinding/swt/WidgetProperties.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2015 Matthew Hall and others.
+ * Copyright (c) 2008, 2011, 2011 Matthew Hall and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,19 +10,9 @@
  *     Matthew Hall - bugs 256543, 213893, 262320, 262946, 264286, 266563,
  *                    169876, 306203
  *     Eugen Neufeld - bug 461560
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 482486
  ******************************************************************************/
 
 package org.eclipse.jface.databinding.swt;
-
-import java.awt.Button;
-import java.awt.Label;
-import java.awt.Menu;
-import java.awt.MenuItem;
-
-import javax.swing.JScrollPane.ScrollBar;
-import javax.swing.table.TableColumn;
-import javax.xml.soap.Text;
 
 import org.eclipse.jface.internal.databinding.swt.ControlBackgroundProperty;
 import org.eclipse.jface.internal.databinding.swt.ControlBoundsProperty;
@@ -44,7 +34,36 @@ import org.eclipse.jface.internal.databinding.swt.WidgetSingleSelectionIndexProp
 import org.eclipse.jface.internal.databinding.swt.WidgetTextProperty;
 import org.eclipse.jface.internal.databinding.swt.WidgetTextWithEventsProperty;
 import org.eclipse.jface.internal.databinding.swt.WidgetTooltipTextProperty;
-import org.eclipse.jface.window.ToolTip;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Item;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Scale;
+import org.eclipse.swt.widgets.ScrollBar;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Slider;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.widgets.ToolTip;
+import org.eclipse.swt.widgets.TrayItem;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.swt.widgets.Widget;
 
 /**
  * A factory for creating properties of SWT {@link Widget widgets}.
@@ -262,14 +281,14 @@ public class WidgetProperties {
 	 * or {@link Text}.
 	 *
 	 * @param events
-	 *            varags of SWT event types to register for change events. May
+	 *            array of SWT event types to register for change events. May
 	 *            include {@link SWT#None}, {@link SWT#Modify},
 	 *            {@link SWT#FocusOut} or {@link SWT#DefaultSelection}.
 	 *
 	 * @return a value property for observing the text of a {@link StyledText}
 	 *         or {@link Text}.
 	 */
-	public static IWidgetValueProperty text(int... events) {
+	public static IWidgetValueProperty text(int[] events) {
 		return new WidgetTextWithEventsProperty(events.clone());
 	}
 
