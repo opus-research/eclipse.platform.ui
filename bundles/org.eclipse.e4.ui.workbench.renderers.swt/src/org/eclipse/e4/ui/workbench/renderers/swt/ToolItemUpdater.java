3d2a9d493e35fdef942a68d18f62600fb40409a9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 01:38:02 +0000

    First commited as before_21919_rev12

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolItemUpdater.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolItemUpdater.java
@@ -13,7 +13,7 @@ package org.eclipse.e4.ui.workbench.renderers.swt;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.eclipse.e4.ui.workbench.UIEvents.Selector;
+import org.eclipse.e4.ui.workbench.Selector;
 import org.eclipse.swt.widgets.Display;
 
 public class ToolItemUpdater {
@@ -30,21 +30,6 @@ public class ToolItemUpdater {
 
 	void removeItem(HandledContributionItem item) {
 		itemsToCheck.remove(item);
-	}
-
-	public void updateContributionItems() {
-		for (final HandledContributionItem hci : itemsToCheck) {
-			if (hci.model != null && hci.model.getParent() != null) {
-				hci.updateItemEnablement();
-			} else {
-				orphanedToolItems.add(hci);
-			}
-		}
-		if (!orphanedToolItems.isEmpty()) {
-			itemsToCheck.removeAll(orphanedToolItems);
-			orphanedToolItems.clear();
-		}
-
 	}
 
 	public void updateContributionItems(Selector selector) {
--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/E4Application.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/E4Application.java
@@ -206,11 +206,15 @@ public class E4Application implements IApplication {
 		appContext.set(UISynchronize.class, new UISynchronize() {
 
 			public void syncExec(Runnable runnable) {
-				display.syncExec(runnable);
+				if (display != null && !display.isDisposed()) {
+					display.syncExec(runnable);
+				}
 			}
 
 			public void asyncExec(Runnable runnable) {
-				display.asyncExec(runnable);
+				if (display != null && !display.isDisposed()) {
+					display.asyncExec(runnable);
+				}
 			}
 		});
 		appContext.set(IApplicationContext.class, applicationContext);
@@ -302,12 +306,11 @@ public class E4Application implements IApplication {
 		}
 		appContext.set(E4Application.THEME_ID, themeId);
 
-		// Temporary to support old property as well
-		if (cssURI != null && !cssURI.startsWith("platform:")) {
+		// validate static CSS URI
+		if (cssURI != null && !cssURI.startsWith("platform:/plugin/")) {
 			System.err
-					.println("Warning "
-							+ cssURI
-							+ " changed its meaning it is used now to run without theme support");
+					.println("Warning. Use the \"platform:/plugin/Bundle-SymbolicName/path/filename.extension\" URI for the  parameter:   "
+							+ IWorkbench.CSS_URI_ARG); //$NON-NLS-1$
 			appContext.set(E4Application.THEME_ID, cssURI);
 		}
 
