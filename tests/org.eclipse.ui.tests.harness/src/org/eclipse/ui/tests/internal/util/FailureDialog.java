a2882a2e0bd25c407239f476a26a8c83d2c1ba55
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 07:16:20 +0000

    First commited as after_34400_rev4

--- tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/internal/util/FailureDialog.java
+++ tests/org.eclipse.ui.tests.harness/src/org/eclipse/ui/tests/internal/util/FailureDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 444070
  *******************************************************************************/
 package org.eclipse.ui.tests.internal.util;
 
@@ -44,7 +45,8 @@ public class FailureDialog extends Dialog {
     /* (non-Javadoc)
      * Method declared on Window.
      */
-    protected void configureShell(Shell newShell) {
+    @Override
+	protected void configureShell(Shell newShell) {
         super.configureShell(newShell);
         newShell.setText("Dialog Test Failed");
     }
@@ -52,7 +54,8 @@ public class FailureDialog extends Dialog {
     /* (non-Javadoc)
      * Method declared on Dialog.
      */
-    protected void createButtonsForButtonBar(Composite parent) {
+    @Override
+	protected void createButtonsForButtonBar(Composite parent) {
         createButton(parent, IDialogConstants.OK_ID, "&OK", true);
         createButton(parent, IDialogConstants.CANCEL_ID,
                 IDialogConstants.CANCEL_LABEL, false);
@@ -61,7 +64,8 @@ public class FailureDialog extends Dialog {
     /* (non-Javadoc)
      * Method declared on Dialog.
      */
-    protected Control createDialogArea(Composite parent) {
+    @Override
+	protected Control createDialogArea(Composite parent) {
         // page group
         Composite composite = (Composite) super.createDialogArea(parent);
         composite.setSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
@@ -84,7 +88,8 @@ public class FailureDialog extends Dialog {
     /* (non-Javadoc)
      * Method declared on Dialog.
      */
-    protected void okPressed() {
+    @Override
+	protected void okPressed() {
         _log = _text.getText();
         super.okPressed();
     }
@@ -94,11 +99,7 @@ public class FailureDialog extends Dialog {
      * the dialog.
      */
     String getText() {
-        if (_log == null) {
-            return "Empty entry.";
-        } else {
-            return _log;
-        }
+		return (_log == null) ? "Empty entry." : _log;
     }
 
     /*
@@ -114,7 +115,8 @@ public class FailureDialog extends Dialog {
      * Returns a string representation of this class which
      * the text contained in the input area of the dialog.
      */
-    public String toString() {
+    @Override
+	public String toString() {
         return getText();
     }
 }
