26ef0bf3c1dbf0a84f82575d2ca6651ec2a6e9c2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 02:02:52 +0000

    First commited as after_23002_rev1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/SashRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/SashRenderer.java
@@ -38,6 +38,7 @@ public class SashRenderer extends SWTPartRenderer {
 
 	private EventHandler sashOrientationHandler;
 	private EventHandler sashWeightHandler;
+	private int processedContent = 0;
 
 	public SashRenderer() {
 		super();
@@ -81,6 +82,9 @@ public class SashRenderer extends SWTPartRenderer {
 	 * @param pscModel
 	 */
 	protected void forceLayout(MElementContainer<MUIElement> pscModel) {
+		if (processedContent != 0) {
+			return;
+		}
 		// layout the containing Composite
 		while (!(pscModel.getWidget() instanceof Composite))
 			pscModel = pscModel.getParent();
@@ -161,6 +165,26 @@ public class SashRenderer extends SWTPartRenderer {
 		}
 
 		forceLayout(parentElement);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.e4.ui.workbench.renderers.swt.SWTPartRenderer#processContents
+	 * (org.eclipse.e4.ui.model.application.ui.MElementContainer)
+	 */
+	@Override
+	public void processContents(MElementContainer<MUIElement> container) {
+		try {
+			processedContent++;
+			super.processContents(container);
+		} finally {
+			processedContent--;
+			if (processedContent == 0) {
+				forceLayout(container);
+			}
+		}
 	}
 
 	/*
