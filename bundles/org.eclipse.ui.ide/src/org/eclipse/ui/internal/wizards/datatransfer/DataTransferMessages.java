e68ab5f305b69b122e0ceb538783eeabc0bcf239
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 22:17:25 +0000

    First commited as before_61233_rev6

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/DataTransferMessages.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/DataTransferMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2016 IBM Corporation and others.
+ * Copyright (c) 2005, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -150,67 +150,6 @@ public class DataTransferMessages extends NLS {
 	public static String ArchiveExport_saveInTarFormat;
 
 	public static String TarImport_invalid_tar_format;
-
-	// Smart import
-	public static String SmartImportWizardPage_selectFolderOrArchiveToImport;
-	public static String SmartImportProposals_alreadyImportedAsProject_title;
-	public static String SmartImpprtProposals_areadyImportedAsProject_description;
-	public static String SmartImportProposals_anotherProjectWithSameNameExists_title;
-	public static String SmartImportProposals_anotherProjectWithSameNameExists_description;
-	public static String SmartImportJob_importProject;
-
-	public static String SmartImportJob_eclipseProjectConfigurationLabel;
-
-	public static String SmartImportWizardPage_importProjectsInFolderTitle;
-	public static String SmartImportWizardPage_importProjectsInFolderDescription;
-	public static String SmartImportWizardPage_selectRootDirectory;
-	public static String SmartImportWizardPage_incorrectRootDirectory;
-	public static String SmartImportWizardPage_browse;
-	public static String SmartImportWizardPage_workingSets;
-	public static String SmartImportWizardPage_detectNestedProjects;
-	public static String SmartImportWizardPage_configureProjects;
-	public static String SmartImportWizardPage_showAvailableDetectors;
-	public static String SmartImportWizardPage_availableDetectors_title;
-	public static String SmartImportWizardPage_availableDetectors_description;
-	public static String SmartImportWizardPage_selectArchiveButton;
-	public static String SmartImportWizardPage_selectArchiveTitle;
-	public static String SmartImportWizardPage_allSupportedArchives;
-
-	public static String SmartImportProposals_preliminaryDetection_Title;
-	public static String SmartImportProposals_preliminaryDetection_Description;
-	public static String SmartImportJob_discardRootProject_title;
-	public static String SmartImportJob_discardRootProject_description;
-	public static String SmartImportJob_didntFindImportProposals_title;
-	public static String SmartImportJob_didntFindImportProposals_message;
-	public static String SmartImportProposals_selectionSummary;
-	public static String SmartImportProposals_folder;
-	public static String SmartImportProposals_importAs;
-
-	public static String SmartImportReport_importedProjects;
-	public static String SmartImportReport_importedProjectsWithCount;
-	public static String SmartImportReport_progressBarTooltip;
-	public static String SmartImportReport_relativePath;
-	public static String SmartImportReport_project;
-	public static String SmartImportReport_natures;
-	public static String SmartImportReport_importErrors;
-	public static String SmartImportReport_error;
-	public static String SmartImportReport_completed;
-	public static String SmartImportReport_aborted;
-
-	public static String SmartImportJob_detectAndConfigureProjects;
-	public static String SmartImportJob_configuringSelectedDirectories;
-	public static String SmartImportJob_configuring;
-	public static String SmartImportJob_crawling;
-	public static String SmartImportJob_continuingConfiguration;
-	public static String SmartImportJob_inspecting;
-	public static String SmartImportJob_importingProjectIntoWorkspace;
-	public static String SmartImportProposals_additionalAnalysis;
-	public static String SmartImportJob_sorry;
-	public static String SmartImportJob_internalError;
-
-	public static String SmartImportWizardPage_expandingArchive;
-	public static String SmartImportWizardPage_deletingDirectory;
-
 
 	static {
 		// load message values from bundle file
