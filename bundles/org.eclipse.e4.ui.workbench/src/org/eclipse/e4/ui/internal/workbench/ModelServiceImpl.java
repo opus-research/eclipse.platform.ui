e7a0fb4ada5fdb8c4f9476036f87695ab93cc2e2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:59:57 +0000

    First commited as before_25507_rev1

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelServiceImpl.java
@@ -274,7 +274,7 @@ public class ModelServiceImpl implements EModelService {
 		if (searchRoot instanceof MPart) {
 			MPart part = (MPart) searchRoot;
 
-			if ((searchFlags & IN_PART) != 0) {
+			if (searchFlags != IN_MAIN_MENU) {
 				for (MMenu menu : part.getMenus()) {
 					findElementsRecursive(menu, clazz, matcher, elements, searchFlags);
 				}
