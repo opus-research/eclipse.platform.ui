c5044738a244765e162da22a335c7b6334bce2b3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 01:55:15 +0000

    First commited as before_22728_rev4

--- bundles/org.eclipse.core.commands/src/org/eclipse/core/commands/Parameterization.java
+++ bundles/org.eclipse.core.commands/src/org/eclipse/core/commands/Parameterization.java
@@ -81,7 +81,9 @@ public final class Parameterization {
 		this.value = value;
 	}
 
-	@Override
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	public final boolean equals(final Object object) {
 		if (this == object) {
 			return true;
@@ -148,7 +150,9 @@ public final class Parameterization {
 		return returnValue;
 	}
 
-	@Override
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
 	public final int hashCode() {
 		if (hashCode == HASH_CODE_NOT_COMPUTED) {
 			hashCode = HASH_INITIAL * HASH_FACTOR + Util.hashCode(parameter);
