d65410a348785173f31babac15aed3a60439a9e8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 14:27:10 +0000

    First commited as after_45270_rev1

--- tests/org.eclipse.ui.tests.browser/src/org/eclipse/ui/tests/browser/internal/AllTests.java
+++ tests/org.eclipse.ui.tests.browser/src/org/eclipse/ui/tests/browser/internal/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,6 +25,7 @@ public class AllTests {
 		suite.addTestSuite(DialogsTestCase.class);
 		suite.addTestSuite(PreferencesTestCase.class);
 		suite.addTestSuite(ToolbarBrowserTestCase.class);
+		suite.addTestSuite(WebBrowserUtilTestCase.class);
 		//$JUnit-END$
 		return suite;
 	}
