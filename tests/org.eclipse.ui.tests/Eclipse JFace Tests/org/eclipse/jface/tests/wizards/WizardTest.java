f1827570a482fb1335415038a8267017b720aeef
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:05:56 +0000

    First commited as before_25679_rev3

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/wizards/WizardTest.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/wizards/WizardTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2014 IBM Corporation and others.
+ * Copyright (c) 2008, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,10 +7,11 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 433608
  ******************************************************************************/
 
 package org.eclipse.jface.tests.wizards;
+
+import java.io.IOException;
 
 import junit.framework.TestCase;
 
@@ -51,7 +52,7 @@ public class WizardTest extends TestCase {
 	}
 
 
-	public void testEndingWithFinish() {
+	public void testEndingWithFinish() throws IOException {
 		//test page count
         assertEquals("Wizard has wrong number of pages", NUM_PAGES, wizard.getPageCount());
 
@@ -108,7 +109,7 @@ public class WizardTest extends TestCase {
         assertEquals("Wizard didn't perform finish", true, DID_FINISH);
 	}
 	
-	public void testEndingWithCancel() {
+	public void testEndingWithCancel() throws IOException {
 		assertSame("Wizard not on starting page", wizard.page1, dialog.getCurrentPage());
 
 		//TheTestWizard's performFinish() sets DID_FINISH to true, ensure it was not called
@@ -119,7 +120,7 @@ public class WizardTest extends TestCase {
         assertEquals("Wizard performed finished but should not have", false, DID_FINISH);
 	}
 
-	public void testPageChanging() {
+	public void testPageChanging() throws IOException {    
 		//initially on first page
 	    assertSame("Wizard started on wrong page", wizard.page1, dialog.getCurrentPage());  
 		assertEquals("Back button should be disabled on first page", false, dialog.getBackButton().getEnabled());
@@ -166,7 +167,7 @@ public class WizardTest extends TestCase {
 	    assertSame("Wizard.backPressed() set wrong page", wizard.page1, dialog.getCurrentPage());  	    
 	}
 	
-	public void testShowPage() {
+	public void testShowPage() throws IOException {    
 		//move to page 3
 		dialog.nextPressed();
 		dialog.nextPressed();
@@ -182,12 +183,11 @@ public class WizardTest extends TestCase {
 		assertEquals("Next button should be enabled on first page", true, dialog.getNextButton().getEnabled());
 	}
 
-	public void testPageChangeListening() {
+	public void testPageChangeListening() throws IOException {   
 		pageChanged = false;
 		pageChangingFired = false;
 		
 		IPageChangedListener changedListener = new IPageChangedListener() {
-			@Override
 			public void pageChanged(PageChangedEvent event) {
 				pageChanged = true;				
 			}
@@ -195,7 +195,6 @@ public class WizardTest extends TestCase {
 		};
 
 		IPageChangingListener changingListener = new IPageChangingListener() {
-			@Override
 			public void handlePageChanging(PageChangingEvent event) {
 				assertEquals("Page should not have changed yet", false, pageChanged);
 				pageChangingFired = true;
@@ -230,7 +229,6 @@ public class WizardTest extends TestCase {
 
 		final boolean logged[] = new boolean[1];
 		Policy.setLog(new ILogger() {
-			@Override
 			public void log(IStatus status) {
 				logged[0] = true;
 			}
@@ -247,7 +245,6 @@ public class WizardTest extends TestCase {
         wizard.page2.setThrowExceptionOnDispose(true);
         final boolean logged[] = new boolean[1];
 		Policy.setLog(new ILogger() {
-			@Override
 			public void log(IStatus status) {
 				logged[0] = true;
 			}
@@ -263,7 +260,6 @@ public class WizardTest extends TestCase {
 	//----------------------------------------------------
 
 	
-	@Override
 	protected void setUp() throws Exception {
 		// TODO Auto-generated method stub
 		super.setUp();
@@ -274,7 +270,6 @@ public class WizardTest extends TestCase {
 		createWizardDialog();
 	}
 
-	@Override
 	protected void tearDown() throws Exception {
 		if(dialog.getShell() != null && ! dialog.getShell().isDisposed()) {
 		    dialog.close();
