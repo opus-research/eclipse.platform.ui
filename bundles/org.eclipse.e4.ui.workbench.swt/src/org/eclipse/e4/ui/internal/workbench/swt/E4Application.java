e60b0f8485980b12b025444c4fc5844d19951948
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:17:43 +0000

    First commited as after_25853_rev2

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/E4Application.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/E4Application.java
@@ -11,6 +11,8 @@
  *     		Fix for Bug 2369 [Workbench] Would like to be able to save workspace without exiting
  *     		Implemented workbench auto-save to correctly restore state in case of crash.
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 366364
+ *     Terry Parker <tparker@google.com> - Bug 416673
+ *		[Workbench] Corrupted workbench.xmi file causes silent Eclipse exit
  ******************************************************************************/
 
 package org.eclipse.e4.ui.internal.workbench.swt;
@@ -184,10 +186,19 @@ public class E4Application implements IApplication {
 
 	public void saveModel() {
 		try {
-			handler.save();
+			if (!(handler instanceof ResourceHandler)
+					|| ((ResourceHandler) handler).hasTopLevelWindows()) {
+				handler.save();
+			} else {
+				Logger logger = new WorkbenchLogger(PLUGIN_ID);
+				logger.error(
+						new Exception(), // log a stack trace for debugging
+						"Attempted to save a workbench model that had no top-level windows! " //$NON-NLS-1$
+								+ "Skipped saving the model to avoid corruption."); //$NON-NLS-1$
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Logger logger = new WorkbenchLogger(PLUGIN_ID);
+			logger.error(e, "Error saving the workbench model"); //$NON-NLS-1$
 		}
 	}
 
