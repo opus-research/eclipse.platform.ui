0684d9e9d98c05231f66bf778d215aa0c73928b8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 13:20:15 +0000

    First commited as before_43545_rev5

--- bundles/org.eclipse.core.commands/src/org/eclipse/core/commands/common/HandleObjectManager.java
+++ bundles/org.eclipse.core.commands/src/org/eclipse/core/commands/common/HandleObjectManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2014 IBM Corporation and others.
+ * Copyright (c) 2005, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -26,7 +26,7 @@ import java.util.Set;
  * <p>
  * Clients may extend.
  * </p>
- * 
+ *
  * @since 3.2
  */
 public abstract class HandleObjectManager extends EventManager {
@@ -35,47 +35,46 @@ public abstract class HandleObjectManager extends EventManager {
 	 * The set of handle objects that are defined. This value may be empty, but
 	 * it is never <code>null</code>.
 	 */
-	protected final Set definedHandleObjects = new HashSet();
+	@SuppressWarnings("rawtypes")
+	protected final Set definedHandleObjects = new HashSet<>();
 
 	/**
 	 * The map of identifiers (<code>String</code>) to handle objects (
 	 * <code>HandleObject</code>). This collection may be empty, but it is
 	 * never <code>null</code>.
 	 */
-	protected final Map handleObjectsById = new HashMap();
+	@SuppressWarnings("rawtypes")
+	protected final Map handleObjectsById = new HashMap<>();
 
 	/**
 	 * Verifies that the identifier is valid. Exceptions will be thrown if the
 	 * identifier is invalid in some way.
-	 * 
+	 *
 	 * @param id
 	 *            The identifier to validate; may be anything.
 	 */
 	protected final void checkId(final String id) {
 		if (id == null) {
-			throw new IllegalArgumentException(
-					"A handle object may not have a null identifier"); //$NON-NLS-1$
+			throw new IllegalArgumentException("A handle object may not have a null identifier"); //$NON-NLS-1$
 		}
 
 		if (id.length() < 1) {
-			throw new IllegalArgumentException(
-					"The handle object must not have a zero-length identifier"); //$NON-NLS-1$
+			throw new IllegalArgumentException("The handle object must not have a zero-length identifier"); //$NON-NLS-1$
 		}
 	}
 
 	/**
 	 * Returns the set of identifiers for those handle objects that are defined.
-	 * 
+	 *
 	 * @return The set of defined handle object identifiers; this value may be
 	 *         empty, but it is never <code>null</code>.
 	 */
+	@SuppressWarnings("rawtypes")
 	protected final Set getDefinedHandleObjectIds() {
-		final HashSet definedHandleObjectIds = new HashSet(definedHandleObjects
-				.size());
-		final Iterator handleObjectItr = definedHandleObjects.iterator();
+		final HashSet<String> definedHandleObjectIds = new HashSet<>(definedHandleObjects.size());
+		final Iterator<NamedHandleObject> handleObjectItr = definedHandleObjects.iterator();
 		while (handleObjectItr.hasNext()) {
-			final HandleObject handleObject = (HandleObject) handleObjectItr
-					.next();
+			final HandleObject handleObject = handleObjectItr.next();
 			final String id = handleObject.getId();
 			definedHandleObjectIds.add(id);
 		}
