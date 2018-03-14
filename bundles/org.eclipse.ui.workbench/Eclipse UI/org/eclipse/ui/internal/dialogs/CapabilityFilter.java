9a6944ef67458d197c2ba0a114e88643e4b548ff
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 08:22:09 +0000

    First commited as before_35318_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/CapabilityFilter.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/CapabilityFilter.java
@@ -28,6 +28,9 @@ public class CapabilityFilter extends ViewerFilter {
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ViewerFilter#select(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+	 */
 	@Override
 	public boolean select(Viewer viewer, Object parentElement, Object element) {
 		return ! WorkbenchActivityHelper.filterItem(element);
