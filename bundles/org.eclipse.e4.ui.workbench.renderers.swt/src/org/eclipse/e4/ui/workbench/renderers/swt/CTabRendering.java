1db1066fdd42ae464034654847d5ea35060ed330
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 00:37:57 +0000

    First commited as after_20389_rev2

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/CTabRendering.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/CTabRendering.java
@@ -250,7 +250,7 @@ public class CTabRendering extends CTabFolderRenderer implements
 		// HEADER
 		Rectangle trim = computeTrim(PART_HEADER, state, 0, 0, 0, 0);
 		trim.width = bounds.width - trim.width;
-		trim.height = (parent.getTabHeight() + 1 + header) - trim.height;
+		trim.height = (parent.getTabHeight() + (onBottom ? 7 : 4)) - trim.height;
 		trim.x = -trim.x;
 		trim.y = onBottom ? bounds.height - parent.getTabHeight() - 1 - header
 				: -trim.y;
