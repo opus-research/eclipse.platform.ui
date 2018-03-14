3b5e2cd7ac2c9d61e8efdcd4492369c4200d7729
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:15:22 +0000

    First commited as before_37145_rev2

--- tests/org.eclipse.e4.emf.xpath.test/src/org/eclipse/e4/emf/xpath/test/EMFTestSuite.java
+++ tests/org.eclipse.e4.emf.xpath.test/src/org/eclipse/e4/emf/xpath/test/EMFTestSuite.java
@@ -1,23 +1,22 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBM Corporation and others.
+ * Copyright (c) 2014  Thibault Le Ouay and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Thibault Le Ouay <thibaultleouay@gmail.com> - Bug 452764
- *******************************************************************************/
+ *     Thibault Le Ouay <thibaultleouay@gmail.com> - Bug 450212
+ ******************************************************************************/
 
-package org.eclipse.e4.ui.menu.tests;
+package org.eclipse.e4.emf.xpath.test;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MMenuItemTest.class, MToolItemTest.class })
-public class MenuTestSuite {
+@SuiteClasses(ExampleQueriesTestCase.class)
+public class EMFTestSuite {
 
 }
