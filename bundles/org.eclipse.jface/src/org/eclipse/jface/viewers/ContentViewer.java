dce41b0d41b505646db554c595429f9ed069f65d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:28:50 +0000

    First commited as after_24599_rev3

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ContentViewer.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ContentViewer.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Steven Spungin <steven@spungin.tv> - Bug 401439
  *******************************************************************************/
 package org.eclipse.jface.viewers;
 
@@ -169,17 +170,25 @@ public abstract class ContentViewer extends Viewer {
      * @param event a dispose event
      */
     protected void handleDispose(DisposeEvent event) {
-        if (contentProvider != null) {
-            contentProvider.inputChanged(this, getInput(), null);
-            contentProvider.dispose();
-            contentProvider = null;
-        }
-        if (labelProvider != null) {
-            labelProvider.removeListener(labelProviderListener);
-            labelProvider.dispose();
-            labelProvider = null;
-        }
-        input = null;
+		RuntimeException ex = null;
+		if (contentProvider != null) {
+			try {
+				contentProvider.inputChanged(this, getInput(), null);
+			} catch (RuntimeException e) {
+				ex = e;
+			}
+			contentProvider.dispose();
+			contentProvider = null;
+		}
+		if (labelProvider != null) {
+			labelProvider.removeListener(labelProviderListener);
+			labelProvider.dispose();
+			labelProvider = null;
+		}
+		input = null;
+		if (ex != null) {
+			throw ex;
+		}
     }
 
     /**
