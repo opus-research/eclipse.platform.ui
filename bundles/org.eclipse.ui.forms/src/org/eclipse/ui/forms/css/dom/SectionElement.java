5dfed15b833aab217a5cc69951ba29159ca96c7c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:37:59 +0000

    First commited as before_38381_rev7

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/css/dom/SectionElement.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/css/dom/SectionElement.java
@@ -13,6 +13,7 @@ package org.eclipse.ui.forms.css.dom;
 import org.eclipse.e4.ui.css.core.dom.CSSStylableElement;
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.css.swt.dom.CompositeElement;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.ui.forms.widgets.Section;
 
 /**
@@ -22,14 +23,23 @@ import org.eclipse.ui.forms.widgets.Section;
 public class SectionElement extends CompositeElement {
 
 
+	private Color titleBarBackground;
+	private Color titleBarBorderColor;
+	private Color titleBarGradientBackground;
+
 	public SectionElement(Section section, CSSEngine engine) {
 		super(section, engine);
+		titleBarBackground = section.getTitleBarBackground();
+		titleBarBorderColor = section.getTitleBarBorderColor();
+		titleBarGradientBackground = section.getTitleBarGradientBackground();
 	}
 
 	public void reset() {
 		super.reset();
-		Section widget = (Section) getWidget();
-		widget.setTitleBarBackground(null);
+		Section section = (Section) getWidget();
+		section.setTitleBarBackground(titleBarBackground);
+		section.setTitleBarBorderColor(titleBarBorderColor);
+		section.setTitleBarGradientBackground(titleBarGradientBackground);
 	}
 
 }
