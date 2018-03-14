9ef4d7cb75816d842bee188578eec628f6849b9c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 07:12:17 +0000

    First commited as after_34320_rev3

--- bundles/org.eclipse.ui.monitoring/src/org/eclipse/ui/internal/monitoring/MonitoringStartup.java
+++ bundles/org.eclipse.ui.monitoring/src/org/eclipse/ui/internal/monitoring/MonitoringStartup.java
@@ -58,7 +58,7 @@ public class MonitoringStartup implements IStartup {
 
 		final EventLoopMonitorThread thread = temporaryThread;
 		final Display display = MonitoringPlugin.getDefault().getWorkbench().getDisplay();
-		// Final setup and start synced on display thread
+		// Final setup and start asynchronously on the display thread.
 		display.asyncExec(new Runnable() {
 			@Override
 			public void run() {
@@ -80,12 +80,13 @@ public class MonitoringStartup implements IStartup {
 		IPreferenceStore preferences = MonitoringPlugin.getDefault().getPreferenceStore();
 		EventLoopMonitorThread.Parameters args = new EventLoopMonitorThread.Parameters();
 
-		args.longEventThreshold = preferences.getInt(PreferenceConstants.LONG_EVENT_THRESHOLD_MILLIS);
+		args.longEventWarningThreshold =
+				preferences.getInt(PreferenceConstants.LONG_EVENT_WARNING_THRESHOLD_MILLIS);
+		args.longEventErrorThreshold =
+				preferences.getInt(PreferenceConstants.LONG_EVENT_ERROR_THRESHOLD_MILLIS);
+		args.deadlockThreshold =
+				preferences.getInt(PreferenceConstants.DEADLOCK_REPORTING_THRESHOLD_MILLIS);
 		args.maxStackSamples = preferences.getInt(PreferenceConstants.MAX_STACK_SAMPLES);
-		args.sampleInterval = preferences.getInt(PreferenceConstants.SAMPLE_INTERVAL_MILLIS);
-		args.initialSampleDelay = preferences.getInt(PreferenceConstants.INITIAL_SAMPLE_DELAY_MILLIS);
-		args.deadlockThreshold = preferences.getInt(PreferenceConstants.DEADLOCK_REPORTING_THRESHOLD_MILLIS);
-		args.dumpAllThreads = preferences.getBoolean(PreferenceConstants.DUMP_ALL_THREADS);
 		args.filterTraces = preferences.getString(PreferenceConstants.FILTER_TRACES);
 		args.logToErrorLog = preferences.getBoolean(PreferenceConstants.LOG_TO_ERROR_LOG);
 
