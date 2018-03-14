152d4cd238eb7e9fd071440d4140bdf422945a29
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:29:31 +0000

    First commited as before_24669_rev2

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/PartRenderingEngine.java
@@ -1233,38 +1233,9 @@ public class PartRenderingEngine implements IPresentationEngine {
 			IEclipseContext appContext) {
 		String cssTheme = (String) appContext.get(E4Application.THEME_ID);
 		String cssURI = (String) appContext.get(IWorkbench.CSS_URI_ARG);
+		final IThemeEngine themeEngine = createThemeEngine(display, appContext);
 
-		if ("none".equals(cssTheme)) {
-			appContext.set(IStylingEngine.SERVICE_NAME, new IStylingEngine() {
-				@Override
-				public void setClassname(Object widget, String classname) {
-					WidgetElement.setCSSClass((Widget) widget, classname);
-				}
-
-				@Override
-				public void setId(Object widget, String id) {
-					WidgetElement.setID((Widget) widget, id);
-				}
-
-				@Override
-				public void style(Object widget) {
-				}
-
-				@Override
-				public CSSStyleDeclaration getStyle(Object widget) {
-					return null;
-				}
-
-				@Override
-				public void setClassnameAndId(Object widget, String classname,
-						String id) {
-					WidgetElement.setCSSClass((Widget) widget, classname);
-					WidgetElement.setID((Widget) widget, id);
-				}
-			});
-		} else if (cssTheme != null) {
-			final IThemeEngine themeEngine = createThemeEngine(display,
-					appContext);
+		if (cssTheme != null) {
 			String cssResourcesURI = (String) appContext
 					.get(IWorkbench.CSS_RESOURCE_URI_ARG);
 
@@ -1355,6 +1326,7 @@ public class PartRenderingEngine implements IPresentationEngine {
 					WidgetElement.setID((Widget) widget, id);
 					cssEngine.applyStyles(widget, true);
 				}
+
 			});
 
 			URL url;
