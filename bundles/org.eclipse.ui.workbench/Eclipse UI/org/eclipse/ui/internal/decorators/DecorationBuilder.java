a9f907fcdfc1e812a975adbd0bf177aa31270493
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 02:35:04 +0000

    First commited as after_23683_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/decorators/DecorationBuilder.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/decorators/DecorationBuilder.java
@@ -14,7 +14,6 @@ package org.eclipse.ui.internal.decorators;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.DecorationContext;
 import org.eclipse.jface.viewers.IDecoration;
@@ -30,9 +29,9 @@ public class DecorationBuilder implements IDecoration {
 
 	private static int DECORATOR_ARRAY_SIZE = 6;
 
-	private List prefixes = new ArrayList();
+	private List<String> prefixes = new ArrayList<String>();
 
-	private List suffixes = new ArrayList();
+	private List<String> suffixes = new ArrayList<String>();
 
 	private ImageDescriptor[] descriptors = new ImageDescriptor[DECORATOR_ARRAY_SIZE];
 
@@ -136,8 +135,8 @@ public class DecorationBuilder implements IDecoration {
 		if (clearReplacementImage) {
 			descriptors[IDecoration.REPLACE] = null;
 		}
-		DecorationResult newResult = new DecorationResult(new ArrayList(
-				prefixes), new ArrayList(suffixes), descriptors,
+		DecorationResult newResult = new DecorationResult(new ArrayList<String>(prefixes),
+				new ArrayList<String>(suffixes), descriptors,
 				foregroundColor, backgroundColor, font);
 
 		return newResult;
@@ -185,42 +184,25 @@ public class DecorationBuilder implements IDecoration {
 		valueSet = true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.IDecoration#setBackgroundColor(org.eclipse.swt.graphics.Color)
-	 */
-
+	@Override
 	public void setBackgroundColor(Color bgColor) {
 		this.backgroundColor = bgColor;
 		valueSet = true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.IDecoration#setFont(org.eclipse.swt.graphics.Font)
-	 */
+	@Override
 	public void setFont(Font newFont) {
 		this.font = newFont;
 		valueSet = true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.IDecoration#setForegroundColor(org.eclipse.swt.graphics.Color)
-	 */
+	@Override
 	public void setForegroundColor(Color fgColor) {
 		this.foregroundColor = fgColor;
 		valueSet = true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.viewers.IDecoration#getDecorationContext()
-	 */
+	@Override
 	public IDecorationContext getDecorationContext() {
 		return context;
 	}
