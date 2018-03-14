3079e968867dea7bb81788a9d06e32e574ddd369
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 11:47:09 +0000

    First commited as before_42052_rev4

--- bundles/org.eclipse.jface/src/org/eclipse/jface/action/ToolBarManager.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/action/ToolBarManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,6 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Andrey Loskutov <loskutov@gmx.de> - Bug 457211
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 457214
  *******************************************************************************/
 package org.eclipse.jface.action;
 
@@ -296,8 +295,13 @@ public class ToolBarManager extends ContributionManager implements IToolBarManag
 			}
 		}
 
-		// Turn redraw off to minimize flicker,
-		boolean useRedraw = (clean.size() - (mi.length - toRemove.size())) >= 0;
+		// Turn redraw off if the number of items to be added
+		// is above a certain threshold, to minimize flicker,
+		// otherwise the toolbar can be seen to redraw after each item.
+		// Do this before any modifications are made.
+		// We assume each contribution item will contribute at least one
+		// toolbar item.
+		boolean useRedraw = (clean.size() - (mi.length - toRemove.size())) >= 3;
 		try {
 			if (useRedraw) {
 				toolBar.setRedraw(false);
