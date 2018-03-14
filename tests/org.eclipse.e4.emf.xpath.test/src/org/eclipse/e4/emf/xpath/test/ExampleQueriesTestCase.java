a21aff33ceccb6f60f6468505bace7f822098186
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 08:52:17 +0000

    First commited as after_35995_rev4

--- tests/org.eclipse.e4.emf.xpath.test/src/org/eclipse/e4/emf/xpath/test/ExampleQueriesTestCase.java
+++ tests/org.eclipse.e4.emf.xpath.test/src/org/eclipse/e4/emf/xpath/test/ExampleQueriesTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 BestSolution.at and others.
+ * Copyright (c) 2010, 2014 BestSolution.at and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,19 +7,22 @@
  *
  * Contributors:
  *     Tom Schindl <tom.schindl@bestsolution.at> - adjustment to EObject
+ *     Thibault Le Ouay <thibaultleouay@gmail.com> - Bug 450212
  ******************************************************************************/
 package org.eclipse.e4.emf.xpath.test;
 
-import java.util.Collections;
-import java.util.Iterator;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import junit.framework.TestCase;
+import java.util.Iterator;
 
 import org.apache.commons.jxpath.JXPathNotFoundException;
 import org.eclipse.e4.emf.xpath.EcoreXPathContextFactory;
 import org.eclipse.e4.emf.xpath.XPathContext;
 import org.eclipse.e4.emf.xpath.XPathContextFactory;
-import org.eclipse.e4.emf.xpath.test.model.xpathtest.Menu;
 import org.eclipse.e4.emf.xpath.test.model.xpathtest.XpathtestPackage;
 import org.eclipse.e4.emf.xpath.test.model.xpathtest.impl.ExtendedNodeImpl;
 import org.eclipse.e4.emf.xpath.test.model.xpathtest.impl.MenuImpl;
@@ -31,13 +34,16 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class ExampleQueriesTestCase extends TestCase {
+public class ExampleQueriesTestCase {
 	private ResourceSet resourceSet;
 	private XPathContext xpathContext;
 	private Resource resource;
 
-	
+	@Test
 	public void testSimpleQuery() {
 
 		Object application = xpathContext.getValue("/");
@@ -60,34 +66,39 @@ public class ExampleQueriesTestCase extends TestCase {
 		application = xpathContext.getValue(".[@id='root']");
 		assertNotNull(application);
 		assertSame(RootImpl.class, application.getClass());
-		
-		assertEquals("element1",xpathContext.getValue("nodes[1]/@id"));
-		
-		assertEquals(NodeImpl.class, xpathContext.getValue("//.[@id='element2.2']").getClass());
-		assertEquals(ExtendedNodeImpl.class,xpathContext.getValue("//.[ecore:eClassName(.)='ExtendedNode']").getClass());
-		
+
+		assertEquals("element1", xpathContext.getValue("nodes[1]/@id"));
+
+		assertEquals(NodeImpl.class,
+				xpathContext.getValue("//.[@id='element2.2']").getClass());
+		assertEquals(ExtendedNodeImpl.class,
+				xpathContext
+						.getValue("//.[ecore:eClassName(.)='ExtendedNode']")
+						.getClass());
+
 	}
-	
+
+	@Test
 	public void testMenuQuery() {
 		Object application = xpathContext.getValue("/");
 		assertNotNull(application);
 		assertSame(RootImpl.class, application.getClass());
-		
-		Object node = xpathContext.getValue("//.[@id='menuContainer.1']/menus[@id='menu.1']");
+
+		Object node = xpathContext
+				.getValue("//.[@id='menuContainer.1']/menus[@id='menu.1']");
 		assertNotNull(node);
-		
-		Iterator<Object> i  = xpathContext.iterate("//.[@id='menu.1']");
+
+		Iterator<Object> i = xpathContext.iterate("//.[@id='menu.1']");
 		assertTrue(i.hasNext());
 		assertSame(NodeImpl.class, i.next().getClass());
 		assertTrue(i.hasNext());
 		assertSame(MenuImpl.class, i.next().getClass());
 		// EMF model has a loop in it, it just goes back to the top
-		//assertFalse(i.hasNext());
+		// assertFalse(i.hasNext());
 	}
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
+	@Before
+	public void setUp() {
 		resourceSet = new ResourceSetImpl();
 		resourceSet
 				.getResourceFactoryRegistry()
@@ -106,12 +117,11 @@ public class ExampleQueriesTestCase extends TestCase {
 		xpathContext = f.newContext(resource.getContents().get(0));
 	}
 
-	@Override
-	protected void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
 		xpathContext = null;
 		resource.unload();
 		resourceSet.getResources().remove(resource);
-		super.tearDown();
 	}
 
 }
