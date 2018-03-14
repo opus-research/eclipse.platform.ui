930a390aa5078114dad62eac3f9e51a67996659f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:56:58 +0000

    First commited as before_25368_rev1

--- bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/CleanupProcessor.java
+++ bundles/org.eclipse.e4.ui.workbench.addons.swt/src/org/eclipse/e4/ui/workbench/addons/swt/CleanupProcessor.java
@@ -8,12 +8,11 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Lars Vogel <Lars.Vogel@gmail.com> - Bug 429421
+ *     Louis-Michel Mathurin <mathurin.lm@agiledss.com> - Bug 432146
  *******************************************************************************/
 package org.eclipse.e4.ui.workbench.addons.swt;
 
-import java.util.List;
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.model.application.MAddon;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
@@ -23,20 +22,9 @@ import org.eclipse.e4.ui.workbench.modeling.EModelService;
 public class CleanupProcessor {
 	@Execute
 	void addCleanupAddon(MApplication app, EModelService modelService) {
-		List<MAddon> addons = app.getAddons();
+		final String contributionURI = "bundleclass://org.eclipse.e4.ui.workbench.addons.swt/org.eclipse.e4.ui.workbench.addons.cleanupaddon.CleanupAddon"; //$NON-NLS-1$
+		final String id = "CleanupAddon"; //$NON-NLS-1$
 
-		// prevent multiple copies
-		for (MAddon addon : addons) {
-			if (addon.getContributionURI()
-					.contains("ui.workbench.addons.cleanupaddon.CleanupAddon")) //$NON-NLS-1$
-				return;
-		}
-
-		// adds the add-on to the application model
-		MAddon cleanupAddon = modelService.createModelElement(MAddon.class);
-		cleanupAddon.setElementId("CleanupAddon"); //$NON-NLS-1$
-		cleanupAddon
-				.setContributionURI("bundleclass://org.eclipse.e4.ui.workbench.addons.swt/org.eclipse.e4.ui.workbench.addons.cleanupaddon.CleanupAddon"); //$NON-NLS-1$
-		app.getAddons().add(cleanupAddon);
+		ProcessorUtil.addAddon(app, modelService, contributionURI, id);
 	}
 }
