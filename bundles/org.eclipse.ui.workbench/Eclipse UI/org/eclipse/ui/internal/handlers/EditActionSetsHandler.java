e2e918898cb58b3da9a27a2327c5816dc6f5e6a1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 10:39:31 +0000

    First commited as after_39564_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/EditActionSetsHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/EditActionSetsHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,39 +7,45 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 457364
  ******************************************************************************/
 
 package org.eclipse.ui.internal.handlers;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.jface.window.Window;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.internal.Perspective;
 import org.eclipse.ui.internal.WorkbenchPage;
+import org.eclipse.ui.internal.WorkbenchWindow;
+import org.eclipse.ui.internal.dialogs.cpd.CustomizePerspectiveDialog;
 
 /**
- * Customize the action sets and shortcuts.
+ * Opens the CustomizePerspectiveDialog to configure the available menu and
+ * toolbar entries.
  * 
- * @since 3.4
  */
 public class EditActionSetsHandler extends AbstractHandler {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
-	 */
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil
-				.getActiveWorkbenchWindow(event);
-
+	public Object execute(ExecutionEvent event) {
+		WorkbenchWindow activeWorkbenchWindow = (WorkbenchWindow) HandlerUtil.getActiveWorkbenchWindow(event);
+		IEclipseContext ctx = (IEclipseContext) activeWorkbenchWindow.getService(IEclipseContext.class);
+		MWindow mWindow = ctx.get(MWindow.class);
 		if (activeWorkbenchWindow != null) {
-			WorkbenchPage page = (WorkbenchPage) activeWorkbenchWindow
-					.getActivePage();
-			if (page != null) {
-				page.editActionSets();
+			WorkbenchPage page = (WorkbenchPage) activeWorkbenchWindow.getActivePage();
+			Perspective activePerspective = page.getActivePerspective();
+			// Create and open CPD dialog.
+			CustomizePerspectiveDialog dlg = activeWorkbenchWindow.createCustomizePerspectiveDialog(activePerspective,
+					mWindow.getContext());
+			if (dlg.open() == Window.OK) {
+				activeWorkbenchWindow.updateActionSets();
+				activeWorkbenchWindow.firePerspectiveChanged(page, page.getPerspective(), IWorkbenchPage.CHANGE_RESET);
+				activeWorkbenchWindow.firePerspectiveChanged(page, page.getPerspective(), IWorkbenchPage.CHANGE_RESET);
 			}
 		}
 		return null;
