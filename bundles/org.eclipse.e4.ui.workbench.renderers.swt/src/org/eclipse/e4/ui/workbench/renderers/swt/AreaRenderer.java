a8d05a855c7c20b66e63dbc40c79e4dc3687314b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 10:13:05 +0000

    First commited as before_39128_rev4

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/AreaRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/AreaRenderer.java
@@ -35,8 +35,6 @@ import org.osgi.service.event.EventHandler;
  */
 public class AreaRenderer extends SWTPartRenderer {
 
-	private static final String MAXIMIZEABLE_CHILDREN_TAG = "MaximizeableChildren"; //$NON-NLS-1$
-
 	@Inject
 	private IEventBroker eventBroker;
 
@@ -151,10 +149,8 @@ public class AreaRenderer extends SWTPartRenderer {
 			ctf.setMinimized(curCTF.getMinimized());
 			ctf.setMaximized(curCTF.getMaximized());
 
-			if (!areaModel.getTags().contains(MAXIMIZEABLE_CHILDREN_TAG)) {
-				curCTF.setMinimizeVisible(false);
-				curCTF.setMaximizeVisible(false);
-			}
+			curCTF.setMinimizeVisible(false);
+			curCTF.setMaximizeVisible(false);
 		}
 
 		CTabItem cti = new CTabItem(ctf, SWT.NONE);
