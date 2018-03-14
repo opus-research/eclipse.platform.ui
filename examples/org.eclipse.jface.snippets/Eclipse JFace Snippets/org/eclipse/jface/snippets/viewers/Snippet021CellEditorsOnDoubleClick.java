41242a6d32137e0774f0f8b2ec7da9737b1379c5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 19:07:11 +0000

    First commited as after_54078_rev2

--- examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet021CellEditorsOnDoubleClick.java
+++ examples/org.eclipse.jface.snippets/Eclipse JFace Snippets/org/eclipse/jface/snippets/viewers/Snippet021CellEditorsOnDoubleClick.java
@@ -10,6 +10,7 @@
  *     Dinko Ivanov - bug 164365
  *     Jeanderson Candido <http://jeandersonbc.github.io> - Bug 414565
  *     Simon Scholz <simon.scholz@vogella.com> - Bug 448143
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 475361
  *******************************************************************************/
 
 package org.eclipse.jface.snippets.viewers;
@@ -141,7 +142,7 @@ public class Snippet021CellEditorsOnDoubleClick {
 	}
 
 	private List<MyModel> createModel() {
-		List<MyModel> elements = new ArrayList<MyModel>();
+		List<MyModel> elements = new ArrayList<>();
 
 		for (int i = 0; i < 10; i++) {
 			elements.add(new MyModel(i));
