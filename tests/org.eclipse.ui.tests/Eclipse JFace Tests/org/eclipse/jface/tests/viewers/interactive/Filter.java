79c083454f7d490d6052b0000bd49fd6e334b7eb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 04:08:18 +0000

    First commited as before_25751_rev2

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/interactive/Filter.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/interactive/Filter.java
@@ -17,20 +17,26 @@ import org.eclipse.jface.viewers.ViewerFilter;
 
 public class Filter extends ViewerFilter {
 
-	@Override
-	public Object[] filter(Viewer viewer, Object parent, Object[] elements) {
-		ArrayList result = new ArrayList();
-		for (int i = 0; i < elements.length; ++i) {
-			// toss every second item
-			if (i % 2 == 1) {
-				result.add(elements[i]);
-			}
-		}
-		return result.toArray();
-	}
+    public Object[] filter(Viewer viewer, Object parent, Object[] elements) {
+        ArrayList result = new ArrayList();
+        for (int i = 0; i < elements.length; ++i) {
+            // toss every second item
+            if (i % 2 == 1) {
+                result.add(elements[i]);
+            }
+        }
+        return result.toArray();
+    }
 
-	@Override
-	public boolean select(Viewer viewer, Object parentElement, Object element) {
-		return false;
-	}
+    public boolean isFilterProperty(Object element, Object aspect) {
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * Method declared on ViewerFilter
+     */
+    public boolean select(Viewer viewer, Object parentElement, Object element) {
+        // not used
+        return false;
+    }
 }
