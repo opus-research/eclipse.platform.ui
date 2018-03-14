b29f8ab8031fc7905f712773374cb38ab8b75c5b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 21:35:20 +0000

    First commited as before_59855_rev2

--- bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/widgets/ExpandableComposite.java
+++ bundles/org.eclipse.ui.forms/src/org/eclipse/ui/forms/widgets/ExpandableComposite.java
@@ -348,9 +348,12 @@ public class ExpandableComposite extends Canvas {
 				}
 				textClientCache.setBounds(tcx, y, tcsize.x, tcsize.y);
 			}
-			int height = Math.max(tcsize.y, size.y); // max of label/text client
-			height = Math.max(height, tsize.y); // or max of toggle
-			y += height;
+			int tbarHeight = 0;
+			if (size.y > 0)
+				tbarHeight = size.y;
+			if (tcsize.y > 0)
+				tbarHeight = Math.max(tbarHeight, tcsize.y);
+			y += tbarHeight;
 			if (hasTitleBar())
 				y += tvmargin;
 			if (getSeparatorControl() != null) {
@@ -359,7 +362,7 @@ public class ExpandableComposite extends Canvas {
 						clientArea.width - marginWidth - marginWidth,
 						SEPARATOR_HEIGHT);
 				y += SEPARATOR_HEIGHT;
-				if (expanded && client!=null)
+				if (expanded)
 					y += VSPACE;
 			}
 			if (expanded) {
@@ -396,7 +399,7 @@ public class ExpandableComposite extends Canvas {
 				boolean changed) {
 			initCache(changed);
 
-			int width = 0;
+			int width = 0, height = 0;
 			Point tsize = NULL_SIZE;
 			int twidth = 0;
 			if (toggle != null) {
@@ -456,10 +459,7 @@ public class ExpandableComposite extends Canvas {
 				width += IGAP + tcsize.x;
 			if (toggle != null)
 				width += twidth;
-
-			int height = Math.max(tcsize.y, size.y); // max of label/text client
-			height = Math.max(height, tsize.y); // or max of toggle
-
+			height = tcsize.y > 0 ? Math.max(tcsize.y, size.y) : size.y;
 			if (getSeparatorControl() != null) {
 				height += VSPACE + SEPARATOR_HEIGHT;
 				if (expanded && client != null)
@@ -505,6 +505,8 @@ public class ExpandableComposite extends Canvas {
 				if (expanded)
 					height += csize.y;
 			}
+			if (toggle != null)
+				height = height - size.y + Math.max(size.y, tsize.y);
 
 			Point result = new Point(width + marginWidth + marginWidth
 					+ thmargin + thmargin, height + marginHeight + marginHeight
