b4fceaed27a8895e902cc3ded8a43c21d63015a5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 12:06:38 +0000

    First commited as after_42388_rev1

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/OpenWithMenu.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/actions/OpenWithMenu.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *     IBM Corporation - initial API and implementation
  *     Benjamin Muskalla -	Bug 29633 [EditorMgmt] "Open" menu should
  *     						have Open With-->Other
+ *     Andrey Loskutov <loskutov@gmx.de> - Bug 378485 
  *******************************************************************************/
 package org.eclipse.ui.actions;
 
@@ -209,11 +210,9 @@ public class OpenWithMenu extends ContributionItem {
                 case SWT.Selection:
                    	EditorSelectionDialog dialog = new EditorSelectionDialog(
 							menu.getShell());
-					dialog
-							.setMessage(NLS
-									.bind(
-											IDEWorkbenchMessages.OpenWithMenu_OtherDialogDescription,
-											fileResource.getName()));
+					String fileName = fileResource.getName();
+					dialog.setFileName(fileName);
+					dialog.setMessage(NLS.bind(IDEWorkbenchMessages.OpenWithMenu_OtherDialogDescription, fileName));
 					if (dialog.open() == Window.OK) {
 						IEditorDescriptor editor = dialog.getSelectedEditor();
 						if (editor != null) {
@@ -303,7 +302,7 @@ public class OpenWithMenu extends ContributionItem {
         if (this.file instanceof IFile) {
             return (IFile) this.file;
         }
-        IResource resource = (IResource) this.file
+        IResource resource = this.file
                 .getAdapter(IResource.class);
         if (resource instanceof IFile) {
             return (IFile) resource;
--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchMessages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2014 IBM Corporation and others.
+ * Copyright (c) 2005, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -516,6 +516,7 @@ public class WorkbenchMessages extends NLS {
 	public static String EditorSelection_chooseAnEditor;
 	public static String EditorSelection_internal;
 	public static String EditorSelection_external;
+	public static String EditorSelection_rememberEditor;
 	public static String EditorSelection_browse;
 	public static String EditorSelection_title;
 
