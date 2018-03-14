e5cc9bacbdfe756fbe3078ceea55a440ea0054ac
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:59:47 +0000

    First commited as before_25487_rev4

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/commands/CommandService.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/commands/CommandService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2014 IBM Corporation and others.
+ * Copyright (c) 2005, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,7 +29,6 @@ import org.eclipse.core.commands.State;
 import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.core.runtime.ISafeRunnable;
 import org.eclipse.core.runtime.SafeRunner;
-import org.eclipse.e4.core.commands.internal.ICommandHelpService;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.internal.workbench.renderers.swt.IUpdateService;
 import org.eclipse.e4.ui.model.application.ui.menu.MItem;
@@ -90,8 +89,6 @@ public final class CommandService implements ICommandService, IUpdateService {
 
 	private IEclipseContext context;
 
-	private ICommandHelpService commandHelpService;
-
 	/**
 	 * Constructs a new instance of <code>CommandService</code> using a
 	 * command manager.
@@ -107,7 +104,6 @@ public final class CommandService implements ICommandService, IUpdateService {
 		this.commandManager = commandManager;
 		this.commandPersistence = new CommandPersistence(commandManager);
 		this.context = context;
-		this.commandHelpService = context.get(ICommandHelpService.class);
 	}
 
 	@Override
@@ -197,17 +193,16 @@ public final class CommandService implements ICommandService, IUpdateService {
 	}
 
 	@Override
-	@SuppressWarnings("unused")
 	public final String getHelpContextId(final Command command)
 			throws NotDefinedException {
-		return commandHelpService.getHelpContextId(command.getId(), context);
+		return commandManager.getHelpContextId(command);
 	}
 
 	@Override
 	public final String getHelpContextId(final String commandId)
 			throws NotDefinedException {
 		final Command command = getCommand(commandId);
-		return getHelpContextId(command);
+		return commandManager.getHelpContextId(command);
 	}
 
 	@Override
@@ -226,8 +221,9 @@ public final class CommandService implements ICommandService, IUpdateService {
 	}
 
 	@Override
-	public final void setHelpContextId(final IHandler handler, final String helpContextId) {
-		commandHelpService.setHelpContextId(handler, helpContextId);
+	public final void setHelpContextId(final IHandler handler,
+			final String helpContextId) {
+		commandManager.setHelpContextId(handler, helpContextId);
 	}
 
 	/**
