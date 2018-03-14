5d0835ea67acad35f6a1b9d281fd0005449ba258
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 09:27:33 +0000

    First commited as before_37709_rev9

--- bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/WebBrowserEditor.java
+++ bundles/org.eclipse.ui.browser/src/org/eclipse/ui/internal/browser/WebBrowserEditor.java
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - Initial API and implementation
- *     Snjezana Peco <snjezana.peco@redhat.com> - Bug 448933
  *******************************************************************************/
 package org.eclipse.ui.internal.browser;
 
@@ -18,11 +17,10 @@ import java.net.URL;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.osgi.util.NLS;
-import org.eclipse.swt.browser.ProgressEvent;
-import org.eclipse.swt.browser.ProgressListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
@@ -79,19 +77,7 @@ public class WebBrowserEditor extends EditorPart implements IBrowserViewerContai
 			style += BrowserViewer.BUTTON_BAR;
 		}
 		webBrowser = new BrowserViewer(parent, style);
-		webBrowser.getBrowser().addProgressListener(new ProgressListener() {
 
-			public void completed(ProgressEvent event) {
-				webBrowser.getBrowser().removeProgressListener(this);
-				String url = webBrowser.getURL();
-				if (url != null && url.startsWith("http")) { //$NON-NLS-1$
-					webBrowser.refresh();
-				}
-			}
-
-			public void changed(ProgressEvent event) {
-			}
-		});
 		webBrowser.setURL(initialURL);
 		webBrowser.setContainer(this);
 
