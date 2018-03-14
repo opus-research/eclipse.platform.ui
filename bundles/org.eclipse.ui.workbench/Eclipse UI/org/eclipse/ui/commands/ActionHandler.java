cafce65e0953f3af2adb9ba8a5474d5bd5af5a84
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 02:46:56 +0000

    First commited as before_23810_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/commands/ActionHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/commands/ActionHandler.java
@@ -13,6 +13,7 @@ package org.eclipse.ui.commands;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.eclipse.core.commands.IHandlerAttributes;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.util.IPropertyChangeListener;
@@ -28,8 +29,6 @@ import org.eclipse.ui.actions.RetargetAction;
  * @deprecated Please use the "org.eclipse.core.commands" plug-in instead.
  * @see org.eclipse.jface.commands.ActionHandler
  */
-@Deprecated
-@SuppressWarnings("all")
 public final class ActionHandler extends AbstractHandler {
 
     /**
@@ -94,7 +93,6 @@ public final class ActionHandler extends AbstractHandler {
      * @param action
      *            the action. Must not be <code>null</code>.
      */
-	@Deprecated
     public ActionHandler(IAction action) {
         if (action == null) {
 			throw new NullPointerException();
@@ -107,7 +105,6 @@ public final class ActionHandler extends AbstractHandler {
      * @see org.eclipse.ui.commands.IHandler#addHandlerListener(org.eclipse.ui.commands.IHandlerListener)
      * @since 3.1
      */
-	@Deprecated
     public void addHandlerListener(IHandlerListener handlerListener) {
         if (!hasListeners()) {
             attachListener();
@@ -169,7 +166,6 @@ public final class ActionHandler extends AbstractHandler {
      * 
      * @see org.eclipse.ui.commands.IHandler#dispose()
      */
-	@Deprecated
     public void dispose() {
         if (hasListeners()) {
             action.removePropertyChangeListener(propertyChangeListener);
@@ -180,7 +176,6 @@ public final class ActionHandler extends AbstractHandler {
     /* (non-Javadoc)
      * @see org.eclipse.ui.commands.IHandler#execute(java.util.Map)
      */
-	@Deprecated
     public Object execute(Map parameterValuesByName) throws ExecutionException {
         if ((action.getStyle() == IAction.AS_CHECK_BOX)
                 || (action.getStyle() == IAction.AS_RADIO_BUTTON)) {
@@ -201,7 +196,6 @@ public final class ActionHandler extends AbstractHandler {
      * @return the action associated with this handler (not null)
      * @since 3.1
      */
-	@Deprecated
     public IAction getAction() {
         return action;
     }
@@ -209,7 +203,6 @@ public final class ActionHandler extends AbstractHandler {
     /* (non-Javadoc)
      * @see org.eclipse.ui.commands.IHandler#getAttributeValuesByName()
      */
-	@Deprecated
     public Map getAttributeValuesByName() {
         if (attributeValuesByName == null) {
             return getAttributeValuesByNameFromAction();
@@ -247,7 +240,6 @@ public final class ActionHandler extends AbstractHandler {
      * @see org.eclipse.ui.commands.IHandler#removeHandlerListener(org.eclipse.ui.commands.IHandlerListener)
      * @since 3.1
      */
-	@Deprecated
     public void removeHandlerListener(IHandlerListener handlerListener) {
         super.removeHandlerListener(handlerListener);
 
@@ -259,7 +251,6 @@ public final class ActionHandler extends AbstractHandler {
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
-	@Deprecated
 	public final String toString() {
 		final StringBuffer buffer = new StringBuffer();
 
