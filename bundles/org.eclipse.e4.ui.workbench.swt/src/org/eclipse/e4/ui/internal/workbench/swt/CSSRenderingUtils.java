44a81f0f32b3790d2411ef5564346cc695fc5195
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 03:59:31 +0000

    First commited as before_25467_rev1

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/CSSRenderingUtils.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/CSSRenderingUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 IBM Corporation and others.
+ * Copyright (c) 2012, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,9 +16,6 @@ import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.css.swt.dom.ControlElement;
 import org.eclipse.e4.ui.css.swt.dom.WidgetElement;
 import org.eclipse.e4.ui.widgets.ImageBasedFrame;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
@@ -29,17 +26,12 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.graphics.Transform;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.w3c.dom.css.CSSPrimitiveValue;
 import org.w3c.dom.css.CSSStyleDeclaration;
 import org.w3c.dom.css.CSSValue;
 import org.w3c.dom.css.CSSValueList;
 
 public class CSSRenderingUtils {
-	private final static String FRAME_IMAGE_PROP = "frame-image";
-
-	private final static String HANDLE_IMAGE_PROP = "handle-image";
 
 	// NOTE: The CSS engine 'owns' the image it returns (it caches it)
 	// so we have to cache any rotated versions to match
@@ -48,14 +40,13 @@ public class CSSRenderingUtils {
 	public Control frameMeIfPossible(Control toFrame, String classId,
 			boolean vertical, boolean draggable) {
 		Integer[] frameInts = new Integer[4];
-		Image frameImage = createImage(toFrame, classId, FRAME_IMAGE_PROP,
+		Image frameImage = createImage(toFrame, classId, "frame-image",
 				frameInts);
 		if (vertical && frameImage != null)
 			frameImage = rotateImage(toFrame.getDisplay(), frameImage,
 					frameInts);
 
-		Image handleImage = createImage(toFrame, classId, HANDLE_IMAGE_PROP,
-				null);
+		Image handleImage = createImage(toFrame, classId, "handle-image", null);
 		if (vertical && handleImage != null)
 			handleImage = rotateImage(toFrame.getDisplay(), handleImage, null);
 
@@ -63,13 +54,11 @@ public class CSSRenderingUtils {
 			ImageBasedFrame frame = new ImageBasedFrame(toFrame.getParent(),
 					toFrame, vertical, draggable);
 			frame.setImages(frameImage, frameInts, handleImage);
-			addFrameImageDisposedListener(frame, toFrame, classId, vertical);
 			return frame;
 		} else if (handleImage != null) {
 			ImageBasedFrame frame = new ImageBasedFrame(toFrame.getParent(),
 					toFrame, vertical, draggable);
 			frame.setImages(null, null, handleImage);
-			addHandleImageDisposedListener(frame, toFrame, classId, vertical);
 			return frame;
 		}
 
@@ -219,78 +208,5 @@ public class CSSRenderingUtils {
 			}
 		}
 		return image;
-	}
-
-	private void addHandleImageDisposedListener(
-			ImageBasedFrame imageBasedFrame, final Control toFrame,
-			final String classId, final boolean vertical) {
-		final Listener listener = new Listener() {
-			@Override
-			public void handleEvent(Event event) {
-				if (!(event.widget instanceof ImageBasedFrame)) {
-					return;
-				}
-
-				Image handleImage = createImage(toFrame, classId,
-						HANDLE_IMAGE_PROP, null);
-				if (vertical && handleImage != null) {
-					handleImage = rotateImage(toFrame.getDisplay(),
-							handleImage, null);
-				}
-				if (handleImage != null) {
-					((ImageBasedFrame) event.widget).setImages(null, null,
-							handleImage);
-				}
-			}
-		};
-
-		toFrame.getDisplay().addListener(SWT.Skin, listener);
-
-		imageBasedFrame.addDisposeListener(new DisposeListener() {
-			@Override
-			public void widgetDisposed(DisposeEvent e) {
-				e.widget.getDisplay().removeListener(SWT.Skin, listener);
-			}
-		});
-	}
-
-	private void addFrameImageDisposedListener(ImageBasedFrame imageBasedFrame,
-			final Control toFrame, final String classId, final boolean vertical) {
-		final Listener listener = new Listener() {
-			@Override
-			public void handleEvent(Event event) {
-				if (!(event.widget instanceof ImageBasedFrame)) {
-					return;
-				}
-
-				ImageBasedFrame frame = (ImageBasedFrame) event.widget;
-				Integer[] frameInts = new Integer[4];
-				Image frameImage = createImage(toFrame, classId,
-						FRAME_IMAGE_PROP, frameInts);
-				if (vertical && frameImage != null) {
-					frameImage = rotateImage(toFrame.getDisplay(), frameImage,
-							frameInts);
-				}
-
-				Image handleImage = createImage(toFrame, classId,
-						HANDLE_IMAGE_PROP, null);
-				if (vertical && handleImage != null) {
-					handleImage = rotateImage(toFrame.getDisplay(),
-							handleImage, null);
-				}
-				if (frameImage != null) {
-					frame.setImages(frameImage, frameInts, handleImage);
-				}
-			 }
-		};
-			
-		toFrame.getDisplay().addListener(SWT.Skin, listener);
-
-		imageBasedFrame.addDisposeListener(new DisposeListener() {
-			@Override
-			public void widgetDisposed(DisposeEvent e) {
-				e.widget.getDisplay().removeListener(SWT.Skin, listener);
-			}
-		});
 	}
 }
