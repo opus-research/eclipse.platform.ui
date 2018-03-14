a9b80b1d3393524c8dead26c602ccf51fa2e152d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 23:47:06 +0000

    First commited as after_63890_rev3

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/TableViewerComparatorTest.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/viewers/TableViewerComparatorTest.java
@@ -11,6 +11,15 @@
 
 package org.eclipse.jface.tests.viewers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.util.ILogger;
+import org.eclipse.jface.util.Policy;
 import org.eclipse.jface.viewers.ColumnLayoutData;
 import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.StructuredViewer;
@@ -84,6 +93,96 @@ public class TableViewerComparatorTest extends ViewerComparatorTest {
 		fViewer.setComparator(new ViewerComparator());
 		team1.addMember("Duong");
 		assertSortedResult(TEAM1_SORTED_WITH_INSERT);
+	}
+
+	/**
+	 * Use a comparator that returns random results.
+	 */
+	public void testViewerComparatorViolatesGeneralContract() throws Exception {
+		Random rand = new Random(0);
+		Comparator<String> violatingComparator = new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return rand.nextInt(3) - 1;
+			}
+		};
+		fViewer.setComparator(new ViewerComparator(violatingComparator));
+
+		String[] members = new String[30000];
+		for (int i = 0; i < members.length; i++) {
+			StringBuilder name = rand.ints(5, 'A', 'Z').collect(StringBuilder::new, StringBuilder::appendCodePoint,
+					StringBuilder::append);
+			members[i] = name.toString();
+		}
+		Team randTeam = new Team("RAND", members);
+
+		IllegalArgumentException exception = null;
+		ILogger logger = Policy.getLog();
+		Policy.setLog(new ILogger() {
+			@Override
+			public void log(IStatus status) {
+				// ignore
+			}
+		});
+		try {
+			fViewer.setInput(randTeam);
+		} catch (IllegalArgumentException e) {
+			exception = e;
+			Policy.setLog(logger);
+		}
+		assertNotNull(exception);
+	}
+
+	/**
+	 * Use a label provider that returns unstable labels for the same object.
+	 */
+	public void testViewerComparatorViolatesGeneralContract2() throws Exception {
+		List<Integer> timSortTestList = new ArrayList<Integer>();
+		{
+			for (int i = 0; i < 64; ++i) {
+				timSortTestList.add(i);
+			}
+			Collections.shuffle(timSortTestList, new Random(0));
+		}
+		Comparator<Integer> broken = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (o1 < 5) {
+					return 1;
+				}
+				return Integer.compare(o1, o2);
+			}
+		};
+		Collections.sort(timSortTestList, broken); // throws up
+
+		Random rand = new Random(0);
+		fViewer.setLabelProvider(new TeamModelLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				return Math.abs(rand.nextInt()) + ".";
+			}
+		});
+
+		String[] members = new String[300];
+		for (int i = 0; i < members.length; i++) {
+			String name;
+			if (i % 3 == 0) {
+				name = Integer.toString(i);
+			} else {
+				name = Integer.toString(i - 3);
+			}
+			members[i] = name.toString();
+		}
+		Team randTeam = new Team("RAND", members);
+
+		IllegalArgumentException exception = null;
+		try {
+			fViewer.setComparator(new ViewerComparator());
+			fViewer.setInput(randTeam);
+		} catch (IllegalArgumentException e) {
+			exception = e;
+		}
+		assertNotNull(exception);
 	}
 
 	private void assertSortedResult(String[] expected){
