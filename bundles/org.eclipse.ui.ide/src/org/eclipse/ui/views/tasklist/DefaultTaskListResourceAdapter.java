89c7b9d0a6290dcfdae880d6a9d3f399f85cabbf
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 20:12:32 +0000

    First commited as before_56796_rev3

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/tasklist/DefaultTaskListResourceAdapter.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/tasklist/DefaultTaskListResourceAdapter.java
@@ -13,7 +13,6 @@ package org.eclipse.ui.views.tasklist;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.Adapters;
 import org.eclipse.core.runtime.IAdaptable;
 
 /**
@@ -48,9 +47,9 @@ class DefaultTaskListResourceAdapter implements ITaskListResourceAdapter {
     @Override
 	public IResource getAffectedResource(IAdaptable adaptable) {
 
-		IResource resource = Adapters.getAdapter(adaptable, IResource.class, true);
+        IResource resource = adaptable.getAdapter(IResource.class);
         if (resource == null) {
-			return Adapters.getAdapter(resource, IFile.class, true);
+			return adaptable.getAdapter(IFile.class);
 		}
 		return resource;
 
