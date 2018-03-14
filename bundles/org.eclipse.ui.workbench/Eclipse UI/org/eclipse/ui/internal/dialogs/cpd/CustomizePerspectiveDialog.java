49a84b2ffd423559c24c6925f972191ba463c7eb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-14 11:27:14 +0000

    First commited as before_41385_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/cpd/CustomizePerspectiveDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/cpd/CustomizePerspectiveDialog.java
@@ -2149,9 +2149,6 @@ public class CustomizePerspectiveDialog extends TrayDialog {
 			if (text == null) {
 				text = item.getToolTipText();
 			}
-			if (text == null) {
-				text = item.getText();
-			}
 			if (iconDescriptor == null) {
 				Image image = item.getImage();
 				if (image != null) {
