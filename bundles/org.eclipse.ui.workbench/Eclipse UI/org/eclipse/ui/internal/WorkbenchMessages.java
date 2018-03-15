926c1afb920a182eed7b9434bc20bc4ef6425260
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:46:40 +0000

    First commited as before_87634_rev6

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchMessages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchMessages.java
@@ -242,6 +242,7 @@ public class WorkbenchMessages extends NLS {
 	public static String SavePerspective_text;
 	public static String SavePerspective_toolTip;
 	public static String SavePerspective_shellTitle;
+	public static String SavePerspective_saveButtonLabel;
 	public static String SavePerspectiveDialog_description;
 	public static String SavePerspective_name;
 	public static String SavePerspective_existing;
@@ -634,6 +635,10 @@ public class WorkbenchMessages extends NLS {
 	public static String Save_All;
 	public static String Dont_Save;
 
+	public static String SaveableHelper_Save;
+	public static String SaveableHelper_Cancel;
+	public static String SaveableHelper_Dont_Save;
+
 
 	// ==============================================================================
 	// Perspective Framework
@@ -979,6 +984,7 @@ public class WorkbenchMessages extends NLS {
 	public static String WorkbenchSettings_CouldNotFindLocation;
 	public static String WorkingSets_Name;
 	public static String WorkingSets_CannotSave;
+	public static String WorkbenchPreferences_Name;
 
 	// StatusDialog
 	public static String WorkbenchStatusDialog_SupportTooltip;
@@ -1007,6 +1013,8 @@ public class WorkbenchMessages extends NLS {
 	public static String WorkingSetGroup_WorkingSets_group;
 	public static String WorkingSetGroup_WorkingSetSelection_message;
 	public static String WorkingSetGroup_EnableWorkingSet_button;
+
+	public static String FilteredTableBaseHandler_Close;
 
 	// Util
 	public static String Util_List;
