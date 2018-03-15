938e0284b91d95116e97c63ded4def9159a86532
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:30:30 +0000

    First commited as after_70356_rev1

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarContributionRecord.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/ToolBarContributionRecord.java
@@ -156,11 +156,17 @@ public class ToolBarContributionRecord {
 		}
 
 		for (MToolBarElement child : childrenToInspect) {
-			if (child.getVisibleWhen() != null
-					|| child.getPersistedState().get(
-							MenuManagerRenderer.VISIBILITY_IDENTIFIER) != null) {
+			if (anyVisibleWhen(child)) {
 				return true;
 			}
+		}
+		return false;
+	}
+
+	private boolean anyVisibleWhen(MToolBarElement toolBarElement) {
+		if (toolBarElement.getVisibleWhen() != null
+				|| toolBarElement.getPersistedState().get(MenuManagerRenderer.VISIBILITY_IDENTIFIER) != null) {
+			return true;
 		}
 		return false;
 	}
@@ -186,7 +192,7 @@ public class ToolBarContributionRecord {
 		for (MToolBarElement copy : copyElements) {
 			// if a visibleWhen clause is defined, the item should not be
 			// visible until the clause has been evaluated and returned 'true'
-			copy.setVisible(!anyVisibleWhen());
+			copy.setVisible(!anyVisibleWhen(copy));
 			if (copy instanceof MToolBarSeparator) {
 				MToolBarSeparator shared = findExistingSeparator(copy
 						.getElementId());
