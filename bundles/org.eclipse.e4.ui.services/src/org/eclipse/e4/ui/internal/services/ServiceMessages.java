191b6da2a531780f006e01bf84c75fef121190f5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:54:12 +0000

    First commited as after_81628_rev3

--- bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/internal/services/ServiceMessages.java
+++ bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/internal/services/ServiceMessages.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2005, 2015 IBM Corporation and others. All rights reserved.   This
+ * Copyright (c) 2005, 2016 IBM Corporation and others. All rights reserved.   This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -9,22 +9,10 @@
  **********************************************************************/
 package org.eclipse.e4.ui.internal.services;
 
-import org.eclipse.osgi.util.NLS;
-
-public class ServiceMessages extends NLS {
-
-	private static final String BUNDLE_NAME = "org.eclipse.e4.ui.internal.services.serviceMessages"; //$NON-NLS-1$
+public class ServiceMessages {
 
 	// Event broker
-	public static String NO_EVENT_ADMIN;
-	public static String NO_BUNDLE_CONTEXT;
+	public static final String NO_EVENT_ADMIN = "Event Admin service is not available, unable to publish event {0}.";
+	public static final String NO_BUNDLE_CONTEXT = "Bundle context is not available, unable to subscribe for {0}.";
 
-	static {
-		// load message values from bundle file
-		reloadMessages();
-	}
-
-	public static void reloadMessages() {
-		NLS.initializeMessages(BUNDLE_NAME, ServiceMessages.class);
-	}
 }
