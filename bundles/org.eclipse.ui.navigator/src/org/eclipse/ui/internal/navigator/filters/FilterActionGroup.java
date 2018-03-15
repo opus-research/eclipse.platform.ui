d9e637b624440a8a9602c76b0a6e0abae224f50d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:35:18 +0000

    First commited as before_70848_rev5

--- bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/filters/FilterActionGroup.java
+++ bundles/org.eclipse.ui.navigator/src/org/eclipse/ui/internal/navigator/filters/FilterActionGroup.java
@@ -11,27 +11,21 @@
 
 package org.eclipse.ui.internal.navigator.filters;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.eclipse.core.runtime.Assert;
-import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.actions.ActionGroup;
-import org.eclipse.ui.internal.navigator.CommonNavigatorMessages;
-import org.eclipse.ui.internal.navigator.NavigatorFilterService;
 import org.eclipse.ui.internal.navigator.NavigatorPlugin;
 import org.eclipse.ui.navigator.CommonViewer;
-import org.eclipse.ui.navigator.ICommonFilterDescriptor;
-import org.eclipse.ui.navigator.IMementoAware;
 import org.eclipse.ui.navigator.INavigatorViewerDescriptor;
 
 /**
@@ -40,24 +34,19 @@ import org.eclipse.ui.navigator.INavigatorViewerDescriptor;
  * @since 3.2
  *
  */
-public class FilterActionGroup extends ActionGroup implements IMementoAware {
+public class FilterActionGroup extends ActionGroup {
 
 	private static final String FILTER_ACTION_GROUP = "filterActionGroup"; //$NON-NLS-1$
 
-	private static final int MAX_FILTER_MENU_ENTRIES = 3;
+	private static final String FILTER_ACTION_GROUP_FILTERS_START = FILTER_ACTION_GROUP+"Filters-start"; //$NON-NLS-1$
 
-	private static final String TAG_LRU_FILTERS = "lastRecentlyUsedFilters"; //$NON-NLS-1$
-	private static final String TAG_CHILD = "child"; //$NON-NLS-1$
-	private static final String TAG_FILTER_ID = "filterId"; //$NON-NLS-1$
+	private static final String FILTER_ACTION_GROUP_FILTERS_END = FILTER_ACTION_GROUP+"Filters-end"; //$NON-NLS-1$
 
 	private SelectFiltersAction selectFiltersAction;
-	private IMenuManager menuManager;
-	private IMenuListener menuListener;
-	private IMenuManager filtersMenu;
 	private CommonViewer commonViewer;
 	private INavigatorViewerDescriptor viewerDescriptor;
 
-	private Deque<ICommonFilterDescriptor> lruFilterDescriptorStack = new ArrayDeque<>();
+	private final Set filterShortcutActions = new LinkedHashSet();
 
 
 	/**
@@ -72,65 +61,31 @@ public class FilterActionGroup extends ActionGroup implements IMementoAware {
 
 	@Override
 	public void fillActionBars(IActionBars actionBars) {
-		menuManager = actionBars.getMenuManager();
-		menuManager.appendToGroup(IWorkbenchActionConstants.MB_ADDITIONS,
+		IMenuManager menu = actionBars.getMenuManager();
+		menu.appendToGroup(IWorkbenchActionConstants.MB_ADDITIONS,
 				new Separator(FILTER_ACTION_GROUP));
 		if (selectFiltersAction != null) {
-			menuManager.addMenuListener(menuListener);
-			menuManager.appendToGroup(FILTER_ACTION_GROUP,
-					filtersMenu);
-			menuManager.appendToGroup(FILTER_ACTION_GROUP,
+			menu.appendToGroup(FILTER_ACTION_GROUP,
 					selectFiltersAction);
+
+			menu.appendToGroup(FILTER_ACTION_GROUP,
+					new GroupMarker(FILTER_ACTION_GROUP_FILTERS_START));
+
+			menu.appendToGroup(FILTER_ACTION_GROUP_FILTERS_START,
+					new Separator(FILTER_ACTION_GROUP_FILTERS_END));
+
+
+			for (Iterator iter = filterShortcutActions.iterator(); iter.hasNext();) {
+				IAction action = (IAction) iter.next();
+				menu.appendToGroup(FILTER_ACTION_GROUP_FILTERS_START, action);
+			}
+
 		}
 	}
 
 	@Override
 	public void fillContextMenu(IMenuManager menu) {
 		super.fillContextMenu(menu);
-	}
-
-	@Override
-	public void dispose() {
-		super.dispose();
-		if (menuManager != null) {
-			menuManager.removeMenuListener(menuListener);
-		}
-		if (filtersMenu != null) {
-			filtersMenu.dispose();
-		}
-	}
-
-	@Override
-	public void restoreState(IMemento aMemento) {
-		IMemento lruFilters = aMemento.getChild(TAG_LRU_FILTERS);
-		lruFilterDescriptorStack.clear();
-		if (lruFilters != null) {
-			NavigatorFilterService filterService = (NavigatorFilterService) commonViewer.getNavigatorContentService()
-					.getFilterService();
-			ICommonFilterDescriptor[] visibleFilterDescriptors = filterService.getVisibleFilterDescriptorsForUI();
-			for (IMemento child : lruFilters.getChildren(TAG_CHILD)) {
-				String id = child.getString(TAG_FILTER_ID);
-				if (id != null) {
-					for (ICommonFilterDescriptor visibleFilterDescriptor : visibleFilterDescriptors) {
-						if (visibleFilterDescriptor.getId().equals(id)) {
-							lruFilterDescriptorStack.push(visibleFilterDescriptor);
-							break;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	@Override
-	public void saveState(IMemento aMemento) {
-		IMemento lruFilters = aMemento.createChild(TAG_LRU_FILTERS);
-		if (!lruFilterDescriptorStack.isEmpty()) {
-			for (ICommonFilterDescriptor filterDescriptor : lruFilterDescriptorStack) {
-				IMemento child = lruFilters.createChild(TAG_CHILD);
-				child.putString(TAG_FILTER_ID, filterDescriptor.getId());
-			}
-		}
 	}
 
 	/**
@@ -144,55 +99,10 @@ public class FilterActionGroup extends ActionGroup implements IMementoAware {
 			ImageDescriptor selectFiltersIcon = NavigatorPlugin.getImageDescriptor("icons/full/elcl16/filter_ps.png"); //$NON-NLS-1$
 			selectFiltersAction.setImageDescriptor(selectFiltersIcon);
 			selectFiltersAction.setHoverImageDescriptor(selectFiltersIcon);
-
-			filtersMenu = new MenuManager(CommonNavigatorMessages.FilterActionGroup_Filters);
-			menuListener = new IMenuListener() {
-
-				@Override
-				public void menuAboutToShow(IMenuManager manager) {
-					filtersMenu.removeAll();
-					addLRUFilterActions(filtersMenu);
-				}
-
-			};
 		}
 	}
 
-	private void addLRUFilterActions(IMenuManager manager) {
-		if (lruFilterDescriptorStack.isEmpty()) {
-			return;
-		}
+	protected void updateFilterShortcuts() {
 
-		NavigatorFilterService filterService = (NavigatorFilterService) commonViewer.getNavigatorContentService()
-				.getFilterService();
-		ICommonFilterDescriptor[] filterDescriptors = lruFilterDescriptorStack
-				.toArray(new ICommonFilterDescriptor[lruFilterDescriptorStack.size()]);
-		Arrays.sort(filterDescriptors, new Comparator<ICommonFilterDescriptor>() {
-
-			@Override
-			public int compare(ICommonFilterDescriptor o1, ICommonFilterDescriptor o2) {
-				return o1.getName().compareTo(o2.getName());
-			}
-		});
-
-		for (ICommonFilterDescriptor filterDescriptor : filterDescriptors) {
-			manager.add(new ToggleFilterAction(commonViewer, filterService, filterDescriptor));
-		}
-	}
-
-	protected void updateFilterShortcuts(ICommonFilterDescriptor[] filterDescriptorChangeHistory) {
-		Deque<ICommonFilterDescriptor> oldestFirstStack = new ArrayDeque<>();
-		int length = Math.min(filterDescriptorChangeHistory.length, MAX_FILTER_MENU_ENTRIES);
-		for (int i = 0; i < length; i++) {
-			oldestFirstStack.push(filterDescriptorChangeHistory[i]);
-		}
-
-		length = Math.min(lruFilterDescriptorStack.size(), MAX_FILTER_MENU_ENTRIES - oldestFirstStack.size());
-		for (int i = 0; i < length; i++) {
-			ICommonFilterDescriptor filter = lruFilterDescriptorStack.pollFirst();
-			if (!oldestFirstStack.contains(filter))
-				oldestFirstStack.push(filter);
-		}
-		lruFilterDescriptorStack = oldestFirstStack;
 	}
 }
