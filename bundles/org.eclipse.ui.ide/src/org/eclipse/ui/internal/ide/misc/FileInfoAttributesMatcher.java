bc5fc6b3475d48a5241a4bacc754bca16bdb6ad0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:27:04 +0000

    First commited as before_65695_rev5

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/misc/FileInfoAttributesMatcher.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/misc/FileInfoAttributesMatcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2016 Freescale Semiconductor and others.
+ * Copyright (c) 2008, 2015 Freescale Semiconductor and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,6 @@
  * Contributors:
  *     Serge Beauchamp (Freescale Semiconductor) - [252996] initial API and implementation
  *     IBM Corporation - ongoing implementation
- *     Mickael Istria (Red Hat Inc.) - [Cleanup] Avoid useless string instances
  *******************************************************************************/
 package org.eclipse.ui.internal.ide.misc;
 
@@ -125,7 +124,7 @@ public class FileInfoAttributesMatcher extends AbstractFileInfoMatcher {
 	 */
 	public static class Argument {
 		public String key = KEY_NAME;
-		public String pattern = ""; //$NON-NLS-1$
+		public String pattern = new String();
 		public String operator = OPERATOR_EQUALS;
 		public boolean caseSensitive = false;
 		public boolean regularExpression = false;
@@ -276,7 +275,7 @@ public class FileInfoAttributesMatcher extends AbstractFileInfoMatcher {
 
 		public boolean match(IContainer parent, IFileInfo fileInfo) {
 			if (type.equals(String.class)) {
-				String value = ""; //$NON-NLS-1$
+				String value = new String();
 				if (argument.key.equals(KEY_NAME))
 					value = fileInfo.getName();
 				if (argument.key.equals(KEY_PROPJECT_RELATIVE_PATH))
--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/tasklist/TaskListContentProvider.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/tasklist/TaskListContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,6 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *     Mickael Istria (Red Hat Inc.) - [Cleanup] Take advantage of autoboxing
  *******************************************************************************/
 
 package org.eclipse.ui.views.tasklist;
@@ -98,7 +97,7 @@ class TaskListContentProvider implements IStructuredContentProvider,
             return ""; //$NON-NLS-1$
         }
 
-		return NLS.bind(TaskListMessages.TaskList_statusSummaryVisible, sum(visibleMarkerCounts),
+        return NLS.bind(TaskListMessages.TaskList_statusSummaryVisible,new Integer(sum(visibleMarkerCounts)),
 		getStatusSummaryBreakdown(visibleMarkerCounts));
     }
 
@@ -110,7 +109,7 @@ class TaskListContentProvider implements IStructuredContentProvider,
      */
     public String getStatusSummarySelected(IStructuredSelection selection) {
         int[] selectedMarkerCounts = getMarkerCounts(selection.toList());
-		return NLS.bind(TaskListMessages.TaskList_statusSummarySelected, sum(selectedMarkerCounts),
+        return NLS.bind(TaskListMessages.TaskList_statusSummarySelected, new Integer(sum(selectedMarkerCounts)),
 		getStatusSummaryBreakdown(selectedMarkerCounts) );
     }
 
@@ -121,7 +120,11 @@ class TaskListContentProvider implements IStructuredContentProvider,
     private String getStatusSummaryBreakdown(int[] counts) {
         return NLS.bind(
 				TaskListMessages.TaskList_statusSummaryBreakdown,
-				new Object[] { counts[TASKS], counts[ERRORS], counts[WARNINGS], counts[INFOS] });
+				new Object []{
+						new Integer(counts[TASKS]),
+						new Integer(counts[ERRORS]),
+						new Integer(counts[WARNINGS]),
+						new Integer(counts[INFOS])});
     }
 
     /**
@@ -137,9 +140,10 @@ class TaskListContentProvider implements IStructuredContentProvider,
         TasksFilter filter = taskList.getFilter();
 
         if (filter.isShowingAll()) {
-			return NLS.bind(TaskListMessages.TaskList_titleSummaryUnfiltered, visibleMarkerCount);
+            return NLS.bind(TaskListMessages.TaskList_titleSummaryUnfiltered, new Integer(visibleMarkerCount));
         }
-		return NLS.bind(TaskListMessages.TaskList_titleSummaryFiltered, visibleMarkerCount, getTotalMarkerCount());
+		return NLS.bind(TaskListMessages.TaskList_titleSummaryFiltered, new Integer(visibleMarkerCount),
+				new Integer(getTotalMarkerCount()));
     }
 
     /**
