0657cd50bed2aa9ca33e4af84e0878e19a8adb7c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:10:33 +0000

    First commited as before_90409_rev4

--- bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/IDEWorkbenchWindowAdvisor.java
+++ bundles/org.eclipse.ui.ide.application/src/org/eclipse/ui/internal/ide/application/IDEWorkbenchWindowAdvisor.java
@@ -16,9 +16,7 @@ package org.eclipse.ui.internal.ide.application;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringJoiner;
 
 import org.eclipse.core.resources.IFile;
@@ -31,7 +29,6 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.JFaceResources;
@@ -212,18 +209,12 @@ public class IDEWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 						productName);
 			}
 
-			// use of LinkedHashMap to preserve insertion order
-			Map<String, Integer> buttonLabelToIdMap = new LinkedHashMap<>();
-			buttonLabelToIdMap.put(IDEWorkbenchMessages.PromptOnExitDialog_button_label_exit, IDialogConstants.OK_ID);
-			buttonLabelToIdMap.put(IDialogConstants.CANCEL_LABEL,
-					IDialogConstants.CANCEL_ID);
-			MessageDialogWithToggle dlg =
-					new MessageDialogWithToggle(
-							parentShell,
+			MessageDialogWithToggle dlg = MessageDialogWithToggle
+					.openOkCancelConfirm(parentShell,
 							IDEWorkbenchMessages.PromptOnExitDialog_shellTitle,
-							null,
-							message, MessageDialog.CONFIRM, buttonLabelToIdMap, 0, null, false);
-			dlg.open();
+							message,
+							IDEWorkbenchMessages.PromptOnExitDialog_choice,
+							false, null, null);
 			if (dlg.getReturnCode() != IDialogConstants.OK_ID) {
 				return false;
 			}
