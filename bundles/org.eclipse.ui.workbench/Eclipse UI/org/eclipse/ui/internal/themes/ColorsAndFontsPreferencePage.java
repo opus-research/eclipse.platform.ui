cbee56d248336fa4fd8e761c606108b4a8505d2d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:52:32 +0000

    First commited as after_72438_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/themes/ColorsAndFontsPreferencePage.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/themes/ColorsAndFontsPreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2015 IBM Corporation and others.
+ * Copyright (c) 2003, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2218,10 +2218,15 @@ getPreferenceStore(),
 
 	private boolean isAvailableInCurrentTheme(ThemeElementDefinition definition) {
 		if (definition instanceof ColorDefinition) {
-			RGB value = ((ColorDefinition) definition).getValue();
-			return value != null && value != EMPTY_COLOR_VALUE
-					&& colorRegistry.get(definition.getId()) != null;
+			ColorDefinition colorDef = (ColorDefinition) definition;
+			if (colorDef.getDefaultsTo() == null
+					&& (colorDef.getValue() == null || colorDef.getValue() == EMPTY_COLOR_VALUE)) {
+				return false;
+			}
+
+			return colorRegistry.get(definition.getId()) != null;
 		}
+
 		return true;
 	}
 
