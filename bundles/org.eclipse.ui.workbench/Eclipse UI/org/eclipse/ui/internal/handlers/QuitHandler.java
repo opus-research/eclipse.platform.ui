6f7b415a92cd5de2772baf6ae33bf13e7d0f3c56
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:28:52 +0000

    First commited as after_94056_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/QuitHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/QuitHandler.java
@@ -19,29 +19,20 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
 
 /**
- * Exit the workbench. Normal invocation calls {@link IWorkbench#close()}, which
- * typically doesn't prompt the user before exiting.
- * <p>
- * Invocation with parameter mayPrompt="true" calls {@link Display#close()},
- * which may prompt the user (via a hook installed by
+ * Exit the workbench. Invocation calls {@link Display#close()}, which may
+ * prompt the user (via a hook installed by
  * <code>org.eclipse.ui.internal.ide.application.IDEWorkbenchAdvisor</code>).
  *
  * @since 3.4
  *
  */
 public class QuitHandler extends AbstractHandler {
-	private static final String COMMAND_PARAMETER_ID_MAY_PROMPT = "mayPrompt"; //$NON-NLS-1$
-	private static final String TRUE = "true"; //$NON-NLS-1$
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IEvaluationContext context = (IEvaluationContext) event.getApplicationContext();
 		IWorkbench workbench = (IWorkbench) context.getVariable(IWorkbench.class.getName());
-		if (TRUE.equals(event.getParameter(COMMAND_PARAMETER_ID_MAY_PROMPT))) {
-			workbench.getDisplay().close();
-		} else {
-			workbench.close();
-		}
+		workbench.getDisplay().close();
 		return null;
 	}
 }
