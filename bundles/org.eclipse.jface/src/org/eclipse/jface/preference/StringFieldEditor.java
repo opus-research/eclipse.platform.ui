82fb47507b21ef3fc0add2c6634d1f1ea0bb629b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:20:43 +0000

    First commited as after_78176_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/preference/StringFieldEditor.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/preference/StringFieldEditor.java
@@ -361,7 +361,6 @@ public class StringFieldEditor extends FieldEditor {
                     @Override
 					public void focusLost(FocusEvent e) {
                         valueChanged();
-                        clearErrorMessage();
                     }
                 });
                 break;
