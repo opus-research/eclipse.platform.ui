bb9cdecf0afa9efd430e4296bf6d123c566093e0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:08:43 +0000

    First commited as before_82440_rev17

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/ApiTestSuite.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/ApiTestSuite.java
@@ -12,7 +12,6 @@
 package org.eclipse.ui.tests.api;
 
 import org.eclipse.ui.tests.api.workbenchpart.ArbitraryPropertyTest;
-import org.eclipse.ui.tests.api.workbenchpart.DependencyInjectionView;
 import org.eclipse.ui.tests.api.workbenchpart.LifecycleViewTest;
 import org.eclipse.ui.tests.api.workbenchpart.OverriddenTitleTest;
 import org.eclipse.ui.tests.api.workbenchpart.RawIViewPartTest;
@@ -63,15 +62,13 @@ import org.junit.runners.Suite;
      UIJobTest.class,
      Bug75118Test.class,
      FileEditorInputTest.class,
-	 IDETest.class,
+		IDETest.class,
      IEditorMatchingStrategyTest.class,
      XMLMementoTest.class,
      //IWorkbenchPartTestableTests.class,
      ArbitraryPropertyTest.class,
      LifecycleViewTest.class,
-	 DependencyInjectionView.class, 
-	 Bug407422Test.class, 
-	 MultipleWindowsTest.class
+		Bug407422Test.class, MultipleWindowsTest.class
 })
 public class ApiTestSuite {
 
