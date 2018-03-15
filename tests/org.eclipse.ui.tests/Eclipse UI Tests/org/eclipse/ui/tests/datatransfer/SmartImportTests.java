3d3f25fb0135145e110f00575fd871d3e719d9bd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:12:02 +0000

    First commited as before_82648_rev2

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/datatransfer/SmartImportTests.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/datatransfer/SmartImportTests.java
@@ -80,11 +80,7 @@ public class SmartImportTests extends UITestCase {
 			}
 		}, -1);
 		wizard.performFinish();
-		waitForJobs(100, 1000); // give the job framework time to schedule the
-								// job
 		wizard.getImportJob().join();
-		waitForJobs(100, 5000); // give some time for asynchronous workspace
-								// jobs to complete
 	}
 
 	/**
