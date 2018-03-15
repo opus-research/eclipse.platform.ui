a1eb9f95b0afa9f5847e296956f238f3e277d646
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:18:37 +0000

    First commited as before_101928_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/EditorRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/EditorRegistry.java
@@ -44,7 +44,6 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.SafeRunner;
 import org.eclipse.core.runtime.content.IContentType;
-import org.eclipse.core.runtime.content.IContentTypeManager;
 import org.eclipse.core.runtime.dynamichelpers.ExtensionTracker;
 import org.eclipse.core.runtime.dynamichelpers.IExtensionChangeHandler;
 import org.eclipse.core.runtime.dynamichelpers.IExtensionTracker;
@@ -165,28 +164,18 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
 
 	private RelatedRegistry relatedRegistry;
 
-	private final IContentTypeManager contentTypeManager;
-
 	public static final String EMPTY_EDITOR_ID = "org.eclipse.ui.internal.emptyEditorTab"; //$NON-NLS-1$
 
     /**
      * Return an instance of the receiver. Adds listeners into the extension
      * registry for dynamic UI purposes.
-     * @param contentTypeManager
      */
-    public EditorRegistry(IContentTypeManager contentTypeManager) {
+    public EditorRegistry() {
         super();
-        this.contentTypeManager = contentTypeManager;
         initializeFromStorage();
         IExtensionTracker tracker = PlatformUI.getWorkbench().getExtensionTracker();
         tracker.registerHandler(this, ExtensionTracker.createExtensionPointFilter(getExtensionPointFilter()));
 		relatedRegistry = new RelatedRegistry();
-		contentTypeManager.addContentTypeChangeListener(event -> {
-			if (contentTypeManager.getContentType(event.getContentType().getId()) == null) {
-				contentTypeToEditorMappingsFromUser.remove(event.getContentType());
-				saveAssociations();
-			}
-		});
     }
 
     /**
@@ -261,7 +250,7 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
 
 		for (String contentTypeId : contentTypeVector) {
 			if (contentTypeId != null && contentTypeId.length() > 0) {
-				IContentType contentType = contentTypeManager.getContentType(contentTypeId);
+				IContentType contentType = Platform.getContentTypeManager().getContentType(contentTypeId);
 				if (contentType != null) {
 					addContentTypeBindingFromPlugin(contentType, editor, bDefault);
 				}
@@ -351,7 +340,7 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
 			return defaultEditor;
 		}
 
-		IContentType[] contentTypes = contentTypeManager.findContentTypesFor(filename);
+		IContentType[] contentTypes = Platform.getContentTypeManager().findContentTypesFor(filename);
 		for (IContentType contentType : contentTypes) {
 			IEditorDescriptor editor = getDefaultEditor(filename, contentType);
 			if (editor != null) {
@@ -369,7 +358,7 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
 	 * @since 3.1
 	 */
 	private IContentType guessAtContentType(String filename) {
-		return contentTypeManager.findContentTypeFor(filename);
+		return Platform.getContentTypeManager().findContentTypeFor(filename);
 	}
 
     /**
@@ -758,14 +747,9 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
 
 		for (IMemento childMemento : memento.getChildren(IWorkbenchConstants.TAG_INFO)) {
             List<IEditorDescriptor> editors = getEditorDescriptors(childMemento.getChildren(IWorkbenchConstants.TAG_EDITOR), editorTable);
-			editors.forEach(editor -> {
-				if (!mapIDtoEditor.containsKey(editor.getId())) {
-					mapIDtoEditor.put(editor.getId(), editor);
-				}
-			});
 			String contentTypeId = childMemento.getString(IWorkbenchConstants.TAG_CONTENT_TYPE);
 			if (contentTypeId != null) {
-				IContentType contentType = contentTypeManager.getContentType(contentTypeId);
+				IContentType contentType = Platform.getContentTypeManager().getContentType(contentTypeId);
 				if (contentType != null) {
 					contentTypeToEditorMappingsFromUser.put(contentType, new LinkedHashSet<>(editors));
 				}
@@ -1578,7 +1562,7 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
 
         List<IFileEditorMapping> allMappings = new ArrayList<>(Arrays.asList(standardMappings));
         // mock-up content type extensions into IFileEditorMappings
-		for (IContentType type : contentTypeManager.getAllContentTypes()) {
+		for (IContentType type : Platform.getContentTypeManager().getAllContentTypes()) {
 			for (String extension : type.getFileSpecs(IContentType.FILE_EXTENSION_SPEC)) {
 				boolean found = false;
 				for (IFileEditorMapping mapping : allMappings) {
@@ -1644,9 +1628,6 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
 	public void addUserAssociation(IContentType contentType, IEditorDescriptor selectedEditor) {
 		if (!this.contentTypeToEditorMappingsFromUser.containsKey(contentType)) {
 			this.contentTypeToEditorMappingsFromUser.put(contentType, new LinkedHashSet<>());
-		}
-		if (!mapIDtoEditor.containsKey(selectedEditor.getId())) {
-			mapIDtoEditor.put(selectedEditor.getId(), selectedEditor);
 		}
 		this.contentTypeToEditorMappingsFromUser.get(contentType).add(selectedEditor);
 		saveAssociations();
