9d38c5c589cf943e524d0bada5fee4c3aff90106
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:51:02 +0000

    First commited as before_81318_rev1

--- tests/org.eclipse.e4.core.commands.tests/src/org/eclipse/e4/core/commands/tests/CommandTestSuite.java
+++ tests/org.eclipse.e4.core.commands.tests/src/org/eclipse/e4/core/commands/tests/CommandTestSuite.java
@@ -16,9 +16,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({
-	DefineCommandsTest.class,
-	HandlerTest.class })
-
+@SuiteClasses({ DefineCommandsTest.class, HandlerTest.class })
 public class CommandTestSuite {
+
+	public CommandTestSuite() {
+	}
 }
