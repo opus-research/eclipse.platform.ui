a534dc42cd72912f456fd337ac6607dd69dc71f7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:53:19 +0000

    First commited as before_68081_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/MultiPageEditorPart.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/MultiPageEditorPart.java
@@ -392,7 +392,7 @@ public abstract class MultiPageEditorPart extends EditorPart implements IPageCha
 				int pageCount = getPageCount();
 				Object[] result = new Object[pageCount];
 				for (int i = 0; i < pageCount; i++) {
-					result[i] = Integer.valueOf(i);
+					result[i] = new Integer(i);
 				}
 				return result;
 			}
