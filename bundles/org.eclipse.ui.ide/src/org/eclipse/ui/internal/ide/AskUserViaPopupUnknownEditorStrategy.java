f544d2df7910147a8b9e8ad32b4782658ad79a86
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:07:45 +0000

    First commited as before_65182_rev1

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/AskUserViaPopupUnknownEditorStrategy.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/AskUserViaPopupUnknownEditorStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016 Red Hat Inc. and others
+ * Copyright (c) 2015 Red Hat Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,23 +7,20 @@
  *
  * Contributors:
  *     Mickael Istria (Red Hat Inc.) - initial API and implementation
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 485201
  *******************************************************************************/
 package org.eclipse.ui.internal.ide;
 
-import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IEditorRegistry;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.EditorSelectionDialog;
-import org.eclipse.ui.ide.IUnassociatedEditorStrategy;
+import org.eclipse.ui.ide.IUnknownEditorStrategy;
 
 /**
  * @since 3.12
  *
  */
-public class AskUserViaPopupUnassociatedEditorStrategy implements IUnassociatedEditorStrategy {
+public class AskUserViaPopupUnknownEditorStrategy implements IUnknownEditorStrategy {
 
 	@Override
 	public IEditorDescriptor getEditorDescriptor(String fileName, IEditorRegistry editorRegistry) {
@@ -31,11 +28,7 @@ public class AskUserViaPopupUnassociatedEditorStrategy implements IUnassociatedE
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
 		dialog.setFileName(fileName);
 		dialog.setBlockOnOpen(true);
-
-		if (IDialogConstants.CANCEL_ID == dialog.open()) {
-			throw new OperationCanceledException(IDEWorkbenchMessages.IDE_noFileEditorSelectedUserCanceled);
-		}
-
+		dialog.open();
 		return dialog.getSelectedEditor();
 	}
 
