51d7f423a495c8a540af30b64770f877a74a32c6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:16:53 +0000

    First commited as before_101709_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/DialogSettings.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/DialogSettings.java
@@ -69,7 +69,7 @@ import org.xml.sax.SAXException;
  * @noextend This class is not intended to be subclassed by clients.
  */
 
-public class DialogSettings implements IDialogSettingsExtension {
+public class DialogSettings implements IDialogSettings {
     // The name of the DialogSettings.
     private String name;
 
@@ -565,51 +565,4 @@ public class DialogSettings implements IDialogSettingsExtension {
     	}
     }
 
-	@Override
-	public double getDouble(String key, double defaultValue) {
-		String setting = items.get(key);
-		if (setting != null) {
-			try {
-				return Double.valueOf(setting).doubleValue();
-			} catch (NumberFormatException e) {
-			}
-		}
-		return defaultValue;
-	}
-
-	@Override
-	public float getFloat(String key, float defaultValue) {
-		String setting = items.get(key);
-		if (setting != null) {
-			try {
-				return Float.valueOf(setting).floatValue();
-			} catch (NumberFormatException e) {
-			}
-		}
-		return defaultValue;
-	}
-
-	@Override
-	public int getInt(String key, int defaultValue) {
-		String setting = items.get(key);
-		if (setting != null) {
-			try {
-				return Integer.valueOf(setting).intValue();
-			} catch (NumberFormatException e) {
-			}
-		}
-		return defaultValue;
-	}
-
-	@Override
-	public long getLong(String key, long defaultValue) {
-		String setting = items.get(key);
-		if (setting != null) {
-			try {
-				return Long.valueOf(setting).longValue();
-			} catch (NumberFormatException e) {
-			}
-		}
-		return defaultValue;
-	}
 }
