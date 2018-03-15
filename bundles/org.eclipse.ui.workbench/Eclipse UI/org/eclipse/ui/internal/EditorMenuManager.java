a44b0049156f00e13d4475cd2f0e43919a88e2d4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:52:56 +0000

    First commited as before_68070_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/EditorMenuManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/EditorMenuManager.java
@@ -71,7 +71,7 @@ public class EditorMenuManager extends SubMenuManager {
                         item);
 			} else {
 				// no acclerator if the item is disabled
-				return Integer.valueOf(0);
+                return Integer.valueOf(0);
 			}
         }
 
