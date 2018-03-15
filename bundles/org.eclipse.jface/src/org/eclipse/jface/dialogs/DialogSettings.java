198410486356115a87540d3add227e87edbd4996
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:16:48 +0000

    First commited as before_101550_rev3

--- bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/DialogSettings.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/dialogs/DialogSettings.java
@@ -218,58 +218,6 @@ public class DialogSettings implements IDialogSettings {
         return name;
     }
 
-	@Override
-	public double getDouble(String key, double defaultValue) {
-		String setting = items.get(key);
-		if(setting != null) {
-			try {
-				return Double.valueOf(setting).doubleValue();
-			} catch (NumberFormatException e) {
-				return defaultValue;
-			}
-		}
-		return defaultValue;
-	}
-
-	@Override
-	public float getFloat(String key, float defaultValue) {
-		String setting = items.get(key);
-		if (setting != null) {
-			try {
-				return Float.valueOf(setting).floatValue();
-			} catch (NumberFormatException e) {
-				return defaultValue;
-			}
-		}
-		return defaultValue;
-	}
-
-	@Override
-	public int getInt(String key, int defaultValue) {
-		String setting = items.get(key);
-		if (setting != null) {
-			try {
-				return Integer.valueOf(setting).intValue();
-			} catch (NumberFormatException e) {
-				return defaultValue;
-			}
-		}
-		return defaultValue;
-	}
-
-	@Override
-	public long getLong(String key, long defaultValue) {
-		String setting = items.get(key);
-		if (setting != null) {
-			try {
-				return Long.valueOf(setting).longValue();
-			} catch (NumberFormatException e) {
-				return defaultValue;
-			}
-		}
-		return defaultValue;
-	}
-
 	/**
 	 * Returns a section with the given name in the given dialog settings. If
 	 * the section doesn't exist yet, then it is first created.
@@ -616,4 +564,5 @@ public class DialogSettings implements IDialogSettings {
     		return null;
     	}
     }
+
 }
