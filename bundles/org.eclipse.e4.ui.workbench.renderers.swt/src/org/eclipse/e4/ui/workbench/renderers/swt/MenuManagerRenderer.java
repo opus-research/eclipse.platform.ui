ff669f130d518ea22159d6cbf83dd8ee369bd449
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:00:53 +0000

    First commited as before_99126_rev3

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerRenderer.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/MenuManagerRenderer.java
@@ -891,8 +891,13 @@ public class MenuManagerRenderer extends SWTPartRenderer {
 			IContributionItem ici = getContribution(element);
 			clearModelToContribution(element, ici);
 		}
-		modelToManager.remove(model);
-		managerToModel.remove(manager);
+		MenuManager remove = modelToManager.remove(model);
+		if (manager == null) {
+			managerToModel.remove(remove);
+		} else {
+			managerToModel.remove(manager);
+		}
+
 		if (Policy.DEBUG_RENDERER) {
 			logger.debug("\nMMR:clearModelToManager: modelToManager size = {0}, managerToModel size = {1}", //$NON-NLS-1$
 					modelToManager.size(), managerToModel.size());
@@ -1146,9 +1151,7 @@ public class MenuManagerRenderer extends SWTPartRenderer {
 				ici = getManager(menuElement);
 				clearModelToManager(menuElement, (MenuManager) ici);
 			} else {
-				// Bug 518036: the call below removes too much.
-				// clearModelToContribution(menuModel, ici);
-				contributionToModel.remove(ici);
+				clearModelToContribution(menuModel, ici);
 			}
 			menuManager.remove(ici);
 			clearModelToContribution(mMenuElement, ici);
