ef224153a1321d2b91443ac0abc0d45f53ecc8a4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:25:15 +0000

    First commited as before_102810_rev8

--- bundles/org.eclipse.jface/src/org/eclipse/jface/wizard/ProgressMonitorPart.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/wizard/ProgressMonitorPart.java
@@ -10,13 +10,10 @@
  *     Eugene Ostroukhov <eugeneo@symbian.org> -  Bug 287887 [Wizards] [api] Cancel button has two distinct roles
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 440270
  *     Jan-Ove Weichel <janove.weichel@vogella.com> - Bug 475879
- *     Karsten Thoms <karsten.thoms@itemis.de> - Bug 520720 Asynchronous, throttled label update
  *******************************************************************************/
 package org.eclipse.jface.wizard;
 
 import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
-
-import java.time.Duration;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -25,7 +22,6 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.dialogs.ProgressIndicator;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
-import org.eclipse.jface.util.Throttler;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.graphics.Font;
@@ -90,7 +86,6 @@ public class ProgressMonitorPart extends Composite implements
 	/** <code>true</code> if this monitor part should show stop button **/
 	private boolean fHasStopButton = false;
 
-	private Throttler throttledUpdate;
 
 	/**
 	 * Creates a <code>ProgressMonitorPart</code> that does not provide a stop button.
@@ -161,7 +156,7 @@ public class ProgressMonitorPart extends Composite implements
 	public void beginTask(String name, int totalWork) {
         fTaskName = name;
         fSubTaskName = ""; //$NON-NLS-1$
-		queueUpdateLabel();
+        updateLabel();
 		if (!fProgressIndicator.isDisposed()) {
 			if (totalWork == IProgressMonitor.UNKNOWN || totalWork == 0) {
 				fProgressIndicator.beginAnimatedTask();
@@ -279,8 +274,6 @@ public class ProgressMonitorPart extends Composite implements
         	fStopButton.setEnabled(false);
 			fStopButton.setToolTipText(JFaceResources.getString("ProgressMonitorPart.cancelToolTip")); //$NON-NLS-1$
         }
-
-		throttledUpdate = new Throttler(fLabel.getDisplay(), Duration.ofMillis(100), this::updateLabel);
     }
 
     @Override
@@ -328,24 +321,14 @@ public class ProgressMonitorPart extends Composite implements
     @Override
 	public void setTaskName(String name) {
         fTaskName = name;
-		queueUpdateLabel();
+        updateLabel();
     }
 
     @Override
 	public void subTask(String name) {
         fSubTaskName = name;
-		queueUpdateLabel();
+        updateLabel();
     }
-
-	/**
-	 * Enqueues a label update for asynchronous execution. The update is performed
-	 * throttled to 100ms, i.e. updates within the throttle range are not displayed.
-	 *
-	 * @since 3.14
-	 */
-	protected void queueUpdateLabel() {
-		throttledUpdate.throttledExec();
-	}
 
     /**
      * Updates the label with the current task and subtask names.
@@ -354,16 +337,16 @@ public class ProgressMonitorPart extends Composite implements
 		if (fLabel.isDisposed() || fLabel.isAutoDirection()) {
 			return;
 		}
-		if (blockedStatus == null) {
-			String text = taskLabel();
-			fLabel.setText(text);
-		} else {
+        if (blockedStatus == null) {
+            String text = taskLabel();
+            fLabel.setText(text);
+        } else {
 			fLabel.setText(blockedStatus.getMessage());
 		}
 
-		// Force an update as we are in the UI Thread
-		fLabel.update();
-	}
+        //Force an update as we are in the UI Thread
+        fLabel.update();
+    }
 
     /**
      * Return the label for showing tasks
@@ -394,13 +377,15 @@ public class ProgressMonitorPart extends Composite implements
     @Override
 	public void clearBlocked() {
         blockedStatus = null;
-		queueUpdateLabel();
+        updateLabel();
+
     }
 
     @Override
 	public void setBlocked(IStatus reason) {
         blockedStatus = reason;
-		queueUpdateLabel();
+        updateLabel();
+
     }
 
    private void setCancelEnabled(boolean enabled) {
