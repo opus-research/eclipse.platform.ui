0fc95184fdccd94b15ab9714fac9896e12ed060f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:26:34 +0000

    First commited as before_79161_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/model/AdaptableList.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/model/AdaptableList.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2016 IBM Corporation and others.
+ * Copyright (c) 2003, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -107,10 +107,10 @@ public class AdaptableList extends WorkbenchAdapter implements IAdaptable {
         return children.size();
     }
 
-	@Override
-	public <T> T getAdapter(Class<T> adapter) {
+    @Override
+	public Object getAdapter(Class adapter) {
         if (adapter == IWorkbenchAdapter.class) {
-			return adapter.cast(this);
+            return this;
         }
         return null;
     }
