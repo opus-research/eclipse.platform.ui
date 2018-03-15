66da34503da61e6349981945534bc7b620a52ede
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 16:07:35 +0000

    First commited as after_112211_rev1

--- tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/FormTextModelTest.java
+++ tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/FormTextModelTest.java
@@ -7,10 +7,14 @@
  *
  * Contributors:
  *     Ralf M Petter<ralf.petter@gmail.com> - initial API and implementation
+ *     Daniel Kruegler <daniel.kruegler@gmail.com> - Bug 322337
  *******************************************************************************/
 package org.eclipse.ui.tests.forms.widgets;
 
 import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 import org.eclipse.ui.internal.forms.widgets.FormTextModel;
 import org.junit.Test;
@@ -27,6 +31,8 @@ public class FormTextModelTest {
 		formTextModel.parseTaggedText("<form><p>   line with   \r\n   <b>  whitespace </b> Test </p></form>", false);
 		assertEquals("FormTextModel does not remove whitespace correctly according to the rules",
 				"line with whitespace Test" + System.lineSeparator(), formTextModel.getAccessibleText());
+		assertEquals("FormTextModel does not return the originally provided text",
+				"<form><p>   line with   \r\n   <b>  whitespace </b> Test </p></form>", formTextModel.getRawText());
 	}
 
 	@Test
@@ -36,6 +42,8 @@ public class FormTextModelTest {
 		formTextModel.parseTaggedText("<form><p>   line with      <b>  whitespace </b> Test </p></form>", false);
 		assertEquals("FormTextModel does not preserve whitespace correctly according to the rules",
 				"   line with        whitespace  Test " + System.lineSeparator(), formTextModel.getAccessibleText());
+		assertEquals("FormTextModel does not return the originally provided text",
+				"<form><p>   line with      <b>  whitespace </b> Test </p></form>", formTextModel.getRawText());
 	}
 
 	@Test
@@ -44,6 +52,54 @@ public class FormTextModelTest {
 		formTextModel.parseTaggedText("<form>Foo &Bar</form>", false);
 		assertEquals("Foo &Bar" + System.lineSeparator(),
 				formTextModel.getAccessibleText());
+		assertEquals("FormTextModel does not return the originally provided text", "<form>Foo &Bar</form>",
+				formTextModel.getRawText());
+	}
+
+	@Test
+	public void testXMLWhitespaceNormalized() throws Exception {
+		FormTextModel formTextModel = new FormTextModel();
+		formTextModel.setWhitespaceNormalized(true);
+		formTextModel.parseInputStream(
+				new ByteArrayInputStream("<form><p>   line with   \r\n   <b>  whitespace </b> Test </p></form>"
+						.getBytes(StandardCharsets.UTF_8)),
+				false);
+		assertEquals("FormTextModel does not remove whitespace correctly according to the rules",
+				"line with whitespace Test" + System.lineSeparator(), formTextModel.getAccessibleText());
+		assertEquals("FormTextModel does not return the originally provided text",
+				"<form><p>   line with   \r\n   <b>  whitespace </b> Test </p></form>", formTextModel.getRawText());
+		formTextModel.parseInputStream(new ByteArrayInputStream(
+				"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><form><p>   line with å  \r\n   <b>  whitespace </b> Test </p></form>"
+						.getBytes("ISO-8859-1")),
+				false);
+		assertEquals("FormTextModel does not remove whitespace correctly according to the rules",
+				"line with å whitespace Test" + System.lineSeparator(), formTextModel.getAccessibleText());
+		assertEquals("FormTextModel does not return the originally provided text",
+				"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><form><p>   line with å  \r\n   <b>  whitespace </b> Test </p></form>",
+				formTextModel.getRawText());
+	}
+
+	@Test
+	public void testXMLWhitespaceNotNormalized() throws Exception {
+		FormTextModel formTextModel = new FormTextModel();
+		formTextModel.setWhitespaceNormalized(false);
+		formTextModel.parseInputStream(
+				new ByteArrayInputStream("<form><p>   line with      <b>  whitespace </b> Test </p></form>"
+						.getBytes(StandardCharsets.UTF_8)),
+				false);
+		assertEquals("FormTextModel does not preserve whitespace correctly according to the rules",
+				"   line with        whitespace  Test " + System.lineSeparator(), formTextModel.getAccessibleText());
+		assertEquals("FormTextModel does not return the originally provided text",
+				"<form><p>   line with      <b>  whitespace </b> Test </p></form>", formTextModel.getRawText());
+		formTextModel.parseInputStream(new ByteArrayInputStream(
+				"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><form><p>   line with å     <b>  whitespace </b> Test </p></form>"
+						.getBytes("ISO-8859-1")),
+				false);
+		assertEquals("FormTextModel does not preserve whitespace correctly according to the rules",
+				"   line with å       whitespace  Test " + System.lineSeparator(), formTextModel.getAccessibleText());
+		assertEquals("FormTextModel does not return the originally provided text",
+				"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><form><p>   line with å     <b>  whitespace </b> Test </p></form>",
+				formTextModel.getRawText());
 	}
 
 }
