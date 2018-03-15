ce293380cf333bbc6f8100176fa3edc87d326d91
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:57:57 +0000

    First commited as after_89201_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/PerspectiveMenu.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/actions/PerspectiveMenu.java
@@ -165,8 +165,8 @@ public abstract class PerspectiveMenu extends ContributionItem {
             item.setText(NO_TARGETS_MSG);
             item.setEnabled(false);
         } else {
-            for (int i = 0; i < items.length; i++) {
-                items[i].fill(menu, index++);
+            for (IContributionItem item : items) {
+                item.fill(menu, index++);
             }
         }
         dirty = false;
@@ -264,8 +264,8 @@ public abstract class PerspectiveMenu extends ContributionItem {
 
         String[] ids = page.getPerspectiveShortcuts();
 
-        for (int i = 0; i < ids.length; i++) {
-            IPerspectiveDescriptor desc = reg.findPerspectiveWithId(ids[i]);
+        for (String id2 : ids) {
+            IPerspectiveDescriptor desc = reg.findPerspectiveWithId(id2);
             if (desc != null && !list.contains(desc)) {
                 if (WorkbenchActivityHelper.filterItem(desc)) {
 					continue;
