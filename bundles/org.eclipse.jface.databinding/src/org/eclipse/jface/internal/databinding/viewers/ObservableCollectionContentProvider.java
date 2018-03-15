c9209d5ab73262c9a1103e985d5609491f318829
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:03:23 +0000

    First commited as before_65012_rev4

--- bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/internal/databinding/viewers/ObservableCollectionContentProvider.java
+++ bundles/org.eclipse.jface.databinding/src/org/eclipse/jface/internal/databinding/viewers/ObservableCollectionContentProvider.java
@@ -115,6 +115,7 @@ public abstract class ObservableCollectionContentProvider implements
 		if (observableCollection == null)
 			return new Object[0];
 
+		knownElements.addAll(observableCollection);
 		if (realizedElements != null) {
 			if (!realizedElements.equals(knownElements)) {
 				asyncUpdateRealizedElements();
@@ -201,7 +202,6 @@ public abstract class ObservableCollectionContentProvider implements
 					"Input must be an IObservableCollection"); //$NON-NLS-1$
 			observableCollection = (IObservableCollection) input;
 			addCollectionChangeListener(observableCollection);
-			knownElements.addAll(observableCollection);
 		}
 	}
 
