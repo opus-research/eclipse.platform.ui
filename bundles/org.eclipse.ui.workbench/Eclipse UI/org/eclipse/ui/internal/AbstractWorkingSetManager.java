954d0e8fc16b6644c9a3a782e879f5c33a03cb33
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:15:06 +0000

    First commited as before_90518_rev5

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/AbstractWorkingSetManager.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/AbstractWorkingSetManager.java
@@ -145,8 +145,8 @@ public abstract class AbstractWorkingSetManager extends EventManager implements
 			return registry.getNewPageWorkingSetDescriptors();
 		}
 		List<WorkingSetDescriptor> result = new ArrayList<>(supportedWorkingSetIds.length);
-		for (String supportedWorkingSetId : supportedWorkingSetIds) {
-			WorkingSetDescriptor desc = registry.getWorkingSetDescriptor(supportedWorkingSetId);
+		for (int i = 0; i < supportedWorkingSetIds.length; i++) {
+			WorkingSetDescriptor desc = registry.getWorkingSetDescriptor(supportedWorkingSetIds[i]);
 			if (desc != null && desc.isEditable()) {
 				result.add(desc);
 			}
@@ -366,13 +366,13 @@ public abstract class AbstractWorkingSetManager extends EventManager implements
         final PropertyChangeEvent event = new PropertyChangeEvent(this,
                 changeId, oldValue, newValue);
 		Runnable notifier = () -> {
-			for (Object listener : listeners) {
-				final IPropertyChangeListener propertyChangeListener = (IPropertyChangeListener) listener;
+			for (int i = 0; i < listeners.length; i++) {
+				final IPropertyChangeListener listener = (IPropertyChangeListener) listeners[i];
 				ISafeRunnable safetyWrapper = new ISafeRunnable() {
 
 					@Override
 					public void run() throws Exception {
-						propertyChangeListener.propertyChange(event);
+						listener.propertyChange(event);
 					}
 
 					@Override
@@ -477,8 +477,10 @@ public abstract class AbstractWorkingSetManager extends EventManager implements
      * @param memento the persistence store
      */
 	protected void restoreWorkingSetState(IMemento memento) {
-		for (IMemento child : memento.getChildren(IWorkbenchConstants.TAG_WORKING_SET)) {
-			AbstractWorkingSet workingSet = (AbstractWorkingSet) restoreWorkingSet(child);
+		IMemento[] children = memento.getChildren(IWorkbenchConstants.TAG_WORKING_SET);
+
+		for (int i = 0; i < children.length; i++) {
+			AbstractWorkingSet workingSet = (AbstractWorkingSet) restoreWorkingSet(children[i]);
 			if (workingSet != null) {
 				internalAddWorkingSet(workingSet);
 			}
@@ -658,7 +660,8 @@ public abstract class AbstractWorkingSetManager extends EventManager implements
 				@Override
 				public IStatus runInUIThread(IProgressMonitor monitor) {
 					synchronized (updaters) {
-						for (WorkingSetDescriptor descriptor : descriptors) {
+						for (int i = 0; i < descriptors.length; i++) {
+							WorkingSetDescriptor descriptor = descriptors[i];
 							List workingSets = getWorkingSetsForId(descriptor
 									.getId());
 							if (workingSets.size() == 0) {
@@ -691,7 +694,8 @@ public abstract class AbstractWorkingSetManager extends EventManager implements
 
 	private List getWorkingSetsForId(String id) {
 		List result= new ArrayList();
-		for (IWorkingSet ws : workingSets) {
+    	for (Iterator iter= workingSets.iterator(); iter.hasNext();) {
+    		IWorkingSet ws= (IWorkingSet)iter.next();
     		if (id.equals(ws.getId())) {
 				result.add(ws);
 			}
@@ -796,7 +800,8 @@ public abstract class AbstractWorkingSetManager extends EventManager implements
 
 	@Override
 	public void removeExtension(IExtension extension, Object[] objects) {
-		for (Object object : objects) {
+		for (int i = 0; i < objects.length; i++) {
+			Object object = objects[i];
 			if (object instanceof IWorkingSetUpdater) {
 				removeUpdater((IWorkingSetUpdater)object);
 
@@ -854,7 +859,8 @@ public abstract class AbstractWorkingSetManager extends EventManager implements
 		// ideally this method would be in a static util class of some kind but
 		// we dont have any such beast for working sets and making one for one
 		// method is overkill.
-		for (final IWorkingSet workingSet : workingSets) {
+		for (int i = 0; i < workingSets.length; i++) {
+			final IWorkingSet workingSet = workingSets[i];
 			SafeRunner.run(new WorkingSetRunnable() {
 
 				@Override
