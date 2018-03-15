9290c5ecb5d84cfb1f1bde7f03465cdd55cb9df3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:20:01 +0000

    First commited as after_102041_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchWindow.java
@@ -2216,7 +2216,7 @@ STATUS_LINE_ID, model);
 				manager.setCancelEnabled(wasCancelEnabled);
 
 				// re-enable the main menu if necessary
-				if (enableMainMenu) {
+				if (enableMainMenu && model != null && model.getMainMenu() != null) {
 					Menu mainMenu = (Menu) model.getMainMenu().getWidget();
 					mainMenu.setEnabled(true);
 				}
