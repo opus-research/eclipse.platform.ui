b355dca5d8eddf0cfc06b63ac026b4c48ebf35e8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:25:03 +0000

    First commited as before_84520_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/MenuHelper.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/MenuHelper.java
@@ -147,18 +147,13 @@ public class MenuHelper {
 		return getIconURI(imageDescriptor, null);
 	}
 
-	private static String getUrl(Class<?> idc, ImageDescriptor imageDescriptor) {
+	private static URL getUrl(Class<?> idc, ImageDescriptor imageDescriptor) {
 		try {
 			if (urlField == null) {
 				urlField = idc.getDeclaredField("url"); //$NON-NLS-1$
 				urlField.setAccessible(true);
 			}
-			// type check for backwards compatibility
-			Object value = urlField.get(imageDescriptor);
-			if (value instanceof URL) {
-				return ((URL) value).toExternalForm();
-			}
-			return value.toString();
+			return (URL) urlField.get(imageDescriptor);
 		} catch (SecurityException | NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
@@ -1103,8 +1098,8 @@ public class MenuHelper {
 		// Attempt to retrieve URIs from the descriptor and convert into a more
 		// durable form in case it's to be persisted
 		if (descriptor.getClass().toString().endsWith("URLImageDescriptor")) { //$NON-NLS-1$
-			String url = getUrl(descriptor.getClass(), descriptor);
-			return rewriteDurableURL(url);
+			URL url = getUrl(descriptor.getClass(), descriptor);
+			return rewriteDurableURL(url.toExternalForm());
 		} else if (descriptor.getClass().toString().endsWith("FileImageDescriptor")) { //$NON-NLS-1$
 			Class<?> sourceClass = getLocation(descriptor);
 			if (sourceClass == null) {
