ef8db3c7d508416bb72ff38db45652c172dc76ec
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:12:14 +0000

    First commited as before_77100_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/Workbench.java
@@ -460,7 +460,7 @@ public final class Workbench extends EventManager implements IWorkbench,
 	/**
 	 * Listener list for registered IWorkbenchListeners .
 	 */
-	private ListenerList<IWorkbenchListener> workbenchListeners = new ListenerList<>(ListenerList.IDENTITY);
+	private ListenerList workbenchListeners = new ListenerList(ListenerList.IDENTITY);
 
 	private ServiceRegistration workbenchService;
 
@@ -661,7 +661,6 @@ public final class Workbench extends EventManager implements IWorkbench,
 									IWorkbenchPreferenceConstants.SHOW_PROGRESS_ON_STARTUP);
 
 					IProgressMonitor progressMonitor = null;
-					SynchronousBundleListener bundleListener = null;
 					if (handler != null && showProgress) {
 						progressMonitor = handler.getBundleProgressMonitor();
 						if (progressMonitor != null) {
@@ -669,7 +668,7 @@ public final class Workbench extends EventManager implements IWorkbench,
 							int expectedProgressCount = Math.max(1, WorkbenchPlugin.getDefault()
 									.getBundleCount() / 10);
 							progressMonitor.beginTask("", expectedProgressCount); //$NON-NLS-1$
-							bundleListener = workbench.new StartupProgressBundleListener(
+							SynchronousBundleListener bundleListener = workbench.new StartupProgressBundleListener(
 									progressMonitor, (int) (expectedProgressCount * cutoff));
 							WorkbenchPlugin.getDefault().addBundleListener(bundleListener);
 						}
@@ -683,12 +682,8 @@ public final class Workbench extends EventManager implements IWorkbench,
 								WorkbenchPlugin.getDefault().getViewRegistry());
 						WorkbenchPlugin.log(StatusUtil.newStatus(IStatus.INFO, "Workbench migration finished", null)); //$NON-NLS-1$
 					}
-
 					if (returnCode[0] == PlatformUI.RETURN_OK) {
 						// run the e4 event loop and instantiate ... well, stuff
-						if (bundleListener != null) {
-							WorkbenchPlugin.getDefault().removeBundleListener(bundleListener);
-						}
 						e4Workbench.createAndRunUI(e4Workbench.getApplication());
 						IMenuService wms = e4Workbench.getContext().get(IMenuService.class);
 						wms.dispose();
@@ -983,7 +978,9 @@ public final class Workbench extends EventManager implements IWorkbench,
 	 * @since 3.2
 	 */
 	boolean firePreShutdown(final boolean forced) {
-		for (final IWorkbenchListener l : workbenchListeners) {
+		Object list[] = workbenchListeners.getListeners();
+		for (Object element : list) {
+			final IWorkbenchListener l = (IWorkbenchListener) element;
 			final boolean[] result = new boolean[] { false };
 			SafeRunnable.run(new SafeRunnable() {
 				@Override
@@ -1004,7 +1001,9 @@ public final class Workbench extends EventManager implements IWorkbench,
 	 * @since 3.2
 	 */
 	void firePostShutdown() {
-		for (final IWorkbenchListener l : workbenchListeners) {
+		Object list[] = workbenchListeners.getListeners();
+		for (Object element : list) {
+			final IWorkbenchListener l = (IWorkbenchListener) element;
 			SafeRunnable.run(new SafeRunnable() {
 				@Override
 				public void run() {
@@ -3527,8 +3526,8 @@ UIEvents.Context.TOPIC_CONTEXT,
 	}
 
 	@Override
-	public final <T> T getAdapter(final Class<T> key) {
-		return key.cast(serviceLocator.getService(key));
+	public final Object getAdapter(final Class key) {
+		return serviceLocator.getService(key);
 	}
 
 
