04436637f93bb98381103ef15265c588ab1ffbd3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:52:50 +0000

    First commited as before_68069_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/statushandlers/WorkbenchErrorHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/statushandlers/WorkbenchErrorHandler.java
@@ -39,7 +39,8 @@ public class WorkbenchErrorHandler extends AbstractStatusHandler {
 
 	@Override
 	public void handle(final StatusAdapter statusAdapter, int style) {
-		statusAdapter.setProperty(WorkbenchStatusDialogManager.HINT, Integer.valueOf(style));
+		statusAdapter.setProperty(WorkbenchStatusDialogManager.HINT,
+				Integer.valueOf(style));
 		if (((style & StatusManager.SHOW) == StatusManager.SHOW)
 				|| ((style & StatusManager.BLOCK) == StatusManager.BLOCK)) {
 
