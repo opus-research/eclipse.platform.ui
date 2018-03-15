875388180265cdfe80c75fb7c107b22cd22f9aa2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:20:48 +0000

    First commited as after_91417_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbookEditorsHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbookEditorsHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2016 IBM Corporation and others.
+ * Copyright (c) 2007, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,18 +8,34 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Marc-Andre Laperle (Ericsson) - Bug 413278
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 497618, 368977, 504088
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 497618, 368977, 504088, 506019
  ******************************************************************************/
 
 package org.eclipse.ui.internal;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.renderers.swt.StackRenderer;
 import org.eclipse.e4.ui.workbench.swt.internal.copy.SearchPattern;
+import org.eclipse.jface.viewers.StyledCellLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.commands.ICommandService;
 
 /**
@@ -31,13 +47,53 @@ import org.eclipse.ui.commands.ICommandService;
 public class WorkbookEditorsHandler extends FilteredTableBaseHandler {
 
 	/**
-	 *
+	 * Tag used on the active part
+	 */
+	private static final String TAG_ACTIVE = "active"; //$NON-NLS-1$
+
+	/**
+	 * Preference node for the workbench SWT renderer
+	 */
+	private static final String ORG_ECLIPSE_E4_UI_WORKBENCH_RENDERERS_SWT = "org.eclipse.e4.ui.workbench.renderers.swt"; //$NON-NLS-1$
+
+	/**
+	 * Id for the command that opens the editor drop down
 	 */
 	private static final String ORG_ECLIPSE_UI_WINDOW_OPEN_EDITOR_DROP_DOWN = "org.eclipse.ui.window.openEditorDropDown"; //$NON-NLS-1$
 
+	/**
+	 * Color to use to represent hidden editors
+	 */
+	protected static Color COLOR_HIDDEN = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GRAY);
+
+	/**
+	 * Gets the preference "show most recently used tabs" (MRU tabs)
+	 *
+	 * @return Returns the enableMRU.
+	 */
+	public Boolean isEnableMRU() {
+		//
+		IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(ORG_ECLIPSE_E4_UI_WORKBENCH_RENDERERS_SWT);
+		boolean initialMRUValue = preferences.getBoolean(StackRenderer.MRU_KEY_DEFAULT, StackRenderer.MRU_DEFAULT);
+		boolean enableMRU = preferences.getBoolean(StackRenderer.MRU_KEY, initialMRUValue);
+		//
+		return enableMRU;
+	}
+
 	@Override
 	protected Object getInput(WorkbenchPage page) {
-		List<EditorReference> refs = page.getSortedEditorReferences();
+		List<EditorReference> refs;
+
+		if (isEnableMRU()) {
+			// sorted, MRU order
+			refs = page.getSortedEditorReferences();
+		} else {
+			// non sorted, First Opened order
+			refs = new ArrayList<>();
+			for (IEditorReference ier : page.getEditorReferences()) {
+				refs.add((EditorReference) ier);
+			}
+		}
 		return refs;
 	}
 
@@ -61,6 +117,88 @@ public class WorkbookEditorsHandler extends FilteredTableBaseHandler {
 			patternMatcher.setPattern("*" + pattern); //$NON-NLS-1$
 			searchPattern = patternMatcher;
 		}
+	}
+
+	/**
+	 * Build a single style range and sets it for the entire content of the
+	 * given cell
+	 *
+	 * @param cell {@link ViewerCell} to which the style is applied
+	 * @param fontStyle a bit or combination of NORMAL, BOLD, ITALIC
+	 * @param foreground foreground color or null
+	 */
+	protected static void setStyle(ViewerCell cell, int fontStyle, Color foreground) {
+		FontData data = cell.getFont().getFontData()[0];
+		Font bold = new Font(Display.getCurrent(), data.getName(), data.getHeight(), fontStyle);
+		StyleRange style1 = new StyleRange();
+		style1.start = 0;
+		style1.length = cell.getText().length();
+		style1.font = bold;
+		if (foreground != null) {
+			style1.foreground = foreground;
+		}
+		cell.setStyleRanges(new StyleRange[] { style1 });
+	}
+
+	/** True if the given model represents the active editor */
+	protected boolean isActiveEditor(MPart model) {
+		if (model == null || model.getTags() == null) {
+			return false;
+		}
+		return model.getTags().contains(TAG_ACTIVE);
+	}
+
+	/** True is the given model represents an hidden editor */
+	protected boolean isHiddenEditor(MPart model) {
+		if (model == null || model.getParent() == null || !(model.getParent().getRenderer() instanceof StackRenderer)) {
+			return false;
+		}
+		StackRenderer renderer = (StackRenderer) model.getParent().getRenderer();
+		CTabItem item = renderer.findItemForPart(model);
+		return (item != null && !item.isShowing());
+	}
+
+	/**
+	 * Specializes
+	 * {@link FilteredTableBaseHandler#setLabelProvider(TableViewerColumn)} by
+	 * providing custom styles to the table cells (non-Javadoc)
+	 */
+	@Override
+	protected void setLabelProvider(TableViewerColumn tableViewerColumn) {
+
+		tableViewerColumn.setLabelProvider(new StyledCellLabelProvider() {
+
+			/** updates the cell, depending on the element's properties */
+			@Override
+			public void update(ViewerCell cell) {
+				Object element = cell.getElement();
+				if (element instanceof WorkbenchPartReference) {
+					WorkbenchPartReference ref = (WorkbenchPartReference) element;
+					String text = getWorkbenchPartReferenceText(ref);
+					cell.setText(text);
+					cell.setImage(ref.getTitleImage());
+					// check active, visible, hidden
+					MPart model = ref.getModel();
+					Color fontForegroundColor = null;
+					Integer fontStyle = (isActiveEditor(model)) ? SWT.BOLD : SWT.NORMAL;
+					if (isHiddenEditor(model)) {
+						fontForegroundColor = COLOR_HIDDEN;
+						fontStyle |= SWT.ITALIC;
+					}
+					// set style
+					setStyle(cell, fontStyle, fontForegroundColor);
+				}
+			}
+
+			@Override
+			public String getToolTipText(Object element) {
+				if (element instanceof WorkbenchPartReference) {
+					return ((WorkbenchPartReference) element).getTitleToolTip();
+				}
+				return super.getToolTipText(element);
+			}
+
+		});
 	}
 
 	@Override
