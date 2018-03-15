38df7c8b26d83036373011eb2dcaf781dd5a4fb7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:17:40 +0000

    First commited as after_83421_rev1

--- tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngineTest.java
+++ tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngineTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2016 Daniel Raap and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Daniel Raap <raap@subshell.com> - initial implementation
+ *******************************************************************************/
 package org.eclipse.e4.ui.css.core.impl.engine;
 
 import static org.junit.Assert.assertNull;
