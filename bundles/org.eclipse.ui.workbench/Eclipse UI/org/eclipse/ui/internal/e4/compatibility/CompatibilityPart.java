b04125aeb816f66e7417c5452619a5787c3649fd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:09:06 +0000

    First commited as before_82488_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityPart.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/e4/compatibility/CompatibilityPart.java
@@ -50,6 +50,7 @@ import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.internal.ErrorEditorPart;
 import org.eclipse.ui.internal.ErrorViewPart;
+import org.eclipse.ui.internal.IWorkbenchConstants;
 import org.eclipse.ui.internal.PartSite;
 import org.eclipse.ui.internal.SaveableHelper;
 import org.eclipse.ui.internal.WorkbenchPage;
@@ -65,6 +66,8 @@ public abstract class CompatibilityPart implements ISelectionChangedListener {
 	public static final String COMPATIBILITY_EDITOR_URI = "bundleclass://org.eclipse.ui.workbench/org.eclipse.ui.internal.e4.compatibility.CompatibilityEditor"; //$NON-NLS-1$
 
 	public static final String COMPATIBILITY_VIEW_URI = "bundleclass://org.eclipse.ui.workbench/org.eclipse.ui.internal.e4.compatibility.CompatibilityView"; //$NON-NLS-1$
+
+	public static final String ENABLE_DEPENDENCY_INJECTION_FOR_E3_PARTS = "ENABLE_DEPENDENCY_INJECTION_FOR_E3"; ////$NON-NLS-1$
 
 	@Inject
 	Composite composite;
@@ -151,9 +154,12 @@ public abstract class CompatibilityPart implements ISelectionChangedListener {
 		try {
 			site = legacyPart.getSite();
 			part.getContext().set(Composite.class, parent);
-			ContextInjectionFactory.inject(legacyPart, part.getContext());
 			// call createPartControl after dependency injection
+			if (part.getTags().contains(IWorkbenchConstants.TAG_USE_DEPENDENCY_INJECTION)) {
+				ContextInjectionFactory.inject(legacyPart, part.getContext());
+			}
 			legacyPart.createPartControl(parent);
+
 		} catch (RuntimeException e) {
 			logger.error(e);
 
@@ -205,6 +211,11 @@ public abstract class CompatibilityPart implements ISelectionChangedListener {
 	@Focus
 	void delegateSetFocus() {
 		try {
+			// first involve @Focus if present
+			if (part.getTags().contains(IWorkbenchConstants.TAG_USE_DEPENDENCY_INJECTION)) {
+				ContextInjectionFactory.invoke(wrapped, Focus.class, part.getContext(), null);
+			}
+			// ensure to comply to the 3.x API contract
 			wrapped.setFocus();
 		} catch (Exception e) {
 			if (logger != null) {
@@ -426,7 +437,9 @@ public abstract class CompatibilityPart implements ISelectionChangedListener {
 	 */
 	void disposeSite(PartSite site) {
 		site.dispose();
-		ContextInjectionFactory.uninject(legacyPart, part.getContext());
+		if (part.getTags().contains(IWorkbenchConstants.TAG_USE_DEPENDENCY_INJECTION)) {
+			ContextInjectionFactory.inject(legacyPart, part.getContext());
+		}
 	}
 
 	@Persist
