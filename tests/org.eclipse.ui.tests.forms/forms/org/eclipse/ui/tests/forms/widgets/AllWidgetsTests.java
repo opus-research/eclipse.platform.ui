25d369292f9c55547e5ce09b0fcb4abfd671d92d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:45:58 +0000

    First commited as before_87579_rev3

--- tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/AllWidgetsTests.java
+++ tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/AllWidgetsTests.java
@@ -1,25 +1,26 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2017 IBM Corporation and others.
+ * Copyright (c) 2016, 2017 Ralf M Petter<ralf.petter@gmail.com> and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Ralf M Petter<ralf.petter@gmail.com> - Bug 510241
+ *     Ralf M Petter<ralf.petter@gmail.com> - initial API and implementation
  *******************************************************************************/
-package org.eclipse.ui.tests.forms;
+package org.eclipse.ui.tests.forms.widgets;
 
-import org.eclipse.ui.tests.forms.performance.FormsPerformanceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/*
- * Tests forms performance (automated).
+/**
+ * Tests forms widgets (automated).
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ FormsPerformanceTest.class })
-public class AllFormsPerformanceTests {
+@Suite.SuiteClasses({
+	ExpandableCompositeTest.class,
+	FormTextModelTest.class
+})
+public class AllWidgetsTests {
 
 }
