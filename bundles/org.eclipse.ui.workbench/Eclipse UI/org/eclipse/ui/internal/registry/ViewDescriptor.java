5359eb0ee85f688149762935fd6ead01f5259a74
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:16:55 +0000

    First commited as before_77720_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/ViewDescriptor.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/ViewDescriptor.java
@@ -8,7 +8,6 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Markus Alexander Kuppe, Versant Corporation - bug #215797
- *     Friederike Schertel <friederike@schertel.org> - Bug 478336
  *******************************************************************************/
 package org.eclipse.ui.internal.registry;
 
@@ -101,6 +100,7 @@ public class ViewDescriptor implements IViewDescriptor, IPluginContribution {
 
 	@Override
 	public float getFastViewWidthRatio() {
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
