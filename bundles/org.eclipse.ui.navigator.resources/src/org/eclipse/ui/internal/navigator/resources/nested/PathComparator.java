c84352d39fb30b7a2b43c240d0f9b553b36989fe
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:04:33 +0000

    First commited as after_76022_rev2

--- bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/PathComparator.java
+++ bundles/org.eclipse.ui.navigator.resources/src/org/eclipse/ui/internal/navigator/resources/nested/PathComparator.java
@@ -23,7 +23,44 @@ public class PathComparator implements Comparator<IPath> {
 
 	@Override
 	public int compare(IPath arg0, IPath arg1) {
-		return arg0.toString().compareTo(arg1.toString());
+		if (arg0.equals(arg1)) {
+			return 0;
+		}
+		// First, Device
+		if (arg0.getDevice() != null && arg1.getDevice() == null) {
+			return +1;
+		}
+		if (arg0.getDevice() == null && arg1.getDevice() != null) {
+			return -1;
+		}
+		if (arg0.getDevice() != null && arg1.getDevice() != null) {
+			int compare = arg0.getDevice().compareTo(arg1.getDevice());
+			if (compare != 0) {
+				return compare;
+			}
+		}
+		// then, Absolute
+		if (arg0.isAbsolute() && !arg1.isAbsolute()) {
+			return +1;
+		}
+		if (!arg0.isAbsolute() && arg1.isAbsolute()) {
+			return -1;
+		}
+		// then, UNC
+		if (arg0.isUNC() && !arg1.isUNC()) {
+			return +1;
+		}
+		if (!arg0.isUNC() && arg1.isUNC()) {
+			return -1;
+		}
+		// then, Segments
+		for (int i = 0; i < Math.min(arg0.segmentCount(), arg1.segmentCount()); i++) {
+			int res = arg0.segment(i).compareTo(arg1.segment(i));
+			if (res != 0) {
+				return res;
+			}
+		}
+		return arg0.segmentCount() - arg1.segmentCount();
 	}
 
 }
