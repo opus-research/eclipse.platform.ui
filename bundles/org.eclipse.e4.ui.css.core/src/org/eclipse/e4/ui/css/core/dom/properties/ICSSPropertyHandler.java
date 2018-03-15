864eeb3bc30f8937470c93c27f052197e91998f1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 01:15:11 +0000

    First commited as after_69989_rev1

--- bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/dom/properties/ICSSPropertyHandler.java
+++ bundles/org.eclipse.e4.ui.css.core/src/org/eclipse/e4/ui/css/core/dom/properties/ICSSPropertyHandler.java
@@ -8,6 +8,7 @@
  * Contributors:
  *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 490912
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 490931
  *******************************************************************************/
 package org.eclipse.e4.ui.css.core.dom.properties;
 
@@ -58,9 +59,7 @@ public interface ICSSPropertyHandler {
 	 * @return retrieved CSS properties or null
 	 * @throws Exception
 	 */
-	default public String retrieveCSSProperty(Object element, String property, String pseudo, CSSEngine engine)
-			throws Exception {
-		return null;
-	}
+	public String retrieveCSSProperty(Object element, String property, String pseudo, CSSEngine engine)
+			throws Exception;
 
 }
