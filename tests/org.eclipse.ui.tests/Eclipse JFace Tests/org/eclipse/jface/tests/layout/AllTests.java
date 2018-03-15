b58c177ea8a22cd62c81a2f3651127bb3a799049
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:45:07 +0000

    First commited as after_71414_rev1

--- tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/layout/AllTests.java
+++ tests/org.eclipse.ui.tests/Eclipse JFace Tests/org/eclipse/jface/tests/layout/AllTests.java
@@ -26,8 +26,6 @@ public class AllTests extends TestSuite {
 	public AllTests() {
 		addTestSuite(GeometryTest.class);
 		addTestSuite(AbstractColumnLayoutTest.class);
-		addTestSuite(GridDataFactoryTest.class);
-		addTestSuite(GridLayoutFactoryTest.class);
 		addTestSuite(TreeColumnLayoutTest.class);
 	}
 }
