2cf2398a3e3a30f990a6a19bacd1e4dccf0e5603
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:37:21 +0000

    First commited as after_105660_rev1

--- tests/org.eclipse.jface.tests.databinding/src/org/eclipse/core/tests/internal/databinding/DifferentRealmsBindingTest.java
+++ tests/org.eclipse.jface.tests.databinding/src/org/eclipse/core/tests/internal/databinding/DifferentRealmsBindingTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2016 Conrad Groth and others.
+ * Copyright (c) 2015, 2017 Conrad Groth and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,22 +12,28 @@ package org.eclipse.core.tests.internal.databinding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.UpdateSetStrategy;
+import org.eclipse.core.databinding.observable.IDisposeListener;
 import org.eclipse.core.databinding.observable.list.ObservableList;
 import org.eclipse.core.databinding.observable.list.WritableList;
+import org.eclipse.core.databinding.observable.set.IObservableSet;
 import org.eclipse.core.databinding.observable.set.ObservableSet;
 import org.eclipse.core.databinding.observable.set.WritableSet;
 import org.eclipse.core.databinding.util.ILogger;
 import org.eclipse.core.databinding.util.Policy;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.tests.databinding.observable.ThreadRealm;
+import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class DifferentRealmsBindingTest extends TestCase {
 
-	ThreadRealm targetAndModelRealm = new ThreadRealm();
+	ThreadRealm targetAndModelRealm = createRealm();
 	ThreadRealm validationRealm = new ThreadRealm();
 
 	List<IStatus> errorStatusses = new ArrayList<>();
@@ -45,13 +51,6 @@ public class DifferentRealmsBindingTest extends TestCase {
 	@Override
 	protected void setUp() throws Exception {
 		errorStatusses.clear();
-		new Thread() {
-			@Override
-			public void run() {
-				targetAndModelRealm.init(Thread.currentThread());
-				targetAndModelRealm.block();
-			}
-		}.start();
 
 		validationRealm.init(Thread.currentThread());
 		dbc = new DataBindingContext(validationRealm);
@@ -83,5 +82,58 @@ public class DifferentRealmsBindingTest extends TestCase {
 		targetAndModelRealm.processQueue();
 		targetAndModelRealm.unblock();
 		assertTrue(errorStatusses.toString(), errorStatusses.isEmpty());
+	}
+
+	@Test(timeout = 5000)
+	public void testDisposingBothsObservablesConcurrently() throws InterruptedException {
+		final CountDownLatch latch1 = new CountDownLatch(1);
+		final CountDownLatch latch2 = new CountDownLatch(1);
+		ThreadRealm targetRealm = createRealm();
+		final IObservableSet<String> target = new WritableSet<String>(targetRealm) {
+			@Override
+			public synchronized void dispose() {
+				super.dispose();
+
+			}
+			@Override
+			public synchronized void removeDisposeListener(IDisposeListener listener) {
+				latch1.countDown();
+				try {
+					latch2.await();
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					throw new RuntimeException();
+				}
+			}
+		};
+		ThreadRealm modelRealm = createRealm();
+		final IObservableSet<String> model = new WritableSet<>(modelRealm);
+
+		ThreadRealm validationRealm = createRealm();
+		DataBindingContext context = new DataBindingContext(validationRealm);
+
+		context.getValidationRealm()
+				.exec(() -> context.bindSet(target, model, new UpdateSetStrategy(UpdateSetStrategy.POLICY_NEVER),
+				new UpdateSetStrategy(UpdateSetStrategy.POLICY_NEVER)));
+		validationRealm.processQueue();
+
+		targetRealm.exec(() -> target.dispose());
+		targetRealm.processQueue();
+
+		assertTrue(latch1.await(2, TimeUnit.SECONDS));
+
+		modelRealm.exec(() -> model.dispose());
+		modelRealm.processQueue();
+
+		assertTrue(errorStatusses.toString(), errorStatusses.isEmpty());
+	}
+
+	private ThreadRealm createRealm() {
+		final ThreadRealm realm = new ThreadRealm();
+		new Thread(() -> {
+			realm.init(Thread.currentThread());
+			realm.block();
+		}).start();
+		return realm;
 	}
 }
