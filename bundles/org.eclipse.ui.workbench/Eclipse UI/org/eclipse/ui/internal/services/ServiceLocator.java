f1176f21035efe91c238468aafa1e08afe3c97aa
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:22:21 +0000

    First commited as after_78449_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/services/ServiceLocator.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/services/ServiceLocator.java
@@ -21,6 +21,7 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.eclipse.ui.internal.misc.StatusUtil;
 import org.eclipse.ui.services.AbstractServiceFactory;
@@ -170,6 +171,7 @@ public final class ServiceLocator implements IDisposable, INestable,
 		if (IEclipseContext.class.equals(key)) {
 			return (T) context;
 		}
+		checkUiThread();
 
 		Object service = context.get(key.getName());
 		if (service == null) {
@@ -247,6 +249,8 @@ public final class ServiceLocator implements IDisposable, INestable,
 																								// ;
 		}
 
+		checkUiThread();
+
 		if (service instanceof INestable && activated) {
 			((INestable) service).activate();
 		}
@@ -267,6 +271,14 @@ public final class ServiceLocator implements IDisposable, INestable,
 	 */
 	public boolean isDisposed() {
 		return disposed;
+	}
+
+	void checkUiThread() {
+		if (Display.getCurrent() == null) {
+			IllegalStateException e = new IllegalStateException(
+					"Unexpected access to ServiceLocator from non-UI thread " + Thread.currentThread().getName()); //$NON-NLS-1$
+			WorkbenchPlugin.log(StatusUtil.newStatus(IStatus.WARNING, e.getMessage(), e));
+		}
 	}
 
 	/**
