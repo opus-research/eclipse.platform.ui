d895354f0c47310538193c43442e632d24aa5947
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:11:42 +0000

    First commited as before_65268_rev2

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ViewerFilter.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ViewerFilter.java
@@ -113,5 +113,6 @@ public abstract class ViewerFilter {
 	 * @return <code>true</code> if element is included in the filtered set, and
 	 *         <code>false</code> if excluded
 	 */
-	public abstract boolean select(Viewer viewer, Object parentElement, Object element);
+	public abstract boolean select(Viewer viewer, Object parentElement,
+			Object element);
 }
