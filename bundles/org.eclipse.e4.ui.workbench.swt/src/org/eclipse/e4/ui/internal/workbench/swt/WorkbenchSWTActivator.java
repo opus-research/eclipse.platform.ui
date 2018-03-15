b948fb72451d8a7deba55dbdb209ef64fbb8834e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:07:01 +0000

    First commited as before_90272_rev1

--- bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/WorkbenchSWTActivator.java
+++ bundles/org.eclipse.e4.ui.workbench.swt/src/org/eclipse/e4/ui/internal/workbench/swt/WorkbenchSWTActivator.java
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.e4.ui.internal.workbench.swt;
 
-import static org.eclipse.e4.ui.internal.workbench.swt.Policy.*;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -19,7 +17,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Hashtable;
 import org.eclipse.core.internal.runtime.InternalPlatform;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
@@ -28,7 +25,6 @@ import org.eclipse.jface.dialogs.DialogSettings;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.osgi.service.debug.DebugOptions;
-import org.eclipse.osgi.service.debug.DebugOptionsListener;
 import org.eclipse.osgi.service.debug.DebugTrace;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
@@ -41,7 +37,8 @@ import org.osgi.util.tracker.ServiceTracker;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class WorkbenchSWTActivator implements BundleActivator, DebugOptionsListener {
+public class WorkbenchSWTActivator implements BundleActivator { // extends
+																// Plugin {
 	public static final String PI_RENDERERS = "org.eclipse.e4.ui.workbench.swt"; //$NON-NLS-1$
 
 	private BundleContext context;
@@ -72,9 +69,6 @@ public class WorkbenchSWTActivator implements BundleActivator, DebugOptionsListe
 	public void start(BundleContext context) throws Exception {
 		activator = this;
 		this.context = context;
-		Hashtable<String, String> props = new Hashtable<>(2);
-		props.put(DebugOptions.LISTENER_SYMBOLICNAME, PI_RENDERERS);
-		context.registerService(DebugOptionsListener.class, this, props);
 	}
 
 	@Override
@@ -126,7 +120,9 @@ public class WorkbenchSWTActivator implements BundleActivator, DebugOptionsListe
 	}
 
 	public static void trace(String option, String msg, Throwable error) {
-		if (DEBUG) {
+		final DebugOptions debugOptions = activator.getDebugOptions();
+		if (debugOptions.isDebugEnabled()
+				&& debugOptions.getBooleanOption(PI_RENDERERS + option, false)) {
 			System.out.println(msg);
 			if (error != null) {
 				error.printStackTrace(System.out);
@@ -135,15 +131,21 @@ public class WorkbenchSWTActivator implements BundleActivator, DebugOptionsListe
 		activator.getTrace().trace(option, msg, error);
 	}
 
-	@Override
-	public void optionsChanged(DebugOptions options) {
-		trace = options.newDebugTrace(PI_RENDERERS);
-		DEBUG = options.getBooleanOption(PI_RENDERERS + DEBUG_FLAG, false);
-		DEBUG_MENUS = options.getBooleanOption(PI_RENDERERS + DEBUG_MENUS_FLAG, false);
-		DEBUG_RENDERER = options.getBooleanOption(PI_RENDERERS + DEBUG_RENDERER_FLAG, false);
+	public DebugOptions getDebugOptions() {
+		if (debugTracker == null) {
+			if (context == null) {
+				return null;
+			}
+			debugTracker = new ServiceTracker<>(context, DebugOptions.class, null);
+			debugTracker.open();
+		}
+		return debugTracker.getService();
 	}
 
 	public DebugTrace getTrace() {
+		if (trace == null) {
+			trace = getDebugOptions().newDebugTrace(PI_RENDERERS);
+		}
 		return trace;
 	}
 
