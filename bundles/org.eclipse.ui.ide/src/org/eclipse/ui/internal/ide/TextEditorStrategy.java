4e41636fb7b4ac23c9485e7f5db56719e4cfea09
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:31:57 +0000

    First commited as before_66647_rev1

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/TextEditorStrategy.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/TextEditorStrategy.java
@@ -12,13 +12,13 @@ package org.eclipse.ui.internal.ide;
 
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IEditorRegistry;
-import org.eclipse.ui.ide.IUnassociatedEditorStrategy;
+import org.eclipse.ui.ide.IUnknownEditorStrategy;
 
 /**
  * @since 3.12
  *
  */
-public final class TextEditorStrategy implements IUnassociatedEditorStrategy {
+public final class TextEditorStrategy implements IUnknownEditorStrategy {
 
 	@Override
 	public IEditorDescriptor getEditorDescriptor(String name, IEditorRegistry editorReg) {
