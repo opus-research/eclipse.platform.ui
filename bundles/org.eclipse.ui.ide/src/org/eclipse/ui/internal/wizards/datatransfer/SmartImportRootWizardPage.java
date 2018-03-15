2dd5b00b7fd4bf68755d547203857dac7a5cc962
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:31:04 +0000

    First commited as before_70371_rev3

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportRootWizardPage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportRootWizardPage.java
@@ -102,11 +102,9 @@ public class SmartImportRootWizardPage extends WizardPage {
 		public String getText(Object o) {
 			File file = (File) o;
 			String label = file.getAbsolutePath();
-			File root = getWizard().getImportJob().getRoot();
-			if (label.startsWith(root.getAbsolutePath())) {
-				if (root.getParentFile() != null) {
-					label = label.substring(root.getParentFile().getAbsolutePath().length() + 1);
-				}
+			if (label.startsWith(getWizard().getImportJob().getRoot().getAbsolutePath())) {
+				label = label
+						.substring(getWizard().getImportJob().getRoot().getParentFile().getAbsolutePath().length() + 1);
 			}
 			return label;
 		}
