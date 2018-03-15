ca26281bc3c2253f31eb13ea7e0e3a130c0286cf
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:12:35 +0000

    First commited as before_77276_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/EditorRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/EditorRegistry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,6 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -640,7 +639,7 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
                         .append(IWorkbenchConstants.EDITOR_FILE_NAME)
                         .toOSString());
                 reader = new BufferedReader(new InputStreamReader(stream,
-						StandardCharsets.UTF_8));
+                        "utf-8")); //$NON-NLS-1$
             } else {
                 reader = new StringReader(xmlString);
             }
@@ -859,7 +858,7 @@ public class EditorRegistry extends EventManager implements IEditorRegistry, IEx
                         .append(IWorkbenchConstants.RESOURCE_TYPE_FILE_NAME)
                         .toOSString());
                 reader = new BufferedReader(new InputStreamReader(stream,
-						StandardCharsets.UTF_8));
+                        "utf-8")); //$NON-NLS-1$
             } else {
                 reader = new StringReader(xmlString);
             }
