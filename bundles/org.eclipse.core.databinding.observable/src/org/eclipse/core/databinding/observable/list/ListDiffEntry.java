0ef0f0b446154c06c99b2631aeb9dc6b816bd234
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:56:57 +0000

    First commited as before_98838_rev1

--- bundles/org.eclipse.core.databinding.observable/src/org/eclipse/core/databinding/observable/list/ListDiffEntry.java
+++ bundles/org.eclipse.core.databinding.observable/src/org/eclipse/core/databinding/observable/list/ListDiffEntry.java
@@ -39,6 +39,9 @@ public abstract class ListDiffEntry<E> {
 	 */
 	public abstract E getElement();
 
+	/**
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		StringBuilder buffer = new StringBuilder();
