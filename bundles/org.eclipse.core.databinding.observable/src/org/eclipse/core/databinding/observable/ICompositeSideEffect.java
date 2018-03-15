8f2105081d3f09066f6fc475f38c6e24d97591db
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:55:22 +0000

    First commited as after_68282_rev1

---
+++ bundles/org.eclipse.core.databinding.observable/src/org/eclipse/core/databinding/observable/ICompositeSideEffect.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2016 vogella GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Simon Scholz <simon.scholz@vogella.com> - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.core.databinding.observable;
+
+import java.util.function.Consumer;
+
+import org.eclipse.core.internal.databinding.observable.CompositeSideEffect;
+
+/**
+ * An {@link ICompositeSideEffect} is used to aggregate several
+ * {@link ISideEffect} instances together in one. So all {@link ISideEffect}
+ * methods called on an instance of a {@link ICompositeSideEffect} will be
+ * delegated to the aggregated {@link ISideEffect} objects.
+ *
+ * @since 1.6
+ */
+public interface ICompositeSideEffect extends ISideEffect {
+
+	/**
+	 * This {@link Consumer} is supposed to be passed to an
+	 * {@link ISideEffectFactory}, so that this composite can be filled
+	 * automatically by the factory.
+	 *
+	 * @return Consumer<ISideEffect> and never <code>null</code>
+	 */
+	Consumer<ISideEffect> getSideEffectConsumer();
+
+	/**
+	 * This factory method creates an {@link ICompositeSideEffect}.
+	 *
+	 * @return ICompositeSideEffect
+	 */
+	static ICompositeSideEffect create() {
+		return new CompositeSideEffect();
+	}
+
+	/**
+	 * This factory method creates an {@link ICompositeSideEffect}.
+	 *
+	 * @param realm
+	 *            {@link Realm}
+	 *
+	 * @return ICompositeSideEffect
+	 */
+	static ICompositeSideEffect create(Realm realm) {
+		return new CompositeSideEffect(realm);
+	}
+}
