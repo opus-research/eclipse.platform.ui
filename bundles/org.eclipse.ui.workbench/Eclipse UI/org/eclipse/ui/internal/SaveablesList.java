8389b5cebc54030ebd1ff5d9d8fb8417cc8cbccd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:29:06 +0000

    First commited as before_94123_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/SaveablesList.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/SaveablesList.java
@@ -793,9 +793,11 @@ public class SaveablesList implements ISaveablesLifecycleListener {
 
 		@Override
 		protected void createButtonsForButtonBar(Composite parent) {
-			createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+			createButton(parent, IDialogConstants.OK_ID,
+					WorkbenchMessages.SaveableHelper_Save, true);
 			if (canCancel) {
-				createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+				createButton(parent, IDialogConstants.CANCEL_ID,
+						WorkbenchMessages.SaveableHelper_Cancel, false);
 			}
 		}
 
