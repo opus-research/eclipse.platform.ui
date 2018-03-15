b4889071c11927164bbf59f44ed976ae45b05850
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:12:13 +0000

    First commited as before_65348_rev6

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/UnknownEditorStrategyRegistry.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/UnknownEditorStrategyRegistry.java
@@ -106,25 +106,4 @@ public class UnknownEditorStrategyRegistry {
 		idsToLabel = res;
 	}
 
-	/**
-	 * @param strategyId
-	 * @return Whether the specified strategy is interactive, or false is
-	 *         strategy is unknown
-	 */
-	public static boolean isInteractive(String strategyId) {
-		if (strategyId == null) {
-			return false;
-		}
-		IExtensionRegistry extRegistry = Platform.getExtensionRegistry();
-		IConfigurationElement[] extensions = extRegistry.getConfigurationElementsFor(EXTENSION_POINT_ID);
-		if (extensions != null) {
-			for (IConfigurationElement extension : extensions) {
-				if (strategyId.equals(readAttribute(extension, "id"))) { //$NON-NLS-1$
-					return Boolean.parseBoolean(readAttribute(extension, "interactive")); //$NON-NLS-1$
-				}
-			}
-		}
-		return false;
-	}
-
 }
