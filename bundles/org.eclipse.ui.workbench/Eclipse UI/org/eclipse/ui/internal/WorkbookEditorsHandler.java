4219208e6ad0afecfd1a08a72ce4f1d56b2233db
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:00:52 +0000

    First commited as before_82039_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbookEditorsHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbookEditorsHandler.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Marc-Andre Laperle (Ericsson) - Bug 413278
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 497618, 368977, 502123
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 497618
  ******************************************************************************/
 
 package org.eclipse.ui.internal;
@@ -16,21 +16,19 @@ package org.eclipse.ui.internal;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.e4.ui.internal.workbench.renderers.swt.AbstractTableInformationControl;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPlaceholder;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.workbench.renderers.swt.StackRenderer;
 import org.eclipse.swt.custom.CTabFolder;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
- * Shows a list of open editor and parts in the current or last active workbook.
+ * Shows a list of open editors in the current or last active workbook.
  *
  * @since 3.4
  *
@@ -41,12 +39,12 @@ public class WorkbookEditorsHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		MUIElement uiElement = null;
 
-		IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
-		if (activePart != null) {
+		IEditorPart activeEditor = HandlerUtil.getActiveEditor(event);
+		if (activeEditor != null) {
 			IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 			WorkbenchPage page = (WorkbenchPage) workbenchWindow.getActivePage();
 			if (page != null) {
-				IWorkbenchPartReference reference = page.getReference(activePart);
+				IWorkbenchPartReference reference = page.getReference(activeEditor);
 				if (reference != null) {
 					uiElement = page.getActiveElement(reference);
 				}
@@ -57,19 +55,13 @@ public class WorkbookEditorsHandler extends AbstractHandler {
 			uiElement = ((MPlaceholder) uiElement).getRef();
 		}
 
-		Shell activeShell = HandlerUtil.getActiveShell(event);
 		MPartStack activeStack = getActiveStack(uiElement);
 		if (activeStack != null) {
-			// Items list is not visible, to be displayed
 			if (activeStack.getRenderer() instanceof StackRenderer
 					&& activeStack.getWidget() instanceof CTabFolder) {
 				StackRenderer stackRenderer = (StackRenderer) activeStack.getRenderer();
 				stackRenderer.showAvailableItems(activeStack, (CTabFolder) activeStack.getWidget(), true);
 			}
-		} else if (activeShell != null && activeShell.getData() instanceof AbstractTableInformationControl) {
-			// Items list is already displayed, we cycle through the list
-			AbstractTableInformationControl atc = (AbstractTableInformationControl) activeShell.getData();
-			atc.next();
 		}
 		return null;
 	}
