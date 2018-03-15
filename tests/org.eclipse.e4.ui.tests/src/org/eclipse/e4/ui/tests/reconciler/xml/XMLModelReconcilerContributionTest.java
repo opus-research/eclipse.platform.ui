aac4617a17b03cf3ab85136bfdbba2dec481f4b6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:14:20 +0000

    First commited as before_65494_rev2

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/reconciler/xml/XMLModelReconcilerContributionTest.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/reconciler/xml/XMLModelReconcilerContributionTest.java
@@ -23,11 +23,4 @@ public class XMLModelReconcilerContributionTest extends
 		return new ModelReconcilingService();
 	}
 
-	protected boolean serializedStateHasChanges(Object state) {
-		assertTrue(state instanceof org.w3c.dom.Document);
-		org.w3c.dom.Document doc = (org.w3c.dom.Document) state;
-		assertTrue(doc.hasChildNodes() && doc.getChildNodes().getLength() == 1
-				&& "changes".equals(doc.getFirstChild().getNodeName()));
-		return doc.getFirstChild().hasChildNodes();
-	}
 }
