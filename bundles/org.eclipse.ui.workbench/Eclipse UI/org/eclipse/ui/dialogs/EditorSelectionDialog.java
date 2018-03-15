5d4a396f4e9a40906382bd514d6865e849cac888
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:18:53 +0000

    First commited as before_65513_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/EditorSelectionDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/dialogs/EditorSelectionDialog.java
@@ -11,7 +11,6 @@
  *     Helena Halperin - Bug 298747
  *     Andrey Loskutov <loskutov@gmx.de> - Bug 378485, 460555, 463262
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 472654
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 486859
  *******************************************************************************/
 package org.eclipse.ui.dialogs;
 
@@ -329,8 +328,10 @@ public class EditorSelectionDialog extends Dialog {
 				rememberTypeButton.addListener(SWT.Selection, listener);
 				data = new GridData();
 				data.horizontalSpan = 2;
+				data.horizontalIndent = 15;
 				rememberTypeButton.setLayoutData(data);
 				rememberTypeButton.setFont(font);
+				rememberTypeButton.setEnabled(false);
 			}
 		}
 
@@ -719,6 +720,9 @@ public class EditorSelectionDialog extends Dialog {
 	protected void updateEnableState() {
 		boolean enableExternal = externalButton.getSelection();
 		browseExternalEditorsButton.setEnabled(enableExternal);
+		if (rememberEditorButton != null && rememberTypeButton != null) {
+			rememberTypeButton.setEnabled(rememberEditorButton.getSelection());
+		}
 		updateOkButton();
 	}
 
@@ -768,16 +772,6 @@ public class EditorSelectionDialog extends Dialog {
 				} else {
 					selectedEditor = null;
 					okButton.setEnabled(false);
-				}
-			}
-			// 486859 both checked: checking one box unchecks the other
-			if (rememberEditorButton != null && rememberTypeButton != null && rememberEditorButton.getSelection()
-					&& rememberTypeButton.getSelection()) {
-				if (event.widget == rememberEditorButton) {
-					rememberTypeButton.setSelection(false);
-				}
-				if (event.widget == rememberTypeButton) {
-					rememberEditorButton.setSelection(false);
 				}
 			}
 			updateEnableState();
