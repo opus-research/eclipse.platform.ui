d12cd6cd087e64a9fa3fbe44cd339706bbe87afe
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:33:42 +0000

    First commited as after_95014_rev1

--- bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelServiceImpl.java
+++ bundles/org.eclipse.e4.ui.workbench/src/org/eclipse/e4/ui/internal/workbench/ModelServiceImpl.java
@@ -508,6 +508,21 @@ public class ModelServiceImpl implements EModelService {
 		if (appElement instanceof MApplication) {
 			getNullRefPlaceHolders(element, refWin, true);
 		}
+		List<MPlaceholder> elementsToHide = findElements(element, null, MPlaceholder.class, null);
+		for (MPlaceholder elementToHide : elementsToHide) {
+			if (elementToHide.getRef().getTags().contains(IPresentationEngine.NO_RESTORE)) {
+				elementToHide.setToBeRendered(false);
+				MElementContainer<MUIElement> phParent = elementToHide.getParent();
+				if (phParent.getSelectedElement() == elementToHide) {
+					phParent.setSelectedElement(null);
+				}
+				int vc = countRenderableChildren(phParent);
+				if (vc == 0) {
+					if (!isLastEditorStack(phParent))
+						phParent.setToBeRendered(false);
+				}
+			}
+		}
 
 		return element;
 	}
