cfdc7eef5ed3ae0e68ed084143c8e4c66596c658
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:53:33 +0000

    First commited as before_88657_rev3

--- tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/FormTextModelTest.java
+++ tests/org.eclipse.ui.tests.forms/forms/org/eclipse/ui/tests/forms/widgets/FormTextModelTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Ralf M Petter<ralf.petter@gmail.com> and others.
+ * Copyright (c) 2016 Ralf M Petter<ralf.petter@gmail.com> and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,14 +12,11 @@ package org.eclipse.ui.tests.forms.widgets;
 
 import org.eclipse.ui.internal.forms.widgets.FormTextModel;
 import org.junit.Assert;
-import org.junit.Test;
 
-/**
- * Tests for FormTextModel
- */
-public class FormTextModelTest {
+import junit.framework.TestCase;
 
-	@Test
+public class FormTextModelTest extends TestCase {
+
 	public void testWhitespaceNormalized() {
 		FormTextModel formTextModel = new FormTextModel();
 		formTextModel.setWhitespaceNormalized(true);
@@ -28,7 +25,6 @@ public class FormTextModelTest {
 				"line with whitespace Test" + System.lineSeparator(), formTextModel.getAccessibleText());
 	}
 
-	@Test
 	public void testWhitespaceNotNormalized() {
 		FormTextModel formTextModel = new FormTextModel();
 		formTextModel.setWhitespaceNormalized(false);
