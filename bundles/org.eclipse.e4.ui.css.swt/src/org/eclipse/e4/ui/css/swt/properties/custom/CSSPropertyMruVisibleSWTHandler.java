74d7f97edd1598dfa71c4f3deb58fea9893492b6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:46:35 +0000

    First commited as after_81046_rev1

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/properties/custom/CSSPropertyMruVisibleSWTHandler.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/css/swt/properties/custom/CSSPropertyMruVisibleSWTHandler.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.e4.ui.css.swt.properties.custom;
 
+import org.eclipse.e4.ui.css.core.dom.properties.ICSSPropertyHandler;
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
 import org.eclipse.e4.ui.css.swt.properties.AbstractCSSPropertySWTHandler;
 import org.eclipse.swt.custom.CTabFolder;
@@ -17,6 +18,8 @@ import org.eclipse.swt.widgets.Control;
 import org.w3c.dom.css.CSSValue;
 
 public class CSSPropertyMruVisibleSWTHandler extends AbstractCSSPropertySWTHandler{
+
+	public static final ICSSPropertyHandler INSTANCE = new CSSPropertyMruVisibleSWTHandler();
 
 	private static boolean mruControlledByCSS = true;
 
