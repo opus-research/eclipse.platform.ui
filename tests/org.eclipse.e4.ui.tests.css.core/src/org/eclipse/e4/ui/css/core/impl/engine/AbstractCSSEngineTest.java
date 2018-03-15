cfece43fecf484ec0a60d4d71a13618a6e512fd0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:17:32 +0000

    First commited as after_83416_rev1

---
+++ tests/org.eclipse.e4.ui.tests.css.core/src/org/eclipse/e4/ui/css/core/impl/engine/AbstractCSSEngineTest.java
@@ -0,0 +1,56 @@
+package org.eclipse.e4.ui.css.core.impl.engine;
+
+import static org.junit.Assert.assertNull;
+
+import java.util.Objects;
+
+import org.eclipse.e4.ui.css.core.dom.IElementProvider;
+import org.eclipse.e4.ui.css.core.dom.parsers.CSSParser;
+import org.eclipse.e4.ui.css.core.engine.CSSEngine;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Element;
+
+public class AbstractCSSEngineTest {
+	private AbstractCSSEngine objectUnderTest;
+
+	@Before
+	public void setUp() {
+		objectUnderTest = new AbstractCSSEngine() {
+
+			@Override
+			public void reapply() {
+				// mock does nothing
+			}
+
+			@Override
+			public CSSParser makeCSSParser() {
+				return null;
+			}
+
+		};
+		objectUnderTest.setElementProvider(new IElementProvider() {
+
+			@Override
+			public Element getElement(Object element, CSSEngine engine) {
+				// throws NPE if parameter is null
+				Objects.requireNonNull(element);
+				if (element instanceof Element) {
+					return (Element) element;
+				}
+				return null;
+			}
+		});
+	}
+
+	/**
+	 * @see <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=506120">Bug
+	 *      506120 - [CSS] NPE if CSS styling is disabled</a>
+	 */
+	@Test
+	public void testGetElement_null() {
+		Element result = objectUnderTest.getElement(null);
+		assertNull(result);
+	}
+
+}
