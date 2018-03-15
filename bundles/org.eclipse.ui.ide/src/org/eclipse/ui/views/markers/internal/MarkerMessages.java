e22a746b6f12a9ef3f765a83dfbec8dc1f4b1d22
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:55:51 +0000

    First commited as before_98596_rev2

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/MarkerMessages.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/views/markers/internal/MarkerMessages.java
@@ -72,7 +72,6 @@ public class MarkerMessages extends NLS {
 
 	public static String deleteActionConfirmTitle;
 	public static String deleteActionConfirmMessage;
-	public static String deleteActionConfirm_buttonDeleteLabel;
 
 	public static String filtersDialog_title;
 	public static String configureFiltersCommand_title;
