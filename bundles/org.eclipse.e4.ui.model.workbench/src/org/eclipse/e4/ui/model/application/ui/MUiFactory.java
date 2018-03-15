4d63e89edd0c2df87208ede9104bfdfbdb3113bb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:52:17 +0000

    First commited as before_88492_rev6

--- bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/application/ui/MUiFactory.java
+++ bundles/org.eclipse.e4.ui.model.workbench/src/org/eclipse/e4/ui/model/application/ui/MUiFactory.java
@@ -37,14 +37,4 @@ public interface MUiFactory {
 	 */
 	MCoreExpression createCoreExpression();
 
-	/**
-	 * Returns a new object of class '<em>Imperative Expression</em>'. <!--
-	 * begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @return a new object of class '<em>Imperative Expression</em>'.
-	 * @generated
-	 * @since 1.3
-	 */
-	MImperativeExpression createImperativeExpression();
-
 } //MUiFactory
