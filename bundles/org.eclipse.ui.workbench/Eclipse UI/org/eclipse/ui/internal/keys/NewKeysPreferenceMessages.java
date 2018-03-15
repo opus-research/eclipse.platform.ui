aa363b2dbd4cd850cd261f336f213b8b478e4a98
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:20:07 +0000

    First commited as before_77949_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/NewKeysPreferenceMessages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/keys/NewKeysPreferenceMessages.java
@@ -63,8 +63,6 @@ public class NewKeysPreferenceMessages extends NLS {
 	public static String InternalFilterCheckBox_Text;
 	public static String UncategorizedFilterCheckBox_Text;
 
-	public static String OverrriddenBy_Text;
-
 	static {
 		// load message values from bundle file
 		NLS.initializeMessages(BUNDLE_NAME, NewKeysPreferenceMessages.class);
