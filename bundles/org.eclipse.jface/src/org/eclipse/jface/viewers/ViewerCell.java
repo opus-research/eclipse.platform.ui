42995140a25805eea04fa98ec7a19e7f0fbe2eac
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:34:21 +0000

    First commited as after_104956_rev2

--- bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ViewerCell.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/viewers/ViewerCell.java
@@ -289,8 +289,7 @@ public class ViewerCell {
 			if (columnIndex >= 0 && columnIndex < row.getColumnCount()) {
 				ViewerCell cell = row.getCellAtVisualIndex(columnIndex);
 				if (cell != null) {
-					while (cell != null
-							&& columnIndex < row.getColumnCount() - 1
+					while (columnIndex < row.getColumnCount() - 1
 							&& columnIndex > 0) {
 						if (cell.isVisible()) {
 							break;
