18c5ba827c3d75543edc60c5952d3f4375c1e3f6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:22:37 +0000

    First commited as after_83984_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/AboutDialog.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/dialogs/AboutDialog.java
@@ -79,7 +79,9 @@ public class AboutDialog extends TrayDialog {
 
     private final static int DETAILS_ID = IDialogConstants.CLIENT_ID + 1;
 
-	private final static int EXPORT_ID = DETAILS_ID + 1;
+	private final static int EXPORT_ID = IDialogConstants.CLIENT_ID + 2;
+
+	private final static int IMPORT_ID = IDialogConstants.CLIENT_ID + 3;
 
     private String productName;
 
@@ -185,6 +187,7 @@ public class AboutDialog extends TrayDialog {
 
         createButton(parent, DETAILS_ID, WorkbenchMessages.AboutDialog_DetailsButton, false);
 		createButton(parent, EXPORT_ID, WorkbenchMessages.AboutDialog_ExportButton, false);
+		createButton(parent, IMPORT_ID, WorkbenchMessages.AboutDialog_ImportButton, false);
 
         Label l = new Label(parent, SWT.NONE);
         l.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
