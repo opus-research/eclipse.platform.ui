2f33a635ff880764b1058f586b22b164cec477e6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:03:32 +0000

    First commited as after_99862_rev2

--- tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/IWorkbenchPageTest.java
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/api/IWorkbenchPageTest.java
@@ -3214,4 +3214,21 @@ public class IWorkbenchPageTest extends UITestCase {
 		assertEquals(0, page.getEditorReferences().length);
 	}
 
+	public void testGetViewStack() throws PartInitException {
+		IViewPart part = fActivePage.showView(MockViewPart.ID);
+
+		IViewPart[] stack = fActivePage.getViewStack(part);
+		assertEquals(1, stack.length);
+
+		assertEquals(part, stack[0]);
+	}
+
+	public void testGetViewStackWithSecondaryId() throws PartInitException {
+		IViewPart part = fActivePage.showView(MockViewPart.ID, "1", IWorkbenchPage.VIEW_CREATE);
+
+		IViewPart[] stack = fActivePage.getViewStack(part);
+		assertEquals(1, stack.length);
+
+		assertEquals(part, stack[0]);
+	}
 }
