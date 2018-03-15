93f7f481d5fcb7b6644988613ecf3c45ef38bacb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:38:57 +0000

    First commited as before_96058_rev2

--- bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/internal/css/swt/dom/scrollbar/StyledTextThemedScrollBarAdapter.java
+++ bundles/org.eclipse.e4.ui.css.swt/src/org/eclipse/e4/ui/internal/css/swt/dom/scrollbar/StyledTextThemedScrollBarAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Fabio Zadrozny and others.
+ * Copyright (c) 2016 Fabio Zadrozny and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -284,18 +284,6 @@ public class StyledTextThemedScrollBarAdapter extends AbstractThemedScrollBarAda
 				gc.setBackground(background);
 			}
 		}
-
-		/**
-		 * This function is overridden to force an instantaneous redraw in the
-		 * StyledText whenever we change the selection on the vertical
-		 * scrollbar.
-		 */
-		@Override
-		protected void notifyScrollbarSelectionChanged(Scrollable scrollable, int detail) {
-			super.notifyScrollbarSelectionChanged(scrollable, detail);
-			StyledText styledText = (StyledText) scrollable;
-			styledText.update();
-		}
 	}
 
 	/**
@@ -391,18 +379,6 @@ public class StyledTextThemedScrollBarAdapter extends AbstractThemedScrollBarAda
 						fHandleDrawnRect.height, borderRadius, borderRadius);
 				gc.setBackground(background);
 			}
-		}
-
-		/**
-		 * This function is overridden to force an instantaneous redraw in the
-		 * StyledText whenever we change the selection on the horizontal
-		 * scrollbar.
-		 */
-		@Override
-		protected void notifyScrollbarSelectionChanged(Scrollable scrollable, int detail) {
-			super.notifyScrollbarSelectionChanged(scrollable, detail);
-			StyledText styledText = (StyledText) scrollable;
-			styledText.update();
 		}
 	}
 
