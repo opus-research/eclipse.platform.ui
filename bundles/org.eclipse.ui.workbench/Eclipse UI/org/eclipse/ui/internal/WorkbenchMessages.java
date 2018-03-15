6952c5bb4a31df44b27a3380321e88ac47d01177
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:30:16 +0000

    First commited as before_70347_rev4

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchMessages.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/WorkbenchMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2015 IBM Corporation and others.
+ * Copyright (c) 2005, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@
  * 		Implemented workbench auto-save to correctly restore state in case of crash.
  * Andrey Loskutov <loskutov@gmx.de> - Bug 388476, 445538, 463262
  * Alain Bernard <alain.bernard1224@gmail.com> - Bug 281490
+ * Patrik Suzzi <psuzzi@gmail.com> - Bug 491785
  *******************************************************************************/
 package org.eclipse.ui.internal;
 
@@ -458,6 +459,9 @@ public class WorkbenchMessages extends NLS {
 	public static String ViewsPreference_visibleTabs_description;
 	public static String ViewsPreference_enableMRU;
 	public static String ViewsPreference_useColoredLabels;
+	public static String ToggleFullScreenMode_ActivationPopup_Description;
+	public static String ToggleFullScreenMode_ActivationPopup_Description_NoKeybinding;
+	public static String ToggleFullScreenMode_ActivationPopup_DoNotShowAgain;
 
 	// --- File Editors ---
 	public static String FileEditorPreference_fileTypes;
@@ -953,6 +957,8 @@ public class WorkbenchMessages extends NLS {
     public static String ContentAssist_Cue_Description_Key;
 
 	//Settings transfer
+	public static String WorkbenchLayoutSettings_Name;
+	public static String WorkbenchSettings_CouldNotCreateDirectories;
 	public static String WorkbenchSettings_CouldNotFindLocation;
 	public static String WorkingSets_Name;
 	public static String WorkingSets_CannotSave;
