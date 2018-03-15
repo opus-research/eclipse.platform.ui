884ba89d6f00d0629f73fc16b4c25f5cab031ee5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:59:18 +0000

    First commited as before_74275_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/FullScreenHandler.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/handlers/FullScreenHandler.java
@@ -105,10 +105,7 @@ public class FullScreenHandler extends AbstractHandler {
 
 		@Override
 		protected Point getInitialLocation(Point initialSize) {
-			if (getShell().getParent() == null) {
-				return super.getInitialLocation(initialSize);
-			}
-			Rectangle bounds = getShell().getParent().getMonitor().getBounds();
+			Rectangle bounds = getShell().getMonitor().getBounds();
 			GC gc = new GC(getShell().getDisplay());
 			int textExtendX = gc.textExtent(message).x;
 			gc.dispose();
