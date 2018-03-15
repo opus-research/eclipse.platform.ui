12a843999b9af96692bebce1f3eec50127d3dd72
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:27:56 +0000

    First commited as before_93969_rev1

--- bundles/org.eclipse.jface/src/org/eclipse/jface/preference/PreferenceDialog.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/preference/PreferenceDialog.java
@@ -332,8 +332,7 @@ public class PreferenceDialog extends TrayDialog implements IPreferencePageConta
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
 		// create OK and Cancel buttons by default
-		okButton = createButton(parent, IDialogConstants.OK_ID,
-				JFaceResources.getString("PreferencesDialog.okButtonLabel"), true); //$NON-NLS-1$
+		okButton = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
 		getShell().setDefaultButton(okButton);
 		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
 	}
@@ -1481,4 +1480,5 @@ public class PreferenceDialog extends TrayDialog implements IPreferencePageConta
 	protected boolean isResizable() {
     	return true;
     }
+
 }
