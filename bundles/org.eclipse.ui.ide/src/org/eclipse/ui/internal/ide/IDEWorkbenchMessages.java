47e1fc496fa3dfe1383c971937653f656a1fd376
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:25:08 +0000

    First commited as before_93398_rev3

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/IDEWorkbenchMessages.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/IDEWorkbenchMessages.java
@@ -954,7 +954,6 @@ public class IDEWorkbenchMessages extends NLS {
 	public static String ChooseWorkspaceDialog_defaultProductName;
 	public static String ChooseWorkspaceDialog_workspaceEntryLabel;
 	public static String ChooseWorkspaceDialog_browseLabel;
-	public static String ChooseWorkspaceDialog_launchLabel;
 	public static String ChooseWorkspaceDialog_directoryBrowserTitle;
 	public static String ChooseWorkspaceDialog_directoryBrowserMessage;
 	public static String ChooseWorkspaceDialog_removeWorkspaceSelection;
@@ -989,6 +988,7 @@ public class IDEWorkbenchMessages extends NLS {
 	public static String CleanDialog_buildCleanAuto;
 	public static String CleanDialog_buildCleanManual;
 	public static String CleanDialog_title;
+	public static String CleanDialog_clean_button_label;
 	public static String CleanDialog_cleanAllButton;
 	public static String CleanDialog_cleanSelectedButton;
 	public static String CleanDialog_buildNowButton;
