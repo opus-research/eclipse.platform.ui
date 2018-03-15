4b40a968ed7508d1b436ff57ebcef5da7c743f30
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:09:42 +0000

    First commited as before_65221_rev3

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/e4/ui/workbench/addons/perspectiveswitcher/PerspectiveSwitcher.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/e4/ui/workbench/addons/perspectiveswitcher/PerspectiveSwitcher.java
@@ -8,8 +8,9 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Sopot Cela <sopotcela@gmail.com> - Bug 391961
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 440810
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 440810, 485840
  *     Andrey Loskutov <loskutov@gmx.de> - Bug 380233
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 485829
  ******************************************************************************/
 
 package org.eclipse.e4.ui.workbench.addons.perspectiveswitcher;
@@ -374,21 +375,25 @@ public class PerspectiveSwitcher {
 
 		hookupDnD(perspSwitcherToolbar);
 
-		final ToolItem createItem = new ToolItem(perspSwitcherToolbar, SWT.PUSH);
-		createItem.setImage(getOpenPerspectiveImage());
-		createItem.setToolTipText(WorkbenchMessages.OpenPerspectiveDialogAction_tooltip);
-		createItem.addSelectionListener(new SelectionListener() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				selectPerspective();
-			}
+		boolean showOpenOnPerspectiveBar = PrefUtil.getAPIPreferenceStore()
+				.getBoolean(IWorkbenchPreferenceConstants.SHOW_OPEN_ON_PERSPECTIVE_BAR);
+		if (showOpenOnPerspectiveBar) {
+			final ToolItem openPerspectiveItem = new ToolItem(perspSwitcherToolbar, SWT.PUSH);
+			openPerspectiveItem.setImage(getOpenPerspectiveImage());
+			openPerspectiveItem.setToolTipText(WorkbenchMessages.OpenPerspectiveDialogAction_tooltip);
+			openPerspectiveItem.addSelectionListener(new SelectionListener() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					selectPerspective();
+				}
 
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				selectPerspective();
-			}
-		});
-		new ToolItem(perspSwitcherToolbar, SWT.SEPARATOR);
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {
+					selectPerspective();
+				}
+			});
+			new ToolItem(perspSwitcherToolbar, SWT.SEPARATOR);
+		}
 
 		MPerspectiveStack stack = getPerspectiveStack();
 		if (stack != null) {
@@ -609,7 +614,7 @@ public class PerspectiveSwitcher {
 		return psItem;
 	}
 
-	// FIXME see https://bugs.eclipse.org/bugs/show_bug.cgi?id=313771
+	// FIXME see https://bugs.eclipse.org/bugs/show_bug.cgi?id=385547
 	private IPerspectiveDescriptor getDescriptorFor(String id) {
 		IPerspectiveRegistry perspectiveRegistry = PlatformUI.getWorkbench()
 				.getPerspectiveRegistry();
@@ -684,8 +689,7 @@ public class PerspectiveSwitcher {
 	}
 
 	private void closePerspective(MPerspective persp) {
-		MWindow win = modelService.getTopLevelWindowFor(persp);
-		WorkbenchPage page = (WorkbenchPage) win.getContext().get(IWorkbenchPage.class);
+		WorkbenchPage page = (WorkbenchPage) window.getContext().get(IWorkbenchPage.class);
 		String perspectiveId = persp.getElementId();
 		IPerspectiveDescriptor desc = getDescriptorFor(perspectiveId);
 		page.closePerspective(desc, perspectiveId, true, true);
