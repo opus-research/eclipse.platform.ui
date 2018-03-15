a9ad7ee7128316a27a50a7d184656a7034c3afb1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:54:08 +0000

    First commited as after_81628_rev1

--- bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/internal/services/ServiceMessages.java
+++ bundles/org.eclipse.e4.ui.services/src/org/eclipse/e4/ui/internal/services/ServiceMessages.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2005, 2015 IBM Corporation and others. All rights reserved.   This
+ * Copyright (c) 2005, 2016 IBM Corporation and others. All rights reserved.   This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -9,22 +9,48 @@
  **********************************************************************/
 package org.eclipse.e4.ui.internal.services;
 
-import org.eclipse.osgi.util.NLS;
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
-public class ServiceMessages extends NLS {
+public class ServiceMessages {
 
 	private static final String BUNDLE_NAME = "org.eclipse.e4.ui.internal.services.serviceMessages"; //$NON-NLS-1$
+	private static ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
 	// Event broker
-	public static String NO_EVENT_ADMIN;
-	public static String NO_BUNDLE_CONTEXT;
+	public static final String NO_EVENT_ADMIN = "NO_EVENT_ADMIN";
+	public static final String NO_BUNDLE_CONTEXT = "NO_BUNDLE_CONTEXT";
 
-	static {
-		// load message values from bundle file
-		reloadMessages();
-	}
-
+	/**
+	 * Reload the ResourceBundle for the current set default locale.
+	 */
 	public static void reloadMessages() {
-		NLS.initializeMessages(BUNDLE_NAME, ServiceMessages.class);
+		RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 	}
+
+	/**
+	 * Return the localized message for the given key and the given placeholder
+	 * replacements.
+	 *
+	 * @param key
+	 *            The key of the message to return.
+	 *            <ul>
+	 *            <li>{@link #NO_BUNDLE_CONTEXT}</li>
+	 *            <li>{@link #NO_EVENT_ADMIN}</li>
+	 *            </ul>
+	 * @param args
+	 *            objects that are used to format/replace placeholder in the
+	 *            message.
+	 * @return the localized message for the given key formatted with the given
+	 *         objects.
+	 */
+	public static String getMessage(String key, Object... args) {
+		try {
+			return MessageFormat.format(RESOURCE_BUNDLE.getString(key), args);
+		} catch (MissingResourceException e) {
+			return "!" + key + "!";
+		}
+	}
+
 }
