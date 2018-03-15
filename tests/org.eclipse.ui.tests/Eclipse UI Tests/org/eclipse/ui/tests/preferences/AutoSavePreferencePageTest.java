b443afcc3a1a597d61d9f4d492273de54f0f548b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:32:26 +0000

    First commited as after_66742_rev5

---
+++ tests/org.eclipse.ui.tests/Eclipse UI Tests/org/eclipse/ui/tests/preferences/AutoSavePreferencePageTest.java
@@ -0,0 +1,211 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Axel Richard <axel.richard@obeo.fr>
+ *******************************************************************************/
+package org.eclipse.ui.tests.preferences;
+
+import org.eclipse.jface.preference.IPreferencePage;
+import org.eclipse.jface.preference.PreferenceDialog;
+import org.eclipse.jface.preference.PreferenceManager;
+import org.eclipse.jface.preference.PreferenceNode;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.IPreferenceConstants;
+import org.eclipse.ui.internal.WorkbenchPlugin;
+import org.eclipse.ui.internal.ide.dialogs.AutoSavePreferencePage;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @since 3.11
+ *
+ */
+public class AutoSavePreferencePageTest {
+
+	/**
+	 * The default interval auto-save value.
+	 */
+	private static final int DEFAULT_VALUE = 20;
+
+	static PreferenceDialog dialog;
+
+	@BeforeClass
+	public static void setUpClass() {
+		setupPreferenceManager();
+		dialog = setupDialog();
+		dialog.open();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		dialog.close();
+		tearDownPreferenceManager();
+	}
+
+	@After
+	public void restoreAndCheckDefaults() {
+		MyAutoSavePreferencePage page = (MyAutoSavePreferencePage) dialog.getSelectedPage();
+		page.performDefaults();
+		page.performOk();
+		boolean autoSave = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getBoolean(IPreferenceConstants.SAVE_AUTOMATICALLY);
+		Assert.assertFalse(autoSave);
+		Assert.assertFalse(page.isAutoSaveButtonSelected());
+		int autoSaveInterval = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getInt(IPreferenceConstants.SAVE_AUTOMATICALLY_INTERVAL);
+		Assert.assertEquals(DEFAULT_VALUE, autoSaveInterval);
+		Assert.assertEquals(DEFAULT_VALUE, page.getAutoSaveIntervalTextValue());
+	}
+
+	@Test
+	public void testEnableAutoSave() {
+		MyAutoSavePreferencePage page = (MyAutoSavePreferencePage) dialog.getSelectedPage();
+		page.selectAutoSaveButton(true);
+		boolean autoSave = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getBoolean(IPreferenceConstants.SAVE_AUTOMATICALLY);
+		Assert.assertTrue(autoSave);
+		Assert.assertTrue(page.isAutoSaveButtonSelected());
+		page.performOk();
+	}
+
+	@Test
+	public void testDisableAutoSave() {
+		MyAutoSavePreferencePage page = (MyAutoSavePreferencePage) dialog.getSelectedPage();
+		page.selectAutoSaveButton(false);
+		boolean autoSave = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getBoolean(IPreferenceConstants.SAVE_AUTOMATICALLY);
+		Assert.assertFalse(autoSave);
+		Assert.assertFalse(page.isAutoSaveButtonSelected());
+		page.performOk();
+	}
+
+	@Test
+	public void testUpdateAutoSaveInterval() {
+		MyAutoSavePreferencePage page = (MyAutoSavePreferencePage) dialog.getSelectedPage();
+		page.selectAutoSaveButton(true);
+		page.setAutoSaveIntervalTextValue(30);
+		int autoSaveInterval = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getInt(IPreferenceConstants.SAVE_AUTOMATICALLY_INTERVAL);
+		// the interval preference in preference store is only updated after
+		// performOk, so the value must be DEFAULT_VALUE before the perform and
+		// 30 after
+		// the perform.
+		Assert.assertEquals(DEFAULT_VALUE, autoSaveInterval);
+		Assert.assertEquals(30, page.getAutoSaveIntervalTextValue());
+		page.performOk();
+		autoSaveInterval = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getInt(IPreferenceConstants.SAVE_AUTOMATICALLY_INTERVAL);
+		Assert.assertEquals(30, autoSaveInterval);
+	}
+
+	@Test
+	public void testUpdateAutoSaveIntervalWithOutOfScopeValue() {
+		MyAutoSavePreferencePage page = (MyAutoSavePreferencePage) dialog.getSelectedPage();
+		page.selectAutoSaveButton(true);
+		page.setAutoSaveIntervalTextValue(0);
+		int autoSaveInterval = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getInt(IPreferenceConstants.SAVE_AUTOMATICALLY_INTERVAL);
+		// the value must be DEFAULT_VALUE before the perform and DEFAULT_VALUE
+		// after
+		// the perform, because the value to set is out of scope
+		Assert.assertEquals(DEFAULT_VALUE, autoSaveInterval);
+		Assert.assertEquals(0, page.getAutoSaveIntervalTextValue());
+		Assert.assertFalse(page.isValid());
+		page.performOk();
+		autoSaveInterval = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getInt(IPreferenceConstants.SAVE_AUTOMATICALLY_INTERVAL);
+		Assert.assertEquals(DEFAULT_VALUE, autoSaveInterval);
+	}
+
+	@Test
+	public void testCheckDefaultAutoSaveConfiguration() {
+		boolean autoSave = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getBoolean(IPreferenceConstants.SAVE_AUTOMATICALLY);
+		Assert.assertFalse(autoSave);
+		int autoSaveInterval = WorkbenchPlugin.getDefault().getPreferenceStore()
+				.getInt(IPreferenceConstants.SAVE_AUTOMATICALLY_INTERVAL);
+		Assert.assertEquals(DEFAULT_VALUE, autoSaveInterval);
+	}
+
+	private static void setupPreferenceManager() {
+		PreferenceManager manager = PlatformUI.getWorkbench().getPreferenceManager();
+
+		IPreferencePage page = new MyAutoSavePreferencePage();
+		PreferenceNode node = new PreferenceNode("org.eclipse.ui.preferencePages.AutoSaveTest", "Auto-save", null,
+				MyAutoSavePreferencePage.class.getName());
+		node.setPage(page);
+		manager.addToRoot(node);
+	}
+
+	private static void tearDownPreferenceManager() {
+		PreferenceManager manager = PlatformUI.getWorkbench().getPreferenceManager();
+
+		manager.remove("org.eclipse.ui.preferencePages.AutoSaveTest");
+	}
+
+	private static PreferenceDialog setupDialog() {
+		PreferenceDialog dial = new PreferenceDialog(Display.getCurrent().getActiveShell(),
+				PlatformUI.getWorkbench().getPreferenceManager());
+		dial.setSelectedNode("org.eclipse.ui.preferencePages.AutoSaveTest");
+		dial.setBlockOnOpen(false);
+
+		return dial;
+	}
+
+	private static class MyAutoSavePreferencePage extends AutoSavePreferencePage {
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see org.eclipse.ui.internal.ide.dialogs.AutoSavePreferencePage#
+		 * isAutoSaveButtonSelected()
+		 */
+		@Override
+		public boolean isAutoSaveButtonSelected() {
+			return super.isAutoSaveButtonSelected();
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see org.eclipse.ui.internal.ide.dialogs.AutoSavePreferencePage#
+		 * selectAutoSaveButton(boolean)
+		 */
+		@Override
+		public void selectAutoSaveButton(boolean enable) {
+			super.selectAutoSaveButton(enable);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see org.eclipse.ui.internal.ide.dialogs.AutoSavePreferencePage#
+		 * getAutoSaveIntervalTextValue()
+		 */
+		@Override
+		public int getAutoSaveIntervalTextValue() {
+			return super.getAutoSaveIntervalTextValue();
+		}
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see org.eclipse.ui.internal.ide.dialogs.AutoSavePreferencePage#
+		 * setAutoSaveIntervalTextValue(int)
+		 */
+		@Override
+		public void setAutoSaveIntervalTextValue(int interval) {
+			super.setAutoSaveIntervalTextValue(interval);
+		}
+
+	}
+}
