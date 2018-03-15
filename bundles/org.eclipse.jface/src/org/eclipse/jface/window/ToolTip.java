77a6e7471c716c3502f72fa65b15b39317d59401
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 15:02:44 +0000

    First commited as before_99561_rev4

--- bundles/org.eclipse.jface/src/org/eclipse/jface/window/ToolTip.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/window/ToolTip.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2017 IBM Corporation and others.
+ * Copyright (c) 2006, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -643,6 +643,8 @@ public abstract class ToolTip {
 					 * a wider area ;-)
 					 */
 					Rectangle rect = shell.getBounds();
+					rect.x += 5;
+					rect.y += 5;
 					rect.width -= 10;
 					rect.height -= 10;
 
