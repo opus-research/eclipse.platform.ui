1713c8f4a332c851fa4bc143707999f66f5bffcc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:52:58 +0000

    First commited as before_68070_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/util/ConfigurationElementMemento.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/util/ConfigurationElementMemento.java
@@ -109,7 +109,7 @@ public final class ConfigurationElementMemento implements IMemento {
 
         if (string != null) {
 			try {
-                return Integer.valueOf(string);
+				return Integer.valueOf(string);
             } catch (NumberFormatException eNumberFormat) {
             }
 		}
