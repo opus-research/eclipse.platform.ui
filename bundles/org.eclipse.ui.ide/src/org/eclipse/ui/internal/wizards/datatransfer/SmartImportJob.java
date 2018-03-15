e6d74b081536bf7efc8b4fec6303e846e4d63308
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:52:51 +0000

    First commited as before_72464_rev1

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportJob.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportJob.java
@@ -611,17 +611,11 @@ public class SmartImportJob extends Job {
 						SmartImportJob.this.rootDirectory,
 						loopMonitor.split(1));
 				if (supportedDirectories != null) {
-					for (File supportedDirectory : supportedDirectories) {
-						if (supportedDirectory.isDirectory()) {
-							if (!res.containsKey(supportedDirectory)) {
-								res.put(supportedDirectory, new ArrayList<ProjectConfigurator>());
-							}
-							res.get(supportedDirectory).add(configurator);
-						} else {
-							IDEWorkbenchPlugin.log("Project detection must return only directories.\n" //$NON-NLS-1$
-									+ supportedDirectory + " is not a directory.\nContributed by " //$NON-NLS-1$
-									+ configurator.getClass().getName());
+					for (File supportedFile : supportedDirectories) {
+						if (!res.containsKey(supportedFile)) {
+							res.put(supportedFile, new ArrayList<ProjectConfigurator>());
 						}
+						res.get(supportedFile).add(configurator);
 					}
 				}
 			}
