fba4145df2a226c8ba3a6ba9e615ab1d76e6372f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:07:52 +0000

    First commited as before_82431_rev2

--- tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/NavigatorTestsPlugin.java
+++ tests/org.eclipse.ui.tests.navigator/src/org/eclipse/ui/tests/navigator/NavigatorTestsPlugin.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.ui.tests.navigator;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
@@ -68,6 +70,42 @@ public class NavigatorTestsPlugin extends AbstractUIPlugin {
 	 *
 	 */
 
+	public static void log(String message) {
+		getDefault().getLog().log(new Status(IStatus.ERROR, PLUGIN_ID, 0, message, null));
+		System.err.println(message);
+		//1FTTJKV: ITPCORE:ALL - log(status) does not allow plugin information to be recorded
+	}
+
+	/**
+	 * Logs errors.
+	 */
+	public static void log(String message, IStatus status) {
+		if (message != null) {
+			getDefault().getLog().log(new Status(IStatus.ERROR, PLUGIN_ID, 0, message, null));
+			System.err.println(message + "\nReason:"); //$NON-NLS-1$
+		}
+		getDefault().getLog().log(status);
+		System.err.println(status.getMessage());
+	}
 
 
+	public static void logError(int aCode, String aMessage, Throwable anException) {
+		getDefault().getLog().log(createErrorStatus(aCode, aMessage, anException));
+	}
+
+	public static void log(int severity, int aCode, String aMessage, Throwable exception) {
+		log(createStatus(severity, aCode, aMessage, exception));
+	}
+
+	public static void log(IStatus aStatus) {
+		getDefault().getLog().log(aStatus);
+	}
+
+	public static IStatus createStatus(int severity, int aCode, String aMessage, Throwable exception) {
+		return new Status(severity, PLUGIN_ID, aCode, aMessage, exception);
+	}
+
+	public static IStatus createErrorStatus(int aCode, String aMessage, Throwable exception) {
+		return createStatus(IStatus.ERROR, aCode, aMessage, exception);
+	}
 }
