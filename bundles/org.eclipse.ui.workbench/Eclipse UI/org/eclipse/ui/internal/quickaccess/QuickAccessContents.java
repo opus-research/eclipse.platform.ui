c43fbee8a72380d1d564007c409e99649b75589d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:22:15 +0000

    First commited as before_70204_rev4

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessContents.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/quickaccess/QuickAccessContents.java
@@ -8,8 +8,9 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Tom Hochstein (Freescale) - Bug 393703 - NotHandledException selecting inactive command under 'Previous Choices' in Quick access
- *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 472654, 491272
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 472654, 491272, 491398
  *     Leung Wang Hei <gemaspecial@yahoo.com.hk> - Bug 483343
+ *     Patrik Suzzi <psuzzi@gmail.com> - Bug 491291
  *******************************************************************************/
 package org.eclipse.ui.internal.quickaccess;
 
@@ -143,6 +144,9 @@ public abstract class QuickAccessContents {
 				item.setForeground(0, grayColor);
 			}
 
+			// update info as-you-type
+			updateInfoLabel();
+
 			updateFeedback(filterTextEmpty, showAllMatches);
 		}
 	}
@@ -187,17 +191,16 @@ public abstract class QuickAccessContents {
 	private void updateInfoLabel() {
 		if (infoLabel != null) {
 			TriggerSequence sequence = getTriggerSequence();
-			if (sequence == null) {
+			boolean forceHide = (getNumberOfFilteredResults() == 0)
+					|| (showAllMatches && (table.getItemCount() <= computeNumberOfItems()));
+			if (sequence == null || forceHide) {
 				infoLabel.setText(""); //$NON-NLS-1$
 			} else if (showAllMatches) {
-				infoLabel
-.setText(NLS.bind(
-						QuickAccessMessages.QuickAccessContents_PressKeyToLimitResults,
-						sequence.format()));
+				infoLabel.setText(
+						NLS.bind(QuickAccessMessages.QuickAccessContents_PressKeyToLimitResults, sequence.format()));
 			} else {
-				infoLabel.setText(NLS.bind(
-						QuickAccessMessages.QuickAccess_PressKeyToShowAllMatches,
-								sequence.format()));
+				infoLabel
+						.setText(NLS.bind(QuickAccessMessages.QuickAccess_PressKeyToShowAllMatches, sequence.format()));
 			}
 			infoLabel.getParent().layout(true);
 		}
@@ -278,6 +281,19 @@ public abstract class QuickAccessContents {
 		return selectionIndex;
 	}
 
+	int numberOfFilteredResults;
+
+	/**
+	 * Compute how many items are effectively filtered at a specific point in
+	 * time. So doing, the quick access content can perform operations that
+	 * depends on this number, i.e. hide the info label.
+	 *
+	 * @return number number of elements filtered
+	 */
+	protected int getNumberOfFilteredResults() {
+		return numberOfFilteredResults;
+	}
+
 	/**
 	 * Returns a list per provider containing matching {@link QuickAccessEntry}
 	 * that should be displayed in the table given a text filter and a perfect
@@ -303,6 +319,7 @@ public abstract class QuickAccessContents {
 		int[] indexPerProvider = new int[providers.length];
 		int countPerProvider = Math.min(maxCount / 4,
 				INITIAL_COUNT_PER_PROVIDER);
+		int prevPick = 0;
 		int countTotal = 0;
 		boolean perfectMatchAdded = true;
 		if (perfectMatch != null) {
@@ -326,6 +343,11 @@ public abstract class QuickAccessContents {
 				if (filter.length() > 0 || provider.isAlwaysPresent() || showAllMatches) {
 					QuickAccessElement[] sortedElements = provider.getElementsSorted();
 					List<QuickAccessEntry> poorFilterMatches = new ArrayList<>();
+
+					// count number or previous picks
+					if ((provider instanceof PreviousPicksProvider)) {
+						prevPick = sortedElements.length;
+					}
 
 					int j = indexPerProvider[i];
 					while (j < sortedElements.length
@@ -398,6 +420,8 @@ public abstract class QuickAccessContents {
 				entries[0].add(entry);
 			}
 		}
+		// number of items matching the filtered search
+		numberOfFilteredResults = countTotal - prevPick;
 		return entries;
 	}
 
@@ -554,20 +578,18 @@ public abstract class QuickAccessContents {
 				JFaceResources.getDialogFont()).setStyle(SWT.BOLD));
 		textLayout.setFont(table.getFont());
 		textLayout.setText(QuickAccessMessages.QuickAccess_AvailableCategories);
-		int maxProviderWidth = (int) (textLayout.getBounds().width * 1.1);
+		int maxProviderWidth = (textLayout.getBounds().width);
 		textLayout.setFont(boldFont);
 		for (int i = 0; i < providers.length; i++) {
 			QuickAccessProvider provider = providers[i];
 			textLayout.setText(provider.getName());
-			int width = (int) (textLayout.getBounds().width * 1.1);
+			int width = (textLayout.getBounds().width);
 			if (width > maxProviderWidth) {
 				maxProviderWidth = width;
 			}
 		}
-		tableColumnLayout.setColumnData(new TableColumn(table, SWT.NONE), new ColumnWeightData(0,
-				maxProviderWidth));
-		tableColumnLayout.setColumnData(new TableColumn(table, SWT.NONE), new ColumnWeightData(100,
-				100));
+		tableColumnLayout.setColumnData(new TableColumn(table, SWT.NONE), new ColumnWeightData(0, maxProviderWidth));
+		tableColumnLayout.setColumnData(new TableColumn(table, SWT.NONE), new ColumnWeightData(100, 100));
 		table.getShell().addControlListener(new ControlAdapter() {
 			@Override
 			public void controlResized(ControlEvent e) {
