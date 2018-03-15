98bc81c95006d71d9d753c91c82942989cb81b14
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 11:55:52 +0000

    First commited as after_73004_rev3

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportJob.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/wizards/datatransfer/SmartImportJob.java
@@ -546,12 +546,24 @@ public class SmartImportJob extends Job {
 			}
 		} else {
 			String projectName = directory.getName();
-			if (this.workspaceRoot.getProject(directory.getName()).exists()) {
-				int i = 1;
-				do {
-					projectName = directory.getName() + '(' + i + ')';
-					i++;
-				} while (this.workspaceRoot.getProject(projectName).exists());
+			if (this.workspaceRoot.getProject(projectName).exists()) {
+				StringBuilder projectNameBuilder = new StringBuilder(projectName);
+				File currentDirectory = directory;
+				while (currentDirectory.canRead() && directory.getParentFile() != null
+						&& this.workspaceRoot.getProject(projectNameBuilder.toString()).exists()) {
+					projectNameBuilder.insert(0, "_"); //$NON-NLS-1$
+					projectNameBuilder.insert(0, currentDirectory.getParentFile().getName());
+					currentDirectory = currentDirectory.getParentFile();
+				}
+				if (!this.workspaceRoot.getProject(projectNameBuilder.toString()).exists()) {
+					projectName = projectNameBuilder.toString();
+				} else {
+					int i = 1;
+					do {
+						projectName = directory.getName() + '(' + i + ')';
+						i++;
+					} while (this.workspaceRoot.getProject(projectName).exists());
+				}
 			}
 
 			desc = ResourcesPlugin.getWorkspace().newProjectDescription(projectName);
