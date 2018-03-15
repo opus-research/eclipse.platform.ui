bc5eb9ee662c68bba9aa8f08ee23feea6f23ac5f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 12:58:47 +0000

    First commited as before_81905_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/activities/ExtensionActivityRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/activities/ExtensionActivityRegistry.java
@@ -93,7 +93,7 @@ final class ExtensionActivityRegistry extends AbstractActivityRegistry {
             IExtension extension = configurationElement.getDeclaringExtension();
 
             if (extension != null) {
-				namespace = extension.getNamespaceIdentifier();
+				namespace = extension.getNamespace();
 			}
         }
 
