b10aff921157535ddf579382309f8ce339500065
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:25:01 +0000

    First commited as before_84520_rev2

--- bundles/org.eclipse.jface/src/org/eclipse/jface/resource/URLImageDescriptor.java
+++ bundles/org.eclipse.jface/src/org/eclipse/jface/resource/URLImageDescriptor.java
@@ -39,45 +39,33 @@ import org.eclipse.swt.graphics.ImageFileNameProvider;
 class URLImageDescriptor extends ImageDescriptor {
 
 	private static class URLImageFileNameProvider implements ImageFileNameProvider {
-		private String url;
+		private URL url;
 
-		public URLImageFileNameProvider(String url) {
+		public URLImageFileNameProvider(URL url) {
 			this.url = url;
 		}
 
 		@Override
 		public String getImagePath(int zoom) {
-			URL xUrl = null;
-			try {
-				xUrl = getxURL(new URL(url), zoom);
-			} catch (MalformedURLException e) {
-				Policy.getLog().log(new Status(IStatus.ERROR, Policy.JFACE, e.getLocalizedMessage(), e));
-			}
-			if (xUrl == null) {
+			URL xUrl = getxURL(url, zoom);
+			if (xUrl == null)
 				return null;
-			}
 			return getFilePath(xUrl, zoom == 100); // can be null!
 		}
 	}
 
 	private static class URLImageDataProvider implements ImageDataProvider {
-		private String url;
+		private URL url;
 
-		public URLImageDataProvider(String url) {
+		public URLImageDataProvider(URL url) {
 			this.url = url;
 		}
 
 		@Override
 		public ImageData getImageData(int zoom) {
-			URL xUrl = null;
-			try {
-				xUrl = getxURL(new URL(url), zoom);
-			} catch (MalformedURLException e) {
-				Policy.getLog().log(new Status(IStatus.ERROR, Policy.JFACE, e.getLocalizedMessage(), e));
-			}
-			if (xUrl == null) {
+			URL xUrl = getxURL(url, zoom);
+			if (xUrl == null)
 				return null;
-			}
 			return URLImageDescriptor.getImageData(xUrl);
 		}
 	}
@@ -88,7 +76,7 @@ class URLImageDescriptor extends ImageDescriptor {
 	 * Constant for the file protocol for optimized loading
 	 */
 	private static final String FILE_PROTOCOL = "file";  //$NON-NLS-1$
-	private String url;
+	private URL url;
 
 	/**
 	 * Creates a new URLImageDescriptor.
@@ -97,7 +85,7 @@ class URLImageDescriptor extends ImageDescriptor {
 	 *            The URL to load the image from. Must be non-null.
 	 */
 	URLImageDescriptor(URL url) {
-		this.url = url.toExternalForm();
+		this.url = url;
 	}
 
 	@Override
@@ -105,18 +93,12 @@ class URLImageDescriptor extends ImageDescriptor {
 		if (!(o instanceof URLImageDescriptor)) {
 			return false;
 		}
-		return ((URLImageDescriptor) o).url.equals(this.url);
+		return ((URLImageDescriptor) o).url.toExternalForm().equals(this.url.toExternalForm());
 	}
 
 	@Override
 	public ImageData getImageData() {
-		ImageData result = null;
-		try {
-			result = getImageData(new URL(url));
-		} catch (MalformedURLException e) {
-			Policy.getLog().log(new Status(IStatus.ERROR, Policy.JFACE, e.getLocalizedMessage(), e));
-		}
-		return result;
+		return getImageData(url);
 	}
 
 	private static ImageData getImageData(URL url) {
@@ -143,13 +125,7 @@ class URLImageDescriptor extends ImageDescriptor {
 	 * @return the stream for loading the data
 	 */
 	protected InputStream getStream() {
-		InputStream result = null;
-		try {
-			result = getStream(new URL(url));
-		} catch (MalformedURLException e) {
-			Policy.getLog().log(new Status(IStatus.ERROR, Policy.JFACE, e.getLocalizedMessage(), e));
-		}
-		return result;
+		return getStream(url);
 	}
 
 	private static InputStream getStream(URL url) {
@@ -175,7 +151,7 @@ class URLImageDescriptor extends ImageDescriptor {
 
 	@Override
 	public int hashCode() {
-		return url.hashCode();
+		return url.toExternalForm().hashCode();
 	}
 
 	/**
@@ -298,7 +274,7 @@ class URLImageDescriptor extends ImageDescriptor {
 			}
 
 			// Try to see if we can optimize using SWTs file based image support.
-			String path = getFilePath(new URL(url), true);
+			String path = getFilePath(url, true);
 			if (path != null) {
 				try {
 					return new Image(device, path);
@@ -308,9 +284,6 @@ class URLImageDescriptor extends ImageDescriptor {
 			}
 			return super.createImage(returnMissingImageOnError, device);
 
-		} catch (MalformedURLException e) {
-			Policy.getLog().log(new Status(IStatus.ERROR, Policy.JFACE, e.getLocalizedMessage(), e));
-			return null;
 		} finally {
 			if (InternalPolicy.DEBUG_TRACE_URL_IMAGE_DESCRIPTOR) {
 				long time = System.nanoTime() - start;
