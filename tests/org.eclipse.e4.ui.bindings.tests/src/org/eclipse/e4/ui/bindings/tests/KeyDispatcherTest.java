b45af97719bdbf9f35ab2588e30b6a06642ed56e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:03:19 +0000

    First commited as before_89953_rev5

--- tests/org.eclipse.e4.ui.bindings.tests/src/org/eclipse/e4/ui/bindings/tests/KeyDispatcherTest.java
+++ tests/org.eclipse.e4.ui.bindings.tests/src/org/eclipse/e4/ui/bindings/tests/KeyDispatcherTest.java
@@ -16,10 +16,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.Closeable;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,10 +30,6 @@ import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.core.macros.EMacroContext;
-import org.eclipse.e4.core.macros.IMacroPlaybackContext;
-import org.eclipse.e4.core.macros.MacroServiceAddon;
-import org.eclipse.e4.core.macros.internal.MacroManager;
 import org.eclipse.e4.ui.bindings.BindingServiceAddon;
 import org.eclipse.e4.ui.bindings.EBindingService;
 import org.eclipse.e4.ui.bindings.internal.BindingTable;
@@ -57,9 +49,7 @@ import org.eclipse.swt.widgets.Shell;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 public class KeyDispatcherTest {
 	private static final String ID_DIALOG = "org.eclipse.ui.contexts.dialog";
@@ -141,7 +131,6 @@ public class KeyDispatcherTest {
 		ContextInjectionFactory.make(CommandServiceAddon.class, workbenchContext);
 		ContextInjectionFactory.make(ContextServiceAddon.class, workbenchContext);
 		ContextInjectionFactory.make(BindingServiceAddon.class, workbenchContext);
-		ContextInjectionFactory.make(MacroServiceAddon.class, workbenchContext);
 		defineContexts(workbenchContext);
 		defineBindingTables(workbenchContext);
 		defineCommands(workbenchContext);
@@ -189,7 +178,16 @@ public class KeyDispatcherTest {
 
 		Shell shell = new Shell(display, SWT.NONE);
 
-		notifyCtrlA(shell);
+		Event event = new Event();
+		event.type = SWT.KeyDown;
+		event.keyCode = SWT.CTRL;
+		shell.notifyListeners(SWT.KeyDown, event);
+
+		event = new Event();
+		event.type = SWT.KeyDown;
+		event.stateMask = SWT.CTRL;
+		event.keyCode = 'A';
+		shell.notifyListeners(SWT.KeyDown, event);
 
 		assertTrue(handler.q2);
 	}
@@ -314,130 +312,6 @@ public class KeyDispatcherTest {
 
 		assertEquals("(", text.getText());
 	}
-
-
-	@SuppressWarnings("restriction")
-	@Test
-	public void testMacroIntegration() throws Exception {
-		KeyBindingDispatcher dispatcher = new KeyBindingDispatcher();
-		workbenchContext.set(KeyBindingDispatcher.class, dispatcher);
-		ContextInjectionFactory.inject(dispatcher, workbenchContext);
-		EMacroContext macroContext = workbenchContext.get(EMacroContext.class);
-		macroContext.toggleMacroRecord();
-		assertTrue(macroContext.isRecording());
-
-
-		final Listener listener = dispatcher.getKeyDownFilter();
-		display.addFilter(SWT.KeyDown, listener);
-		display.addFilter(SWT.Traverse, listener);
-
-		assertFalse(handler.q2);
-
-		Shell shell = new Shell(display, SWT.NONE);
-
-		// Whitelist command and record
-		HashMap<String, Boolean> macroAcceptedCommandIds = new HashMap<>();
-		macroAcceptedCommandIds.put(TEST_ID1, true);
-		setAcceptedMacroCommandIds(dispatcher, macroAcceptedCommandIds);
-		notifyCtrlA(shell);
-
-		assertTrue(handler.q2);
-
-		macroContext.toggleMacroRecord();
-		assertFalse(macroContext.isRecording());
-
-		handler.q2 = false;
-		macroContext.playbackLastMacro(new IMacroPlaybackContext() {
-		});
-		assertTrue(handler.q2);
-
-		// Blacklist command
-		handler.q2 = false;
-		setAcceptedMacroCommandIds(dispatcher, new HashMap<>());
-
-		macroContext.toggleMacroRecord();
-		assertTrue(macroContext.isRecording());
-		notifyCtrlA(shell);
-		macroContext.toggleMacroRecord();
-		assertFalse(handler.q2);
-	}
-
-	@SuppressWarnings("restriction")
-	private void setAcceptedMacroCommandIds(KeyBindingDispatcher dispatcher,
-			HashMap<String, Boolean> macroAcceptedCommandIds) throws Exception {
-		// Usually gotten through an extension point, but for unit-testing,
-		// let's just change it directly.
-		Field macroAcceptedCommandIdsField = dispatcher.getClass().getDeclaredField("fMacroAcceptedCommandIds");
-		macroAcceptedCommandIdsField.setAccessible(true);
-		macroAcceptedCommandIdsField.set(dispatcher, macroAcceptedCommandIds);
-	}
-
-	private void notifyCtrlA(Shell shell) {
-		Event event = new Event();
-		event.type = SWT.KeyDown;
-		event.keyCode = SWT.CTRL;
-		shell.notifyListeners(SWT.KeyDown, event);
-
-		event = new Event();
-		event.type = SWT.KeyDown;
-		event.stateMask = SWT.CTRL;
-		event.keyCode = 'A';
-		shell.notifyListeners(SWT.KeyDown, event);
-	}
-
-	@Rule
-	public TemporaryFolder folder = new TemporaryFolder();
-
-	@SuppressWarnings("restriction")
-	@Test
-	public void testMacroIntegrationSaveRestore() throws Exception {
-		File macrosDirectory = folder.getRoot();
-		MacroManager macroManager = new MacroManager(macrosDirectory);
-		try (Closeable closeable = MacroManager.withTemporaryDefaultInstance(macroManager)) {
-			FilenameFilter macrosFilter = new FilenameFilter() {
-
-				@Override
-				public boolean accept(File dir, String name) {
-					return name.endsWith(".js");
-				}
-			};
-			assertEquals(0, macrosDirectory.list(macrosFilter).length);
-
-			KeyBindingDispatcher dispatcher = new KeyBindingDispatcher();
-			workbenchContext.set(KeyBindingDispatcher.class, dispatcher);
-			ContextInjectionFactory.inject(dispatcher, workbenchContext);
-			EMacroContext macroContext = workbenchContext.get(EMacroContext.class);
-
-			final Listener listener = dispatcher.getKeyDownFilter();
-			display.addFilter(SWT.KeyDown, listener);
-			display.addFilter(SWT.Traverse, listener);
-
-			assertFalse(handler.q2);
-
-			Shell shell = new Shell(display, SWT.NONE);
-
-			// Accept/record command
-			HashMap<String, Boolean> macroAcceptedCommandIds = new HashMap<>();
-			macroAcceptedCommandIds.put(TEST_ID1, true);
-			setAcceptedMacroCommandIds(dispatcher, macroAcceptedCommandIds);
-
-			macroContext.toggleMacroRecord();
-			notifyCtrlA(shell);
-			assertTrue(handler.q2);
-			macroContext.toggleMacroRecord();
-
-			// Macro was saved in the dir.
-			assertEquals(1, macrosDirectory.list(macrosFilter).length);
-
-			// Check if reloading from disk and playing it back works.
-			macroManager.reloadMacros();
-			handler.q2 = false;
-			macroContext.playbackLastMacro(new IMacroPlaybackContext() {
-			});
-			assertTrue(handler.q2);
-		}
-	}
-
 
 	private void processEvents() {
 		while (display.readAndDispatch())
