589adfdd8bff5791c60a1b7c350280e86b5a8e44
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:54:30 +0000

    First commited as before_98376_rev1

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ReopenEditorMenu.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/ReopenEditorMenu.java
@@ -229,7 +229,6 @@ public class ReopenEditorMenu extends ContributionItem {
                     MenuItem mi = new MenuItem(menu, SWT.PUSH, menuIndex[0]);
                     ++menuIndex[0];
                     mi.setText(text);
-					mi.setToolTipText(item.getToolTipText());
                     mi.addSelectionListener(widgetSelectedAdapter(e -> open(item)));
                 }
 
