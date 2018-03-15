53ef1d6926551082894c09e6f2405c6b6be8959b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 01:02:38 +0000

    First commited as before_69010_rev4

--- bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewFileCreationPage.java
+++ bundles/org.eclipse.ui.ide/extensions/org/eclipse/ui/dialogs/WizardNewFileCreationPage.java
@@ -822,24 +822,7 @@ public class WizardNewFileCreationPage extends WizardPage implements Listener {
 
 		String resourceName = resourceGroup.getResource();
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		if (resourceName == null || resourceName.isEmpty()) {
-			IStatus result = workspace.validateName(resourceName, IResource.FILE);
-			setErrorMessage(result.getMessage());
-			return false;
-		}
-		IPath path = new Path(resourceName);
-		if (path.isAbsolute() || path.isRoot() || path.hasTrailingSeparator() || resourceName.endsWith("/.") //$NON-NLS-1$
-				|| resourceName.endsWith("/..")) { //$NON-NLS-1$
-			setErrorMessage(NLS.bind(IDEWorkbenchMessages.ResourceGroup_invalidFilename, resourceName));
-			return false;
-		}
-		IPath resourcePath = getContainerFullPath().append(getFileName());
-		IStatus result = workspace.validatePath(resourcePath.toString(), IResource.FOLDER);
-		if (!result.isOK()) {
-			setErrorMessage(result.getMessage());
-			return false;
-		}
-		result = workspace.validateName(resourcePath.lastSegment(), IResource.FILE);
+		IStatus result = workspace.validateName(resourceName, IResource.FILE);
 		if (!result.isOK()) {
 			setErrorMessage(result.getMessage());
 			return false;
@@ -863,6 +846,8 @@ public class WizardNewFileCreationPage extends WizardPage implements Listener {
 			if (resourceGroup.getAllowExistingResources()) {
 				String problemMessage = NLS.bind(
 						IDEWorkbenchMessages.ResourceGroup_nameExists,
+						getFileName());
+				IPath resourcePath = getContainerFullPath().append(
 						getFileName());
 				if (workspace.getRoot().getFolder(resourcePath).exists()) {
 					setErrorMessage(problemMessage);
@@ -892,12 +877,7 @@ public class WizardNewFileCreationPage extends WizardPage implements Listener {
 			return false;
 		if (resourceName.length() > 0) {
 			IPath newFolderPath = containerPath.append(resourceName);
-			IFile newFileHandle;
-			try {
-				newFileHandle = createFileHandle(newFolderPath);
-			} catch (Exception e) {
-				return false;
-			}
+			IFile newFileHandle = createFileHandle(newFolderPath);
 			IWorkspace workspace = newFileHandle.getWorkspace();
 			return !workspace.validateFiltered(newFileHandle).isOK();
 		}
