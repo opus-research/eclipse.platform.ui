b72abdd926eb5e518ea22eec85a56b11f699892e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 16:08:16 +0000

    First commited as before_112539_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/PageBookView.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/part/PageBookView.java
@@ -19,6 +19,8 @@ import org.eclipse.core.commands.common.EventManager;
 import org.eclipse.core.runtime.Adapters;
 import org.eclipse.core.runtime.SafeRunner;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.SafeRunnable;
@@ -41,6 +43,7 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.SubActionBars;
+import org.eclipse.ui.internal.WorkbenchPage;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 
 /**
@@ -953,9 +956,6 @@ public abstract class PageBookView extends ViewPart implements IPartListener {
 				} else {
 					provider.removeSelectionChangedListener(postSelectionListener);
 				}
-
-				selectionProvider.selectionChanged(new SelectionChangedEvent(provider, StructuredSelection.EMPTY));
-				selectionProvider.postSelectionChanged(new SelectionChangedEvent(provider, StructuredSelection.EMPTY));
 			}
 		}
 
@@ -976,10 +976,16 @@ public abstract class PageBookView extends ViewPart implements IPartListener {
 
 			// add our selection listener
 			ISelectionProvider provider = pageSite.getSelectionProvider();
-			if (provider != null) {
-				selectionProvider.selectionChanged(new SelectionChangedEvent(provider, provider.getSelection()));
-				selectionProvider.postSelectionChanged(new SelectionChangedEvent(provider, provider.getSelection()));
-
+			if (provider == null) {
+				/* BEGIN workaround for bug 319846 BEGIN */
+				WorkbenchPage page = (WorkbenchPage) getSite().getPage();
+				MPart part = page.findPart(this);
+				if (part != null) {
+					part.getContext().get(ESelectionService.class)
+							.setSelection(StructuredSelection.EMPTY);
+				}
+				/* END workaround for bug 319846 END */
+			} else {
 				provider.addSelectionChangedListener(selectionChangedListener);
 				if (provider instanceof IPostSelectionProvider) {
 					((IPostSelectionProvider) provider)
@@ -987,6 +993,15 @@ public abstract class PageBookView extends ViewPart implements IPartListener {
 				} else {
 					provider.addSelectionChangedListener(postSelectionListener);
 				}
+
+				/* BEGIN workaround for bug 319846 BEGIN */
+				WorkbenchPage page = (WorkbenchPage) getSite().getPage();
+				MPart part = page.findPart(this);
+				if (part != null) {
+					part.getContext().get(ESelectionService.class)
+							.setSelection(provider.getSelection());
+				}
+				/* END workaround for bug 319846 END */
 			}
 			// Update action bars.
 			getViewSite().getActionBars().updateActionBars();
