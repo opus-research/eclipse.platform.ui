0f1818de81a9d8e0206bb63dbff951ad63c68abf
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 00:55:43 +0000

    First commited as after_68282_rev12

---
+++ bundles/org.eclipse.core.databinding.observable/src/org/eclipse/core/internal/databinding/observable/sideeffect/CompositeSideEffect.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2016 vogella GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Simon Scholz <simon.scholz@vogella.com> - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.core.internal.databinding.observable.sideeffect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.databinding.observable.ISideEffect;
+import org.eclipse.core.databinding.observable.sideeffect.ICompositeSideEffect;
+import org.eclipse.core.internal.databinding.observable.SideEffect;
+
+/**
+ * Implementation of the ICompositeSideEffect.
+ *
+ * @since 1.6
+ *
+ */
+public final class CompositeSideEffect implements ICompositeSideEffect {
+
+	private List<ISideEffect> sideEffects;
+
+	/**
+	 * Default constructor of an CompositeSideEffect.
+	 */
+	public CompositeSideEffect() {
+		sideEffects = new ArrayList<>();
+	}
+
+	@Override
+	public void dispose() {
+		sideEffects.forEach(s -> s.dispose());
+		sideEffects.clear();
+	}
+
+	@Override
+	public void pause() {
+		sideEffects.forEach(s -> s.pause());
+	}
+
+	@Override
+	public void resume() {
+		sideEffects.forEach(s -> s.resume());
+	}
+
+	@Override
+	public void resumeAndRunIfDirty() {
+		sideEffects.forEach(s -> s.resumeAndRunIfDirty());
+	}
+
+	@Override
+	public void runIfDirty() {
+		sideEffects.forEach(s -> s.runIfDirty());
+	}
+
+	@Override
+	public void add(ISideEffect sideEffect) {
+		if (!SideEffect.NULL_SIDE_EFFECT.equals(sideEffect)) {
+			sideEffects.add(sideEffect);
+		}
+	}
+
+	@Override
+	public void remove(ISideEffect sideEffect) {
+		sideEffects.remove(sideEffect);
+	}
+}
