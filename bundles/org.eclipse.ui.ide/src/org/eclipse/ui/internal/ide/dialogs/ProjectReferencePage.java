dceb6bcf358500da50aa780bf9661ce98c2f12a0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:16:28 +0000

    First commited as before_83191_rev1

--- bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ProjectReferencePage.java
+++ bundles/org.eclipse.ui.ide/src/org/eclipse/ui/internal/ide/dialogs/ProjectReferencePage.java
@@ -19,7 +19,6 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.runtime.Adapters;
-import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -168,12 +167,7 @@ public class ProjectReferencePage extends PropertyPage {
      * Initializes a ProjectReferencePage.
      */
     private void initialize() {
-		project = Adapters.adapt(getElement(), IProject.class);
-		if (project == null) {
-			IResource resource = Adapters.adapt(getElement(), IResource.class);
-			Assert.isNotNull(resource, "unable to adapt element to a project"); //$NON-NLS-1$
-			project = resource.getProject();
-		}
+		project = (IProject) Adapters.adapt(getElement(), IResource.class);
         noDefaultAndApplyButton();
         setDescription(NLS.bind(IDEWorkbenchMessages.ProjectReferencesPage_label, project.getName()));
     }
