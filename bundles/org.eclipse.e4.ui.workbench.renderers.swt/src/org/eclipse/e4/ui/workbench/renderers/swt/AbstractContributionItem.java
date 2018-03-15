08d7df6ce846977341d3da0fbdba5ea6ca549dea
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 14:27:51 +0000

    First commited as before_93942_rev6

--- bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/AbstractContributionItem.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.swt/src/org/eclipse/e4/ui/workbench/renderers/swt/AbstractContributionItem.java
@@ -542,23 +542,7 @@ public abstract class AbstractContributionItem extends ContributionItem {
 		return null;
 	}
 
-	private Runnable scheduledUpdate;
-
 	protected void updateItemEnablement() {
-		if (scheduledUpdate == null) {
-			Display current = Display.getCurrent();
-			scheduledUpdate = () -> {
-				try {
-					_updateItemEnablement();
-				} finally {
-					scheduledUpdate = null;
-				}
-			};
-			current.asyncExec(scheduledUpdate);
-		}
-	}
-
-	private void _updateItemEnablement() {
 		if (!(modelItem.getWidget() instanceof ToolItem))
 			return;
 
