d2e2ba711024a3674deebd3a744af3df08234689
Matheus Paixao <mhepaixao@gmail.com>
2018-03-15 13:33:03 +0000

    First commited as before_85821_rev2

--- bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/PerspectiveRegistry.java
+++ bundles/org.eclipse.ui.workbench/Eclipse UI/org/eclipse/ui/internal/registry/PerspectiveRegistry.java
@@ -10,7 +10,6 @@
  *     Jan-Hendrik Diederich, Bredex GmbH - bug 201052
  *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 472654
  *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 473063
- *     Patrik Suzzi <psuzzi@gmail.com> - Bug 500420
  *******************************************************************************/
 package org.eclipse.ui.internal.registry;
 
@@ -112,7 +111,7 @@ public class PerspectiveRegistry implements IPerspectiveRegistry, IExtensionChan
 
 	private void createDescriptor(MPerspective perspective) {
 		String label = perspective.getLocalizedLabel();
-		String originalId = getOriginalId(perspective);
+		String originalId = getOriginalId(perspective.getElementId());
 		PerspectiveDescriptor originalDescriptor = descriptors.get(originalId);
 		String id = perspective.getElementId();
 		PerspectiveDescriptor newDescriptor = new PerspectiveDescriptor(id, label, originalDescriptor);
@@ -320,23 +319,10 @@ public class PerspectiveRegistry implements IPerspectiveRegistry, IExtensionChan
 		return originalDescriptor.getOriginalId() + '.' + label;
 	}
 
-	private String getOriginalId(MPerspective p) {
-		String id = p.getElementId();
-		String label = p.getLabel();
+	private String getOriginalId(String id) {
 		int index = id.lastIndexOf('.');
-		// Custom perspectives store the user defined names in their labels
-		String trimE4 = label.trim();
-		String trimE3 = label.replace(' ', '_').trim();
-		if (id.endsWith(label)) {
-			index = id.lastIndexOf(label) - 1;
-		} else if (id.endsWith(trimE4)) {
-			index = id.lastIndexOf(trimE4) - 1;
-		} else if (id.endsWith(trimE3)) {
-			index = id.lastIndexOf(trimE3) - 1;
-		}
-		if (index >= 0 && index < id.length()) {
-			return id.substring(0, index);
-		}
-		return id;
+		if (index == -1)
+			return id;
+		return id.substring(0, index);
 	}
 }
